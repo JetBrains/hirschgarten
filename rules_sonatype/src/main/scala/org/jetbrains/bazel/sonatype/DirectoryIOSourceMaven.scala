02d0937c039d922865de07b3c2707e3759ecf0c9
Andre Rocha <andrefmrocha@live.com.pt>
2021-04-28 16:04:15 +0000

    Add sign and hashing of files
    GitOrigin-RevId: 75ec653092a3f4d51160c50f22338af50b133a58

--- rules_sonatype/src/main/scala/org/jetbrains/bazel/sonatype/DirectoryIOSourceMaven.scala
+++ rules_sonatype/src/main/scala/org/jetbrains/bazel/sonatype/DirectoryIOSourceMaven.scala
@@ -4,12 +4,38 @@ import org.sonatype.spice.zapper.fs.DirectoryIOSource
 import org.sonatype.spice.zapper.{Path, ZFile}
 
 import java.io.File
+import java.nio.file.{Files, Paths}
 import java.util
+import java.math.BigInteger
+import java.nio.charset.{Charset, StandardCharsets}
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 import scala.jdk.CollectionConverters._
+import java.io.IOException
 
 class DirectoryIOSourceMaven(filesPaths: List[Path]) extends DirectoryIOSource(new File("").getCanonicalFile) {
+
+  def processFiles(filesPaths: List[Path]): List[Path] =
+    filesPaths
+      .map(file => Paths.get(file.stringValue()))
+      .flatMap(file => {
+        val toHash = Files.readAllBytes(file)
+        val md5    = Files.createFile(Paths.get(s"${file.getFileName.toString}.md5"))
+        val sha1   = Files.createFile(Paths.get(s"${file.getFileName.toString}.sha1"))
+        Files.write(md5, toMd5(toHash).getBytes(StandardCharsets.UTF_8))
+        Files.write(sha1, toSha1(toHash).getBytes(StandardCharsets.UTF_8))
+        List(file, md5, sha1)
+      })
+      .flatMap(file => {
+        val signed = Paths.get(s"${file.getFileName.toString}.asc")
+        sign(file, signed)
+        List(file, signed)
+      })
+      .map(file => new Path(file.getFileName.toString))
+
   override def scanDirectory(dir: File, zfiles: util.List[ZFile]): Int = {
-    val files = filesPaths.map { path =>
+    val processedFiles = processFiles(filesPaths)
+    val files = processedFiles.map { path =>
       {
         createZFile(path)
       }
@@ -17,5 +43,50 @@ class DirectoryIOSourceMaven(filesPaths: List[Path]) extends DirectoryIOSource(n
 
     zfiles.addAll(files)
     0
+  }
+
+  private def toSha1(toHash: Array[Byte]) = toHexS("%040x", "SHA-1", toHash)
+
+  private def toMd5(toHash: Array[Byte]) = toHexS("%032x", "MD5", toHash)
+
+  private def toHexS(fmt: String, algorithm: String, toHash: Array[Byte]) = try {
+    val digest = MessageDigest.getInstance(algorithm)
+    digest.update(toHash)
+    String.format(fmt, new BigInteger(1, digest.digest))
+  } catch {
+    case e: NoSuchAlgorithmException =>
+      throw new RuntimeException(e)
+  }
+
+  @throws[IOException]
+  @throws[InterruptedException]
+  private def sign(toSign: java.nio.file.Path, signed: java.nio.file.Path): Unit = {
+    // Ideally, we'd use BouncyCastle for this, but for now brute force by assuming
+    // the gpg binary is on the path
+
+    val gpg = new ProcessBuilder(
+      "gpg",
+      "--use-agent",
+      "--armor",
+      "--detach-sign",
+      "--no-tty",
+      "-o",
+      signed.toAbsolutePath.toString,
+      toSign.toAbsolutePath.toString
+    ).inheritIO.start
+    gpg.waitFor
+    if (gpg.exitValue != 0) throw new IllegalStateException("Unable to sign: " + toSign)
+
+    // Verify the signature
+    val verify = new ProcessBuilder(
+      "gpg",
+      "--verify",
+      "--verbose",
+      "--verbose",
+      signed.toAbsolutePath.toString,
+      toSign.toAbsolutePath.toString
+    ).inheritIO.start
+    verify.waitFor
+    if (verify.exitValue != 0) throw new IllegalStateException("Unable to verify signature of " + toSign)
   }
 }
