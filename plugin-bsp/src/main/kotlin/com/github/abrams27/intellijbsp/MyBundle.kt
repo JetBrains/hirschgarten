a6195ac49b0fb560200b9f515fd003516d194117
GitHub Action <action@github.com>
2021-08-25 14:16:47 +0000

    Template cleanup
    GitOrigin-RevId: 0813cd25404483a7e0a8ece6d6b87abcd177aca5

--- plugin-bsp/src/main/kotlin/com/github/abrams27/intellijbsp/MyBundle.kt
+++ plugin-bsp/src/main/kotlin/com/github/abrams27/intellijbsp/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.abrams27.intellijbsp
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- plugin-bsp/src/main/kotlin/com/github/abrams27/intellijbsp/services/MyApplicationService.kt
+++ plugin-bsp/src/main/kotlin/com/github/abrams27/intellijbsp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.abrams27.intellijbsp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.abrams27.intellijbsp.MyBundle
 
 class MyApplicationService {
 
