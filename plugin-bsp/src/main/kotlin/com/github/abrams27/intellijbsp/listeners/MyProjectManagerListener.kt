22b2dc3175687bfa6e35465f93dd2b962129c1a2
GitHub Action <action@github.com>
2021-08-25 14:16:47 +0000

    Template cleanup

--- plugin-bsp/src/main/kotlin/com/github/abrams27/intellijbsp/listeners/MyProjectManagerListener.kt
+++ plugin-bsp/src/main/kotlin/com/github/abrams27/intellijbsp/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.abrams27.intellijbsp.listeners
 
+import com.github.abrams27.intellijbsp.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- plugin-bsp/src/main/kotlin/com/github/abrams27/intellijbsp/services/MyProjectService.kt
+++ plugin-bsp/src/main/kotlin/com/github/abrams27/intellijbsp/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.abrams27.intellijbsp.services
 
+import com.github.abrams27.intellijbsp.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
