e03becc5ef9f5651ea3a538b06ef2233ecda2417
Justin Kaeser <justin.kaeser@jetbrains.com>
2022-10-21 16:35:51 +0200

    log gpg output on sign and verify

--- rules_sonatype/src/main/scala/org/jetbrains/bazel/sonatype/DirectoryIOSourceMaven.scala
+++ rules_sonatype/src/main/scala/org/jetbrains/bazel/sonatype/DirectoryIOSourceMaven.scala
@@ -9,9 +9,13 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
 import java.security.{MessageDigest, NoSuchAlgorithmException}
 import java.util
+import java.util.logging.Logger
 import scala.jdk.CollectionConverters._
+import scala.sys.process.ProcessLogger
 
 class DirectoryIOSourceMaven(filesPaths: List[Path]) extends DirectoryIOSource(new File("").getCanonicalFile) {
+
+  private val LOG = Logger.getLogger(classOf[DirectoryIOSourceMaven].getName)
 
   def processFiles(filesPaths: List[Path]): List[Path] =
     filesPaths
@@ -66,7 +70,17 @@ class DirectoryIOSourceMaven(filesPaths: List[Path]) extends DirectoryIOSource(n
     // Ideally, we'd use BouncyCastle for this, but for now brute force by assuming
     // the gpg binary is on the path
 
-    val gpg = new ProcessBuilder(
+    import scala.sys.process._
+
+    val proclog = ProcessLogger.apply(LOG.info, LOG.warning)
+    val io = BasicIO(withIn = false, proclog).withInput { out =>
+      val writer = new BufferedWriter(new OutputStreamWriter(out))
+      writer.write(sys.env.getOrElse("PGP_PASSPHRASE", "''"))
+      writer.flush()
+      writer.close()
+    }
+
+    val gpgSign = Seq(
       "gpg",
       "--use-agent",
       "--armor",
@@ -78,25 +92,18 @@ class DirectoryIOSourceMaven(filesPaths: List[Path]) extends DirectoryIOSource(n
       "-o",
       signed.toAbsolutePath.toString,
       toSign.toAbsolutePath.toString
-    ).start
-
-    val writer = new BufferedWriter(new OutputStreamWriter(gpg.getOutputStream))
-    writer.write(sys.env.getOrElse("PGP_PASSPHRASE", "''"))
-    writer.flush()
-    writer.close()
-    gpg.waitFor
-    if (gpg.exitValue != 0) throw new IllegalStateException("Unable to sign: " + toSign)
+    ).run(io)
+    if (gpgSign.exitValue != 0) throw new IllegalStateException("Unable to sign: " + toSign)
 
     // Verify the signature
-    val verify = new ProcessBuilder(
+    val gpgVerify = Seq(
       "gpg",
       "--verify",
       "--verbose",
       "--verbose",
       signed.toAbsolutePath.toString,
       toSign.toAbsolutePath.toString
-    ).inheritIO.start
-    verify.waitFor
-    if (verify.exitValue != 0) throw new IllegalStateException("Unable to verify signature of " + toSign)
+    ).run(proclog)
+    if (gpgVerify.exitValue != 0) throw new IllegalStateException("Unable to verify signature of " + toSign)
   }
 }
