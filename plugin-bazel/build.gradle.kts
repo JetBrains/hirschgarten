665bbd12f85418f07897f878ca1c75df06d740ad
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2022-10-31 15:06:52 +0000

    [maintenance][temporary] we dont need the dependency to bsp plugin now
    Merge-request: BAZEL-MR-64
    Merged-by: Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
    
    GitOrigin-RevId: e7ec67e5e619267624f6ba63ea09c73bbd5065fc

--- plugin-bazel/build.gradle.kts
+++ plugin-bazel/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.7.10"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.8.0"
+    id("org.jetbrains.intellij") version "1.10.0-SNAPSHOT"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
     // Gradle Qodana Plugin
@@ -26,7 +26,7 @@ repositories {
 // Set the JVM language level used to compile sources and generate files - Java 11 is required since 2020.3
 kotlin {
     jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
@@ -38,9 +38,10 @@ intellij {
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
 
+    // TODO for now we dont need this
     // change for location where your intellij-bsp-0.0.1-plugin.jar is created (as a result of the  pluginJar task)
-    val bsp = file("/Users/azdrojowa/IdeaProjects/intellij-bsp/build/libs/intellij-bsp-0.0.1-plugin.jar")
-    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty) + bsp)
+    // val bsp = file("/Users/azdrojowa/IdeaProjects/intellij-bsp/build/libs/intellij-bsp-0.0.1-plugin.jar")
+    // plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty) + bsp)
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
@@ -50,12 +51,12 @@ changelog {
 }
 
 // Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-qodana {
-    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
-    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
-    saveReport.set(true)
-    showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
-}
+//qodana {
+//    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
+//    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
+//    saveReport.set(true)
+//    showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
+//}
 
 tasks {
 
--- plugin-bazel/src/main/kotlin/org/jetbrains/bsp/bazel/extension/BazelBspConnectionDetailsGenerator.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bsp/bazel/extension/BazelBspConnectionDetailsGenerator.kt
@@ -1,19 +1,16 @@
 package org.jetbrains.bsp.bazel.extension
 
-import com.intellij.openapi.vfs.VirtualFile
-import java.io.OutputStream
-import org.jetbrains.plugins.bsp.extension.points.BspConnectionDetailsGeneratorExtension
 
-
-class BazelBspConnectionDetailsGenerator : BspConnectionDetailsGeneratorExtension      {
-    override fun name(): String = "bazel"
-
-    override fun canGenerateBspConnectionDetailsFile(projectPath: VirtualFile): Boolean =
-        projectPath.children.any { it.name == "WORKSPACE" }
-
-    override fun generateBspConnectionDetailsFile(projectPath: VirtualFile, outputStream: OutputStream): VirtualFile {
-        executeAndWait("cs launch org.jetbrains.bsp:bazel-bsp:2.1.0 -M org.jetbrains.bsp.bazel.install.Install", projectPath, outputStream)
-        return getChild(projectPath, listOf(".bsp", "bazelbsp.json"))!!
-    }
-
-}
+// TODO we will do it later
+//class BazelBspConnectionDetailsGenerator : BspConnectionDetailsGeneratorExtension      {
+//    override fun name(): String = "bazel"
+//
+//    override fun canGenerateBspConnectionDetailsFile(projectPath: VirtualFile): Boolean =
+//        projectPath.children.any { it.name == "WORKSPACE" }
+//
+//    override fun generateBspConnectionDetailsFile(projectPath: VirtualFile, outputStream: OutputStream): VirtualFile {
+//        executeAndWait("cs launch org.jetbrains.bsp:bazel-bsp:2.1.0 -M org.jetbrains.bsp.bazel.install.Install", projectPath, outputStream)
+//        return getChild(projectPath, listOf(".bsp", "bazelbsp.json"))!!
+//    }
+//
+//}
