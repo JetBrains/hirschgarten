47e939cc361ff2caa47f80124abaaf627b9df200
Tomasz Pasternak <1066003+tpasternak@users.noreply.github.com>
2022-11-21 08:16:08 -0800

    Log performance in some interesting places (#21)

---
+++ plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/PerformanceLogger.kt
@@ -0,0 +1,18 @@
+package org.jetbrains.magicmetamodel.impl
+
+import com.intellij.openapi.diagnostic.logger
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+public object PerformanceLogger {
+  private val log = logger<PerformanceLogger>()
+
+  @OptIn(ExperimentalTime::class)
+  public fun <T> logPerformance(computationId: String, block: () -> T): T {
+    log.debug("Task '${computationId}' started")
+    return measureTimedValue (block).let {
+      log.debug("Task '$computationId' finished in ${it.duration.inWholeMilliseconds}ms")
+      it.value
+    }
+  }
+}
