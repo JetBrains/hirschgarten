a1aef3cd99e672f3a19b3b914afda737f8afb415
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2023-02-03 10:02:49 +0000

    [maintenance] versions bump
    Merge-request: BAZEL-MR-108
    Merged-by: Marcin Abramowicz <marcin.abramowicz@jetbrains.com>

--- plugin-bazel/build.gradle.kts
+++ plugin-bazel/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = project.findProperty(key).toString()
 
@@ -8,11 +9,9 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.7.10"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jetbrains.intellij") version "1.12.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
-    // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.13"
 }
 
 group = properties("pluginGroup")
@@ -108,6 +107,20 @@ tasks {
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
         channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
+    }
+
+// Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = properties("javaVersion")
+        targetCompatibility = properties("javaVersion")
+    }
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = properties("javaVersion")
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.languageVersion = properties("kotlinVersion")
+        kotlinOptions.apiVersion = properties("kotlinVersion")
     }
 }
 
--- plugin-bazel/settings.gradle.kts
+++ plugin-bazel/settings.gradle.kts
@@ -1,8 +1,1 @@
 rootProject.name = "intellij-bsp-bazel"
-
-pluginManagement {
-    repositories {
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
-        gradlePluginPortal()
-    }
-}
