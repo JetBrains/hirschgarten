3c8300c95f3f789aa1eef622de7638c8d1d934a3
Błażej Kardyś <blazej.kardys@jetbrains.com>
2023-02-21 12:30:27 +0000

    [probe] Adding build console parsing and running multiple probe test in one setup
    GitOrigin-RevId: 304169a01c869e75129d77e2c07e6bf4cf398d2c

--- plugin-bsp/probe-setup/src/main/scala/org/jetbrains/bsp/probe/test/IdeProbeTestRunner.scala
+++ plugin-bsp/probe-setup/src/main/scala/org/jetbrains/bsp/probe/test/IdeProbeTestRunner.scala
@@ -1,10 +1,14 @@
 package org.jetbrains.bsp.probe.test
 
+import com.intellij.remoterobot.utils.WaitForConditionTimeoutException
+import org.apache.commons.io.file.spi.FileSystemProviders.installed
 import org.virtuslab.ideprobe.config.WorkspaceConfig
 import org.virtuslab.ideprobe.dependencies.Resource
 import org.virtuslab.ideprobe.protocol.Endpoints
+import org.virtuslab.ideprobe.ide.intellij.InstalledIntelliJ
+import org.virtuslab.ideprobe.reporting.AfterTestChecks
 import org.virtuslab.ideprobe.robot.{RobotPluginExtension, RobotProbeDriver}
-import org.virtuslab.ideprobe.{IdeProbeFixture, IntelliJFixture, ProbeDriver, WaitDecision, WaitLogic, WorkspaceProvider}
+import org.virtuslab.ideprobe.{IdeProbeFixture, IntelliJFixture, ProbeDriver, RunnableIntelliJFixture, RunningIntelliJFixture, WaitDecision, WaitLogic, WorkspaceProvider}
 
 import java.nio.file.Path
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
@@ -20,25 +24,63 @@ class IdeProbeTestRunner extends IdeProbeFixture with RobotPluginExtension {
     ))
   }
 
-  def runAfterOpen(updateFixture: IntelliJFixture, action: (ProbeDriver, RobotProbeDriver) => Unit): Unit = {
+  def openProject(intellij: RunningIntelliJFixture, configuration: Option[String] = None): (ProbeDriver, RobotProbeDriver) = {
+    val probe = intellij.probe
+    val robot = probe.withRobot
+    val openingProjectWaitLogic = openProjectAsync(intellij.probe, intellij.workspace)
+    robot.clickDialogButton("Open or Import Project", "OK", continueOnFail = true)
+    probe.await(openingProjectWaitLogic)
+    robot.clickDialogButton("Import Project via BSP", "Next", 360.second)
+    configuration.map(robot.setText(query.className("JBTextArea"), _))
+    robot.clickDialogButton("Import Project via BSP", "Create")
+    probe.await(robot.extendWaitLogic(WaitLogic.Default))
+    (probe, robot)
+  }
+
+  def runAfterOpen(updateFixture: IntelliJFixture, configuration: Option[String] = None, action: (ProbeDriver, RobotProbeDriver) => Unit): Unit = {
     updateFixture.run { intellij =>
-      val probe = intellij.probe
-      val robot = probe.withRobot
-      val openingProjectWaitLogic = openProjectAsync(intellij.probe, intellij.workspace)
-      robot.clickDialogButton("Open or Import Project", "OK")
-      probe.await(openingProjectWaitLogic)
-      robot.clickDialogButton("Import Project via BSP", "Next", 60.second)
-      robot.clickDialogButton("Import Project via BSP", "Create")
-      probe.await(robot.extendWaitLogic(WaitLogic.Default))
+      val (probe, robot) = openProject(intellij, configuration)
       action(probe, robot)
     }
   }
 
+  def prepareInstance(baseFixture: IntelliJFixture) = {
+    val workspace = baseFixture.setupWorkspace()
+    val installed = baseFixture.installIntelliJ()
+    (baseFixture, workspace, installed)
+  }
+
+  def runIntellij[A](data: (IntelliJFixture, Path, InstalledIntelliJ), action: RunningIntelliJFixture => A): A = {
+    val (baseFixture, workspace, installed) = data
+    val running = baseFixture.startIntelliJ(workspace, installed)
+    val runningData = new RunningIntelliJFixture(workspace, running.probe, baseFixture.config, installed.paths)
+    try action(runningData)
+    finally {
+      AfterTestChecks(baseFixture.intelliJProvider.config.check, runningData.probe)
+      baseFixture.closeIntellij(running)
+    }
+  }
+
+  def cleanInstance(data: (IntelliJFixture, Path, InstalledIntelliJ)): Unit = {
+    val (baseFixture, workspace, installed) = data
+    baseFixture.cleanupIntelliJ(installed)
+    baseFixture.deleteWorkspace(workspace)
+  }
+
+
   implicit class RobotOpts(p: RobotProbeDriver) {
-    def clickDialogButton(dialogTitle: String, buttonTitle: String, timeout: FiniteDuration = 10.second): Unit = {
-      p.findWithTimeout(query.dialog(dialogTitle), timeout)
-        .findWithTimeout(query.button("text" -> buttonTitle), 1.second)
-        .doClick()
+    def clickDialogButton(dialogTitle: String, buttonTitle: String, timeout: FiniteDuration = 10.second, continueOnFail: Boolean = false): Unit = {
+      try {
+        p.findWithTimeout(query.dialog(dialogTitle), timeout)
+          .findWithTimeout(query.button("text" -> buttonTitle), 1.second)
+          .doClick()
+      } catch {
+        case e: WaitForConditionTimeoutException => if (!continueOnFail) throw e
+      }
+    }
+
+    def setText(query: String, text: String): Unit = {
+      p.findWithTimeout(query, 60.second).setText(text)
     }
   }
 
@@ -46,9 +88,9 @@ class IdeProbeTestRunner extends IdeProbeFixture with RobotPluginExtension {
     val future = driver.sendAsync(Endpoints.OpenProject, path)
     WaitLogic.basic() {
       if (future.isCompleted)
-        WaitDecision.KeepWaiting("Waiting for opening the project")
-      else
         WaitDecision.Done
+      else
+        WaitDecision.KeepWaiting("Waiting for opening the project")
     }
   }
 }
--- plugin-bsp/probe/src/test/kotlin/org/jetbrains/bsp/probe/test/SingleProbeTests.kt
+++ plugin-bsp/probe/src/test/kotlin/org/jetbrains/bsp/probe/test/SingleProbeTests.kt
@@ -2,9 +2,10 @@ package org.jetbrains.bsp.probe.test
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import scala.Option
 import scala.runtime.BoxedUnit
 
-class OpenProjectWithProbeTest {
+class SingleProbeTests {
 
   @Test
   fun `open fresh instance of bazel-bsp project and check imported targets`() {
@@ -12,8 +13,8 @@ class OpenProjectWithProbeTest {
       val fixture = fixtureWithWorkspaceFromGit(
         "https://github.com/JetBrains/bazel-bsp.git",
         "2.3.0"
-      ).withBuild("223.8214.27")
-      runAfterOpen(fixture) { _, robot ->
+      ).withBuild("231.5920.14-EAP-SNAPSHOT")
+      runAfterOpen(fixture, Option.apply(null)) { _, robot ->
         val stripeButton = robot.findElement(query.className("StripeButton", "text" to "BSP"))
         stripeButton.doClick()
         val buildPanel = robot.findElement(query.className("BspToolWindowPanel"))
@@ -22,6 +23,21 @@ class OpenProjectWithProbeTest {
         loaded.click()
         val targetsTree = buildPanel.findElement(query.className("Tree"))
         Assertions.assertEquals(9, targetsTree.fullTexts().size)
+        BoxedUnit.UNIT
+      }
+    }
+  }
+
+  @Test
+  fun `open fresh instance of bazel project and check build console output for errors`() {
+    with(IdeProbeTestRunner()) {
+      val fixture = fixtureWithWorkspaceFromGit(
+        "https://github.com/bazelbuild/bazel.git",
+        "6.0.0"
+      ).withBuild("231.5920.14-EAP-SNAPSHOT")
+      runAfterOpen(fixture, Option.apply(null)) { _, robot ->
+        val numberOfErrorsInBuildOutput = robot.getBuildConsoleOutput().filter { it.startsWith("ERROR: ") }.size
+        Assertions.assertEquals(0, numberOfErrorsInBuildOutput)
         BoxedUnit.UNIT
       }
     }
