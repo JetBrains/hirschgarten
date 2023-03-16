624040c393e874adb9413579eca7f8700d142ec9
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2023-03-16 16:12:23 +0000

    [maintenance] tests should be green now
    [maintenance] tests should be green now 2
    
    [maintenance] tests should be green now
    
    Merge-request: BAZEL-MR-169
    Merged-by: Marcin Abramowicz <marcin.abramowicz@jetbrains.com>

--- plugin-bsp/src/test/kotlin/org/jetbrains/plugins/bsp/extensions/TemporaryTestTargetClassifierTest.kt
+++ plugin-bsp/src/test/kotlin/org/jetbrains/plugins/bsp/extensions/TemporaryTestTargetClassifierTest.kt
@@ -11,38 +11,35 @@ class TemporaryTestTargetClassifierTest {
   private val classifier = TemporaryTestTargetClassifier()
   @Test
   fun mainRepoTest() {
-    val target = makeTarget("@//a/b/c:label")
-    classifier.getBuildTargetName(target) shouldBe "label"
-    classifier.getBuildTargetPath(target) shouldBe listOf("a", "b", "c")
+    val targetId = BuildTargetIdentifier("@//a/b/c:label")
+    classifier.getBuildTargetName(targetId) shouldBe "label"
+    classifier.getBuildTargetPath(targetId) shouldBe listOf("a", "b", "c")
   }
   @Test
   fun mainRepoTestOldSyntax() { // pre-bazel 6 syntax
-    val target = makeTarget("//a/b/c:label")
-    classifier.getBuildTargetName(target) shouldBe "label"
-    classifier.getBuildTargetPath(target) shouldBe listOf("a", "b", "c")
+    val targetId = BuildTargetIdentifier("//a/b/c:label")
+    classifier.getBuildTargetName(targetId) shouldBe "label"
+    classifier.getBuildTargetPath(targetId) shouldBe listOf("a", "b", "c")
   }
 
   @Test
   fun emptyPath() { // pre-bazel 6 syntax
-    val target = makeTarget("//:label")
-    classifier.getBuildTargetName(target) shouldBe "label"
-    classifier.getBuildTargetPath(target) shouldBe listOf()
+    val targetId = BuildTargetIdentifier("//:label")
+    classifier.getBuildTargetName(targetId) shouldBe "label"
+    classifier.getBuildTargetPath(targetId) shouldBe listOf()
   }
   @Test
   fun labelNotMatchingBazelPattern() {
-    val target = makeTarget("foo")
-    classifier.getBuildTargetName(target) shouldBe "foo"
-    classifier.getBuildTargetPath(target) shouldBe listOf()
+    val targetId = BuildTargetIdentifier("foo")
+    classifier.getBuildTargetName(targetId) shouldBe "foo"
+    classifier.getBuildTargetPath(targetId) shouldBe listOf()
   }
 
   @Test
   fun labelNotMatchingBazelPatternWithSlash() {
     // this test documents behavior, it is unclear what should be the result for this kind of label
-    val target = makeTarget("foo/bar")
-    classifier.getBuildTargetName(target) shouldBe "foo/bar"
-    classifier.getBuildTargetPath(target) shouldBe listOf()
+    val targetId = BuildTargetIdentifier("foo/bar")
+    classifier.getBuildTargetName(targetId) shouldBe "foo/bar"
+    classifier.getBuildTargetPath(targetId) shouldBe listOf()
   }
-
-  private fun makeTarget(label: String) =
-    BuildTarget(BuildTargetIdentifier(label), emptyList(), emptyList(), emptyList(), BuildTargetCapabilities())
 }
