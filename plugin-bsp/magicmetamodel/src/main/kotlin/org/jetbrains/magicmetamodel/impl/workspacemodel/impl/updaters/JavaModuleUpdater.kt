e28cb55e51afaff5bfffd419453e6a4a6ac49060
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2023-03-16 13:24:14 +0000

    [bug] attempt to handle unresolved references with single file sources in java plugin using workaround | #BAZEL-300 Done | #BAZEL-295 Done
    Merge-request: BAZEL-MR-130
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: 103fcd4dc3cd1164b9f06f97b69ae4b537dde5a2

--- plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/workspacemodel/impl/updaters/JavaModuleUpdater.kt
+++ plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/workspacemodel/impl/updaters/JavaModuleUpdater.kt
@@ -1,11 +1,18 @@
 package org.jetbrains.magicmetamodel.impl.workspacemodel.impl.updaters
 
+import ch.epfl.scala.bsp4j.BuildTargetIdentifier
+import com.intellij.openapi.module.ModuleTypeId
 import com.intellij.workspaceModel.storage.MutableEntityStorage
 import com.intellij.workspaceModel.storage.bridgeEntities.ModuleDependencyItem
 import com.intellij.workspaceModel.storage.bridgeEntities.ModuleEntity
+import com.intellij.workspaceModel.storage.bridgeEntities.ModuleId
 import com.intellij.workspaceModel.storage.bridgeEntities.addJavaModuleSettingsEntity
 import com.intellij.workspaceModel.storage.impl.url.toVirtualFileUrl
+import java.io.File
 import java.nio.file.Path
+import java.nio.file.Paths
+import java.security.MessageDigest
+import kotlin.io.path.isDirectory
 
 internal data class JvmJdkInfo(val javaVersion: String, val javaHome: String)
 
@@ -24,7 +31,7 @@ internal class JavaModuleWithSourcesUpdater(
 ) : WorkspaceModelEntityWithoutParentModuleUpdater<JavaModule, ModuleEntity> {
 
   override fun addEntity(entityToAdd: JavaModule): ModuleEntity {
-    val moduleEntityUpdater = ModuleEntityUpdater(workspaceModelEntityUpdaterConfig, calculateModuleDefaultDependencies(entityToAdd))
+    val moduleEntityUpdater = ModuleEntityUpdater(workspaceModelEntityUpdaterConfig, calculateJavaModuleDependencies(entityToAdd))
 
     val moduleEntity = moduleEntityUpdater.addEntity(entityToAdd.module)
 
@@ -42,11 +49,20 @@ internal class JavaModuleWithSourcesUpdater(
     return moduleEntity
   }
 
-  private fun calculateModuleDefaultDependencies(entityToAdd: JavaModule): List<ModuleDependencyItem> =
+  private fun calculateJavaModuleDependencies(entityToAdd: JavaModule): List<ModuleDependencyItem> {
+    if (DummyJavaSourceModuleUtils.isDummyJavaSourceModule(entityToAdd)) return defaultDependencies
+
+    // Add java sdk as dependency if jvmJdkInfo is not null
+    val finalDependencies: MutableList<ModuleDependencyItem> = defaultDependencies.toMutableList()
     if (entityToAdd.jvmJdkInfo != null) {
-      defaultDependencies + ModuleDependencyItem.SdkDependency(entityToAdd.jvmJdkInfo.javaVersion, "JavaSDK")
+      finalDependencies += ModuleDependencyItem.SdkDependency(entityToAdd.jvmJdkInfo.javaVersion, "JavaSDK")
     }
-    else defaultDependencies
+
+    // Add DummyJavaSourceModules as dependency corresponding to the entity
+    val moduleNames = DummyJavaSourceModuleUtils.calculateDummyJavaSourceModuleNames(entityToAdd)
+    finalDependencies += moduleNames.map { toModuleDependencyItemModuleDependency(ModuleDependency(it)) }
+    return finalDependencies
+  }
 
   private fun addJavaModuleSettingsEntity(builder: MutableEntityStorage, entityToAdd: JavaModule, moduleEntity: ModuleEntity) {
     if (entityToAdd.compilerOutput != null) {
@@ -69,6 +85,93 @@ internal class JavaModuleWithSourcesUpdater(
   }
 }
 
+// This is a workaround for handling unresolved references issue with java classes within the same package
+private object DummyJavaSourceModuleUtils {
+  const val DUMMY_JAVA_SOURCE_MODULE_SIGNATURE = "java-source"
+  const val DUMMY_JAVA_SOURCE_MODULE_ROOT_TYPE = "java-source"
+  val TYPICAL_STRUCTURES = listOf(
+    "" / "src" / "main" / "java",
+    "" / "src" / "main" / "kotlin",
+    "" / "src" / "test" / "java",
+    "" / "src" / "test" / "kotlin",
+    "" / "src",
+    "" / "test"
+  )
+
+  private operator fun String.div(s: String) = "$this${File.separator}$s"
+
+  fun calculateDummyJavaSourceModuleNames(entity: JavaModule) =
+    calculateDummyJavaSourceModuleRoots(entity).map(::calculateDummyJavaSourceModuleName)
+
+  private fun calculateDummyJavaSourceModuleRoots(entity: JavaModule) =
+    entity.sourceRoots.mapNotNull(::calculateDummyJavaSourceModuleRoot).distinct()
+
+  private fun calculateDummyJavaSourceModuleRoot(entitySourceRoot: JavaSourceRoot): String? {
+    val sourceRootPackagePrefix = entitySourceRoot.packagePrefix
+    if (sourceRootPackagePrefix.isEmpty()) return null
+    val entitySourceRootSourcePath = entitySourceRoot.sourcePath
+    val sourceRootDirPath = entitySourceRootSourcePath.let { if (it.isDirectory()) it else it.parent }.toString().appendTrailSeparator()
+    return sourceRootDirPath
+      .substringBeforeLast(
+        sourceRootPackagePrefix.replace(".", File.separator).prependSeparator().appendTrailSeparator(),
+        missingDelimiterValue = ""
+      )
+      .removeTrailSeparator()
+      .ifEmpty { null }
+  }
+
+  private fun calculateDummyJavaSourceModuleName(extractedDummyJavaSourceModuleRoot: String): String {
+    val extractedStructure = TYPICAL_STRUCTURES.find { extractedDummyJavaSourceModuleRoot.endsWith(it) }
+    val extractedParent = extractedStructure?.let { extractedDummyJavaSourceModuleRoot.substringBeforeLast(it) } ?: extractedDummyJavaSourceModuleRoot
+    val extractedName = extractedParent.split(File.separator).let { if (it.isEmpty()) "" else it.last() }
+    return "${DUMMY_JAVA_SOURCE_MODULE_SIGNATURE}-${extractedName}-${hashString(extractedDummyJavaSourceModuleRoot)}"
+  }
+
+  private fun hashString(input: String, algorithm: String = "MD5", take: Int = 7) =
+    MessageDigest
+      .getInstance(algorithm)
+      .digest(input.toByteArray())
+      .fold("") { str, it -> str + "%02x".format(it) }
+      .take(take)
+
+  private fun String.prependSeparator() = if (this.startsWith(File.separator)) this else "" / this
+  private fun String.appendTrailSeparator() = if (this.endsWith(File.separator)) this else this / ""
+  private fun String.removeTrailSeparator() = this.replace(Regex("${File.separator}$"), "")
+
+  fun isDummyJavaSourceModule(entity: JavaModule) = entity.module.name.startsWith(DUMMY_JAVA_SOURCE_MODULE_SIGNATURE)
+
+  fun calculateDummyJavaSourceModules(entity: JavaModule): List<JavaModule> {
+    val dummyJavaSourceModuleRootPaths = calculateDummyJavaSourceModuleRoots(entity).map { it.toPath() }
+    val dummyJavaSourceModuleNames = calculateDummyJavaSourceModuleNames(entity)
+    return dummyJavaSourceModuleNames.zip(dummyJavaSourceModuleRootPaths).map { (dummyJavaSourceModuleName, dummyJavaSourceModuleRootPath) ->
+      JavaModule(
+        module = Module(
+          name = dummyJavaSourceModuleName,
+          type = ModuleTypeId.JAVA_MODULE,
+          modulesDependencies = listOf(),
+          librariesDependencies = listOf()
+        ),
+        baseDirContentRoot = ContentRoot(url = dummyJavaSourceModuleRootPath),
+        sourceRoots = listOf(
+          JavaSourceRoot(
+            sourcePath = dummyJavaSourceModuleRootPath,
+            generated = true,
+            packagePrefix = "",
+            rootType = DUMMY_JAVA_SOURCE_MODULE_ROOT_TYPE,
+            targetId = BuildTargetIdentifier(dummyJavaSourceModuleName)
+          )
+        ),
+        resourceRoots = listOf(),
+        libraries = listOf(),
+        compilerOutput = null,
+        jvmJdkInfo = null
+      )
+    }
+  }
+
+  private fun String.toPath() = Paths.get(this)
+}
+
 internal class JavaModuleWithoutSourcesUpdater(
   private val workspaceModelEntityUpdaterConfig: WorkspaceModelEntityUpdaterConfig,
 ) : WorkspaceModelEntityWithoutParentModuleUpdater<JavaModule, ModuleEntity> {
@@ -85,15 +188,30 @@ internal class JavaModuleWithoutSourcesUpdater(
 }
 
 internal class JavaModuleUpdater(
-  workspaceModelEntityUpdaterConfig: WorkspaceModelEntityUpdaterConfig,
+  private val workspaceModelEntityUpdaterConfig: WorkspaceModelEntityUpdaterConfig,
 ) : WorkspaceModelEntityWithoutParentModuleUpdater<JavaModule, ModuleEntity> {
 
   private val javaModuleWithSourcesUpdater = JavaModuleWithSourcesUpdater(workspaceModelEntityUpdaterConfig)
   private val javaModuleWithoutSourcesUpdater = JavaModuleWithoutSourcesUpdater(workspaceModelEntityUpdaterConfig)
 
   override fun addEntity(entityToAdd: JavaModule): ModuleEntity =
-    when (Pair(entityToAdd.sourceRoots.size, entityToAdd.resourceRoots.size)) {
-      Pair(0, 0) -> javaModuleWithoutSourcesUpdater.addEntity(entityToAdd)
-      else -> javaModuleWithSourcesUpdater.addEntity(entityToAdd)
+    if (entityToAdd.sourceRoots.isEmpty() && entityToAdd.resourceRoots.isEmpty())
+      javaModuleWithoutSourcesUpdater.addEntity(entityToAdd)
+    else {
+      javaModuleWithSourcesUpdater.addDummyJavaSourceModuleIfNotYetAdded(entityToAdd)
+      javaModuleWithSourcesUpdater.addEntity(entityToAdd)
     }
+
+
+  private fun JavaModuleWithSourcesUpdater.addDummyJavaSourceModuleIfNotYetAdded(
+    entity: JavaModule) {
+    val dummyJavaSourceModuleNames = DummyJavaSourceModuleUtils.calculateDummyJavaSourceModuleNames(entity)
+    val dummyJavaSourceModules = DummyJavaSourceModuleUtils.calculateDummyJavaSourceModules(entity)
+    dummyJavaSourceModuleNames.zip(dummyJavaSourceModules).forEach { (dummyJavaSourceModuleName, dummyJavaSourceModule) ->
+      run {
+        if (!workspaceModelEntityUpdaterConfig.workspaceEntityStorageBuilder.contains(ModuleId(name = dummyJavaSourceModuleName)))
+          this.addEntity(dummyJavaSourceModule)
+      }
+    }
+  }
 }
