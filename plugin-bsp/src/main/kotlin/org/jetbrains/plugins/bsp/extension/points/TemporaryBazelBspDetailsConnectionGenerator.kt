ed68a24fbafb11568fe0814edbf6513697adc1f9
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2023-03-17 17:13:21 +0000

    [maintenance] bazel-bsp version bump
    Merge-request: BAZEL-MR-173
    Merged-by: Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
    
    GitOrigin-RevId: e8c21a63dad1a94a9d6fb5a604fc3f453b3790e4

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/extension/points/TemporaryBazelBspDetailsConnectionGenerator.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/extension/points/TemporaryBazelBspDetailsConnectionGenerator.kt
@@ -67,7 +67,7 @@ public class TemporaryBazelBspDetailsConnectionGenerator : BspConnectionDetailsG
     return listOf(
       coursierExecutable.toString(),
       "launch",
-      "org.jetbrains.bsp:bazel-bsp:2.5.1",
+      "org.jetbrains.bsp:bazel-bsp:2.6.0",
       "-M",
       "org.jetbrains.bsp.bazel.install.Install",
     ) + calculateProjectViewFileInstallerOption()
