4e4cbfe9bb6189ef1888054bf9481f6e612fe24f
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2023-03-20 14:00:05 +0000

    [fix] Check if dummy module is already added before adding.
    Merge-request: BAZEL-MR-177
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: f09749ac473a4cede29fd7eec4df4d12c71cf4bc

--- plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/workspacemodel/impl/WorkspaceModelUpdaterImpl.kt
+++ plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/workspacemodel/impl/WorkspaceModelUpdaterImpl.kt
@@ -2,11 +2,13 @@ package org.jetbrains.magicmetamodel.impl.workspacemodel.impl
 
 import ch.epfl.scala.bsp4j.BuildTargetIdentifier
 import com.intellij.workspaceModel.storage.MutableEntityStorage
+import com.intellij.workspaceModel.storage.bridgeEntities.ModuleId
 import com.intellij.workspaceModel.storage.url.VirtualFileUrlManager
 import org.jetbrains.magicmetamodel.impl.workspacemodel.ModuleDetails
 import org.jetbrains.magicmetamodel.impl.workspacemodel.ModuleName
 import org.jetbrains.magicmetamodel.impl.workspacemodel.WorkspaceModelUpdater
 import org.jetbrains.magicmetamodel.impl.workspacemodel.impl.updaters.JavaModuleUpdater
+import org.jetbrains.magicmetamodel.impl.workspacemodel.impl.updaters.Module
 import org.jetbrains.magicmetamodel.impl.workspacemodel.impl.updaters.WorkspaceModelEntityUpdaterConfig
 import org.jetbrains.magicmetamodel.impl.workspacemodel.impl.updaters.WorkspaceModuleRemover
 import org.jetbrains.magicmetamodel.impl.workspacemodel.impl.updaters.transformers.ModuleDetailsToDummyJavaModulesTransformerHACK
@@ -33,11 +35,13 @@ internal class WorkspaceModelUpdaterImpl(
   override fun loadModule(moduleDetails: ModuleDetails) {
     // TODO for now we are supporting only java modules
     val dummyJavaModules = moduleDetailsToDummyJavaModulesTransformerHACK.transform(moduleDetails)
-    javaModuleUpdater.addEntries(dummyJavaModules)
+    javaModuleUpdater.addEntries(dummyJavaModules.filterNot { it.module.isAlreadyAdded() })
     val javaModule = moduleDetailsToJavaModuleTransformer.transform(moduleDetails)
     javaModuleUpdater.addEntity(javaModule)
 
   }
+
+  private fun Module.isAlreadyAdded() = workspaceModelEntityUpdaterConfig.workspaceEntityStorageBuilder.contains(ModuleId(this.name))
 
   override fun removeModule(module: ModuleName) {
     workspaceModuleRemover.removeEntity(module)
