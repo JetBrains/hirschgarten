27c8303f371d8d05ec8941f0953123ff693b3ecf
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2023-05-12 11:31:24 +0000

    [maintenance] platform version bump to 2023.2 EAP
    probe update
    
    plugin version bump
    
    [maintenance] platform version bump to 2023.2 EAP
    
    Merge-request: BAZEL-MR-254
    Merged-by: Marcin Abramowicz <marcin.abramowicz@jetbrains.com>

--- plugin-bsp/buildSrc/src/main/kotlin/versions.kt
+++ plugin-bsp/buildSrc/src/main/kotlin/versions.kt
@@ -1,25 +1,23 @@
-import org.gradle.kotlin.dsl.provideDelegate
-
 // IntelliJ Platform Artifacts Repositories
 // -> https://plugins.jetbrains.com/docs/intellij/intellij-artifacts.html
 object Plugin {
   const val group = "org.jetbrains"
   const val name = "intellij-bsp"
-  const val version = "2023.1.0-EAP"
+  const val version = "2023.2.0-EAP"
 
 // See https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html
 // for insight into build numbers and IntelliJ Platform versions.
-  const val sinceBuild = "231"
-  const val untilBuild = "231.*"
+  const val sinceBuild = "232"
+  const val untilBuild = "232.*"
 }
 
 // Plugin Verifier integration -> https://github.com/JetBrains/gradle-intellij-plugin//plugin-verifier-dsl
 // See https://jb.gg/intellij-platform-builds-list for available build versions.
-const val pluginVerifierIdeVersions = "2023.1"
+const val pluginVerifierIdeVersions = "2023.2"
 
 object Platform {
   const val type = "IC"
-  const val version = "231.8109.175"
+  const val version = "232.5150-EAP-CANDIDATE-SNAPSHOT"
   const val downloadSources = true
 
   // Plugin Dependencies -> https://plugins.jetbrains.com/docs/intellij/plugin-dependencies.html
