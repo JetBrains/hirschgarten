bc2bc478a6c5896656fcaa7342c40b4293689599
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2023-06-19 15:06:34 +0000

    [fix] build project actions shouldnt block ui now | #BAZEL-437 Done
    detekt fix
    
    [fix] build project actions shouldnt block ui now
    
    Merge-request: BAZEL-MR-302
    Merged-by: Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
    
    GitOrigin-RevId: 6eb3e39e5b5c44ec7f5ae990856a80225950a39b

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/actions/building/BspProjectTaskRunner.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/actions/building/BspProjectTaskRunner.kt
@@ -3,19 +3,20 @@ package org.jetbrains.plugins.bsp.actions.building
 import ch.epfl.scala.bsp4j.BuildTarget
 import ch.epfl.scala.bsp4j.CompileResult
 import ch.epfl.scala.bsp4j.StatusCode
-import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.progress.withBackgroundProgress
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.rd.util.toPromise
 import com.intellij.task.ModuleBuildTask
 import com.intellij.task.ProjectTask
 import com.intellij.task.ProjectTaskContext
 import com.intellij.task.ProjectTaskRunner
 import com.intellij.task.TaskRunnerResults
-import com.intellij.util.ModalityUiUtil
 import org.jetbrains.concurrency.AsyncPromise
 import org.jetbrains.concurrency.Promise
 import org.jetbrains.magicmetamodel.DefaultModuleNameProvider
 import org.jetbrains.plugins.bsp.config.BspProjectPropertiesService
 import org.jetbrains.plugins.bsp.server.tasks.BuildTargetTask
+import org.jetbrains.plugins.bsp.services.BspCoroutineService
 import org.jetbrains.plugins.bsp.services.MagicMetaModelService
 
 public class BspProjectTaskRunner : ProjectTaskRunner() {
@@ -34,10 +35,8 @@ public class BspProjectTaskRunner : ProjectTaskRunner() {
   ): Promise<Result> {
     val result = AsyncPromise<Result>()
 
-    ModalityUiUtil.invokeLaterIfNeeded(ModalityState.defaultModalityState(), project.disposed) {
-      val res = runModuleBuildTasks(project, tasks.filterIsInstance<ModuleBuildTask>())
-      result.setResult(res)
-    }
+    val res = runModuleBuildTasks(project, tasks.filterIsInstance<ModuleBuildTask>())
+    res.then { result.setResult(it) }
 
     return result
   }
@@ -45,7 +44,7 @@ public class BspProjectTaskRunner : ProjectTaskRunner() {
   private fun runModuleBuildTasks(
     project: Project,
     tasks: List<ModuleBuildTask>
-  ): Result {
+  ): Promise<Result> {
     val targetsToBuild = obtainTargetsToBuild(project, tasks)
     return buildBspTargets(project, targetsToBuild)
   }
@@ -77,10 +76,16 @@ public class BspProjectTaskRunner : ProjectTaskRunner() {
   private infix fun String.isSubmoduleOf(module: String): Boolean =
     this.startsWith("$module.", false)
 
-  private fun buildBspTargets(project: Project, targetsToBuild: List<BuildTarget>): Result {
+  private fun buildBspTargets(project: Project, targetsToBuild: List<BuildTarget>): Promise<Result> {
     val targetIdentifiers = targetsToBuild.filter { it.capabilities.canCompile }.map { it.id }
-    val result = BuildTargetTask(project).executeIfConnected(targetIdentifiers)
-    return result?.toTaskRunnerResult() ?: TaskRunnerResults.FAILURE
+    val result = BspCoroutineService.getInstance().startAsync {
+      withBackgroundProgress(project, "Building project...") {
+        BuildTargetTask(project).executeIfConnected(targetIdentifiers)
+      }
+    }
+    return result
+      .toPromise()
+      .then { it?.toTaskRunnerResult() ?: TaskRunnerResults.FAILURE }
   }
 
   private fun CompileResult.toTaskRunnerResult() =
