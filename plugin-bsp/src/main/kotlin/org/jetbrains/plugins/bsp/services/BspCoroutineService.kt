84cdc206b78bf19193573690b8b756a6a35c7483
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2023-06-19 15:06:34 +0000

    [fix] build project actions shouldnt block ui now | #BAZEL-437 Done
    detekt fix
    
    [fix] build project actions shouldnt block ui now
    
    Merge-request: BAZEL-MR-302
    Merged-by: Marcin Abramowicz <marcin.abramowicz@jetbrains.com>

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/services/BspCoroutineService.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/services/BspCoroutineService.kt
@@ -13,7 +13,7 @@ import org.jetbrains.annotations.ApiStatus
 @ApiStatus.Internal
 internal class BspCoroutineService(private val cs: CoroutineScope) {
   fun start(callable: suspend () -> Unit): Job = cs.launch { callable() }
-  fun startAsync(callable: suspend () -> Unit): Deferred<Unit> = cs.async { callable() }
+  fun <T>startAsync(callable: suspend () -> T): Deferred<T> = cs.async { callable() }
 
   companion object {
     @JvmStatic
