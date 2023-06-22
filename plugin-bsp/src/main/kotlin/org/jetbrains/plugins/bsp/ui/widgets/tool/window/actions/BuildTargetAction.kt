0241fe6d72fb24ffa021c84dfb7df8f01c1dd611
Błażej Kardyś <blazej.kardys@jetbrains.com>
2023-06-22 08:23:20 +0000

    [maintenance] Removing class parameters from Actions | #BAZEL-428 Done
    Merge-request: BAZEL-MR-309
    Merged-by: Blazej Kardys <Blazej.Kardys@jetbrains.com>

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/actions/BuildTargetAction.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/actions/BuildTargetAction.kt
@@ -1,7 +1,5 @@
 package org.jetbrains.plugins.bsp.ui.widgets.tool.window.actions
 
-import ch.epfl.scala.bsp4j.BuildTargetIdentifier
-import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.progress.runBackgroundableTask
@@ -9,9 +7,8 @@ import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.bsp.server.tasks.BuildTargetTask
 import org.jetbrains.plugins.bsp.ui.widgets.tool.window.all.targets.BspAllTargetsWidgetBundle
 
-public class BuildTargetAction(
-  private val target: BuildTargetIdentifier
-) : AnAction(BspAllTargetsWidgetBundle.message("widget.build.target.popup.message")) {
+public class BuildTargetAction : AbstractActionWithTarget(
+        BspAllTargetsWidgetBundle.message("widget.build.target.popup.message")) {
 
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project
@@ -25,7 +22,9 @@ public class BuildTargetAction(
 
   private fun doAction(project: Project) {
     runBackgroundableTask("Building...", project) {
-      BuildTargetTask(project).executeIfConnected(target)
+      target?.let {
+          BuildTargetTask(project).executeIfConnected(it)
+      }
     }
   }
 
--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/actions/TestTargetAction.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/actions/TestTargetAction.kt
@@ -4,9 +4,8 @@ import ch.epfl.scala.bsp4j.BuildTargetIdentifier
 import org.jetbrains.plugins.bsp.ui.configuration.test.BspConfigurationType
 import org.jetbrains.plugins.bsp.ui.widgets.tool.window.all.targets.BspAllTargetsWidgetBundle
 
-internal class TestTargetAction(
-  target: BuildTargetIdentifier
-) : SideMenuTargetAction(target, BspAllTargetsWidgetBundle.message("widget.test.target.popup.message")) {
+internal class TestTargetAction : SideMenuRunnerAction(
+        BspAllTargetsWidgetBundle.message("widget.test.target.popup.message")) {
   override fun getConfigurationType() = BspConfigurationType()
 
   override fun getName(target: BuildTargetIdentifier): String = target.uri.toString().substringAfter(':')
