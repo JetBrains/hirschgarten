5844e72e3e3f8d3e6069c572d646776a7824ac18
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2023-06-30 14:14:05 +0000

    [maintenance] updated to the latest EAP `232.8453.116`
    Merge-request: BAZEL-MR-334
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: 66a75c2879dbe0906825a8d6f68bbb52e01ee0d7

--- plugin-bsp/probe/src/test/kotlin/org/jetbrains/bsp/probe/test/SingleProbeTests.kt
+++ plugin-bsp/probe/src/test/kotlin/org/jetbrains/bsp/probe/test/SingleProbeTests.kt
@@ -13,7 +13,7 @@ import scala.runtime.BoxedUnit
 class SingleProbeTests {
 
   companion object {
-    const val LATEST_NIGHTLY_VERSION = "232.8296.17"
+    const val LATEST_NIGHTLY_VERSION = "232.8453.116"
     val LATEST_VERSION : String? = null
   }
 
