ecf78581c5679e6bb4ba8f34ceaa327595028f5f
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2023-07-05 09:17:03 +0000

    [fix] revised coroutine cancelation flow | #BAZEL-497 Done Handles BAZEL-497 Merge-request: BAZEL-MR-339 Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/flow/close/ProjectClosingListener.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/flow/close/ProjectClosingListener.kt
@@ -1,12 +1,11 @@
 package org.jetbrains.plugins.bsp.flow.close
 
 import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.progress.withModalProgress
+import com.intellij.openapi.progress.runWithModalProgressBlocking
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
 import org.jetbrains.plugins.bsp.config.isBspProject
 import org.jetbrains.plugins.bsp.server.connection.BspConnectionService
-import org.jetbrains.plugins.bsp.services.BspCoroutineService
 
 public class ProjectClosingListener : ProjectManagerListener {
 
@@ -17,15 +16,13 @@ public class ProjectClosingListener : ProjectManagerListener {
   }
 
   private fun doProjectClosing(project: Project) {
-    BspCoroutineService.getInstance(project).start {
-      withModalProgress(project, "Disconnecting...") {
+      runWithModalProgressBlocking(project, "Disconnecting...") {
         try {
           BspConnectionService.getInstance(project).value?.disconnect()
         } catch (e: Exception) {
           log.warn("One of the disconnect actions has failed!", e)
         }
       }
-    }
   }
 
   private companion object {
