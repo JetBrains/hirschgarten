87b3e434a960acebf8f639724606fc8d01db5dcd
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2023-07-11 12:20:11 +0000

    [maintenance] stopped using backgroundable task with progress indicator in `BuildTargetAction` | #BAZEL-506 Done
    Merge-request: BAZEL-MR-356
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/actions/BuildTargetAction.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/actions/BuildTargetAction.kt
@@ -2,9 +2,10 @@ package org.jetbrains.plugins.bsp.ui.widgets.tool.window.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.progress.runBackgroundableTask
+import com.intellij.openapi.progress.withBackgroundProgress
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.bsp.server.tasks.BuildTargetTask
+import org.jetbrains.plugins.bsp.services.BspCoroutineService
 import org.jetbrains.plugins.bsp.ui.widgets.tool.window.all.targets.BspAllTargetsWidgetBundle
 
 public class BuildTargetAction : AbstractActionWithTarget(
@@ -21,9 +22,11 @@ public class BuildTargetAction : AbstractActionWithTarget(
   }
 
   private fun doAction(project: Project) {
-    runBackgroundableTask("Building...", project) {
-      target?.let {
+    BspCoroutineService.getInstance(project).start {
+      withBackgroundProgress(project, "Building...") {
+        target?.let {
           BuildTargetTask(project).connectAndExecute(it)
+        }
       }
     }
   }
