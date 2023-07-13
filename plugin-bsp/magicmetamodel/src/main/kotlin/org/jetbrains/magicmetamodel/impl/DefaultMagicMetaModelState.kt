fbf5f7aafd4f5802fab404c329d1778de9fd22cd
Tomasz Pasternak <tomasz.pasternak@jetbrains.com>
2023-07-13 15:19:05 +0000

    [fix] Handle null values in ProjectDetailsState
    Merge-request: BAZEL-MR-365
    Merged-by: Tomasz Pasternak <Tomasz.Pasternak@jetbrains.com>

--- plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/DefaultMagicMetaModelState.kt
+++ plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/DefaultMagicMetaModelState.kt
@@ -12,6 +12,7 @@ import ch.epfl.scala.bsp4j.SourceItem
 import ch.epfl.scala.bsp4j.SourceItemKind
 import ch.epfl.scala.bsp4j.SourcesItem
 import com.google.gson.Gson
+import org.jetbrains.annotations.Nullable
 import org.jetbrains.magicmetamodel.LibraryItem
 import org.jetbrains.magicmetamodel.ProjectDetails
 import org.jetbrains.magicmetamodel.impl.workspacemodel.ModuleDetails
@@ -230,7 +231,7 @@ public data class ProjectDetailsState(
   public var dependenciesSources: List<DependencySourcesItemState> = emptyList(),
   public var javacOptions: List<JavacOptionsItemState> = emptyList(),
   public var outputPathUris: List<String> = emptyList(),
-  public var libraries: List<LibraryItemState>? = emptyList(),
+  @Nullable public var libraries: List<LibraryItemState>? = null,
   ) : ConvertableFromState<ProjectDetails> {
 
   public override fun fromState(): ProjectDetails =
