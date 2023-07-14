bazelbuild/intellij

Update sdkcompat for AS 2023.1
To make following cherry-picks from the `google` branch to `master` easier.

- move refactoring compat classes for AS 2023.1 to a separate directory because of differences in the methods parameter types.
- update `UIController` to include methods available in both AS and IJ 2023.1 APIs.

PiperOrigin-RevId: 546409372

sdkcompat/v231/com/google/idea/sdkcompat/editor/markup/UIControllerCreator.java
@@ -55,7 +55,10 @@ public final class UIControllerCreator {
       @Override
       public void fillHectorPanels(Container container, GridBag bag) {}
 
-      @Override
+      public boolean isToolbarEnabled() {
+        return true;
+      }
+
       public boolean enableToolbar() {
         return true;
       }

JetBrains/hirschgarten

(cherry picked from commit 2ce9ed7be7ab71cf09d79de94f2ef3883d317df8)
GitOrigin-RevId: 2dd0af02d3a9ed482db5acb453aa789c5a068660

rules_intellij/sdkcompat/v231/com/google/idea/sdkcompat/editor/markup/UIControllerCreator.java
@@ -59,12 +59,12 @@ public final class UIControllerCreator {
         return true;
       }
 
-      @Override
-      public boolean canClosePopup() {
+      public boolean enableToolbar() {
         return true;
       }
-      
-      public boolean enableToolbar() {
+
+      @Override
+      public boolean canClosePopup() {
         return true;
       }
     };