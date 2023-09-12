d392f157f8bc64b1858fc1e00ca9de2c384c2838
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2023-09-12 08:43:42 +0000

    [feature] Enhance Kotlinc Opts support (client) | #BAZEL-461 Done
    Merge-request: BAZEL-MR-509
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>

--- plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/workspacemodel/impl/updaters/KotlinFacetEntityUpdater.kt
+++ plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/workspacemodel/impl/updaters/KotlinFacetEntityUpdater.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.magicmetamodel.impl.workspacemodel.impl.updaters
 
+import com.intellij.facet.FacetType
 import com.intellij.facet.impl.FacetUtil
 import com.intellij.openapi.util.JDOMUtil
 import com.intellij.platform.workspace.jps.entities.FacetEntity
@@ -7,27 +8,32 @@ import com.intellij.platform.workspace.jps.entities.ModuleEntity
 import org.jetbrains.kotlin.cli.common.arguments.CommonCompilerArguments
 import org.jetbrains.kotlin.cli.common.arguments.Freezable
 import org.jetbrains.kotlin.cli.common.arguments.copyCommonCompilerArguments
+import org.jetbrains.kotlin.config.JvmTarget
 import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.idea.facet.KotlinFacet
+import org.jetbrains.kotlin.idea.facet.KotlinFacetConfiguration
 import org.jetbrains.kotlin.idea.facet.KotlinFacetType
+import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.magicmetamodel.impl.workspacemodel.JavaModule
 import org.jetbrains.magicmetamodel.impl.workspacemodel.KotlincOpts
 
-internal class CommonCompilerArgsHolder: CommonCompilerArguments() {
+internal class CommonCompilerArgsHolder : CommonCompilerArguments() {
   override fun copyOf(): Freezable =
     copyCommonCompilerArguments(this, CommonCompilerArgsHolder())
 
   fun configure(kotlincOpts: KotlincOpts) {
-    allowResultReturnType = kotlincOpts.xAllowResultReturnType
-    explicitApi = kotlincOpts.xExplicitApiMode
-    inlineClasses = kotlincOpts.xInlineClasses
-    multiPlatform = kotlincOpts.xMultiPlatform
-    optIn = kotlincOpts.xOptinList.toTypedArray()
-    reportPerf = kotlincOpts.xReportPerf
-    skipPrereleaseCheck = kotlincOpts.xSkipPrereleaseCheck
-    useFirLT = kotlincOpts.xUseFirLt
-    useK2 = kotlincOpts.xUseK2
+    if (kotlincOpts.xAllowResultReturnType != null) allowResultReturnType = kotlincOpts.xAllowResultReturnType
+    if (kotlincOpts.xExplicitApiMode != null) explicitApi = kotlincOpts.xExplicitApiMode
+    if (kotlincOpts.xInlineClasses != null) inlineClasses = kotlincOpts.xInlineClasses
+    if (kotlincOpts.xMultiPlatform != null) multiPlatform = kotlincOpts.xMultiPlatform
+    if (kotlincOpts.xOptinList != null) optIn = kotlincOpts.xOptinList.toTypedArray()
+    if (kotlincOpts.xReportPerf != null) reportPerf = kotlincOpts.xReportPerf
+    if (kotlincOpts.xSkipPrereleaseCheck != null) skipPrereleaseCheck = kotlincOpts.xSkipPrereleaseCheck
+    if (kotlincOpts.xUseFirLt != null) useFirLT = kotlincOpts.xUseFirLt
+    if (kotlincOpts.xUseK2 != null) useK2 = kotlincOpts.xUseK2
   }
 }
+
 public fun CommonCompilerArguments.toKotlinCOption(): KotlincOpts = KotlincOpts(
   xAllowResultReturnType = allowResultReturnType,
   xExplicitApiMode = explicitApi,
@@ -49,16 +55,24 @@ internal class KotlinFacetEntityUpdater(
     facet.settings.useProjectSettings = false
     facet.settings.additionalVisibleModuleNames =
       entityToAdd.genericModuleInfo.associates.map { it.moduleName }.toSet()
-    entityToAdd.kotlinAddendum?.let { kotlinAddendum ->
-      val commonCompilerArguments = CommonCompilerArgsHolder()
-      kotlinAddendum.kotlincOptions?.let { kotlincOpts ->
-        commonCompilerArguments.configure(kotlincOpts)
-        facet.settings.compilerArguments = commonCompilerArguments
-        // Settings are not saved without compiler arguments so there is no point in adding levels outside of this loop
-        facet.settings.languageLevel = LanguageVersion.fromVersionString(kotlinAddendum.languageVersion)
-        facet.settings.apiLevel = LanguageVersion.fromVersionString(kotlinAddendum.apiVersion)
-      }
-    }
+    val kotlinAddendum = entityToAdd.kotlinAddendum
+    val kotlincOpts = kotlinAddendum?.kotlincOptions ?: return addFacetEntity(facet, parentModuleEntity, facetType)
+    if (!kotlincOpts.jvmTarget.isNullOrBlank())
+      facet.settings.targetPlatform =
+        JvmTarget.fromString(kotlincOpts.jvmTarget)?.let { JvmPlatforms.jvmPlatformByTargetVersion(it) }
+    val commonCompilerArguments = CommonCompilerArgsHolder()
+    commonCompilerArguments.configure(kotlincOpts)
+    facet.settings.compilerArguments = commonCompilerArguments
+    facet.settings.languageLevel = LanguageVersion.fromVersionString(kotlinAddendum.languageVersion)
+    facet.settings.apiLevel = LanguageVersion.fromVersionString(kotlinAddendum.apiVersion)
+    return addFacetEntity(facet, parentModuleEntity, facetType)
+  }
+
+  private fun addFacetEntity(
+    facet: KotlinFacetConfiguration,
+    parentModuleEntity: ModuleEntity,
+    facetType: FacetType<KotlinFacet, KotlinFacetConfiguration>,
+  ): FacetEntity {
     val facetConfigurationXml = FacetUtil.saveFacetConfiguration(facet)?.let { JDOMUtil.write(it) }
     return workspaceModelEntityUpdaterConfig.workspaceEntityStorageBuilder.addEntity(
       FacetEntity(
