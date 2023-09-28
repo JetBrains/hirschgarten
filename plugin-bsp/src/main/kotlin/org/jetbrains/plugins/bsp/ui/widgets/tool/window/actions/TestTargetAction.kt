cc8a7d42e413ae25fc24e0bf74192d023e3f3f62
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2023-09-28 07:30:54 +0000

    [feature] support for gutters for tests and main methods (jvm langs) | #BAZEL-193 Done
    review
    
    detekt
    
    wow final impl
    
    slow info impl
    
    post merge
    
    todo
    
    package info
    
    final cleanup
    
    all the run actions
    
    BuildTargetAction
    
    LoadTargetAction
    
    OpenBazelProjectViaBspPluginAction
    
    registered actions are dumb aware now!
    
    reload action
    
    restart action
    
    connect  action
    
    disconnect action
    
    base actions + LoadTargetAction migrated
    
    
    Merge-request: BAZEL-MR-539
    Merged-by: Marcin Abramowicz <marcin.abramowicz@jetbrains.com>

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/actions/TestTargetAction.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/actions/TestTargetAction.kt
@@ -1,15 +1,14 @@
 package org.jetbrains.plugins.bsp.ui.widgets.tool.window.actions
 
-import com.intellij.icons.AllIcons
 import org.jetbrains.magicmetamodel.impl.workspacemodel.BuildTargetId
-import org.jetbrains.plugins.bsp.config.BspPluginBundle
 import org.jetbrains.plugins.bsp.ui.configuration.test.BspConfigurationType
+import javax.swing.Icon
 
-internal class TestTargetAction(targetId: BuildTargetId) : SideMenuRunnerAction(
-  targetId = targetId,
-  text = { BspPluginBundle.message("widget.test.target.popup.message") },
-  icon = AllIcons.Toolwindows.ToolWindowRun,
-) {
+internal class TestTargetAction(
+  targetId: BuildTargetId,
+  text: () -> String,
+  icon: Icon,
+) : SideMenuRunnerAction(targetId, text, icon) {
   override fun getConfigurationType() = BspConfigurationType()
 
   override fun getName(target: BuildTargetId): String = target.substringAfter(':')
