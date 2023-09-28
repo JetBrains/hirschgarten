ff34377ef359848c517689415993b57a86c5e17f
Tomasz Głąb <32012724+Toomimi@users.noreply.github.com>
2023-09-28 22:19:32 +0200

    Include Jewel

--- plugin-bsp/build.gradle.kts
+++ plugin-bsp/build.gradle.kts
@@ -1,17 +1,16 @@
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
-import org.jetbrains.intellij.tasks.BuildSearchableOptionsTask
-import org.jetbrains.intellij.tasks.PublishPluginTask
-import org.jetbrains.intellij.tasks.RunIdeTask
+import org.jetbrains.intellij.tasks.*
 import org.jetbrains.intellij.tasks.RunPluginVerifierTask.FailureLevel
-import org.jetbrains.intellij.tasks.VerifyPluginTask
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
 
 plugins {
   // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
   alias(libs.plugins.intellij)
   // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
   alias(libs.plugins.changelog)
-
+  alias(libs.plugins.composeDesktop)
+  alias(libs.plugins.shadow)
   id("intellijbsp.kotlin-conventions")
 }
 
@@ -27,6 +26,12 @@ dependencies {
   implementation(libs.bsp4j)
   implementation(libs.gson)
   implementation(libs.coursier)
+  implementation(compose.desktop.linux_arm64)
+  implementation(compose.desktop.linux_x64)
+  implementation(compose.desktop.macos_arm64)
+  implementation(compose.desktop.macos_x64)
+  implementation(compose.desktop.windows_x64)
+  implementation(libs.jewelIdeLafBridge)
 
   testImplementation(libs.junitJupiter)
   testImplementation(libs.kotest)
@@ -81,9 +86,22 @@ subprojects {
 }
 repositories {
   mavenCentral()
+  maven("https://packages.jetbrains.team/maven/p/kpm/public/")
+  maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 tasks {
+    shadowJar {
+      archiveClassifier = null
+      relocate("kotlinx.serialization", "shadow.kotlinx.serialization")
+      relocate("io.ktor", "shadow.io.ktor")
+      relocate("kotlinx.datetime", "shadow.kotlinx.datetime")
+      val exclusions = listOf("kotlin-stdlib", "slf4j", "kotlin-reflect", "kotlinx-coroutines", "lockback")
+      exclude { element ->
+        exclusions.any { it in element.name }
+      }
+
+    }
   patchPluginXml {
     version.set(Plugin.version)
     sinceBuild.set(Plugin.sinceBuild)
@@ -114,6 +132,37 @@ tasks {
     ))
   }
 
+  prepareSandbox {
+    pluginJar = shadowJar.flatMap { it.archiveFile }
+    runtimeClasspathFiles = objects.fileCollection()
+  }
+
+  val buildShadowPlugin by registering(Zip::class) {
+    from(shadowJar) {
+      into("org.jetbrains.bsp/lib")
+    }
+    from(jarSearchableOptions) {
+      into("org.jetbrains.bsp/lib")
+    }
+    archiveBaseName = project.name + "-shadow"
+    destinationDirectory = layout.buildDirectory.dir("distributions")
+  }
+
+  val publishShadowPlugin by registering(PublishPluginTask::class) {
+    group = "publishing"
+    distributionFile = buildShadowPlugin.flatMap { it.archiveFile }
+    dependsOn("patchChangelog")
+    token.set(provider { myToken })
+    // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+    // Specify pre-release label to publish the plugin in a custom Release Channel. Read more:
+    // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+    // Release channel is set via command-line param "releaseChannel"
+    // Marketplace token is set via command-line parm "myToken"
+    // Example command "./gradlew publishPlugin -PmyToken="perm:YOUR_TOKEN -PreleaseChannel=nightly"
+    channels.set(provider { listOf(releaseChannel) })
+  }
+
+
   publishPlugin {
     dependsOn("patchChangelog")
     token.set(provider { myToken })
@@ -126,7 +175,6 @@ tasks {
     channels.set(provider { listOf(releaseChannel) })
   }
 }
-
 
 tasks {
   test {
