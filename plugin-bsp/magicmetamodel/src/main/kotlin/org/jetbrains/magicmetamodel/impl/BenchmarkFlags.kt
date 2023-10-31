d72d595c0f5a066ef4abef4397d4425f2db274de
Tomasz Pasternak <tomasz.pasternak@jetbrains.com>
2023-10-31 17:51:30 +0000

    [fix] Fix benchmarks
    Merge-request: BAZEL-MR-622
    Merged-by: Tomasz Pasternak <Tomasz.Pasternak@jetbrains.com>
    
    GitOrigin-RevId: 8385a8b968cb2aab45f06feb59178967d4bf6362

--- plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/BenchmarkFlags.kt
+++ plugin-bsp/magicmetamodel/src/main/kotlin/org/jetbrains/magicmetamodel/impl/BenchmarkFlags.kt
@@ -3,11 +3,13 @@ package org.jetbrains.magicmetamodel.impl
 import java.nio.file.Path
 import java.nio.file.Paths
 
+private const val BENCHMARK_PROJECT_PATH: String = "bsp.benchmark.project.path"
 private const val BENCHMARK_METRICS_FILE: String = "bsp.benchmark.metrics.file"
-private const val IS_BENCHMARK: String = "bsp.is.benchmark"
 
 public object BenchmarkFlags {
+  public fun benchmarkProjectPath(): Path? = System.getProperty(BENCHMARK_PROJECT_PATH)?.let { Paths.get(it) }
+
   public fun metricsFile(): Path? = System.getProperty(BENCHMARK_METRICS_FILE)?.let { Paths.get(it) }
 
-  public fun isBenchmark(): Boolean = System.getProperty(IS_BENCHMARK, "false").toBoolean()
+  public fun isBenchmark(): Boolean = benchmarkProjectPath() != null
 }
