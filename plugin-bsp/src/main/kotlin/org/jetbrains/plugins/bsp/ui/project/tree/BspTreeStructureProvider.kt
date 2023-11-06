53bba88d0caeb87ab496cd4722b21b6c37e8cb8b
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2023-11-06 22:21:52 +0000

    [fix] The project tree no longer collapses after any change | #BAZEL-706 Done
    Merge-request: BAZEL-MR-621
    Merged-by: Andrzej Gluszak <Andrzej.Gluszak@jetbrains.com>
    
    GitOrigin-RevId: bb5ab434cbfd2cdd308066cfb19652b1337c42b1

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/project/tree/BspTreeStructureProvider.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/project/tree/BspTreeStructureProvider.kt
@@ -8,48 +8,40 @@ import com.intellij.ide.projectView.impl.nodes.ProjectViewModuleNode
 import com.intellij.ide.projectView.impl.nodes.ProjectViewProjectNode
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode
 import com.intellij.ide.util.treeView.AbstractTreeNode
-import com.intellij.psi.impl.PsiManagerImpl
-import com.intellij.psi.impl.file.PsiDirectoryImpl
+import com.intellij.openapi.components.service
+import com.intellij.psi.PsiManager
 import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 import org.jetbrains.plugins.bsp.config.isBspProject
 import org.jetbrains.plugins.bsp.config.rootDir
 
-public class BspTreeStructureProvider: TreeStructureProvider {
-  override fun modify(
-    parent: AbstractTreeNode<*>,
-    children: MutableCollection<AbstractTreeNode<*>>,
-    settings: ViewSettings,
-  ): MutableCollection<AbstractTreeNode<*>> =
-    if (parent.project.isBspProject) doModify(parent, children)
-    else children
-
-  private fun doModify(
-    parent: AbstractTreeNode<*>,
-    children: MutableCollection<AbstractTreeNode<*>>,
-  ): MutableCollection<AbstractTreeNode<*>> =
-    if (parent is ProjectViewProjectNode) createRootDirAndAddExternalLibraries(parent, children)
-    else children.removeModuleAndModuleGroupsNodes()
-
-  private fun createRootDirAndAddExternalLibraries(
-    parent: ProjectViewProjectNode,
-    children: MutableCollection<AbstractTreeNode<*>>,
-  ): MutableCollection<AbstractTreeNode<*>> {
-    val rootDirectory = PsiDirectoryImpl(PsiManagerImpl(parent.project), parent.project.rootDir)
-    val rootDirectoryNode = PsiDirectoryNode(parent.project, rootDirectory, parent.settings)
-
-    val externalLibrariesNode = children.firstIsInstanceOrNull<ExternalLibrariesNode>()
-
-    return listOfNotNull(rootDirectoryNode, externalLibrariesNode).toMutableList()
-  }
-
-  // we want to get rid of all the module (group) nodes from the project view tree;
+public class BspTreeStructureProvider : TreeStructureProvider {
+  // We want to get rid of all the module (group) nodes from the project view tree;
   // in rare cases with complicated project (modules) structure IJ
   // doesn't know how to render the project tree,
-  // what causes flat directory structure
-  private fun MutableCollection<AbstractTreeNode<*>>.removeModuleAndModuleGroupsNodes():
-    MutableCollection<AbstractTreeNode<*>> =
+  // what causes flat directory structure.
+  // Instead we use the root directory node as the root of the project view tree and ignore modules altogether.
+  // This results in displaying the folder structure of the project as it is on the file system.
+  override fun modify(
+    parent: AbstractTreeNode<*>,
+    children: Collection<AbstractTreeNode<*>>,
+    settings: ViewSettings,
+  ): Collection<AbstractTreeNode<*>> {
+    val project = parent.project ?: return children
+    if (!project.isBspProject) return children
+
+    if (parent is ProjectViewProjectNode) {
+      val rootDirectory =
+        project.service<PsiManager>().findDirectory(project.rootDir) ?: return children // should never happen
+      val rootDirectoryNode = PsiDirectoryNode(project, rootDirectory, settings)
+      val externalLibrariesNode = children.firstIsInstanceOrNull<ExternalLibrariesNode>()
+      return listOfNotNull(rootDirectoryNode, externalLibrariesNode)
+    }
+
+    return children.removeModuleAndModuleGroupNodes()
+  }
+
+  private fun Collection<AbstractTreeNode<*>>.removeModuleAndModuleGroupNodes(): Collection<AbstractTreeNode<*>> =
     this
       .filterNot { it is ProjectViewModuleGroupNode }
       .filterNot { it is ProjectViewModuleNode }
-      .toMutableList()
 }
