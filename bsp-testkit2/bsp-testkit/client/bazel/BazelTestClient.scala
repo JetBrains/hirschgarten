89b11c4f549bd8b5df480a749a2040278bdfbcdb
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2023-11-06 23:36:14 +0100

    Migrate to bzlmod, don't run bazel inside BazelTestClient

--- bsp-testkit2/bsp-testkit/client/bazel/BazelTestClient.scala
+++ bsp-testkit2/bsp-testkit/client/bazel/BazelTestClient.scala
@@ -4,17 +4,8 @@ import ch.epfl.scala.bsp4j.InitializeBuildParams
 import org.jetbrains.bsp.testkit.client.TestClient
 
 import java.nio.file.Path
-import java.util.stream.Collectors
-import scala.util.Try
 
 object BazelTestClient {
-
-  import scala.sys.process._
-
-  private def processOutput(workspace: Path, command: Seq[String]): String = {
-    Process(command, Some(workspace.toFile)).!!
-  }
-
   private val osFamily: String = {
     val osName = System.getProperty("os.name").toLowerCase()
     if (osName.startsWith("windows")) {
@@ -25,27 +16,13 @@ object BazelTestClient {
       "linux"
     }
   }
-
-  def transform(workspacePath: Path): String => String = {
-    Try(processOutput(workspacePath, Seq("bazel", "version"))).toOption match {
-      case Some(version) => println(s"Testkit client uses Bazel version:\n$version")
-      case None => throw new IllegalStateException("Bazel is not installed")
-    }
-
-    val bazelCachePath = processOutput(workspacePath, Seq("bazel", "info", "execution_root"))
-      .lines()
-      .collect(Collectors.joining())
-
-    val bazelOutputBasePath = processOutput(workspacePath, Seq("bazel", "info", "output_base"))
-      .lines()
-      .collect(Collectors.joining())
-
-    (s: String) =>
-      s.replace("$WORKSPACE", workspacePath.toString)
-        .replace("$BAZEL_CACHE", bazelCachePath)
-        .replace("$BAZEL_OUTPUT_BASE_PATH", bazelOutputBasePath)
-        .replace("$OS", osFamily)
-  }
 }
 
-class BazelTestClient(override val workspacePath: Path, override val initializeParams: InitializeBuildParams) extends TestClient(workspacePath, initializeParams, BazelTestClient.transform(workspacePath))
+class BazelTestClient(override val workspacePath: Path, override val initializeParams: InitializeBuildParams,
+                      val bazelCache: Path, val bazelOutputBase: Path)
+  extends TestClient(workspacePath, initializeParams, (s: String) =>
+    s.replace("$WORKSPACE", workspacePath.toString)
+      .replace("$BAZEL_CACHE", bazelCache.toString)
+      .replace("$BAZEL_OUTPUT_BASE_PATH", bazelOutputBase.toString)
+      .replace("$OS", BazelTestClient.osFamily)
+  )
