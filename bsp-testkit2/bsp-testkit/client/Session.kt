9d2a5820f373e1bfc1e13831b4b1f74d42e07b58
ANdrzej Głuszak <andrzej.gluszak@jetbrains.com>
2023-11-29 09:19:59 +0100

    Update

--- bsp-testkit2/bsp-testkit/client/Session.kt
+++ bsp-testkit2/bsp-testkit/client/Session.kt
@@ -15,7 +15,7 @@ import java.util.concurrent.Executors
  * A session is a "physical" connection to a BSP server. It must be closed when it is no longer
  * needed. The user is responsible for maintaining the correct BSP life-cycle.
  */
-class Session(val workspacePath: Path, val coroutineScope: CoroutineScope) : AutoCloseable {
+class Session(val workspacePath: Path) : AutoCloseable {
   private val workspaceFile = workspacePath.toFile()
   private val connectionDetails = readBspConnectionDetails(workspaceFile)
 
