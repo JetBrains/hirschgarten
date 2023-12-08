fc7d303229a5bd0938fd2929089e82042be5194a
Błażej Kardyś <blazej.kardys@jetbrains.com>
2023-12-08 18:00:22 +0000

    [maintenance] Moving probe tests to intellij-bazel
    Merge-request: BAZEL-MR-702
    Merged-by: Blazej Kardys <Blazej.Kardys@jetbrains.com>
    
    GitOrigin-RevId: 4adf21f7b19783ce6589877914d8d15cf4f9c7f9

--- plugin-bsp/settings.gradle.kts
+++ plugin-bsp/settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "intellij-bsp"
-include("test-utils", "magicmetamodel", "probe-setup", "probe", "protocol", "workspacemodel")
+include("test-utils", "magicmetamodel", "protocol", "workspacemodel")
