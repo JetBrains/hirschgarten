a3c191aade6cc5dc40298d72a6299331be57c5a7
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2024-01-25 13:18:11 +0000

    [maintenance] java shim of `ConnectionDetailsProviderExtension`
    separate interface
    
    [maintenance] java shim of `ConnectionDetailsProviderExtension#onFirstOpening`
    
    
    Merge-request: BAZEL-MR-769
    Merged-by: Marcin Abramowicz <marcin.abramowicz@jetbrains.com>

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/server/connection/BspConnection.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/server/connection/BspConnection.kt
@@ -1,20 +1,14 @@
 package org.jetbrains.plugins.bsp.server.connection
 
-import ch.epfl.scala.bsp4j.BspConnectionDetails
 import ch.epfl.scala.bsp4j.BuildServer
 import ch.epfl.scala.bsp4j.JavaBuildServer
 import ch.epfl.scala.bsp4j.JvmBuildServer
 import ch.epfl.scala.bsp4j.PythonBuildServer
 import ch.epfl.scala.bsp4j.ScalaBuildServer
 import com.intellij.openapi.components.Service
-import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.bsp.BazelBuildServer
 import org.jetbrains.bsp.BazelBuildServerCapabilities
-import org.jetbrains.plugins.bsp.config.buildToolId
-import org.jetbrains.plugins.bsp.extension.points.WithBuildToolId
-import org.jetbrains.plugins.bsp.extension.points.withBuildToolIdOrDefault
 
 public interface BspServer :
   BuildServer,
@@ -54,63 +48,12 @@ public interface BspConnection {
   public fun isConnected(): Boolean
 }
 
-/**
- * Extension that provides connection details for connecting to a BSP server.
- *
- * It should provide the latest available connection details so
- * the client is always connected to the latest server.
- * For example if the connection file has changed after initial import of the project,
- * this extension allows the client to disconnect from the old server and connection to the new one.
- *
- * Implementation should take care of its state (e.g. using a dedicated service with state).
- */
-public interface ConnectionDetailsProviderExtension : WithBuildToolId {
-  /**
-   * Method called only on the first opening of the project (so initial sync).
-   * It should be used to run an initial configuration, e.g. show a wizard.
-   *
-   * Note: UI actions should be executed under: [com.intellij.openapi.application.writeAction]
-   *
-   * @return [true] if all the actions have succeeded and initial sync should continue;
-   * [false] if something has failed and initial sync should be terminated.
-   */
-  public suspend fun onFirstOpening(project: Project, projectPath: VirtualFile): Boolean
-
-  /**
-   * Provides the new connection details if are available.
-   *
-   * The method is called before each task executed on server.
-   * Client will disconnect from the old server and connect to the new one if non-null value is returned.
-   *
-   * [currentConnectionDetails] set to [null] means that client is not aware of any connection details
-   * (like during initial sync or after reopening the project)
-   *
-   * @return [null] if the newest available connection details are equal to [currentConnectionDetails];
-   * otherwise new [BspConnectionDetails] if available
-   */
-  public fun provideNewConnectionDetails(
-    project: Project,
-    currentConnectionDetails: BspConnectionDetails?,
-  ): BspConnectionDetails?
-
-  public companion object {
-    internal val ep: ExtensionPointName<ConnectionDetailsProviderExtension> =
-      ExtensionPointName.create("org.jetbrains.bsp.connectionDetailsProviderExtension")
-  }
-}
-
 internal var Project.connection: BspConnection
   get() = findOrCreateConnection().also { connection = it }
   set(value) { BspConnectionService.getInstance(this).connection = value }
 
 private fun Project.findOrCreateConnection(): BspConnection =
-  BspConnectionService.getInstance(this).connection ?: createNewConnection()
-
-private fun Project.createNewConnection(): BspConnection {
-  val extension = ConnectionDetailsProviderExtension.ep.withBuildToolIdOrDefault(buildToolId)
-
-  return DefaultBspConnection(this, extension)
-}
+  BspConnectionService.getInstance(this).connection ?: DefaultBspConnection(this, connectionDetailsProvider)
 
 @Service(Service.Level.PROJECT)
 internal class BspConnectionService {
