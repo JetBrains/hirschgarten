9520592ab4af8f04e496b9722a407a769b405c3c
Marcin Kocot <marcin.kocot@jetbrains.com>
2024-01-28 16:32:19 +0100

    Describe the debugging logic

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/debug/platform/StarlarkBreakpointProperties.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/debug/platform/StarlarkBreakpointProperties.kt
@@ -3,9 +3,9 @@ package org.jetbrains.bazel.debug.platform
 import com.intellij.xdebugger.breakpoints.XBreakpointProperties
 
 // File path and line number are always preserved automatically, so they don't have to be kept here
-// Currently this class is empty, but needs to be kept nevertheless to satisfy XLineBreakpointType's type parameter
+// This class seems useless, but needs to be kept nevertheless to satisfy XLineBreakpointType's type parameter
 class StarlarkBreakpointProperties : XBreakpointProperties<StarlarkBreakpointProperties>() {
   override fun getState(): StarlarkBreakpointProperties = this
 
-  override fun loadState(state: StarlarkBreakpointProperties) { }
+  override fun loadState(state: StarlarkBreakpointProperties) = Unit
 }
