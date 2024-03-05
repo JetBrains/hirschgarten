64550fbb6c74dcbe6de474cb8c22b92ee0e4276b
Lev Leontev <lev.leontev@jetbrains.com>
2024-03-05 20:03:50 +0000

    [maintenance] Reduce the custom classes amount for Android resources
    Merge-request: BAZEL-MR-880
    Merged-by: Lev Leontev <lev.leontev@jetbrains.com>
    
    GitOrigin-RevId: e03bad48f9acc599aad5f37ae6f98987aeba6864

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/android/BspProjectSystemSyncManager.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/android/BspProjectSystemSyncManager.kt
@@ -4,28 +4,37 @@ import com.android.tools.idea.projectsystem.PROJECT_SYSTEM_SYNC_TOPIC
 import com.android.tools.idea.projectsystem.ProjectSystemSyncManager
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
-import com.intellij.ui.AppUIUtil
+import org.jetbrains.plugins.bsp.services.MagicMetaModelService
 
 public class BspProjectSystemSyncManager(private val project: Project) : ProjectSystemSyncManager {
-  override fun getLastSyncResult(): ProjectSystemSyncManager.SyncResult = ProjectSystemSyncManager.SyncResult.SUCCESS
+  init {
+    val magicMetaModel = MagicMetaModelService.getInstance(project).value
+    magicMetaModel.registerTargetLoadListener {
+      onSyncEnded(project)
+    }
+    onSyncEnded(project)
+  }
+
+  private fun onSyncEnded(project: Project) {
+    DumbService.getInstance(project).smartInvokeLater {
+      project.messageBus
+        .syncPublisher(PROJECT_SYSTEM_SYNC_TOPIC)
+        .syncEnded(ProjectSystemSyncManager.SyncResult.SUCCESS)
+    }
+  }
+
+  override fun syncProject(
+    reason: ProjectSystemSyncManager.SyncReason,
+  ): ListenableFuture<ProjectSystemSyncManager.SyncResult> {
+    onSyncEnded(project)
+    return Futures.immediateFuture(ProjectSystemSyncManager.SyncResult.SUCCESS)
+  }
 
   override fun isSyncInProgress(): Boolean = false
 
   override fun isSyncNeeded(): Boolean = false
 
-  override fun syncProject(
-    reason: ProjectSystemSyncManager.SyncReason,
-  ): ListenableFuture<ProjectSystemSyncManager.SyncResult> {
-    AppUIUtil.invokeLaterIfProjectAlive(project) {
-      onSyncEnded(project)
-    }
-    return Futures.immediateFuture(ProjectSystemSyncManager.SyncResult.SUCCESS)
-  }
-
-  public companion object {
-    public fun onSyncEnded(project: Project) {
-      project.messageBus.syncPublisher(PROJECT_SYSTEM_SYNC_TOPIC).syncEnded(ProjectSystemSyncManager.SyncResult.SUCCESS)
-    }
-  }
+  override fun getLastSyncResult(): ProjectSystemSyncManager.SyncResult = ProjectSystemSyncManager.SyncResult.SUCCESS
 }
