76af70def3fb53d2cc873e1b2e3302e3dfc26954
Lev Leontev <lev.leontev@jetbrains.com>
2024-03-06 02:33:43 +0100

    [fix] Fix UI preview race condition

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/android/BspProjectSystemSyncManager.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/android/BspProjectSystemSyncManager.kt
@@ -2,6 +2,7 @@ package org.jetbrains.plugins.bsp.android
 
 import com.android.tools.idea.projectsystem.PROJECT_SYSTEM_SYNC_TOPIC
 import com.android.tools.idea.projectsystem.ProjectSystemSyncManager
+import com.android.tools.idea.startup.ClearResourceCacheAfterFirstBuild
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
 import com.intellij.openapi.project.DumbService
@@ -12,12 +13,12 @@ public class BspProjectSystemSyncManager(private val project: Project) : Project
   init {
     val magicMetaModel = MagicMetaModelService.getInstance(project).value
     magicMetaModel.registerTargetLoadListener {
-      onSyncEnded(project)
+      notifySyncEnded(project)
     }
-    onSyncEnded(project)
+    initialNotifySyncEnded(project)
   }
 
-  private fun onSyncEnded(project: Project) {
+  private fun notifySyncEnded(project: Project) {
     DumbService.getInstance(project).smartInvokeLater {
       project.messageBus
         .syncPublisher(PROJECT_SYSTEM_SYNC_TOPIC)
@@ -25,10 +26,22 @@ public class BspProjectSystemSyncManager(private val project: Project) : Project
     }
   }
 
+  private fun initialNotifySyncEnded(project: Project) {
+    notifySyncEnded(project)
+
+    // Hack: Android UI preview waits for ClearResourceCacheAfterFirstBuild to have received a successful sync message.
+    // But unfortunately, we have a race condition between us sending the initial syncEnded message
+    // and ClearResourceCacheAfterFirstBuild starting to listen to PROJECT_SYSTEM_SYNC_TOPIC.
+    // Therefore, we have to notify ClearResourceCacheAfterFirstBuild directly.
+    DumbService.getInstance(project).smartInvokeLater {
+      ClearResourceCacheAfterFirstBuild.getInstance(project).syncSucceeded()
+    }
+  }
+
   override fun syncProject(
     reason: ProjectSystemSyncManager.SyncReason,
   ): ListenableFuture<ProjectSystemSyncManager.SyncResult> {
-    onSyncEnded(project)
+    notifySyncEnded(project)
     return Futures.immediateFuture(ProjectSystemSyncManager.SyncResult.SUCCESS)
   }
 
