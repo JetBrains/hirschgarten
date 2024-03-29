2161500577dde9b792486e1ea7fe1be0629bca3e
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2024-03-29 14:04:40 +0100

    Refactor
    GitOrigin-RevId: 3bc9c1c0fecacd5a2cc9bafea2e8d1bf91801bca

--- bsp-testkit2/bsp-testkit/client/bazel/BazelJsonTransformer.kt
+++ bsp-testkit2/bsp-testkit/client/bazel/BazelJsonTransformer.kt
@@ -1,31 +1,21 @@
 package org.jetbrains.bsp.testkit.client.bazel
 
-import ch.epfl.scala.bsp4j.BuildClient
-import ch.epfl.scala.bsp4j.BuildServer
-import ch.epfl.scala.bsp4j.InitializeBuildParams
-import org.jetbrains.bsp.testkit.client.BasicTestClient
 import java.nio.file.Path
 import java.util.Locale
 
-class BazelTestClient<Server : BuildServer, Client : BuildClient>(
-  workspacePath: Path,
-  initializeParams: InitializeBuildParams,
+class BazelJsonTransformer(
+  private val workspacePath: Path,
   private val bazelCache: Path,
   private val bazelOutputBase: Path,
-  client: Client,
-  serverClass: Class<Server>
-) : BasicTestClient<Server, Client>(
-  workspacePath,
-  initializeParams,
-  { s: String ->
-    s.replace("\$WORKSPACE", workspacePath.toString())
+) {
+  fun transformJson(s: String): String {
+    return s
+      .replace("\$WORKSPACE", workspacePath.toString())
       .replace("\$BAZEL_CACHE", bazelCache.toString())
       .replace("\$BAZEL_OUTPUT_BASE_PATH", bazelOutputBase.toString())
       .replace("\$OS", osFamily)
-  },
-  client = client,
-  serverClass = serverClass
-) {
+  }
+
   companion object {
     private val osFamily: String = System.getProperty("os.name").lowercase(Locale.getDefault()).let { osName ->
       when {
