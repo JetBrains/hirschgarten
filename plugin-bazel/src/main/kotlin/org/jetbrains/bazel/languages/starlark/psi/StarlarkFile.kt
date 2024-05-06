1e909599544f041995643e4a3229f65d59e7765d
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2024-05-06 15:04:04 +0000

    [feature] add bazel file types and global function completions
    Add CR adjustments
    
    Add CR adjustments
    
    Add file type and global function completions
    
    
    Merge-request: BAZEL-MR-1069
    Merged-by: Miłosz Piekutowski <milosz.piekutowski@jetbrains.com>

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/StarlarkFile.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/StarlarkFile.kt
@@ -5,8 +5,12 @@ import com.intellij.openapi.fileTypes.FileType
 import com.intellij.psi.FileViewProvider
 import org.jetbrains.bazel.languages.starlark.StarlarkFileType
 import org.jetbrains.bazel.languages.starlark.StarlarkLanguage
-
+import org.jetbrains.bazel.languages.starlark.bazel.BazelFileType
 
 class StarlarkFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, StarlarkLanguage), StarlarkElement {
   override fun getFileType(): FileType = StarlarkFileType
+
+  fun isBuildFile(): Boolean = this.getBazelFileType() == BazelFileType.BUILD
+
+  fun getBazelFileType(): BazelFileType = BazelFileType.ofFileName.getOrDefault(name, BazelFileType.EXTENSION)
 }
