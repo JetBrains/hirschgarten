d14965015a55315190ce230fce0ea187d9a77568
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-05-10 18:02:44 +0000

    [fix] md5 hash should be consistent when being used in multiple threads
    GitOrigin-RevId: 2b5b6995d4285ee586e2f21323751f0033e8ddc2

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/utils/StringUtils.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/utils/StringUtils.kt
@@ -3,8 +3,6 @@ package org.jetbrains.plugins.bsp.utils
 import java.security.MessageDigest
 
 internal object StringUtils {
-  private val MD5 = MessageDigest.getInstance("MD5")
-
   /**
    * Creates a md5 hashed string from an original string
    * @param s the original string
@@ -12,7 +10,7 @@ internal object StringUtils {
    */
   @OptIn(ExperimentalStdlibApi::class)
   fun md5Hash(s: String, n: Int): String {
-    val hash = MD5.digest(s.toByteArray())
+    val hash = MessageDigest.getInstance("MD5".intern()).digest(s.toByteArray())
     return hash.toHexString(0, n / 2 + n % 2).take(n)
   }
 }
