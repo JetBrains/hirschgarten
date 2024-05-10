0cd9e9291f0e057d46e17e3b733401f9a9fd9219
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2024-05-10 11:08:13 +0000

    [feature] add empty line for enter between square brackets
    GitOrigin-RevId: ed298d391f95c02933e4e0b9b552c90a15fb2798

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/indentation/StarlarkEnterBetweenBracesHandler.kt
@@ -0,0 +1,8 @@
+package org.jetbrains.bazel.languages.starlark.indentation
+
+import com.intellij.codeInsight.editorActions.enter.EnterBetweenBracesDelegate
+
+class StarlarkEnterBetweenBracesHandler : EnterBetweenBracesDelegate() {
+  override fun isBracePair(lBrace: Char, rBrace: Char): Boolean =
+    super.isBracePair(lBrace, rBrace) || lBrace == '[' && rBrace == ']'
+}
---
+++ plugin-bazel/src/test/kotlin/org/jetbrains/bazel/languages/starlark/indentation/StarlarkEnterBetweenBracesTest.kt
@@ -0,0 +1,22 @@
+package org.jetbrains.bazel.languages.starlark.indentation
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class StarlarkEnterBetweenBracesTest : BasePlatformTestCase() {
+  @Test
+  fun `should add empty line after enter between braces`() {
+    // given
+    myFixture.configureByText("dummy.bzl", "[<caret>]")
+    val expected = "[\n<caret>\n]"
+
+    // when
+    myFixture.type("\n")
+
+    // then
+    myFixture.checkResult(expected)
+  }
+}
