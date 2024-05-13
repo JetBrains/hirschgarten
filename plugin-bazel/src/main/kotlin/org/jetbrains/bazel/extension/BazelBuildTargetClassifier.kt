dc42f289cd9edd7a93249c0c494f66a88d7ad503
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-05-13 13:34:36 +0000

    [maintenance] optimize `BazelBuildTargetClassifier` by staying away from regex

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/extension/BazelBuildTargetClassifier.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/extension/BazelBuildTargetClassifier.kt
@@ -5,22 +5,50 @@ import org.jetbrains.plugins.bsp.extension.points.BuildTargetClassifierExtension
 import org.jetbrains.plugins.bsp.extension.points.BuildToolId
 import org.jetbrains.plugins.bsp.magicmetamodel.impl.workspacemodel.BuildTargetInfo
 
+private data class TargetIdComponents(
+  val packagePath: String,
+  val targetName: String,
+)
+
 internal class BazelBuildTargetClassifier : BuildTargetClassifierExtension {
   override val buildToolId: BuildToolId = bazelBspBuildToolId
 
   override val separator: String = "/"
 
-  private val bazelLabelRegex = """@?@?(?<repository>.*)//(?<package>.*):(?<target>.*)""".toRegex()
-
-  override fun calculateBuildTargetPath(buildTargetInfo: BuildTargetInfo): List<String> {
-    return bazelLabelRegex.find(buildTargetInfo.id)?.groups
-      ?.get("package")
-      ?.value
+  override fun calculateBuildTargetPath(buildTargetInfo: BuildTargetInfo): List<String> =
+    extractTargetIdComponents(buildTargetInfo.id)
+      ?.packagePath
       ?.split("/")
       ?.filter { it.isNotEmpty() }
       .orEmpty()
-  }
 
   override fun calculateBuildTargetName(buildTargetInfo: BuildTargetInfo): String =
-    bazelLabelRegex.find(buildTargetInfo.id)?.groups?.get("target")?.value ?: buildTargetInfo.id
+    extractTargetIdComponents(buildTargetInfo.id)
+      ?.targetName
+      ?: buildTargetInfo.id
+
+  /**
+   * This method uses string manipulation to extract useful components from Bazel target id,
+   * it assumes the target id has the format `@?@?(?<repository>.*)//(?<package>.*):(?<target>.*)`
+   */
+  private fun extractTargetIdComponents(label: String): TargetIdComponents? {
+    val repositoryAndTheRest = label.split("//", limit = 2)
+    if (repositoryAndTheRest.size != 2) return null
+    val repository = repositoryAndTheRest[0].trimStart { it == '@' }
+    val packagePathAndTargetName = repositoryAndTheRest[1].split(":", limit = 2)
+    if (packagePathAndTargetName.isEmpty()) return null
+    val fullPackagePath =
+      if (repository.isEmpty()) packagePathAndTargetName[0]
+      else "$repository/${packagePathAndTargetName[0]}"
+    return if (packagePathAndTargetName.size == 1)
+      TargetIdComponents(
+        packagePath = fullPackagePath,
+        targetName = label,
+      )
+    else
+      TargetIdComponents(
+        packagePath = fullPackagePath,
+        targetName = packagePathAndTargetName[1],
+      )
+  }
 }
