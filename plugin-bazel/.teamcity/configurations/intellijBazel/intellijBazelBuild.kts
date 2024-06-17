09808c6bdd167ac3476060f9d2b29d673b8121e2
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-06-17 14:10:18 +0000

    [maintenance] migrate all TeamCity pipelines to bazel.teamcity.com; refactor TeamCity config;
    GitOrigin-RevId: 2201f1f4d25071967bf8837357af76a6169f3779

--- plugin-bazel/.teamcity/configurations/intellijBazel/intellijBazelBuild.kts
+++ plugin-bazel/.teamcity/configurations/intellijBazel/intellijBazelBuild.kts
@@ -2,11 +2,14 @@ package configurations.intellijBazel
 
 import configurations.BaseConfiguration
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-object BuildTheProject : BaseConfiguration.BaseBuildType(
+open class Build (
+    vcsRoot: GitVcsRoot,
+): BaseConfiguration.BaseBuildType(
         name = "[build] build intellij-bazel",
         setupSteps = false,
-        vcsRoot = BaseConfiguration.IntellijBazelVcs,
+        vcsRoot = vcsRoot,
         steps = {
             gradle {
                 name = "build plugin"
@@ -14,4 +17,12 @@ object BuildTheProject : BaseConfiguration.BaseBuildType(
                 jdkHome = "%env.JDK_17_0%"
             }
         },
+)
+
+object GitHub : Build(
+    vcsRoot = BaseConfiguration.GitHubVcs,
+)
+
+object Space : Build(
+    vcsRoot = BaseConfiguration.SpaceVcs
 )
