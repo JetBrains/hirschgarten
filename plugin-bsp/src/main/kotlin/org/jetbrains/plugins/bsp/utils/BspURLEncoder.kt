4a66e309084af9cd1428f44c37eb7777aae19418
Lev Leontev <lev.leontev@jetbrains.com>
2024-06-26 13:17:04 +0000

    [fix] Fix non-ASCII source paths

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/utils/BspURLEncoder.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/utils/BspURLEncoder.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.plugins.bsp.utils
 
 import java.net.URI
+import java.net.URLEncoder
 
 /**
  * Utility class for encoding and decoding text using URL encoding, which can be used for creating URI.
@@ -8,15 +9,16 @@ import java.net.URI
  */
 internal object BspURLEncoder {
   /**
-   * The method accepts a (maybe malformed) URI, e.g., with spaces, and returns an encoded valid URI string.
+   * The method accepts a (maybe malformed) URI, e.g., with spaces, and returns a valid URI string.
    */
   fun encode(uri: String): String {
-    return java.net.URLEncoder.encode(uri, Charsets.UTF_8)
+    return URLEncoder.encode(uri, Charsets.UTF_8)
       // this is a valid character in URI
       .replace("+", "%20")
       // `java.net.URI` cannot parse these 2 characters correctly after encoding
       .replace("%3A", ":")
       .replace("%2F", "/")
+      .replace("%25", "%")
   }
 }
 
--- plugin-bsp/src/test/kotlin/org/jetbrains/plugins/bsp/utils/BspURLEncoderTest.kt
+++ plugin-bsp/src/test/kotlin/org/jetbrains/plugins/bsp/utils/BspURLEncoderTest.kt
@@ -35,24 +35,36 @@ class BspURLEncoderTest {
   @Test
   fun `malformed URI with weird but valid file characters should be encoded properly`() {
     // given
-    val uri = "file:///path/to/weird/project/my-project&*&'^@*&^!(*#%\\$>?><"
+    val uri = "file:///path/to/weird/project/my-project&*&'^@*&^!(*#\\$>?><"
 
     // when
     val encodedUri = BspURLEncoder.encode(uri)
 
     // then
-    encodedUri shouldBe "file:///path/to/weird/project/my-project%26*%26%27%5E%40*%26%5E%21%28*%23%25%5C%24%3E%3F%3E%3C"
+    encodedUri shouldBe "file:///path/to/weird/project/my-project%26*%26%27%5E%40*%26%5E%21%28*%23%5C%24%3E%3F%3E%3C"
   }
 
   @Test
   fun `malformed URI of a valid directory should be fully functional`() {
     // given
-    val tempDir = createTempDirectory("my project&*&'^@*&^!(*#%\\\$>?><").also { it.toFile().deleteOnExit() }
+    val tempDir = createTempDirectory("my project&*&'^@*&^!(*#\\\$>?><").also { it.toFile().deleteOnExit() }
 
     // this malformedURI will have the format of file:///.../my project&*&'^@*&^!(*#%\$>?><.../
     val malformedURI = URLDecoder.decode(tempDir.toUri().toString(), StandardCharsets.UTF_8.name())
 
     // when and then
     tempDir shouldBe malformedURI.safeCastToURI().toPath()
+  }
+
+  @Test
+  fun `escape codes should not be changed`() {
+    // given
+    val uri = "file:///%D1%82%D0%B5%D1%81%D1%82" // "file:///тест"
+
+    // when
+    val encodedUri = BspURLEncoder.encode(uri)
+
+    // then
+    encodedUri shouldBe uri
   }
 }
