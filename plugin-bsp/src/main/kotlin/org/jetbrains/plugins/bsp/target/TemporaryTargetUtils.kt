79d8a0cb46a5523aa60da769691202b1fd8b0f01
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2024-07-24 06:35:25 +0000

    [ui] no more bazel icons in the project tree
    GitOrigin-RevId: 696d9de7dcefa63e5b43e32acd89996c68d44132

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/target/TemporaryTargetUtils.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/target/TemporaryTargetUtils.kt
@@ -10,7 +10,6 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VirtualFileManager
 import org.jetbrains.plugins.bsp.config.BspFeatureFlags
 import org.jetbrains.plugins.bsp.config.rootDir
 import org.jetbrains.plugins.bsp.magicmetamodel.impl.BuildTargetInfoState
@@ -28,7 +27,6 @@ public data class TemporaryTargetUtilsState(
   var idToTargetInfo: Map<String, BuildTargetInfoState> = emptyMap(),
   var moduleIdToBuildTargetId: Map<String, String> = emptyMap(),
   var fileToId: Map<String, List<String>> = emptyMap(),
-  var targetsBaseDir: List<String> = emptyList(),
   var libraries: List<LibraryState> = emptyList(),
 )
 
@@ -45,7 +43,6 @@ public class TemporaryTargetUtils : PersistentStateComponent<TemporaryTargetUtil
   // we must use URI as comparing URI path strings is susceptible to errors.
   // e.g., file:/test and file:///test should be similar in the URI world
   private var fileToTargetId: Map<URI, List<BuildTargetIdentifier>> = hashMapOf()
-  private var targetsBaseDir: Set<VirtualFile> = emptySet()
   private var libraries: List<Library> = emptyList()
   private var libraryModules: List<JavaModule> = emptyList()
   private var libraryModulesLookupTable: HashSet<String> = hashSetOf()
@@ -66,11 +63,6 @@ public class TemporaryTargetUtils : PersistentStateComponent<TemporaryTargetUtil
     fileToTargetId = targetIdToModuleDetails.values.flatMap { it.toPairsUrlToId() }
       .groupBy { it.first }
       .mapValues { it.value.map { pair -> pair.second } }
-    val virtualFileUrlManager = VirtualFileManager.getInstance()
-    targetsBaseDir = this.targetIdToTargetInfo.values
-      .mapNotNull { it.baseDirectory }
-      .mapNotNull { virtualFileUrlManager.findFileByUrl(it) }
-      .toSet()
     this.libraries = libraries
     this.libraryModules = libraryModules
     this.libraryModulesLookupTable = createLibraryModulesLookupTable()
@@ -118,9 +110,6 @@ public class TemporaryTargetUtils : PersistentStateComponent<TemporaryTargetUtil
   public fun getBuildTargetInfoForId(buildTargetIdentifier: BuildTargetIdentifier): BuildTargetInfo? =
     targetIdToTargetInfo[buildTargetIdentifier]
 
-  public fun isBaseDir(virtualFile: VirtualFile): Boolean =
-    targetsBaseDir.contains(virtualFile)
-
   public fun getAllLibraries(): List<Library> = libraries
 
   public fun isLibraryModule(name: String): Boolean = name in libraryModulesLookupTable
@@ -132,20 +121,16 @@ public class TemporaryTargetUtils : PersistentStateComponent<TemporaryTargetUtil
       idToTargetInfo = targetIdToTargetInfo.mapKeys { it.key.uri }.mapValues { it.value.toState() },
       moduleIdToBuildTargetId = moduleIdToBuildTargetId.mapValues { it.value.uri },
       fileToId = fileToTargetId.mapKeys { o -> o.key.toString() }.mapValues { o -> o.value.map { it.uri } },
-      targetsBaseDir = targetsBaseDir.map { it.url }.toList(),
       libraries = libraries.map { it.toState() },
     )
 
   override fun loadState(state: TemporaryTargetUtilsState) {
-    val virtualFileUrlManager = VirtualFileManager.getInstance()
-
     targetIdToTargetInfo = state.idToTargetInfo
       .mapKeys { BuildTargetIdentifier(it.key) }
       .mapValues { it.value.fromState() }
     moduleIdToBuildTargetId = state.moduleIdToBuildTargetId.mapValues { BuildTargetIdentifier(it.value) }
     fileToTargetId =
       state.fileToId.mapKeys { o -> o.key.safeCastToURI() }.mapValues { o -> o.value.map { BuildTargetIdentifier(it) } }
-    targetsBaseDir = state.targetsBaseDir.mapNotNull { virtualFileUrlManager.findFileByUrl(it) }.toSet()
     libraries = state.libraries.map { it.fromState() }
   }
 }
--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/project/tree/BspProjectViewNodeDecorator.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/project/tree/BspProjectViewNodeDecorator.kt
@@ -5,13 +5,9 @@ import com.intellij.ide.projectView.ProjectViewNode
 import com.intellij.ide.projectView.ProjectViewNodeDecorator
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode
 import com.intellij.openapi.project.Project
-import com.intellij.ui.SimpleTextAttributes
-import org.jetbrains.plugins.bsp.assets.assets
 import org.jetbrains.plugins.bsp.config.isBspProject
-import org.jetbrains.plugins.bsp.config.rootDir
-import org.jetbrains.plugins.bsp.target.temporaryTargetUtils
 
-public class BspProjectViewNodeDecorator(private val project: Project) : ProjectViewNodeDecorator {
+class BspProjectViewNodeDecorator(private val project: Project) : ProjectViewNodeDecorator {
   override fun decorate(node: ProjectViewNode<*>, data: PresentationData) {
     if (project.isBspProject) {
       decorateForBsp(node, data)
@@ -20,27 +16,8 @@ public class BspProjectViewNodeDecorator(private val project: Project) : Project
 
   private fun decorateForBsp(node: ProjectViewNode<*>, data: PresentationData) {
     if (node is PsiDirectoryNode) {
-      decorateIfBspTarget(node, data)
       data.removeJavaHackSourceRoot()
     }
-  }
-
-  private fun decorateIfBspTarget(node: PsiDirectoryNode, data: PresentationData) {
-    when {
-      node.isRootModule() -> data.addIconAndHighlight(node.virtualFile?.name)
-      node.isBspModule() -> data.addIconAndHighlight(data.presentableText)
-    }
-  }
-
-  private fun PsiDirectoryNode.isRootModule(): Boolean =
-    project.rootDir == virtualFile
-
-  private fun PsiDirectoryNode.isBspModule(): Boolean =
-    virtualFile?.let { project.temporaryTargetUtils.isBaseDir(it) } == true
-
-  private fun PresentationData.addIconAndHighlight(text: String?) {
-    setIcon(project.assets.icon)
-    addText(text, SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES)
   }
 
   private fun PresentationData.removeJavaHackSourceRoot() {
