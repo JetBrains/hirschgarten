ce30d7488940d2bf0dede4aef1aa5143a7a332de
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2024-07-24 16:06:15 +0000

    [maintenance] Update SDK
    GitOrigin-RevId: 3229851893bb9b3962f10ac2a08c5a9adc628c68

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/android/BspProjectSystemBuildManager.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/android/BspProjectSystemBuildManager.kt
@@ -23,12 +23,12 @@ public class BspProjectSystemBuildManager(private val project: Project) : Projec
   override fun addBuildListener(parentDisposable: Disposable, buildListener: ProjectSystemBuildManager.BuildListener) {
     project.messageBus.connect(parentDisposable).subscribe(BspWorkspaceListener.TOPIC, object : BspWorkspaceListener {
       override fun syncStarted() {
-        buildListener.buildStarted(BuildMode.COMPILE)
+        buildListener.buildStarted(BuildMode.COMPILE_OR_ASSEMBLE)
       }
 
       override fun syncFinished(canceled: Boolean) {
         val status = if (canceled) BuildStatus.CANCELLED else BuildStatus.SUCCESS
-        buildListener.buildCompleted(BuildResult(BuildMode.COMPILE, status, System.currentTimeMillis()))
+        buildListener.buildCompleted(BuildResult(BuildMode.COMPILE_OR_ASSEMBLE, status, System.currentTimeMillis()))
       }
     })
   }
