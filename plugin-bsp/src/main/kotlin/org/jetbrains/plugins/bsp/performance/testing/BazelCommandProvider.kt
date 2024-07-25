bb1e6e6dded93995018f35fb6bee6805407dfca1
Lev Leontev <lev.leontev@jetbrains.com>
2024-07-25 13:08:13 +0000

    [feature] Measure memory after indexing;
    rename metric "bsp.used.at.exit.mb" to "bsp.used.after.sync.mb";
    add new metric "bsp.used.after.indexing.mb"

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/performance/testing/BazelCommandProvider.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/performance/testing/BazelCommandProvider.kt
@@ -6,8 +6,9 @@ import com.jetbrains.performancePlugin.CreateCommand
 public class BazelCommandProvider : CommandProvider {
   override fun getCommands(): Map<String, CreateCommand> = mapOf(
     WaitForBazelSyncCommand.PREFIX to CreateCommand(::WaitForBazelSyncCommand),
-    StartMemoryProfilingCommand.PREFIX to CreateCommand(::StartMemoryProfilingCommand),
-    StopMemoryProfilingCommand.PREFIX to CreateCommand(::StopMemoryProfilingCommand),
+    StartRecordingMaxMemoryCommand.PREFIX to CreateCommand(::StartRecordingMaxMemoryCommand),
+    StopRecordingMaxMemoryCommand.PREFIX to CreateCommand(::StopRecordingMaxMemoryCommand),
+    RecordMemoryCommand.PREFIX to CreateCommand(::RecordMemoryCommand),
     OpenBspToolWindowCommand.PREFIX to CreateCommand(::OpenBspToolWindowCommand),
   )
 }
--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/performance/testing/StopRecordingMaxMemoryCommand.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/performance/testing/StopRecordingMaxMemoryCommand.kt
@@ -3,12 +3,12 @@ package org.jetbrains.plugins.bsp.performance.testing
 import com.intellij.openapi.ui.playback.PlaybackContext
 import com.intellij.openapi.ui.playback.commands.PlaybackCommandCoroutineAdapter
 
-internal class StartMemoryProfilingCommand(text: String, line: Int) : PlaybackCommandCoroutineAdapter(text, line) {
+internal class StopRecordingMaxMemoryCommand(text: String, line: Int) : PlaybackCommandCoroutineAdapter(text, line) {
   companion object {
-    const val PREFIX = CMD_PREFIX + "startMemoryProfiling"
+    const val PREFIX = CMD_PREFIX + "stopRecordingMaxMemory"
   }
 
   override suspend fun doExecute(context: PlaybackContext) {
-    MemoryProfiler.startRecording()
+    MemoryProfiler.stopRecordingMaxMemory()
   }
 }
