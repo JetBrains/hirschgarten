cbc9acdba8af279036b77ae26ee949ff821b2c59
Lev Leontev <lev.leontev@jetbrains.com>
2024-07-29 10:45:48 +0000

    [fix] Fix "More than one runner found for ID: Debug" error
    GitOrigin-RevId: 275ffd13fe0d0d832c68d12ff58268b3722d3fd4

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/intellij/IntellijPluginDebuggerRunner.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/intellij/IntellijPluginDebuggerRunner.kt
@@ -6,6 +6,8 @@ import com.intellij.execution.executors.DefaultDebugExecutor
 import org.jetbrains.plugins.bsp.ui.configuration.BspRunConfiguration
 
 public class IntellijPluginDebuggerRunner : GenericDebuggerRunner() {
+  override fun getRunnerId(): String = "org.jetbrains.plugins.bsp.intellij.IntellijPluginDebuggerRunner"
+
   override fun canRun(executorId: String, profile: RunProfile): Boolean {
     if (executorId != DefaultDebugExecutor.EXECUTOR_ID) return false
     if (profile !is BspRunConfiguration) return false
