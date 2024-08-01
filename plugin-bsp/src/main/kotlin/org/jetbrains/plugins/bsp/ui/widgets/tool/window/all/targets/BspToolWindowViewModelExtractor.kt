c722a2a0533c3328ba2903a95ed007a7a4b5f6ac
Lev Leontev <lev.leontev@jetbrains.com>
2024-08-01 14:59:30 +0000

    [feature] Code With Me support | #BAZEL-1063 done
    GitOrigin-RevId: 864b1c5ac54ad6edcc36720dbf4aaca58dc63819

---
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/widgets/tool/window/all/targets/BspToolWindowViewModelExtractor.kt
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.bsp.ui.widgets.tool.window.all.targets
+
+import com.intellij.openapi.client.ClientProjectSession
+import com.intellij.ui.viewModel.extraction.ToolWindowExtractorMode
+import com.intellij.ui.viewModel.extraction.ToolWindowViewModelExtractor
+
+class BspToolWindowViewModelExtractor : ToolWindowViewModelExtractor {
+  override fun isApplicable(toolWindowId: String, session: ClientProjectSession): Boolean =
+    session.project.bspToolWindowIdOrNull == toolWindowId
+
+  override fun getMode(): ToolWindowExtractorMode = ToolWindowExtractorMode.MIRROR
+}
