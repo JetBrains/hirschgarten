9a538f4ba956b5104a077cedcc6a0ef5cb11579e
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-08-06 14:19:50 +0000

    [fix] fix project view to show all the contents inside the project during initial sync | #BAZEL-1069 Done
    GitOrigin-RevId: aaf877bb371f966d8792cb4cfc90d6fdd8526b3a

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/project/tree/BspTreeStructureProvider.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/project/tree/BspTreeStructureProvider.kt
@@ -7,11 +7,16 @@ import com.intellij.ide.projectView.impl.nodes.ProjectViewModuleGroupNode
 import com.intellij.ide.projectView.impl.nodes.ProjectViewModuleNode
 import com.intellij.ide.projectView.impl.nodes.ProjectViewProjectNode
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode
+import com.intellij.ide.projectView.impl.nodes.PsiFileNode
 import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 import org.jetbrains.plugins.bsp.config.isBspProject
+import org.jetbrains.plugins.bsp.config.isBspProjectInitialized
 import org.jetbrains.plugins.bsp.config.rootDir
 
 public class BspTreeStructureProvider : TreeStructureProvider {
@@ -29,19 +34,59 @@ public class BspTreeStructureProvider : TreeStructureProvider {
     val project = parent.project ?: return children
     if (!project.isBspProject) return children
 
-    if (parent is ProjectViewProjectNode) {
-      val rootDirectory =
-        project.service<PsiManager>().findDirectory(project.rootDir) ?: return children // should never happen
-      val rootDirectoryNode = PsiDirectoryNode(project, rootDirectory, settings)
-      val externalLibrariesNode = children.firstIsInstanceOrNull<ExternalLibrariesNode>()
-      return listOfNotNull(rootDirectoryNode, externalLibrariesNode)
+    return when (parent) {
+      is ProjectViewProjectNode -> calculateChildrenForProjectNode(project, children, settings)
+      else -> children.removeModuleAndModuleGroupNodes()
     }
+  }
 
-    return children.removeModuleAndModuleGroupNodes()
+  private fun calculateChildrenForProjectNode(
+    project: Project,
+    children: Collection<AbstractTreeNode<*>>,
+    settings: ViewSettings,
+  ): Collection<AbstractTreeNode<*>> {
+    val rootDirectory =
+      project.service<PsiManager>().findDirectory(project.rootDir) ?: return children // should never happen
+    val rootDirectoryNode = BspDirectoryNode(project, rootDirectory, settings)
+    val externalLibrariesNode = children.firstIsInstanceOrNull<ExternalLibrariesNode>()
+    return listOfNotNull(rootDirectoryNode, externalLibrariesNode)
   }
 
   private fun Collection<AbstractTreeNode<*>>.removeModuleAndModuleGroupNodes(): Collection<AbstractTreeNode<*>> =
     this
       .filterNot { it is ProjectViewModuleGroupNode }
       .filterNot { it is ProjectViewModuleNode }
+}
+
+/**
+ * Custom PsiDirectoryNode for BSP projects to show the project view with full contents during initial sync.
+ */
+class BspDirectoryNode(
+  project: Project,
+  directory: PsiDirectory,
+  viewSettings: ViewSettings?,
+) : PsiDirectoryNode(project, directory, viewSettings) {
+  private val lazyChildren: Collection<AbstractTreeNode<*>> by lazy {
+    directory.calculateChildrenNodes(project, settings)
+  }
+
+  private fun PsiDirectory.calculateChildrenNodes(project: Project, settings: ViewSettings?): Collection<AbstractTreeNode<*>> =
+    if (project.isBspProjectInitialized) {
+      emptyList()
+    } else {
+      children.mapNotNull { psiChild ->
+        when (psiChild) {
+          is PsiDirectory -> BspDirectoryNode(project, psiChild, settings)
+          is PsiFile -> PsiFileNode(project, psiChild, settings)
+          else -> null
+        }
+      }
+    }
+
+  override fun getChildrenImpl(): Collection<AbstractTreeNode<*>?>? =
+    if (lazyChildren.isNotEmpty()) {
+      lazyChildren
+    } else {
+      super.getChildrenImpl()
+    }
 }
