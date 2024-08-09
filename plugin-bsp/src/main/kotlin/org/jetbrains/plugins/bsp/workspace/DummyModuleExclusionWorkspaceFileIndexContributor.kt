0d6db183a5b13d45021ea77d59876d0c8c16ba30
Lev Leontev <lev.leontev@jetbrains.com>
2024-08-09 12:26:40 +0000

    [fix] Don't parse PSI for excluded files in dummy modules

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/workspace/DummyModuleExclusionWorkspaceFileIndexContributor.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/workspace/DummyModuleExclusionWorkspaceFileIndexContributor.kt
@@ -1,15 +1,24 @@
 package org.jetbrains.plugins.bsp.workspace
 
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.PlainTextFileType
+import com.intellij.openapi.fileTypes.impl.FileTypeOverrider
+import com.intellij.openapi.project.ProjectLocator
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.isFile
 import com.intellij.platform.workspace.jps.entities.ModuleEntity
 import com.intellij.platform.workspace.storage.EntityStorage
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndexContributor
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileSetRegistrar
+import org.jetbrains.plugins.bsp.config.isBspProject
 import org.jetbrains.plugins.bsp.workspacemodel.entities.BspDummyEntitySource
 import kotlin.collections.contains
 
-private val SOURCE_EXTENSIONS = listOf("java", "kt", "scala")
+private val SOURCE_EXTENSIONS = listOf("java", "kt", "scala", "py")
+
+private fun VirtualFile.isSourceFile(): Boolean = isFile && extension?.lowercase() in SOURCE_EXTENSIONS
 
 /**
  * If a source file is added to the dummy module but isn't added to any other module, then we should not attempt to index/analyze it.
@@ -33,6 +42,22 @@ class DummyModuleExclusionWorkspaceFileIndexContributor : WorkspaceFileIndexCont
       entity = entity,
     )
   }
+}
 
-  private fun VirtualFile.isSourceFile(): Boolean = isFile && extension?.lowercase() in SOURCE_EXTENSIONS
+/**
+ * Make sure the files we excluded aren't parsed
+ */
+class ExcludedFileTypeOverrider : FileTypeOverrider {
+  override fun getOverriddenFileType(file: VirtualFile): FileType? {
+    if (!file.isSourceFile()) return null
+    val project = ProjectLocator.getInstance().guessProjectForFile(file) ?: return null
+    if (!project.isBspProject) return null
+    val projectFileIndex = ProjectFileIndex.getInstance(project)
+    val excluded = runReadAction { projectFileIndex.isExcluded(file) }
+    return if (excluded) {
+      PlainTextFileType.INSTANCE
+    } else {
+      null
+    }
+  }
 }
