67967fab55057b9172c89645f4fd14134cde669e
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-08-26 17:52:41 +0400

    [maintenance] TeamCity config update;
    switched to using Docker image instead of manual installation of dependencies
    
    GitOrigin-RevId: 9aa3869c90bf5e580b4327c178cce9a012e5fe62

--- .teamcity/configurations/pluginBsp/intellijBenchmark.kts
+++ .teamcity/configurations/pluginBsp/intellijBenchmark.kts
@@ -1,8 +1,9 @@
 package configurations.pluginBsp
 
 import configurations.BaseConfiguration
+import configurations.Utils
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.bazel
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.BazelStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 open class Benchmark(vcsRoot: GitVcsRoot) :
@@ -11,21 +12,7 @@ open class Benchmark(vcsRoot: GitVcsRoot) :
     vcsRoot = vcsRoot,
     artifactRules = "+:%system.teamcity.build.checkoutDir%/bazel-testlogs/** => testlogs.zip",
     steps = {
-      val sysArgs = "--jvmopt=\"-Dbsp.benchmark.project.path=%system.teamcity.build.tempDir%/project_10\" --jvmopt=\"-Dbsp.benchmark.teamcity.url=https://bazel.teamcity.com\""
-      script {
-        this.name = "install xvfb and generate project for benchmark"
-        id = "install_xvfb_and_generate_project_for_benchmark"
-        scriptContent =
-          """
-          #!/bin/bash
-          set -euxo pipefail
-          
-          sudo apt-get update
-          sudo apt-get install -y xvfb
-          
-          bazel run //server/bspcli:generator -- %system.teamcity.build.tempDir%/project_10 10 --targetssize 1
-          """.trimIndent()
-      }
+      val sysArgs = "--jvmopt=\"-Dbsp.benchmark.project.path=/home/hirschuser/project_10\" --jvmopt=\"-Dbsp.benchmark.teamcity.url=https://bazel.teamcity.com\""
       bazel {
         name = "run benchmark"
         id = "run_benchmark"
@@ -33,7 +20,11 @@ open class Benchmark(vcsRoot: GitVcsRoot) :
         targets = "//plugin-bsp/performance-testing"
         arguments =
           "--jvmopt=\"-Xmx12g\" $sysArgs --sandbox_writable_path=/ --action_env=PATH --test_output=errors --announce_rc --show_progress_rate_limit=30 --curses=yes --terminal_columns=140"
-        param("toolPath", "/usr/local/bin")
+        toolPath = "/usr/local/bin"
+        logging = BazelStep.Verbosity.Diagnostic
+        Utils.DockerParams.get().forEach { (key, value) ->
+          param(key, value)
+        }
       }
     },
   )
