af2a929e5c08ba94686842bf2a08425afff4d46c
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-09-05 17:52:19 +0400

    [fix] formatting

--- tools/qodana/run_qodana.sh
+++ tools/qodana/run_qodana.sh
@@ -1,16 +1,16 @@
 rm -rf .qodana
 
 docker run \
--v $(pwd):/data/project/ \
--v $(pwd)/.qodana/plugins/intellij-bazel:/opt/idea/custom-plugins/intellij-bazel \
--v $(pwd)/.qodana/plugins/intellij-bsp:/opt/idea/custom-plugins/intellij-bsp \
--v $(pwd)/.qodana/results/:/data/results \
--e QODANA_TOKEN=$QODANA_TOKEN \
---cpus="10" \
---memory="31g" \
---memory-swap="32g" \
-jetbrains/qodana_bazel \
---property=bsp.build.project.on.sync=true \
---property=idea.is.internal=true \
---report-dir /data/results/report/ \
---save-report
+	-v $(pwd):/data/project/ \
+	-v $(pwd)/.qodana/plugins/intellij-bazel:/opt/idea/custom-plugins/intellij-bazel \
+	-v $(pwd)/.qodana/plugins/intellij-bsp:/opt/idea/custom-plugins/intellij-bsp \
+	-v $(pwd)/.qodana/results/:/data/results \
+	-e QODANA_TOKEN=$QODANA_TOKEN \
+	--cpus="10" \
+	--memory="31g" \
+	--memory-swap="32g" \
+	jetbrains/qodana_bazel \
+	--property=bsp.build.project.on.sync=true \
+	--property=idea.is.internal=true \
+	--report-dir /data/results/report/ \
+	--save-report
