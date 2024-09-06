6d55b966846fadd901cf5cce7b8f327d55c31deb
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-09-06 10:47:58 +0000

    [maintenance] remove feature flag for Scala support

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/config/BspFeatureFlags.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/config/BspFeatureFlags.kt
@@ -3,7 +3,6 @@ package org.jetbrains.plugins.bsp.config
 import com.intellij.openapi.util.registry.Registry
 
 private const val PYTHON_SUPPORT = "bsp.python.support"
-private const val SCALA_SUPPORT = "bsp.scala.support"
 private const val ANDROID_SUPPORT = "bsp.android.support"
 private const val BUILD_PROJECT_ON_SYNC = "bsp.build.project.on.sync"
 private const val SHORTEN_MODULE_LIBRARY_NAMES = "bsp.shorten.module.library.names"
@@ -13,9 +12,6 @@ private const val WRAP_LIBRARIES_INSIDE_MODULES = "bsp.wrap.libraries.inside.mod
 public object BspFeatureFlags {
   public val isPythonSupportEnabled: Boolean
     get() = Registry.`is`(PYTHON_SUPPORT)
-
-  public val isScalaSupportEnabled: Boolean
-    get() = Registry.`is`(SCALA_SUPPORT)
 
   public val isAndroidSupportEnabled: Boolean
     get() = Registry.`is`(ANDROID_SUPPORT)
