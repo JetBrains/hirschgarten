43c01a052557d2a8a2cf019685283ebc952f64a1
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-09-08 11:51:53 +0400

    update remote execution image

--- tools/engflow/docker/build-and-push.sh
+++ tools/engflow/docker/build-and-push.sh
@@ -6,24 +6,24 @@ REGISTRY="registry.jetbrains.team/p/bazel/docker/hirschgarten-engflow"
 
 # Function to check if we're logged in
 check_login() {
-    docker login $REGISTRY -u dummy -p dummy &>/dev/null
-    return $?
+	docker login $REGISTRY -u dummy -p dummy &>/dev/null
+	return $?
 }
 
 # Function to handle login
 handle_login() {
-    echo "Authentication required. Please log in:"
-    docker login $REGISTRY
-    if [ $? -ne 0 ]; then
-        echo "Login failed. Exiting."
-        exit 1
-    fi
+	echo "Authentication required. Please log in:"
+	docker login $REGISTRY
+	if [ $? -ne 0 ]; then
+		echo "Login failed. Exiting."
+		exit 1
+	fi
 }
 
 # Build the Docker image
 if ! docker build -t $REGISTRY:$DATE_TAG .; then
-    echo "Docker build failed. Exiting."
-    exit 1
+	echo "Docker build failed. Exiting."
+	exit 1
 fi
 
 # Tag the image with 'latest'
@@ -31,20 +31,20 @@ docker tag $REGISTRY:$DATE_TAG $REGISTRY:latest
 
 # Check if we're logged in, if not, attempt to log in
 if ! check_login; then
-    handle_login
+	handle_login
 fi
 
 # Function to push image
 push_image() {
-    local tag=$1
-    if ! docker push $REGISTRY:$tag; then
-        echo "Failed to push $REGISTRY:$tag. Attempting to re-authenticate."
-        handle_login
-        if ! docker push $REGISTRY:$tag; then
-            echo "Push failed again for $REGISTRY:$tag. Exiting."
-            exit 1
-        fi
-    fi
+	local tag=$1
+	if ! docker push $REGISTRY:$tag; then
+		echo "Failed to push $REGISTRY:$tag. Attempting to re-authenticate."
+		handle_login
+		if ! docker push $REGISTRY:$tag; then
+			echo "Push failed again for $REGISTRY:$tag. Exiting."
+			exit 1
+		fi
+	fi
 }
 
 # Push the images
