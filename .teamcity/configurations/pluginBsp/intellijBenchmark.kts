2127b66f8b869dca24e12e88215854869dabf2f8
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-09-08 10:16:36 +0400

    update command line params for bench

--- .teamcity/configurations/pluginBsp/intellijBenchmark.kts
+++ .teamcity/configurations/pluginBsp/intellijBenchmark.kts
@@ -13,14 +13,16 @@ open class Benchmark(vcsRoot: GitVcsRoot) :
     vcsRoot = vcsRoot,
     artifactRules = Utils.CommonParams.BazelTestlogsArtifactRules,
     steps = {
-      val sysArgs = "--jvmopt=\"-Dbsp.benchmark.project.path=/home/hirschuser/project_10\" --jvmopt=\"-Dbsp.benchmark.teamcity.url=https://bazel.teamcity.com\""
+//      val sysArgs = "--jvmopt=\"-Dbsp.benchmark.project.path=/home/hirschuser/project_10\" --jvmopt=\"-Dbsp.benchmark.teamcity.url=https://bazel.teamcity.com\""
+      val sysArgs = "--jvmopt=\"-Dbsp.benchmark.teamcity.url=https://bazel.teamcity.com\""
       bazel {
         name = "run benchmark"
         id = "run_benchmark"
         command = "test"
         targets = "//plugin-bsp/performance-testing"
         arguments =
-          "--jvmopt=\"-Xmx12g\" $sysArgs --sandbox_writable_path=/ --action_env=PATH --test_output=errors --announce_rc --show_progress_rate_limit=30 --curses=yes --terminal_columns=140"
+//          "--jvmopt=\"-Xmx12g\" $sysArgs --sandbox_writable_path=/ --action_env=PATH --test_output=errors --announce_rc --show_progress_rate_limit=30 --curses=yes --terminal_columns=140"
+          "--jvmopt=\"-Xmx12g\" $sysArgs --sandbox_writable_path=/ --test_output=errors --announce_rc --show_progress_rate_limit=30 --curses=yes --terminal_columns=140"
         toolPath = "/usr/local/bin"
         logging = BazelStep.Verbosity.Diagnostic
         Utils.DockerParams.get().forEach { (key, value) ->
