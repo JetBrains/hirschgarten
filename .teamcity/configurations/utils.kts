b60bfc7781f93f982b9bfd5f876aa94516cafcbb
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-09-08 09:21:29 +0400

    remove docker container from bazel builds

--- .teamcity/configurations/utils.kts
+++ .teamcity/configurations/utils.kts
@@ -3,10 +3,10 @@ package configurations
 object DockerParams {
   fun get(): Map<String, String> =
     mapOf(
-      "plugin.docker.imagePlatform" to "linux",
-      "plugin.docker.pull.enabled" to "true",
-      "plugin.docker.imageId" to "registry.jetbrains.team/p/bazel/docker/hirschgarten-e2e:latest",
-      "plugin.docker.run.parameters" to "-v %system.agent.persistent.cache%:/home/hirschuser/.cache",
+//      "plugin.docker.imagePlatform" to "linux",
+//      "plugin.docker.pull.enabled" to "true",
+//      "plugin.docker.imageId" to "registry.jetbrains.team/p/bazel/docker/hirschgarten-e2e:latest",
+//      "plugin.docker.run.parameters" to "-v %system.agent.persistent.cache%:/home/hirschuser/.cache",
     )
 }
 
