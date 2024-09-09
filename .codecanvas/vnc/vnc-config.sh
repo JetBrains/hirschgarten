d7af77bf4c67d06b315f6bd7aeeb31862258f648
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-09-09 19:39:06 +0400

    add dockerfile, remove dependencies in docker image from cc warmup

--- .codecanvas/vnc/vnc-config.sh
+++ .codecanvas/vnc/vnc-config.sh
@@ -1,8 +1,3 @@
-if ! command -v vncserver &>/dev/null; then
-	apt-get update
-	apt-get install -y expect xfce4 tigervnc-standalone-server
-fi
-
 mkdir -p ~/.vnc
 
 cat >~/.vnc/config <<EOF
@@ -11,7 +6,3 @@ geometry=3840x2160
 localhost
 alwaysshared
 EOF
-
-if [ ! -d /mnt/jetbrains/system/noVNC ]; then
-	git clone https://github.com/novnc/noVNC /mnt/jetbrains/system/noVNC
-fi
