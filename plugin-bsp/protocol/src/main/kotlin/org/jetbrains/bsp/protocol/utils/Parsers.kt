6cfce8113cff82e803e05a010edc169e5d13e59b
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2024-09-10 10:07:09 +0000

    [fix] `VirtualFile.parseBspConnectionDetails()` now handles directories without warning | #BAZEL-1176 Done

--- plugin-bsp/protocol/src/main/kotlin/org/jetbrains/bsp/protocol/utils/Parsers.kt
+++ plugin-bsp/protocol/src/main/kotlin/org/jetbrains/bsp/protocol/utils/Parsers.kt
@@ -4,11 +4,12 @@ import ch.epfl.scala.bsp4j.BspConnectionDetails
 import com.google.gson.Gson
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.isFile
 import com.intellij.openapi.vfs.readText
 
-public fun VirtualFile.parseBspConnectionDetails(): BspConnectionDetails? =
+fun VirtualFile.parseBspConnectionDetails(): BspConnectionDetails? =
   try {
-    Gson().fromJson(this.readText(), BspConnectionDetails::class.java)
+    if (isFile) Gson().fromJson(this.readText(), BspConnectionDetails::class.java) else null
   } catch (e: Exception) {
     thisLogger().warn("Parsing file '$this' to BspConnectionDetails failed!", e)
     null
