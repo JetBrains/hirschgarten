b325cde02fd12ddc40bd433c1f6b2ab279acf6c0
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-09-11 17:12:13 +0400

    [fix] add better agents for GitHub CI

--- .teamcity/configurations/projectUnitTests.kts
+++ .teamcity/configurations/projectUnitTests.kts
@@ -1,14 +1,16 @@
 package configurations
 
+import jetbrains.buildServer.configs.kotlin.v2019_2.Requirements
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.BazelStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.bazel
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-open class UnitTests(vcsRoot: GitVcsRoot) :
+open class UnitTests(vcsRoot: GitVcsRoot, requirements: (Requirements.() -> Unit)? = null) :
   BaseConfiguration.BaseBuildType(
     name = "[unit tests] project unit tests",
     artifactRules = Utils.CommonParams.BazelTestlogsArtifactRules,
+    requirements = requirements,
     steps = {
       bazel {
         name = "bazel test //..."
@@ -37,6 +39,10 @@ open class UnitTests(vcsRoot: GitVcsRoot) :
 
 object GitHub : UnitTests(
   vcsRoot = BaseConfiguration.GitHubVcs,
+  requirements = {
+    endsWith("cloud.amazon.agent-name-prefix", "Ubuntu-22.04-Large")
+    equals("container.engine.osType", "linux")
+  },
 )
 
 object Space : UnitTests(
