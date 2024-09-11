1aa1ae86624850f0e051999620a71cdb3349152e
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-09-11 17:12:13 +0400

    [fix] add better agents for GitHub CI
    GitOrigin-RevId: 97a6614c121107ea2d7bd3f9c90976f073e816f8

--- .teamcity/configurations/pluginBsp/intellijBenchmark.kts
+++ .teamcity/configurations/pluginBsp/intellijBenchmark.kts
@@ -2,15 +2,17 @@ package configurations.pluginBsp
 
 import configurations.BaseConfiguration
 import configurations.Utils
+import jetbrains.buildServer.configs.kotlin.v2019_2.Requirements
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.BazelStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.bazel
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
-open class Benchmark(vcsRoot: GitVcsRoot) :
+open class Benchmark(vcsRoot: GitVcsRoot, requirements: (Requirements.() -> Unit)? = null) :
   BaseConfiguration.BaseBuildType(
     name = "[benchmark] Plugin BSP 10 targets",
     vcsRoot = vcsRoot,
+    requirements = requirements,
     artifactRules = Utils.CommonParams.BazelTestlogsArtifactRules,
     steps = {
       val sysArgs = "--jvmopt=\"-Dbsp.benchmark.project.path=/home/hirschuser/project_10\" --jvmopt=\"-Dbsp.benchmark.teamcity.url=https://bazel.teamcity.com\""
@@ -42,6 +44,10 @@ open class Benchmark(vcsRoot: GitVcsRoot) :
 
 object GitHub : Benchmark(
   vcsRoot = BaseConfiguration.GitHubVcs,
+  requirements = {
+    endsWith("cloud.amazon.agent-name-prefix", "Ubuntu-22.04-Large")
+    equals("container.engine.osType", "linux")
+  },
 )
 
 object Space : Benchmark(
