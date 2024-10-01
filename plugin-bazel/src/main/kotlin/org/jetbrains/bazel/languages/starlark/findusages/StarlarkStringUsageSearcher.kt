79f912c410739cafcd3e19d3d9d971153af2c43e
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2024-10-01 09:06:36 +0000

    [fix] Revert global scope hack for rename refactoring | #BAZEL-1289 Done

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/findusages/StarlarkStringUsageSearcher.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/findusages/StarlarkStringUsageSearcher.kt
@@ -6,13 +6,15 @@ import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.UsageSearchContext
 import com.intellij.psi.search.searches.ReferencesSearch.SearchParameters
 import com.intellij.util.Processor
+import org.jetbrains.bazel.languages.starlark.StarlarkFileType
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkNamedElement
 
 class StarlarkStringUsageSearcher : QueryExecutorBase<PsiReference, SearchParameters>() {
   override fun processQuery(params: SearchParameters, processor: Processor<in PsiReference>) {
     val searchee = params.elementToSearch as? StarlarkNamedElement ?: return
     val name = searchee.name ?: return
-    val searchScope = GlobalSearchScope.allScope(params.project)
+    val userScope = params.scopeDeterminedByUser as? GlobalSearchScope ?: return
+    val searchScope = GlobalSearchScope.getScopeRestrictedByFileTypes(userScope, StarlarkFileType)
     params.optimizer.searchWord(name, searchScope, UsageSearchContext.IN_STRINGS, true, searchee)
   }
 }
