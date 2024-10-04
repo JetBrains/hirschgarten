bb8f1302cd9893dae725fc446606be129bb45756
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2024-10-04 16:39:28 +0200

    2 more targets in intellij bazel: assets and config

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/config/BazelPluginConstants.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/config/BazelPluginConstants.kt
@@ -3,10 +3,10 @@ package org.jetbrains.bazel.config
 import com.intellij.openapi.externalSystem.model.ProjectSystemId
 import org.jetbrains.plugins.bsp.config.BuildToolId
 
-internal object BazelPluginConstants {
-  val bazelBspBuildToolId = BuildToolId("bazelbsp")
-
+object BazelPluginConstants {
   const val ID = "bazelbsp"
+
+  val bazelBspBuildToolId = BuildToolId(ID)
   val SYSTEM_ID = ProjectSystemId(ID, "Bazel BSP")
   val PROJECT_VIEW_FILE_EXTENSION = "bazelproject"
   val WORKSPACE_FILE_NAMES = listOf("WORKSPACE", "WORKSPACE.bazel", "MODULE.bazel", "WORKSPACE.bzlmod")
