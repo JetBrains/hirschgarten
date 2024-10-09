4e9827b3a0e16cedb6d9ef744d11ac087b93608e
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-10-09 08:42:52 +0000

    [maintenance] rename display name `Bazel BSP` to `Bazel` | #BAZEL-1309 Done
    GitOrigin-RevId: f4719c42cb28d268b324e7cef627279eb8f52df0

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/config/BazelPluginConstants.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/config/BazelPluginConstants.kt
@@ -7,8 +7,8 @@ internal object BazelPluginConstants {
   val bazelBspBuildToolId = BuildToolId("bazelbsp")
 
   const val ID = "bazelbsp"
-  val SYSTEM_ID = ProjectSystemId(ID, "Bazel BSP")
-  val PROJECT_VIEW_FILE_EXTENSION = "bazelproject"
+  val SYSTEM_ID = ProjectSystemId(ID, "Bazel")
+  const val PROJECT_VIEW_FILE_EXTENSION = "bazelproject"
   val WORKSPACE_FILE_NAMES = listOf("WORKSPACE", "WORKSPACE.bazel", "MODULE.bazel", "WORKSPACE.bzlmod")
   val BUILD_FILE_NAMES = listOf("BUILD")
   val SUPPORTED_EXTENSIONS = listOf("bazel", "bazelproject", "bzlmod", "bzl")
