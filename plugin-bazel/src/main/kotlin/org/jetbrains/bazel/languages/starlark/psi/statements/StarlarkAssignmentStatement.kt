d9dd620a4937324ac0bb4bedf86c5eb201a8d323
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2024-10-10 08:00:59 +0000

    [feature] Add resolving to tuple assignment targets
    GitOrigin-RevId: f2092cc9b3954dbcd99079ba5dd28d6e4b9552a9

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/statements/StarlarkAssignmentStatement.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/statements/StarlarkAssignmentStatement.kt
@@ -6,13 +6,19 @@ import org.jetbrains.bazel.languages.starlark.psi.StarlarkBaseElement
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkElement
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkElementVisitor
 import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkTargetExpression
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkTupleExpression
 
 class StarlarkAssignmentStatement(node: ASTNode) : StarlarkBaseElement(node) {
   override fun acceptVisitor(visitor: StarlarkElementVisitor) = visitor.visitAssignmentStatement(this)
 
   override fun getName(): String? = getTargetExpression()?.name ?: super.getName()
 
-  fun check(processor: Processor<StarlarkElement>): Boolean = getTargetExpression()?.let { processor.process(it) } ?: true
+  fun check(processor: Processor<StarlarkElement>): Boolean =
+    getTargetExpression()?.let { processor.process(it) }
+      ?: getTupleExpression()?.getTargetExpressions()?.all { processor.process(it) }
+      ?: true
 
   private fun getTargetExpression(): StarlarkTargetExpression? = findChildByClass(StarlarkTargetExpression::class.java)
+
+  private fun getTupleExpression(): StarlarkTupleExpression? = findChildByClass(StarlarkTupleExpression::class.java)
 }
---
+++ plugin-bazel/src/test/kotlin/org/jetbrains/bazel/languages/starlark/references/StarlarkAssignmentReferenceTest.kt
@@ -0,0 +1,49 @@
+package org.jetbrains.bazel.languages.starlark.references
+
+import io.kotest.matchers.shouldBe
+import org.jetbrains.bazel.languages.starlark.fixtures.StarlarkReferencesTestCase
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkReferenceExpression
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkTargetExpression
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkTupleExpression
+import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkAssignmentStatement
+import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkExpressionStatement
+import org.jetbrains.kotlin.psi.psiUtil.getChildOfType
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class StarlarkAssignmentReferenceTest : StarlarkReferencesTestCase() {
+  @Test
+  fun `should resolve basic assignment`() {
+    // given
+    myFixture.configureByFile("AssignmentReference.bzl")
+    val expectedResolved =
+      myFixture.file
+        .getChildOfType<StarlarkAssignmentStatement>()
+        ?.getChildOfType<StarlarkTargetExpression>()
+    // when
+    val resolved = getUsage()?.reference?.resolve()
+    // then
+    resolved shouldBe expectedResolved
+  }
+
+  fun `should resolve tuple assignment`() {
+    // given
+    myFixture.configureByFile("TupleAssignmentReference.bzl")
+    val expectedResolved =
+      myFixture.file
+        .getChildOfType<StarlarkAssignmentStatement>()
+        ?.getChildOfType<StarlarkTupleExpression>()
+        ?.getChildOfType<StarlarkTargetExpression>()
+    // when
+    val resolved = getUsage()?.reference?.resolve()
+    // then
+    resolved shouldBe expectedResolved
+  }
+
+  private fun getUsage(): StarlarkReferenceExpression? =
+    myFixture.file
+      .getChildOfType<StarlarkExpressionStatement>()
+      ?.getChildOfType<StarlarkReferenceExpression>()
+}
