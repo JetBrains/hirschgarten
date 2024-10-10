9d1b1604a05113218ec45d0e062e89d7bc11ac18
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2024-10-10 07:46:31 +0000

    [feature] Show only runnable non-module targets in the target list

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/projectDetails/CollectProjectDetailsTask.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/projectDetails/CollectProjectDetailsTask.kt
@@ -329,8 +329,11 @@ class CollectProjectDetailsTask(
 
         val targetIdToModuleEntitiesMap =
           bspTracer.spanBuilder("create.target.id.to.module.entities.map.ms").use {
+            // Filter out non-module targets which cannot be run, as they are just cluttering the ui
+            val usefulNonModuleTargets = projectDetails.targets.filter { it.capabilities.canRun }
+
             val syncedTargetIdToTargetInfo =
-              (projectDetails.targets + projectDetails.nonModuleTargets).associate {
+              (projectDetails.targets + usefulNonModuleTargets).associate {
                 it.id to
                   it.toBuildTargetInfo()
               }
