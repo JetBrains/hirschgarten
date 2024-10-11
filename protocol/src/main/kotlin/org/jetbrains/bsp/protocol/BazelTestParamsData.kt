a07d6c58938746a1b73dcf8e2cae6f50ef08a442
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2024-10-11 17:52:50 +0200

    WIP

--- protocol/src/main/kotlin/org/jetbrains/bsp/protocol/BazelTestParamsData.kt
+++ protocol/src/main/kotlin/org/jetbrains/bsp/protocol/BazelTestParamsData.kt
@@ -1,6 +1,6 @@
 package org.jetbrains.bsp.protocol
 
-public data class BazelTestParamsData(val coverage: Boolean?, val testFilter: String?) {
+public data class BazelTestParamsData(val coverage: Boolean?, val testFilter: String?, val additionalBazelParams: List<String>?) {
   companion object {
     const val DATA_KIND = "bazel-test"
   }
