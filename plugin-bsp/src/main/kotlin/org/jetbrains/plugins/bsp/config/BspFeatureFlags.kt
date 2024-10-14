7bbac50ddbe2dc68557c1a62d910fc799d24a1cd
Lev Leontev <lev.leontev@jetbrains.com>
2024-10-14 19:02:46 +0000

    [fix] Add a registry flag to scan for Git roots inside the project directory despite that it's buggy | #BAZEL-948

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/config/BspFeatureFlags.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/config/BspFeatureFlags.kt
@@ -9,6 +9,7 @@ private const val BUILD_PROJECT_ON_SYNC = "bsp.build.project.on.sync"
 private const val SHORTEN_MODULE_LIBRARY_NAMES = "bsp.shorten.module.library.names"
 private const val RETRIEVE_TARGETS_FOR_FILE_FROM_ANCESTORS = "bsp.retrieve.targets.for.file.from.ancestors"
 private const val WRAP_LIBRARIES_INSIDE_MODULES = "bsp.wrap.libraries.inside.modules"
+private const val SCAN_GIT_ROOTS_INSIDE_PROJECT_DIR = "bsp.scan.git.roots.inside.project.dir"
 
 public object BspFeatureFlags {
   public val isPythonSupportEnabled: Boolean
@@ -34,4 +35,7 @@ public object BspFeatureFlags {
 
   internal val isKotlinPluginK2Mode: Boolean
     get() = System.getProperty("idea.kotlin.plugin.use.k2", "false").toBoolean()
+
+  val isScanGitRootsInsideProjectDir: Boolean
+    get() = Registry.`is`(SCAN_GIT_ROOTS_INSIDE_PROJECT_DIR)
 }
