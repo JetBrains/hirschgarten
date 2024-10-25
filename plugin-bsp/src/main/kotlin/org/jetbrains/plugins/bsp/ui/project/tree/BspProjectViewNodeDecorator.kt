e5768530ed21725d52b3897982a7306915e442e6
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-10-25 15:08:23 +0000

    [maintenance] update condition to check if a `PsiDirectoryNode` is a node from ij-generated modules
    - the location string can have the format `package name, sources root` instead of just `sources root`.
    
    GitOrigin-RevId: 958942ff591b3f83612af7fc531c85e5d935ce71

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/project/tree/BspProjectViewNodeDecorator.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/ui/project/tree/BspProjectViewNodeDecorator.kt
@@ -27,7 +27,7 @@ class BspProjectViewNodeDecorator(private val project: Project) : ProjectViewNod
     }
   }
 
-  private fun PresentationData.isJavaHackNode() = locationString == "sources root" || locationString == "resources root"
+  private fun PresentationData.isJavaHackNode() = locationString?.contains("sources root") == true
 
   private fun PresentationData.clearPresentation() {
     clearText()
