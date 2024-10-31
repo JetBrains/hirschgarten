a0d3babb71756d71813707a46da44ebda6e0a140
Lev Leontev <lev.leontev@jetbrains.com>
2024-10-31 12:53:55 +0000

    [maintenance] Add Spring to slow plugins warning | #BAZEL-1236 #IDEA-360171
    GitOrigin-RevId: 8304adc328c218d37ba6ddbb95caf92237ea85e6

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/CheckSlowPluginsStartupActivity.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/CheckSlowPluginsStartupActivity.kt
@@ -1,18 +1,23 @@
 package org.jetbrains.bazel.flow.open
 
+import com.intellij.ide.plugins.IdeaPluginDescriptor
 import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.ide.plugins.newui.MyPluginModel
+import com.intellij.notification.NotificationAction
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 import org.jetbrains.bazel.config.BazelPluginBundle
 import org.jetbrains.plugins.bsp.startup.BspProjectActivity
-import org.jetbrains.plugins.bsp.ui.notifications.BspBalloonNotifier
 
 // See https://youtrack.jetbrains.com/issue/BAZEL-1236
 private val SLOW_PLUGINS =
   listOf(
-    // Spring is slow, but oftentimes necessary
-    // "com.intellij.spring",
+    "com.intellij.spring",
     "com.intellij.python.django",
   )
+
+private const val NOTIFICATION_GROUP = "Bazel slow plugins warning"
 
 internal class CheckSlowPluginsStartupActivity : BspProjectActivity() {
   override suspend fun Project.executeForBspProject() {
@@ -22,13 +27,27 @@ internal class CheckSlowPluginsStartupActivity : BspProjectActivity() {
         it.pluginId.idString in SLOW_PLUGINS
       }
     if (slowPlugins.isEmpty()) return
-    BspBalloonNotifier.warn(
-      BazelPluginBundle.message("widget.check.slow.pluging.title", slowPlugins.size),
-      BazelPluginBundle.message(
-        "widget.check.slow.plugins.message",
-        slowPlugins.joinToString(", ") { it.name },
-        slowPlugins.size,
-      ),
-    )
+    NotificationGroupManager
+      .getInstance()
+      .getNotificationGroup(NOTIFICATION_GROUP)
+      .createNotification(
+        BazelPluginBundle.message("widget.check.slow.plugins.title", slowPlugins.size),
+        BazelPluginBundle.message(
+          "widget.check.slow.plugins.message",
+          slowPlugins.joinToString(", ") { it.name },
+          slowPlugins.size,
+        ),
+        NotificationType.WARNING,
+      ).addAction(
+        NotificationAction.createExpiring(BazelPluginBundle.message("widget.check.slow.plugins.action", slowPlugins.size)) { _, _ ->
+          disablePlugins(this, slowPlugins)
+        },
+      ).notify(this)
+  }
+
+  private fun disablePlugins(project: Project, plugins: List<IdeaPluginDescriptor>) {
+    val model = MyPluginModel(project)
+    model.disable(plugins)
+    model.apply(null)
   }
 }
