07dc5caf1b0acbabb841edfce47767d73091a692
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-11-04 10:28:29 +0000

    [fix/feature] provide new action to load a Maven/Gradle project as a Bazel project | #BAZEL-1387 Done

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/actions/LinkBazelProjectFromScriptAction.kt
@@ -0,0 +1,42 @@
+package org.jetbrains.bazel.flow.open.actions
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.vfs.VirtualFile
+import org.jetbrains.bazel.assets.BazelPluginIcons
+import org.jetbrains.bazel.config.BazelPluginBundle
+import org.jetbrains.bazel.config.BazelPluginConstants
+import org.jetbrains.bazel.config.isBazelProject
+import org.jetbrains.bazel.flow.open.BazelBspOpenProjectProvider
+import org.jetbrains.bazel.flow.open.BazelBspProjectOpenProcessor
+import org.jetbrains.plugins.bsp.coroutines.BspCoroutineService
+
+internal class LinkBazelProjectFromScriptAction :
+  DumbAwareAction(
+    { BazelPluginBundle.message("action.link.bazel.project.from.linked.project") },
+    BazelPluginIcons.bazel,
+  ) {
+  override fun actionPerformed(e: AnActionEvent) {
+    val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+    val projectFile = BazelBspProjectOpenProcessor().calculateProjectFolderToOpen(virtualFile)
+    val project = e.project ?: return
+
+    BspCoroutineService.getInstance(project).start {
+      BazelBspOpenProjectProvider().linkToExistingProjectAsync(projectFile, project)
+    }
+  }
+
+  override fun update(e: AnActionEvent) {
+    val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+    val project = e.project ?: return
+    e.presentation.isEnabledAndVisible = virtualFile.isFileSupported() && !project.isBazelProject
+  }
+
+  private fun VirtualFile.isFileSupported() =
+    BazelBspOpenProjectProvider().isProjectFileCompat(this) ||
+      extension == BazelPluginConstants.PROJECT_VIEW_FILE_EXTENSION
+
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+}
