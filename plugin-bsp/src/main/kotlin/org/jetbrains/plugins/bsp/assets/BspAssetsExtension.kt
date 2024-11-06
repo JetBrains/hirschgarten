2de89c7de51f15fa1e71df1a7002956c843253e8
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-11-06 17:07:13 +0000

    [maintenance] minor house-keeping.

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/assets/BspAssetsExtension.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/assets/BspAssetsExtension.kt
@@ -1,15 +1,16 @@
 package org.jetbrains.plugins.bsp.assets
 
 import com.intellij.icons.AllIcons
+import org.jetbrains.bsp.protocol.BSP_DISPLAY_NAME
 import org.jetbrains.plugins.bsp.config.BspPluginIcons
 import org.jetbrains.plugins.bsp.config.BuildToolId
 import org.jetbrains.plugins.bsp.config.bspBuildToolId
 import javax.swing.Icon
 
-public class BspAssetsExtension : BuildToolAssetsExtension {
+class BspAssetsExtension : BuildToolAssetsExtension {
   override val buildToolId: BuildToolId = bspBuildToolId
 
-  override val presentableName: String = "BSP"
+  override val presentableName: String = BSP_DISPLAY_NAME
 
   override val targetIcon: Icon = BspPluginIcons.bsp
   override val errorTargetIcon: Icon = AllIcons.General.Error
--- protocol/src/main/kotlin/org/jetbrains/bsp/protocol/BspConstants.kt
+++ protocol/src/main/kotlin/org/jetbrains/bsp/protocol/BspConstants.kt
@@ -2,15 +2,17 @@ package org.jetbrains.bsp.protocol
 
 import ch.epfl.scala.bsp4j.BuildClientCapabilities
 
-public const val BSP_CONNECTION_DIR: String = ".bsp"
+const val BSP_CONNECTION_DIR: String = ".bsp"
 
-public const val BSP_CLIENT_NAME: String = "IntelliJ-BSP"
+const val BSP_DISPLAY_NAME: String = "BSP"
 
-public const val BSP_CLIENT_VERSION: String = "2024.1-EAP"
+const val BSP_CLIENT_NAME: String = "IntelliJ-BSP"
 
-public const val BSP_VERSION: String = "2.1.0"
+const val BSP_CLIENT_VERSION: String = "2024.1-EAP"
 
-public val CLIENT_CAPABILITIES: BuildClientCapabilities =
+const val BSP_VERSION: String = "2.1.0"
+
+val CLIENT_CAPABILITIES: BuildClientCapabilities =
   BuildClientCapabilities(listOf("java", "kotlin", "scala", "python")).apply {
     jvmCompileClasspathReceiver = true
   }
