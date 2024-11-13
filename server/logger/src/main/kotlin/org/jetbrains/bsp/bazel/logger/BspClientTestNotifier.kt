JetBrains/hirschgarten

commit f9890dad592d1d0f91eb5e5dbde3c34ecafbdb6d

[feature] add JUnit5 test output parsing | #BAZEL-1281 Done

server/server/src/main/kotlin/org/jetbrains/bsp/bazel/server/bep/Junit5TestVisualOutputParser.kt
@@ -1,0 +1,179 @@
+package org.jetbrains.bsp.bazel.server.bep
+
+import ch.epfl.scala.bsp4j.BuildTargetIdentifier
+import ch.epfl.scala.bsp4j.TaskId
+import ch.epfl.scala.bsp4j.TestReport
+import ch.epfl.scala.bsp4j.TestStatus
+import org.jetbrains.bsp.bazel.logger.BspClientTestNotifier
+import org.jetbrains.bsp.protocol.JUnitStyleTestCaseData
+import java.util.UUID
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+/**
+ * Parses the nice-looking test execution tree Junit5 produces
+ */
+class Junit5TestVisualOutputParser(private val bspClientTestNotifier: BspClientTestNotifier) {
+  private val startedSuites: ArrayDeque<TestOutputLine> = ArrayDeque()
+
+  fun processTestOutput(output: String) {
+    var startedBuildTarget: StartedBuildTarget? = null
+    var previousOutputLine: TestOutputLine? = null
+
+    output.lines().forEach {
+      if (startedBuildTarget == null) {
+        startedBuildTarget = parseStartedBuildTarget(it)
+      } else {
+        val testingEndedMatcher = testingEndedPattern.matcher(it)
+        val currentOutputLine = getCurrentOutputLine(it)
+        if (currentOutputLine != null) {
+          processPreviousOutputLine(previousOutputLine, currentOutputLine)
+          previousOutputLine = currentOutputLine
+        } else if (testingEndedMatcher.find()) {
+          processTestingEndingLine(previousOutputLine, testingEndedMatcher, startedBuildTarget)
+          startedBuildTarget = null
+        }
+      }
+    }
+  }
+
+  private fun processTestingEndingLine(
+    previousOutputLine: TestOutputLine?,
+    testingEndedMatcher: Matcher,
+    startedBuildTarget: StartedBuildTarget?,
+  ) {
+    previousOutputLine?.let { startAndFinishTest(it) }
+    while (startedSuites.isNotEmpty()) {
+      finishTopmostSuite()
+    }
+    val time = testingEndedMatcher.group("time").toLongOrNull() ?: 0
+    endTesting(startedBuildTarget!!, time)
+  }
+
+  private fun processPreviousOutputLine(previousOutputLine: TestOutputLine?, currentOutputLine: TestOutputLine) {
+    if (previousOutputLine != null) {
+      if (currentOutputLine.indent > previousOutputLine.indent) {
+        startSuite(previousOutputLine)
+      } else {
+        startAndFinishTest(previousOutputLine)
+        removeAllFinishedSuites(currentOutputLine)
+      }
+    }
+  }
+
+  private fun removeAllFinishedSuites(currentOutputLine: TestOutputLine) {
+    while (startedSuites.isNotEmpty() && startedSuites.last().indent >= currentOutputLine.indent) {
+      finishTopmostSuite()
+    }
+  }
+
+  private fun getCurrentOutputLine(line: String): TestOutputLine? {
+    val cleanLine = line.removeFormat()
+
+    val currentLineMatcher = testLinePattern.matcher(cleanLine)
+    return if (currentLineMatcher.find()) {
+      TestOutputLine(
+        name = currentLineMatcher.group("name"),
+        status = currentLineMatcher.group("result").toTestStatus(),
+        message = currentLineMatcher.group("message"),
+        indent = currentLineMatcher.start("name"),
+        taskId = TaskId(testUUID()),
+      )
+    } else {
+      null
+    }
+  }
+
+  private fun parseStartedBuildTarget(line: String): StartedBuildTarget? {
+    val testingStartMatcher = testingStartPattern.matcher(line)
+    return if (testingStartMatcher.find()) {
+      StartedBuildTarget(testingStartMatcher.group("target"), TaskId(testUUID())).also {
+        beginTesting(it)
+      }
+    } else {
+      null
+    }
+  }
+
+  private fun beginTesting(startedBuildTarget: StartedBuildTarget) {
+    bspClientTestNotifier.beginTestTarget(BuildTargetIdentifier(startedBuildTarget.uri), startedBuildTarget.taskId)
+  }
+
+  private fun endTesting(testTarget: StartedBuildTarget, millis: Long) {
+    val report = TestReport(BuildTargetIdentifier(testTarget.uri), 0, 0, 0, 0, 0)
+    report.time = millis
+    bspClientTestNotifier.endTestTarget(report, testTarget.taskId)
+  }
+
+  private fun startSuite(suite: TestOutputLine) {
+    bspClientTestNotifier.startTest(suite.name, suite.taskId)
+    startedSuites.addLast(suite)
+  }
+
+  private fun finishTopmostSuite() {
+    with(startedSuites.removeLastOrNull() ?: return) {
+      bspClientTestNotifier.finishTest(
+        displayName = name,
+        taskId = taskId,
+        status = status,
+        message = message,
+      )
+    }
+  }
+
+  private fun startAndFinishTest(test: TestOutputLine) {
+    test.taskId.parents = generateParentList()
+    bspClientTestNotifier.startTest(test.name, test.taskId)
+    bspClientTestNotifier.finishTest(
+      displayName = test.name,
+      taskId = test.taskId,
+      status = test.status,
+      message = test.message,
+      dataKind = JUnitStyleTestCaseData.DATA_KIND,
+      data = createTestCaseData(test.message),
+    )
+  }
+
+  private fun generateParentList(): List<String> = startedSuites.toList().reversed().mapNotNull { it.taskId.id }
+
+  private fun testUUID(): String = "test-" + UUID.randomUUID().toString()
+
+  private fun String.toTestStatus(): TestStatus =
+    when (this) {
+      "✔" -> TestStatus.PASSED
+      "✘" -> TestStatus.FAILED
+      else -> TestStatus.SKIPPED
+    }
+
+  companion object {
+    fun textContainsJunit5VisualOutput(text: String): Boolean =
+      text.contains(Char(0x2577)) // every junit5 visual output starts with that character ("╷")
+  }
+}
+
+private val testingStartPattern = Pattern.compile("^Executing\\htests\\hfrom\\h(?<target>[^:]*:[^:]+)")
+private val testLinePattern =
+  Pattern.compile("^(?:[\\h└├│]{3})+[└├│]─\\h(?<name>.+)\\h(?<result>[✔✘↷])\\h?(?<message>.*)\$")
+private val testingEndedPattern = Pattern.compile("^Test\\hrun\\hfinished\\hafter\\h(?<time>\\d+)\\hms")
+
+private fun String.removeFormat(): String =
+  this.replace(Regex("[?\\u001b]\\[[;\\d]*m"), "") // '1B' symbol appears in console output and test logs, while '?' appears in test XMLs
+
+private fun createTestCaseData(message: String): JUnitStyleTestCaseData =
+  JUnitStyleTestCaseData(
+    time = null,
+    className = null,
+    errorMessage = message,
+    errorContent = null,
+    errorType = null,
+  )
+
+private data class TestOutputLine(
+  val name: String,
+  val status: TestStatus,
+  val message: String,
+  val indent: Int,
+  val taskId: TaskId,
+)
+
+private data class StartedBuildTarget(val uri: String, val taskId: TaskId)

server/server/src/test/kotlin/org/jetbrains/bsp/bazel/server/bep/TestXmlParserTest.kt
@@ -11,16 +11,20 @@ import ch.epfl.scala.bsp4j.TaskId
 import ch.epfl.scala.bsp4j.TaskProgressParams
 import ch.epfl.scala.bsp4j.TaskStartParams
 import ch.epfl.scala.bsp4j.TestFinish
+import ch.epfl.scala.bsp4j.TestReport
 import ch.epfl.scala.bsp4j.TestStatus
+import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.collections.shouldNotBeEmpty
 import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.equals.shouldNotBeEqual
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import io.kotest.matchers.string.shouldContain
 import org.jetbrains.bsp.bazel.logger.BspClientTestNotifier
 import org.jetbrains.bsp.protocol.JUnitStyleTestCaseData
-import org.jetbrains.bsp.protocol.JUnitStyleTestSuiteData
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.nio.file.Path
@@ -77,7 +81,7 @@ class TestXmlParserTest {
     val parentId = TaskId("sample-task")
 
     // when
-    TestXmlParser(parentId, notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
+    TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
 
     // then
     client.taskStartCalls.size shouldBe 5
@@ -176,7 +180,7 @@ class TestXmlParserTest {
     val parentId = TaskId("sample-task")
 
     // when
-    TestXmlParser(parentId, notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
+    TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
 
     // then
     client.taskStartCalls.size shouldBe 14
@@ -281,7 +285,7 @@ class TestXmlParserTest {
     val parentId = TaskId("sample-task")
 
     // when
-    TestXmlParser(parentId, notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
+    TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
 
     // then
     client.taskStartCalls.size shouldBe 18
@@ -391,7 +395,7 @@ class TestXmlParserTest {
     val parentId = TaskId("sample-task")
 
     // when
-    TestXmlParser(parentId, notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
+    TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
 
     // then
     client.taskStartCalls.size shouldBe 17
@@ -449,7 +453,7 @@ class TestXmlParserTest {
   }
 
   @Test
-  fun `junit5 - success, failure and ignored`(
+  fun `junit5 - success, failure and ignored (ANSI colors)`(
     @TempDir tempDir: Path,
   ) {
     val dollar = "${'$'}"
@@ -508,39 +512,261 @@ class TestXmlParserTest {
 
     val client = MockBuildClient()
     val notifier = BspClientTestNotifier(client, "sample-origin")
-    val parentId = TaskId("sample-task")
 
     // when
-    TestXmlParser(parentId, notifier).parseAndReport(writeTempFile(tempDir, sampleContents))
+    TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, sampleContents))
 
     // then
-    client.taskStartCalls.size shouldBe 2
-    client.taskFinishCalls.size shouldBe 2
+    client.taskStartCalls.size shouldBe 5
+    client.taskFinishCalls.size shouldBe 5
 
-    val expectedNames = listOf("src/test/kotlin/org/jetbrains/simple/TripleTest", "TripleTest")
+    val expectedNames =
+      listOf(
+        "TripleTest",
+        "testFailure()",
+        "testIgnored()",
+        "testSuccess()",
+      )
 
-    client.taskFinishCalls.map { (it.data as TestFinish).displayName } shouldContainExactlyInAnyOrder expectedNames
+    client.taskFinishCalls.count { it.data is TestReport } shouldBe 1
+
+    val testFinishes = client.taskFinishCalls.filter { it.data is TestFinish }
+    testFinishes.size shouldBe 4
+
+    testFinishes.map { (it.data as TestFinish).displayName } shouldContainExactlyInAnyOrder expectedNames
     client.taskStartCalls.map { it.taskId } shouldContainExactlyInAnyOrder client.taskFinishCalls.map { it.taskId }
 
-    client.taskFinishCalls.map {
+    testFinishes.forEach {
       val data = (it.data as TestFinish)
       when (data.displayName) {
-        "src/test/kotlin/org/jetbrains/simple/TripleTest" -> {
-          it.taskId.parents shouldBeEqual emptyList()
-          data.status shouldBe TestStatus.FAILED
-          (data.data as JUnitStyleTestSuiteData).systemOut shouldNotBe null
-        }
-
         "TripleTest" -> {
-          it.taskId.parents shouldNotBeEqual emptyList()
+          it.taskId.parents.shouldBeNull()
+        }
+        "testFailure()" -> {
+          it.taskId.parents.shouldNotBeNull()
+          it.taskId.parents.shouldNotBeEmpty()
           data.status shouldBe TestStatus.FAILED
           val details = (data.data as JUnitStyleTestCaseData)
-          details.errorContent shouldBe null
           details.errorMessage shouldNotBe null
-          details.errorType shouldBe null
         }
+        "testIgnored()" -> {
+          it.taskId.parents.shouldNotBeNull()
+          it.taskId.parents.shouldNotBeEmpty()
+          data.status shouldBe TestStatus.SKIPPED
+        }
+        "testSuccess()" -> {
+          it.taskId.parents.shouldNotBeNull()
+          it.taskId.parents.shouldNotBeEmpty()
+          data.status shouldBe TestStatus.PASSED
+        }
+      }
+    }
+  }
 
-        else -> {
+  @Test
+  fun `junit5 - success, failure and ignored (no ANSI colors)`(
+    @TempDir tempDir: Path,
+  ) {
+    val dollar = "${'$'}"
+    val sampleContents =
+      """
+      <?xml version="1.0" encoding="UTF-8"?>
+      <testsuites>
+        <testsuite name="src/test/kotlin/org/jetbrains/simple/TripleTest" tests="1" failures="0" errors="1">
+          <testcase name="src/test/kotlin/org/jetbrains/simple/TripleTest" status="run" duration="0" time="0"><error message="exited with error code 1"></error></testcase>
+            <system-out>
+      Generated test.log (if the file is not UTF-8, then this may be unreadable):
+      <![CDATA[exec $dollar{PAGER:-/ usr / bin / less} "$dollar{'$dollar'}0" || exit 1
+      Executing tests from //src/test/kotlin/org/jetbrains/simple:TripleTest
+      -----------------------------------------------------------------------------
+
+      Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+      ╷
+      └─ JUnit Jupiter ✔
+         └─ TripleTest ✔
+            ├─ testFailure() ✘ This test always fails
+            ├─ testIgnored() ↷ public final void org.jetbrains.simple.TripleTest.testIgnored() is @Disabled
+            └─ testSuccess() ✔
+
+      Failures (1):
+        JUnit Jupiter:TripleTest:testFailure()
+          MethodSource [className = 'org.jetbrains.simple.TripleTest', methodName = 'testFailure', methodParameterTypes = '']
+          => java.lang.AssertionError: This test always fails
+             org.jetbrains.simple.TripleTest.testFailure(TripleTest.kt:11)
+             java.base/java.lang.reflect.Method.invoke(Method.java:580)
+             java.base/java.util.ArrayList.forEach(ArrayList.java:1596)
+             java.base/java.util.ArrayList.forEach(ArrayList.java:1596)
+
+      Test run finished after 53 ms
+      [         2 containers found      ]
+      [         0 containers skipped    ]
+      [         2 containers started    ]
+      [         0 containers aborted    ]
+      [         2 containers successful ]
+      [         0 containers failed     ]
+      [         3 tests found           ]
+      [         1 tests skipped         ]
+      [         2 tests started         ]
+      [         0 tests aborted         ]
+      [         1 tests successful      ]
+      [         1 tests failed          ]
+
+
+      WARNING: Delegated to the 'execute' command.
+               This behaviour has been deprecated and will be removed in a future release.
+               Please use the 'execute' command directly.]]>
+            </system-out>
+          </testsuite>
+      </testsuites>
+      """.trimIndent()
+
+    val client = MockBuildClient()
+    val notifier = BspClientTestNotifier(client, "sample-origin")
+
+    // when
+    TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, sampleContents))
+
+    // then
+    client.taskStartCalls.size shouldBe 5
+    client.taskFinishCalls.size shouldBe 5
+
+    val expectedNames =
+      listOf(
+        "TripleTest",
+        "testFailure()",
+        "testIgnored()",
+        "testSuccess()",
+      )
+
+    client.taskFinishCalls.count { it.data is TestReport } shouldBe 1
+
+    val testFinishes = client.taskFinishCalls.filter { it.data is TestFinish }
+    testFinishes.size shouldBe 4
+
+    testFinishes.map { (it.data as TestFinish).displayName } shouldContainExactlyInAnyOrder expectedNames
+    client.taskStartCalls.map { it.taskId } shouldContainExactlyInAnyOrder client.taskFinishCalls.map { it.taskId }
+
+    testFinishes.forEach {
+      val data = (it.data as TestFinish)
+      when (data.displayName) {
+        "TripleTest" -> {
+          it.taskId.parents.shouldBeNull()
+        }
+        "testFailure()" -> {
+          it.taskId.parents.shouldNotBeNull()
+          it.taskId.parents.shouldNotBeEmpty()
+          data.status shouldBe TestStatus.FAILED
+          val details = (data.data as JUnitStyleTestCaseData)
+          details.errorMessage shouldNotBe null
+        }
+        "testIgnored()" -> {
+          it.taskId.parents.shouldNotBeNull()
+          it.taskId.parents.shouldNotBeEmpty()
+          data.status shouldBe TestStatus.SKIPPED
+        }
+        "testSuccess()" -> {
+          it.taskId.parents.shouldNotBeNull()
+          it.taskId.parents.shouldNotBeEmpty()
+          data.status shouldBe TestStatus.PASSED
+        }
+      }
+    }
+  }
+
+  @Test
+  fun `junit5 - malformed`(
+    @TempDir tempDir: Path,
+  ) {
+    val dollar = "${'$'}"
+    val sampleContents =
+      """
+      <?xml version="1.0" encoding="UTF-8"?>
+      <testsuites>
+        <testsuite name="src/test/kotlin/org/jetbrains/simple/TripleTest" tests="1" failures="0" errors="1">
+          <testcase name="src/test/kotlin/org/jetbrains/simple/TripleTest" status="run" duration="0" time="0"><error message="exited with error code 1"></error></testcase>
+            <system-out>
+      Generated test.log (if the file is not UTF-8, then this may be unreadable):
+      <![CDATA[exec $dollar{PAGER:-/ usr / bin / less} "$dollar{'$dollar'}0" || exit 1
+      Executing tests from //src/test/kotlin/org/jetbrains/simple:TripleTest
+      -----------------------------------------------------------------------------
+
+      Thanks for using JUnit! Support its development at https://junit.org/sponsoring
+
+      ╷
+      └─ JUnit Jupiter ✔
+         └!─ TripleTest ✔
+            ─ testFailure() ✘ This test always fails
+            ├─ testIgnored() ↷ public final void org.jetbrains.simple.TripleTest.testIgnored() is @Disabled
+            └─ testSuccess() ✔
+
+      Failures (1):
+        JUnit Jupiter:TripleTest:testFailure()
+          MethodSource [className = 'org.jetbrains.simple.TripleTest', methodName = 'testFailure', methodParameterTypes = '']
+          => java.lang.AssertionError: This test always fails
+             org.jetbrains.simple.TripleTest.testFailure(TripleTest.kt:11)
+             java.base/java.lang.reflect.Method.invoke(Method.java:580)
+             java.base/java.util.ArrayList.forEach(ArrayList.java:1596)
+             java.base/java.util.ArrayList.forEach(ArrayList.java:1596)
+
+      Test run finished after 53 ms
+      [         2 containers found      ]
+      [         0 containers skipped    ]
+      [         2 containers started    ]
+      [         0 containers aborted    ]
+      [         2 containers successful ]
+      [         0 containers failed     ]
+      [         3 tests found           ]
+      [         1 tests skipped         ]
+      [         2 tests started         ]
+      [         0 tests aborted         ]
+      [         1 tests successful      ]
+      [         1 tests failed          ]
+
+
+      WARNING: Delegated to the 'execute' command.
+               This behaviour has been deprecated and will be removed in a future release.
+               Please use the 'execute' command directly.]]>
+            </system-out>
+          </testsuite>
+      </testsuites>
+      """.trimIndent()
+
+    val client = MockBuildClient()
+    val notifier = BspClientTestNotifier(client, "sample-origin")
+
+    // when
+    TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, sampleContents))
+
+    // then
+    client.taskStartCalls.size shouldBe 3 // two malformed rows should simply be ignored
+    client.taskFinishCalls.size shouldBe 3 // two malformed rows should simply be ignored
+
+    val expectedNames = // two malformed rows should simply be ignored
+      listOf(
+        "testIgnored()",
+        "testSuccess()",
+      )
+
+    client.taskFinishCalls.count { it.data is TestReport } shouldBe 1
+
+    val testFinishes = client.taskFinishCalls.filter { it.data is TestFinish }
+    testFinishes.size shouldBe 2 // two malformed rows should simply be ignored
+
+    testFinishes.map { (it.data as TestFinish).displayName } shouldContainExactlyInAnyOrder expectedNames
+    client.taskStartCalls.map { it.taskId } shouldContainExactlyInAnyOrder client.taskFinishCalls.map { it.taskId }
+
+    testFinishes.forEach {
+      val data = (it.data as TestFinish)
+      when (data.displayName) {
+        "testIgnored()" -> {
+          it.taskId.parents.shouldNotBeNull()
+          it.taskId.parents.shouldBeEmpty() // parent suite should have been ignored, as it was defined in a malformed row
+          data.status shouldBe TestStatus.SKIPPED
+        }
+        "testSuccess()" -> {
+          it.taskId.parents.shouldNotBeNull()
+          it.taskId.parents.shouldBeEmpty() // parent suite should have been ignored, as it was defined in a malformed row
           data.status shouldBe TestStatus.PASSED
         }
       }

server/server/src/main/kotlin/org/jetbrains/bsp/bazel/server/bep/BepServer.kt
@@ -125,7 +125,7 @@ class BepServer(
       val testXmlUri = testResult.testActionOutputList.find { it.name == "test.xml" }?.uri
       if (testXmlUri != null) {
         // Test cases identified and sent to the client by TestXmlParser.
-        TestXmlParser(taskId, bspClientTestNotifier).parseAndReport(testXmlUri)
+        TestXmlParser(bspClientTestNotifier).parseAndReport(testXmlUri)
       } else {
         // Send a generic notification if individual tests cannot be processed.
         val childId = TaskId(UUID.randomUUID().toString())

server/server/src/main/kotlin/org/jetbrains/bsp/bazel/server/bep/TestXmlParser.kt
@@ -90,8 +90,8 @@ class TestResultDetail {
   var content: String? = null
 }
 
-class TestXmlParser(private var parentId: TaskId, private var bspClientTestNotifier: BspClientTestNotifier) {
-  private val fallbackTestXmlParser = FallbackTestXmlParser(parentId, bspClientTestNotifier)
+class TestXmlParser(private var bspClientTestNotifier: BspClientTestNotifier) {
+  private val fallbackTestXmlParser = FallbackTestXmlParser(bspClientTestNotifier)
 
   /**
    * Processes a test result xml file, reporting suite and test case results as task start and finish notifications.
@@ -100,12 +100,14 @@ class TestXmlParser(private var parentId: TaskId, private var bspClientTestNotif
    */
   fun parseAndReport(testXmlUri: String) {
     val testSuites = parseTestXml(testXmlUri, TestSuites::class.java)
-    testSuites?.testsuite?.forEach { suite ->
-      processSuite(suite)
-    } ?: let {
-      parseTestXml(testXmlUri, FallbackTestXmlParser.IncompleteTestSuites::class.java)
-    }?.testsuite?.forEach { suite ->
-      fallbackTestXmlParser.processIncompleteInfoSuite(suite)
+    if (testSuites != null) {
+      testSuites.testsuite.forEach { processSuite(it) }
+    } else {
+      val fallbackTestSuites =
+        parseTestXml(testXmlUri, FallbackTestXmlParser.IncompleteTestSuites::class.java)
+      fallbackTestSuites?.testsuite?.forEach {
+        fallbackTestXmlParser.processIncompleteInfoSuite(it)
+      }
     }
   }
 
@@ -227,7 +229,7 @@ class TestXmlParser(private var parentId: TaskId, private var bspClientTestNotif
 /** Bazel has a separate way of parsing JUnit4 and JUnit5 test results into a xml file, resulting in
  * incomplete data about the latter.
  * **/
-private class FallbackTestXmlParser(private var parentId: TaskId, private var bspClientTestNotifier: BspClientTestNotifier) {
+private class FallbackTestXmlParser(private var bspClientTestNotifier: BspClientTestNotifier) {
   @JacksonXmlRootElement(localName = "testsuites")
   data class IncompleteTestSuites(
     @JacksonXmlElementWrapper(useWrapping = false)
@@ -260,8 +262,18 @@ private class FallbackTestXmlParser(private var parentId: TaskId, private var bs
     val skipped: TestResultDetail? = null,
   )
 
-  // A Bazel target is represented by a test suite containing one test case
   fun processIncompleteInfoSuite(suite: IncompleteTestSuite) {
+    val containsJunit5 = suite.systemOut?.let(Junit5TestVisualOutputParser::textContainsJunit5VisualOutput)
+    if (containsJunit5 == true) {
+      val parser = Junit5TestVisualOutputParser(bspClientTestNotifier)
+      parser.processTestOutput(suite.systemOut)
+    } else {
+      defaultIncompleteInfoSuiteProcessing(suite)
+    }
+  }
+
+  // A Bazel target is represented by a test suite containing one test case
+  private fun defaultIncompleteInfoSuiteProcessing(suite: IncompleteTestSuite) {
     val suiteTaskId = TaskId(UUID.randomUUID().toString())
     suiteTaskId.parents = emptyList()
     val suiteStatus =

commit fd8a668c0d74a341e4d7a24a661f28e71b29f04a

[fix] Fix main after `f9890dad`
Commit f9890dad592d1d0f91eb5e5dbde3c34ecafbdb6d broke the main revision; this commit fixes it

server/logger/src/main/kotlin/org/jetbrains/bsp/bazel/logger/BspClientTestNotifier.kt
@@ -26,7 +26,6 @@ class BspClientTestNotifier(private val bspClient: BuildClient, private val orig
    * Notifies the client about starting a single test or a test suite
    * The presence or lack of parent's taskId indicates whether it's a test case or a test suite.
    *
-   * @param isSuite     `true` if a test suite has been started, `false` if it was a single test instead
    * @param displayName display name of the started test / test suite
    * @param taskId      TaskId of the started test - when parentsId is not empty / test suite - otherwise
    */
@@ -106,7 +105,11 @@ class BspClientTestNotifier(private val bspClient: BuildClient, private val orig
    * @param targetIdentifier identifier of the testing target being finished
    * @param taskId     TaskId of the testing target execution
    */
-  fun endTestTarget(targetIdentifier: BuildTargetIdentifier?, taskId: TaskId) {
+  fun endTestTarget(
+    targetIdentifier: BuildTargetIdentifier?,
+    taskId: TaskId,
+    time: Long? = null,
+  ) {
     val testReport =
       TestReport(
         targetIdentifier,
@@ -116,6 +119,7 @@ class BspClientTestNotifier(private val bspClient: BuildClient, private val orig
         cancelledTests,
         skippedTests,
       )
+    time?.let { testReport.time = it }
 
     val taskFinishParams = TaskFinishParams(taskId, StatusCode.OK)
     taskFinishParams.originId = originId

server/server/src/main/kotlin/org/jetbrains/bsp/bazel/server/bep/Junit5TestVisualOutputParser.kt
@@ -2,7 +2,6 @@ package org.jetbrains.bsp.bazel.server.bep
 
 import ch.epfl.scala.bsp4j.BuildTargetIdentifier
 import ch.epfl.scala.bsp4j.TaskId
-import ch.epfl.scala.bsp4j.TestReport
 import ch.epfl.scala.bsp4j.TestStatus
 import org.jetbrains.bsp.bazel.logger.BspClientTestNotifier
 import org.jetbrains.bsp.protocol.JUnitStyleTestCaseData
@@ -100,9 +99,8 @@ class Junit5TestVisualOutputParser(private val bspClientTestNotifier: BspClientT
   }
 
   private fun endTesting(testTarget: StartedBuildTarget, millis: Long) {
-    val report = TestReport(BuildTargetIdentifier(testTarget.uri), 0, 0, 0, 0, 0)
-    report.time = millis
-    bspClientTestNotifier.endTestTarget(report, testTarget.taskId)
+    val buildTargetId = BuildTargetIdentifier(testTarget.uri)
+    bspClientTestNotifier.endTestTarget(buildTargetId, testTarget.taskId, millis)
   }
 
   private fun startSuite(suite: TestOutputLine) {

server/server/src/test/kotlin/org/jetbrains/bsp/bazel/server/bep/TestXmlParserTest.kt
@@ -7,7 +7,6 @@ import ch.epfl.scala.bsp4j.PrintParams
 import ch.epfl.scala.bsp4j.PublishDiagnosticsParams
 import ch.epfl.scala.bsp4j.ShowMessageParams
 import ch.epfl.scala.bsp4j.TaskFinishParams
-import ch.epfl.scala.bsp4j.TaskId
 import ch.epfl.scala.bsp4j.TaskProgressParams
 import ch.epfl.scala.bsp4j.TaskStartParams
 import ch.epfl.scala.bsp4j.TestFinish
@@ -78,7 +77,6 @@ class TestXmlParserTest {
 
     val client = MockBuildClient()
     val notifier = BspClientTestNotifier(client, "sample-origin")
-    val parentId = TaskId("sample-task")
 
     // when
     TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
@@ -177,7 +175,6 @@ class TestXmlParserTest {
 
     val client = MockBuildClient()
     val notifier = BspClientTestNotifier(client, "sample-origin")
-    val parentId = TaskId("sample-task")
 
     // when
     TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
@@ -282,7 +279,6 @@ class TestXmlParserTest {
 
     val client = MockBuildClient()
     val notifier = BspClientTestNotifier(client, "sample-origin")
-    val parentId = TaskId("sample-task")
 
     // when
     TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
@@ -392,7 +388,6 @@ class TestXmlParserTest {
 
     val client = MockBuildClient()
     val notifier = BspClientTestNotifier(client, "sample-origin")
-    val parentId = TaskId("sample-task")
 
     // when
     TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, samplePassingContents))
@@ -819,10 +814,9 @@ class TestXmlParserTest {
 
     val client = MockBuildClient()
     val notifier = BspClientTestNotifier(client, "sample-origin")
-    val parentId = TaskId("sample-task")
 
     // when
-    TestXmlParser(parentId, notifier).parseAndReport(writeTempFile(tempDir, sampleContents))
+    TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, sampleContents))
 
     // then
     client.taskStartCalls.size shouldBe 3
@@ -865,10 +859,9 @@ class TestXmlParserTest {
 
     val client = MockBuildClient()
     val notifier = BspClientTestNotifier(client, "sample-origin")
-    val parentId = TaskId("sample-task")
 
     // when
-    TestXmlParser(parentId, notifier).parseAndReport(writeTempFile(tempDir, sampleContents))
+    TestXmlParser(notifier).parseAndReport(writeTempFile(tempDir, sampleContents))
 
     // then
     client.taskFinishCalls.size shouldBe 2