73901d246745bfd26ec0c90ce39c038ce8725eb8
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-11-21 15:57:54 +0000

    [maintenance] change icon for `Open project view file in editor` in Bazel Tool Window to Bazel icon to make it stand out more

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/extension/BazelToolWindowConfigFileProviderExtension.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/extension/BazelToolWindowConfigFileProviderExtension.kt
@@ -1,12 +1,14 @@
 package org.jetbrains.bazel.extension
 
 import com.intellij.openapi.project.Project
+import org.jetbrains.bazel.assets.BazelPluginIcons
 import org.jetbrains.bazel.config.BazelPluginBundle
 import org.jetbrains.bazel.config.BazelPluginConstants.bazelBspBuildToolId
 import org.jetbrains.bazel.settings.bazelProjectSettings
 import org.jetbrains.plugins.bsp.config.BuildToolId
 import org.jetbrains.plugins.bsp.extensionPoints.BspToolWindowConfigFileProviderExtension
 import java.nio.file.Path
+import javax.swing.Icon
 
 class BazelToolWindowConfigFileProviderExtension : BspToolWindowConfigFileProviderExtension {
   override val buildToolId: BuildToolId = bazelBspBuildToolId
@@ -14,4 +16,6 @@ class BazelToolWindowConfigFileProviderExtension : BspToolWindowConfigFileProvid
   override fun getConfigFileGenericName(): String = BazelPluginBundle.message("tool.window.generic.config.file")
 
   override fun getConfigFile(project: Project): Path? = project.bazelProjectSettings.projectViewPath
+
+  override fun getConfigFileIcon(): Icon = BazelPluginIcons.bazel
 }
--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/extensionPoints/BspToolWindowConfigFileProviderExtension.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/extensionPoints/BspToolWindowConfigFileProviderExtension.kt
@@ -1,16 +1,20 @@
 package org.jetbrains.plugins.bsp.extensionPoints
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.bsp.config.WithBuildToolId
 import org.jetbrains.plugins.bsp.config.buildToolId
 import org.jetbrains.plugins.bsp.config.withBuildToolId
 import java.nio.file.Path
+import javax.swing.Icon
 
 interface BspToolWindowConfigFileProviderExtension : WithBuildToolId {
   fun getConfigFileGenericName(): String
 
   fun getConfigFile(project: Project): Path?
+
+  fun getConfigFileIcon(): Icon = AllIcons.FileTypes.Any_type
 
   companion object {
     internal val ep =
