cefb86dc087b68057c17a13e47a4d9c8690cc68b
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-11-21 15:08:16 +0000

    [maintenance] remove obsolete Bazel project opening action, which was replaced with `Link Bazel project` action

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/BazelBspOpenProjectProvider.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/BazelBspOpenProjectProvider.kt
@@ -7,16 +7,33 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.isFile
 import org.jetbrains.bazel.config.BazelPluginConstants
+import org.jetbrains.bazel.config.BazelPluginConstants.bazelBspBuildToolId
+import org.jetbrains.bazel.coroutines.CoroutineService
+import org.jetbrains.plugins.bsp.impl.flow.open.initProperties
+import org.jetbrains.plugins.bsp.startup.BspStartupActivity
 
 internal class BazelBspOpenProjectProvider : AbstractOpenProjectProvider() {
   private val log = logger<BazelBspOpenProjectProvider>()
 
   override val systemId: ProjectSystemId = BazelPluginConstants.SYSTEM_ID
 
+  // intentionally overriding the visibility to `public` from `protected` in [AbstractOpenProjectProvider]
+  // DO NOT REMOVE THIS MODIFIER
   public override fun isProjectFile(file: VirtualFile): Boolean = file.isFile && file.name in BazelPluginConstants.WORKSPACE_FILE_NAMES
 
+  // intentionally overriding the visibility to `public` from `protected` in [AbstractOpenProjectProvider]
+  // DO NOT REMOVE THIS MODIFIER
   public override suspend fun linkProject(projectFile: VirtualFile, project: Project) {
     log.debug("Link BazelBsp project $projectFile to existing project ${project.name}")
     performOpenBazelProjectViaBspPlugin(project, projectFile)
+  }
+}
+
+internal fun performOpenBazelProjectViaBspPlugin(project: Project?, projectRootDir: VirtualFile?) {
+  if (projectRootDir != null && project != null) {
+    project.initProperties(projectRootDir, bazelBspBuildToolId)
+    CoroutineService.getInstance(project).start {
+      BspStartupActivity().execute(project)
+    }
   }
 }
