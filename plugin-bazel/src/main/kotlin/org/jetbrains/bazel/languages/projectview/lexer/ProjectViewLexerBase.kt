77f3a3c9a4deb3cdce4324f257437b52e1a74b82
Robert Grigoryan <robert.grigoryan@jetbrains.com>
2024-11-27 17:51:00 +0100

    Initial update to lexer implementation
    This commit is the first step to create a new implementation of the Project View lexer. Previously it was implemented using JFlex, but the new lexer isn't going to use it anymore. This implementation needs ProjectViewKeywords to properly parse the LINEAR_KEYWORD and SCALAR_KEYWORD tokens, but it has yet to be imported. The ProjectViewLexerBase is an implementation of the parsing process. The next commit is going to add an extension of the LexerBase abstract class called ProjectViewLexer. It's going to use the new ProjectViewLexerBase to tokenize a char sequence. The Project View token types have also been updated and their number has been reduced.

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/lexer/ProjectViewLexerBase.kt
@@ -0,0 +1,103 @@
+package org.jetbrains.bazel.languages.projectview.lexer
+
+class ProjectViewLexerBase(input: CharSequence) {
+  private val tokens = mutableListOf<Token>()
+  private var buffer = input.toString()
+  private var pos = 0
+  private var identifierStart: Int? = null
+  private var lineHasPrecedingNonWhitespaceChar = false
+
+  init {
+      tokenize()
+  }
+
+  private fun tokenize() {
+    var c: Char
+
+    while (pos < buffer.length) {
+      c = buffer[pos]
+      pos++
+
+      when {
+        c == '\n' -> {
+          addPrecedingIdentifier(pos - 1)
+          tokens.add(Token(ProjectViewTokenType.NEWLINE, pos - 1, pos))
+          lineHasPrecedingNonWhitespaceChar = false
+        }
+        c.isWhitespace() -> {
+          addPrecedingIdentifier(pos - 1)
+          handleWhiteSpace()
+        }
+        c == ':' -> {
+          addPrecedingIdentifier(pos - 1)
+          tokens.add(Token(ProjectViewTokenType.COLON, pos - 1, pos))
+        }
+        c == '#' && !lineHasPrecedingNonWhitespaceChar -> {
+          addPrecedingIdentifier(pos - 1)
+          addCommentLine(pos - 1)
+        }
+        else -> {
+          lineHasPrecedingNonWhitespaceChar = true
+          if (identifierStart == null) {
+            identifierStart = pos - 1
+          }
+        }
+      }
+    }
+
+    addPrecedingIdentifier(pos)
+  }
+
+  private fun addPrecedingIdentifier(end: Int) {
+    identifierStart?.let {
+      tokens.add(Token(getIdentifierToken(it, end), it, end))
+      identifierStart = null
+    }
+  }
+
+  private fun addCommentLine(start: Int) {
+    while (pos < buffer.length && buffer[pos] != '\n' ) {
+      pos++
+    }
+    tokens.add(Token(ProjectViewTokenType.COMMENT, start, pos))
+  }
+
+  /**
+   * If the whitespace is followed by an end-of-line comment or a newline, it's combined with those
+   * tokens.
+   */
+  private fun handleWhiteSpace() {
+    val oldPos = pos - 1
+    when {
+      pos >= buffer.length ||
+      buffer[pos] == ' ' || buffer[pos] == '\t' || buffer[pos] != '\r' -> {
+        if (pos < buffer.length) {
+          pos++
+        }
+        tokens.add(Token(ProjectViewTokenType.WHITESPACE, oldPos, pos))
+      }
+      lineHasPrecedingNonWhitespaceChar || buffer[pos] == '#' || buffer[pos] == '\n' -> {
+        tokens.add(Token(ProjectViewTokenType.WHITESPACE, oldPos, pos))
+      }
+      else -> {
+        tokens.add(Token(ProjectViewTokenType.INDENT, oldPos, pos))
+      }
+    }
+  }
+
+  private fun getIdentifierToken(start: Int, end: Int): ProjectViewTokenType {
+    val identifier = buffer.substring(start, end)
+    // TODO: Import ProjectViewKeywords
+    return when (identifier) {
+      in ProjectViewKeywords.LIST_KEYWORD_MAP.keys -> ProjectViewTokenType.LIST_KEYWORD
+      in ProjectViewKeywords.SCALAR_KEYWORD_MAP.keys -> ProjectViewTokenType.SCALAR_KEYWORD
+      else -> ProjectViewTokenType.IDENTIFIER
+    }
+  }
+
+  data class Token(
+    val type: ProjectViewTokenType,
+    val left: Int,
+    val right: Int,
+  )
+}
--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/lexer/ProjectViewTokenType.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/lexer/ProjectViewTokenType.kt
@@ -1,8 +1,22 @@
 package org.jetbrains.bazel.languages.projectview.lexer
 
 import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.TokenSet
 import org.jetbrains.bazel.languages.projectview.base.ProjectViewLanguage
 
-class ProjectViewTokenType(debugName: String) : IElementType(debugName, ProjectViewLanguage) {
+class ProjectViewTokenType private constructor(debugName: String) : IElementType(debugName, ProjectViewLanguage) {
+  companion object {
+    val COMMENT = ProjectViewTokenType("comment")
+    val WHITESPACE = ProjectViewTokenType("whitespace");
+    val NEWLINE = ProjectViewTokenType("newline");
+    val COLON = ProjectViewTokenType(":");
+    val INDENT = ProjectViewTokenType("indent");
+    val IDENTIFIER = ProjectViewTokenType("identifier");
+    val LIST_KEYWORD = ProjectViewTokenType("list_keyword");
+    val SCALAR_KEYWORD = ProjectViewTokenType("scalar_keyword");
+
+    val IDENTIFIERS = TokenSet.create(IDENTIFIER, LIST_KEYWORD, SCALAR_KEYWORD)
+  }
+
   override fun toString(): String = "ProjectView:" + super.toString()
 }
