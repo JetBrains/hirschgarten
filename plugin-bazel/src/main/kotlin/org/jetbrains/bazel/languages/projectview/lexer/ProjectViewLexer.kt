0b3a1f81ceca8e751ebb46211bf57e65a1d815d2
Piotr Pływacz <piotrus.plywacz@gmail.com>
2024-11-27 21:09:04 +0100

    Complete ProjectViewLexer class.

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/lexer/ProjectViewLexer.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/lexer/ProjectViewLexer.kt
@@ -1,5 +1,56 @@
 package org.jetbrains.bazel.languages.projectview.lexer
 
-import com.intellij.lexer.FlexAdapter
+import com.intellij.lexer.LexerBase
+import com.intellij.psi.tree.IElementType
 
-class ProjectViewLexer : FlexAdapter(_ProjectViewLexer(null))
+class ProjectViewLexer: LexerBase() {
+
+  private var endOffset = 0
+  private var offsetStart = 0
+  private var buffer = ""
+  private var tokens: Iterator<ProjectViewLexerBase.Token> =
+    emptyList<ProjectViewLexerBase.Token>().iterator()
+  private var currentToken: ProjectViewLexerBase.Token? = null
+
+  override fun start(buffer: CharSequence, startOffset: Int, endOffset: Int, initialState: Int) {
+    this.buffer = buffer.toString()
+    this.offsetStart = startOffset
+    this.endOffset = endOffset
+
+    var lexer: ProjectViewLexerBase =
+      ProjectViewLexerBase(buffer.subSequence(startOffset, endOffset))
+    tokens = lexer.getTokens().iterator()
+    currentToken = if (tokens.hasNext()) tokens.next() else null
+  }
+
+  override fun getState(): Int = 0
+
+  override fun getTokenType(): IElementType? = currentToken?.type
+
+  override fun getTokenStart(): Int {
+    if (currentToken == null) {
+      return 0
+    }
+    return currentToken!!.left + offsetStart
+  }
+
+  override fun getTokenEnd(): Int {
+    if (currentToken == null) {
+      return 0
+    }
+    return currentToken!!.right + offsetStart
+  }
+
+  override fun advance() {
+    if (currentToken != null && tokens.hasNext()) {
+      currentToken = tokens.next()
+    }
+    else {
+      currentToken = null
+    }
+  }
+
+  override fun getBufferSequence(): CharSequence = buffer
+
+  override fun getBufferEnd(): Int = endOffset
+}
