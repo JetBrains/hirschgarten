56454eedf0a019977f83378c907a883e38a5287f
Lev Leontev <lev.leontev@jetbrains.com>
2024-11-28 16:58:44 +0000

    [maintenance] Support Android for the upcoming IDEA 2024.3.1

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/android/BspSourceProvidersFactory.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/android/BspSourceProvidersFactory.kt
@@ -1,11 +1,14 @@
 package org.jetbrains.plugins.bsp.android
 
+import com.android.tools.idea.projectsystem.IdeaSourceProvider
 import com.android.tools.idea.projectsystem.NamedIdeaSourceProvider
 import com.android.tools.idea.projectsystem.NamedIdeaSourceProviderBuilder
 import com.android.tools.idea.projectsystem.ScopeType
 import com.android.tools.idea.projectsystem.SourceProviders
 import com.android.tools.idea.projectsystem.SourceProvidersFactory
 import com.android.tools.idea.projectsystem.SourceProvidersImpl
+import com.android.tools.idea.projectsystem.TestComponentType.DeviceTest
+import com.android.tools.idea.projectsystem.TestComponentType.HostTest
 import com.android.tools.idea.projectsystem.emptySourceProvider
 import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.android.facet.createSourceProvidersForLegacyModule
@@ -16,19 +19,67 @@ class BspSourceProvidersFactory : SourceProvidersFactory {
   override fun createSourceProvidersFor(facet: AndroidFacet): SourceProviders {
     val sourceProvider = createSourceProviderForModule(facet) ?: return createSourceProvidersForLegacyModule(facet)
 
-    return SourceProvidersImpl(
-      mainIdeaSourceProvider = sourceProvider,
-      currentSourceProviders = listOf(sourceProvider),
-      currentTestFixturesSourceProviders = listOf(sourceProvider),
-      currentAndSomeFrequentlyUsedInactiveSourceProviders = listOf(sourceProvider),
-      mainAndFlavorSourceProviders = listOf(sourceProvider),
-      generatedSources = emptySourceProvider(ScopeType.MAIN),
-      currentHostTestSourceProviders = emptyMap(),
-      currentDeviceTestSourceProviders = emptyMap(),
-      generatedHostTestSources = emptyMap(),
-      generatedDeviceTestSources = emptyMap(),
-      generatedTestFixturesSources = emptySourceProvider(ScopeType.TEST_FIXTURES),
-    )
+    val mainIdeaSourceProvider = sourceProvider
+    val currentSourceProviders = listOf(sourceProvider)
+    val currentHostTestSourceProviders = emptyMap<HostTest, List<NamedIdeaSourceProvider>>()
+    val currentDeviceTestSourceProviders = emptyMap<DeviceTest, List<NamedIdeaSourceProvider>>()
+    val currentTestFixturesSourceProviders = listOf(sourceProvider)
+    val allVariantAllArtifactsSourceProviders = listOf(sourceProvider)
+    val currentAndSomeFrequentlyUsedInactiveSourceProviders = listOf(sourceProvider)
+    val mainAndFlavorSourceProviders = listOf(sourceProvider)
+    val generatedSources = emptySourceProvider(ScopeType.MAIN)
+    val generatedHostTestSources = emptyMap<HostTest, NamedIdeaSourceProvider>()
+    val generatedDeviceTestSources = emptyMap<DeviceTest, NamedIdeaSourceProvider>()
+    val generatedTestFixturesSources = emptySourceProvider(ScopeType.TEST_FIXTURES)
+
+    return try {
+      // 2024.3 version
+      SourceProvidersImpl(
+        mainIdeaSourceProvider,
+        currentSourceProviders,
+        currentHostTestSourceProviders,
+        currentDeviceTestSourceProviders,
+        currentTestFixturesSourceProviders,
+        // allVariantAllArtifactsSourceProviders,  TODO: 2024.3.1 added a new parameter that didn't exist in 2024.3.
+        currentAndSomeFrequentlyUsedInactiveSourceProviders,
+        mainAndFlavorSourceProviders,
+        generatedSources,
+        generatedHostTestSources,
+        generatedDeviceTestSources,
+        generatedTestFixturesSources,
+      )
+    } catch (_: NoSuchMethodError) {
+      // 2024.3.1 added a new parameter
+      val constructor =
+        SourceProvidersImpl::class.java.getConstructor(
+          NamedIdeaSourceProvider::class.java, // mainIdeaSourceProvider
+          List::class.java, // currentSourceProviders
+          Map::class.java, // currentHostTestSourceProviders
+          Map::class.java, // currentDeviceTestSourceProviders
+          List::class.java, // currentTestFixturesSourceProviders
+          List::class.java, // allVariantAllArtifactsSourceProviders
+          List::class.java, // currentAndSomeFrequentlyUsedInactiveSourceProviders
+          List::class.java, // mainAndFlavorSourceProviders
+          IdeaSourceProvider::class.java, // generatedSources
+          Map::class.java, // generatedHostTestSources
+          Map::class.java, // generatedDeviceTestSources
+          IdeaSourceProvider::class.java, // generatedTestFixturesSources
+        )
+      constructor.newInstance(
+        mainIdeaSourceProvider,
+        currentSourceProviders,
+        currentHostTestSourceProviders,
+        currentDeviceTestSourceProviders,
+        currentTestFixturesSourceProviders,
+        allVariantAllArtifactsSourceProviders,
+        currentAndSomeFrequentlyUsedInactiveSourceProviders,
+        mainAndFlavorSourceProviders,
+        generatedSources,
+        generatedHostTestSources,
+        generatedDeviceTestSources,
+        generatedTestFixturesSources,
+      )
+    }
   }
 
   private fun createSourceProviderForModule(facet: AndroidFacet): NamedIdeaSourceProvider? {
