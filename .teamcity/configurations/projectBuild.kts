27e84696a5ad55581b7e54df6a5fc58263cfea61
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-12-02 10:19:45 +0400

    save artifacts during build step

--- .teamcity/configurations/projectBuild.kts
+++ .teamcity/configurations/projectBuild.kts
@@ -3,12 +3,14 @@ package configurations
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.BazelStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.bazel
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 open class Build(vcsRoot: GitVcsRoot) :
     BaseConfiguration.BaseBuildType(
         name = "[build] build project",
         vcsRoot = vcsRoot,
+        artifactRules = "+:%system.teamcity.build.checkoutDir%/intellij-*.zip",
         steps = {
             Utils.CommonParams.CrossBuildPlatforms.forEach { platform ->
                 bazel {
@@ -17,7 +19,7 @@ open class Build(vcsRoot: GitVcsRoot) :
                 // enabled = (platform != "242")
                 executionMode = BuildStep.ExecutionMode.RUN_ON_FAILURE
 
-                name = "build //..."
+                name = "build //... $platform"
                 command = "build"
                 targets = "//..."
                 arguments = "--define=ij_product=intellij-20$platformDot ${Utils.CommonParams.BazelCiSpecificArgs}"
@@ -25,6 +27,20 @@ open class Build(vcsRoot: GitVcsRoot) :
                 Utils.DockerParams.get().forEach { (key, value) ->
                   param(key, value)
                 }
+            }
+            script {
+              name = "save plugin $platform"
+              id = "save_plugin_$platform"
+              scriptContent = """
+                  #!/bin/bash
+                  set -uxo
+                  
+                  BAZEL_BIN_PATH="%system.agent.persistent.cache%/bazel/_bazel_hirschuser/*/execroot/_main/bazel-out/k8-fastbuild/bin"
+                  PROJECT_PATH="%system.teamcity.build.checkoutDir%"
+                  
+                  cp ${"$"}{BAZEL_BIN_PATH}/plugin-bsp/intellij-bsp.zip ${"$"}{PROJECT_PATH}/intellij-bsp-$platform.zip
+                  cp ${"$"}{BAZEL_BIN_PATH}/plugin-bazel/intellij-bazel.zip ${"$"}{PROJECT_PATH}/intellij-bazel-$platform.zip
+              """.trimIndent()
             }
         }
     },
