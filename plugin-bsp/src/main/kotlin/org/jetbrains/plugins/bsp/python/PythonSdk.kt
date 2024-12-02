732854807ad960937bb4aa9c4e1f4f67701ca8ac
Katarzyna Mielnik <katarzyna.mielnik@jetbrains.com>
2024-12-02 16:31:29 +0000

    Add SyncHook for Python modules | #BAZEL-841
    GitOrigin-RevId: 75d99266b98a36f6da1e6990a8f6436eb01e139b

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/python/PythonSdk.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.plugins.bsp.python
-
-import ch.epfl.scala.bsp4j.DependencySourcesItem
-
-data class PythonSdk(
-  val name: String,
-  val interpreterUri: String,
-  val dependencies: List<DependencySourcesItem>,
-)
