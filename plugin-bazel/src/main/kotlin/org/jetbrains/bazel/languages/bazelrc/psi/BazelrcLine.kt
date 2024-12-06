b034f937284ebeb90691e013ac98734be5bac720
Mihai Claudiu Toader <mihai@sigmacomputing.com>
2024-12-06 07:37:08 -0800

    [feature] Special case `watchfs` when deprecation | #BAZEL-1553 (#166)

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelrc/psi/BazelrcLine.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelrc/psi/BazelrcLine.kt
@@ -2,10 +2,16 @@ package org.jetbrains.bazel.languages.bazelrc.psi
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
+import com.intellij.psi.tree.TokenSet
 import org.jetbrains.bazel.languages.bazelrc.elements.BazelrcTokenTypes
 
 class BazelrcLine(node: ASTNode) : BazelrcBaseElement(node) {
   override fun acceptVisitor(visitor: BazelrcElementVisitor) = visitor.visitLine(this)
 
-  fun configName(): String? = this.findChildByType<PsiElement>(BazelrcTokenTypes.CONFIG)?.text
+  val config: String? = this.findChildByType<PsiElement>(BazelrcTokenTypes.CONFIG)?.text
+  val command: String = this.findChildByType<PsiElement>(COMMAND_TOKENS)?.text ?: ""
+
+  companion object {
+    val COMMAND_TOKENS = TokenSet.create(BazelrcTokenTypes.COMMAND)
+  }
 }
