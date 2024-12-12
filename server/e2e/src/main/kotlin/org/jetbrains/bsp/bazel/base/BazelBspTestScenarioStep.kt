5b765fc1da4030684a69ce9ad255acee6c98f9d0
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2024-12-12 19:00:29 +0100

    replace logger with print so we can see somethign

--- server/e2e/src/main/kotlin/org/jetbrains/bsp/bazel/base/BazelBspTestScenarioStep.kt
+++ server/e2e/src/main/kotlin/org/jetbrains/bsp/bazel/base/BazelBspTestScenarioStep.kt
@@ -1,22 +1,16 @@
 package org.jetbrains.bsp.bazel.base
 
-import org.apache.logging.log4j.LogManager
-
 class BazelBspTestScenarioStep(private val testName: String, private val testkitCall: () -> Unit) {
   fun executeAndReturnResult(): Boolean {
-    log.info("Executing \"$testName\"...")
+    println("Executing \"$testName\"...")
 
     return try {
       testkitCall()
-      log.info("Step \"$testName\" successful!")
+      println("Step \"$testName\" successful!")
       true
     } catch (e: Exception) {
-      log.error("Step \"$testName\" failed!", e)
+      println("Step \"$testName\" failed! $e")
       false
     }
-  }
-
-  companion object {
-    private val log = LogManager.getLogger(BazelBspTestScenarioStep::class.java)
   }
 }
