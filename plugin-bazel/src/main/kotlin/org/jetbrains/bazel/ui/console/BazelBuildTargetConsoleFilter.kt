e9d21f6c3909ba9ca478f9c4d6cb1f5ec900c9b0
Jiaming Tang <jiaming.tang@jetbrains.com>
2024-12-18 18:02:54 +0100

    add test for jump to location

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/console/BazelBuildTargetConsoleFilter.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/console/BazelBuildTargetConsoleFilter.kt
@@ -2,19 +2,21 @@ package org.jetbrains.bazel.ui.console
 
 import com.intellij.execution.filters.ConsoleFilterProvider
 import com.intellij.execution.filters.Filter
+import com.intellij.execution.filters.HyperlinkInfo
 import com.intellij.execution.filters.OpenFileHyperlinkInfo
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.findDirectory
 import com.intellij.openapi.vfs.findFile
+import com.intellij.openapi.vfs.findPsiFile
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
 import com.intellij.psi.util.descendantsOfType
 import org.jetbrains.bazel.config.isBazelProject
 import org.jetbrains.bazel.languages.bazel.BazelLabel
 import org.jetbrains.bazel.languages.starlark.psi.expressions.arguments.StarlarkNamedArgumentExpression
 import org.jetbrains.bazel.languages.starlark.references.BUILD_FILE_NAMES
-import org.jetbrains.kotlin.idea.base.psi.getLineNumber
-import org.jetbrains.kotlin.idea.core.util.toPsiFile
 import org.jetbrains.plugins.bsp.config.rootDir
 
 class BazelBuildTargetConsoleFilter(private val project: Project) : Filter {
@@ -48,35 +50,40 @@ class BazelBuildTargetConsoleFilter(private val project: Project) : Filter {
       // skip targets in external repo
       return null
     }
-    val virtualFile = pathGroup.value.toBazelFileInProject() ?: return null
-    val hyperLinkInfo =
-      runReadAction {
-        val psiElement =
-          virtualFile
-            .toPsiFile(project)
-            ?.descendantsOfType<StarlarkNamedArgumentExpression>()
-            ?.filter { it.isNameArgument() }
-            ?.firstOrNull {
-              it.getArgumentStringValue()?.let { it1 -> BazelLabel.ofString(it1).targetName } == target.value
-            }
-        OpenFileHyperlinkInfo(project, virtualFile, psiElement?.getLineNumber() ?: 0, 0)
-      }
-
+    val hyperLinkInfo = getHyperLinkInfo(project, pathGroup.value, target.value)
     val highlightStartOffset = entireLength - line.length + highlightGroup.range.first
     val highlightEndOffset = entireLength - line.length + highlightGroup.range.last + 1
 
     return Filter.Result(highlightStartOffset, highlightEndOffset, hyperLinkInfo)
   }
 
-  private fun String.toBazelFileInProject(): VirtualFile? {
-    for (fileName in BUILD_FILE_NAMES) {
-      val buildFile = project.rootDir.findDirectory(this)?.findFile(fileName)
-      if (buildFile != null) {
-        return buildFile
-      }
+  private fun getHyperLinkInfo(
+    project: Project,
+    buildFileName: String,
+    target: String,
+  ): HyperlinkInfo? {
+    val virtualFile = buildFileName.toBazelFileInProject() ?: return null
+
+    return runReadAction {
+      val psiElement =
+        virtualFile
+          .findPsiFile(project)
+          ?.descendantsOfType<StarlarkNamedArgumentExpression>()
+          ?.filter { it.isNameArgument() }
+          ?.firstOrNull {
+            it.getArgumentStringValue()?.let { it1 -> BazelLabel.ofString(it1).targetName } == target
+          }
+      OpenFileHyperlinkInfo(project, virtualFile, psiElement?.calculateLineNumber() ?: 0, 0)
     }
-    return null
   }
+
+  private fun String.toBazelFileInProject(): VirtualFile? =
+    BUILD_FILE_NAMES.firstNotNullOfOrNull {
+      project.rootDir.findDirectory(this)?.findFile(it)
+    }
+
+  private fun PsiElement.calculateLineNumber(): Int? =
+    PsiDocumentManager.getInstance(project).getDocument(containingFile)?.getLineNumber(textOffset)
 }
 
 class BazelBuildTargetConsoleFilterProvider : ConsoleFilterProvider {
--- plugin-bazel/src/test/kotlin/org/jetbrains/bazel/ui/console/BazelBuildTargetConsoleFilterTest.kt
+++ plugin-bazel/src/test/kotlin/org/jetbrains/bazel/ui/console/BazelBuildTargetConsoleFilterTest.kt
@@ -1,37 +1,49 @@
 package org.jetbrains.bazel.ui.console
 
 import com.intellij.execution.filters.OpenFileHyperlinkInfo
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.util.io.findOrCreateFile
 import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.platform.backend.workspace.virtualFile
-import com.intellij.platform.workspace.storage.impl.url.toVirtualFileUrl
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import org.jetbrains.plugins.bsp.config.rootDir
-import org.jetbrains.workspace.model.test.framework.WorkspaceModelBaseTest
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 import java.nio.file.Path
+import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
-import kotlin.io.path.createFile
+import kotlin.io.path.readLines
+import kotlin.io.path.writeText
 
 private const val TEST_LINE_PREFIX = "There are 39 characters in this string "
 private const val TEST_LINE_SUFFIX = " bliep bliep bliep\n"
 
-class BazelBuildTargetConsoleFilterTest : WorkspaceModelBaseTest() {
+@RunWith(JUnit4::class)
+class BazelBuildTargetConsoleFilterTest : BasePlatformTestCase() {
   private lateinit var filter: BazelBuildTargetConsoleFilter
 
-  @BeforeEach
-  override fun beforeEach() {
-    project.rootDir = projectBasePath.toVirtualFileUrl(virtualFileUrlManager).virtualFile!!
+  override fun setUp() {
+    super.setUp()
+    val virtualFileManager = VirtualFileManager.getInstance()
+    project.rootDir = virtualFileManager.findFileByNioPath(Path(project.basePath!!))!!
     filter = BazelBuildTargetConsoleFilter(project)
   }
 
   @Test
   fun `should match an absolute bazel target without @`() {
+    // given
     createBazelFileInProject("plugin-bsp/src")
     val bazelTarget = "//plugin-bsp/src:test_fixtures"
     val line = "$TEST_LINE_PREFIX$bazelTarget$TEST_LINE_SUFFIX"
+
+    // when
     val result = filter.applyFilter(line, line.length + 100)
+
+    // then
     result!!.resultItems.size shouldBe 1
     (result.resultItems[0].hyperlinkInfo as OpenFileHyperlinkInfo).virtualFile?.toNioPath().toString() shouldContain "plugin-bsp"
     result.resultItems[0].highlightStartOffset shouldBe 100 + TEST_LINE_PREFIX.length
@@ -40,10 +52,15 @@ class BazelBuildTargetConsoleFilterTest : WorkspaceModelBaseTest() {
 
   @Test
   fun `should match an absolute bazel target with a @`() {
+    // given
     createBazelFileInProject("plugin-bsp/src")
     val bazelTarget = "@//plugin-bsp/src:test_fixtures"
     val line = "$TEST_LINE_PREFIX$bazelTarget$TEST_LINE_SUFFIX"
+
+    // when
     val result = filter.applyFilter(line, line.length + 100)
+
+    // then
     result!!.resultItems.size shouldBe 1
     (result.resultItems[0].hyperlinkInfo as OpenFileHyperlinkInfo).virtualFile?.toNioPath().toString() shouldContain "plugin-bsp"
     result.resultItems[0].highlightStartOffset shouldBe 100 + TEST_LINE_PREFIX.length
@@ -52,10 +69,15 @@ class BazelBuildTargetConsoleFilterTest : WorkspaceModelBaseTest() {
 
   @Test
   fun `should match an absolute bazel target with @@`() {
+    // given
     createBazelFileInProject("plugin-bsp/src")
     val bazelTarget = "@@//plugin-bsp/src:test_fixtures"
     val line = "$TEST_LINE_PREFIX$bazelTarget$TEST_LINE_SUFFIX"
+
+    // when
     val result = filter.applyFilter(line, line.length + 100)
+
+    // then
     result!!.resultItems.size shouldBe 1
     (result.resultItems[0].hyperlinkInfo as OpenFileHyperlinkInfo).virtualFile?.toNioPath().toString() shouldContain "plugin-bsp"
     result.resultItems[0].highlightStartOffset shouldBe 100 + TEST_LINE_PREFIX.length
@@ -64,37 +86,57 @@ class BazelBuildTargetConsoleFilterTest : WorkspaceModelBaseTest() {
 
   @Test
   fun `should not match an external target with @`() {
+    // given
     createBazelFileInProject("plugin-bsp/src")
     val bazelTarget = "@external//plugin-bsp/src:test_fixtures"
     val line = "$TEST_LINE_PREFIX$bazelTarget$TEST_LINE_SUFFIX"
+
+    // when
     val result = filter.applyFilter(line, line.length + 100)
+
+    // then
     result!!.resultItems.size shouldBe 0
   }
 
   @Test
   fun `should not match an external target with @@`() {
+    // given
     createBazelFileInProject("plugin-bsp/src")
     val bazelTarget = "@@external//plugin-bsp/src:test_fixtures"
     val line = "$TEST_LINE_PREFIX$bazelTarget$TEST_LINE_SUFFIX"
+
+    // when
     val result = filter.applyFilter(line, line.length + 100)
+
+    // then
     result!!.resultItems.size shouldBe 0
   }
 
   @Test
   fun `should not match non-existing folder`() {
+    // given
     createBazelFileInProject("plugin-bsp/src")
     val bazelTarget = "@@external//plugin-bsp/test:test_fixtures"
     val line = "$TEST_LINE_PREFIX$bazelTarget$TEST_LINE_SUFFIX"
+
+    // when
     val result = filter.applyFilter(line, line.length + 100)
+
+    // then
     result!!.resultItems.size shouldBe 0
   }
 
   @Test
   fun `should match top level build files`() {
+    // given
     createBazelFileInProject(".")
     val bazelTarget = "//:format"
     val line = "$TEST_LINE_PREFIX$bazelTarget$TEST_LINE_SUFFIX"
+
+    // when
     val result = filter.applyFilter(line, line.length + 100)
+
+    // then
     result!!.resultItems.size shouldBe 1
     result.resultItems[0].highlightStartOffset shouldBe 100 + TEST_LINE_PREFIX.length
     result.resultItems[0].highlightEndOffset shouldBe 100 + TEST_LINE_PREFIX.length + bazelTarget.length
@@ -102,10 +144,15 @@ class BazelBuildTargetConsoleFilterTest : WorkspaceModelBaseTest() {
 
   @Test
   fun `should match an absolute bazel target without @ to BUILD_bazel`() {
+    // given
     createBazelFileInProject("plugin-bsp/src", "BUILD.bazel")
     val bazelTarget = "//plugin-bsp/src:test_fixtures"
     val line = "$TEST_LINE_PREFIX$bazelTarget$TEST_LINE_SUFFIX"
+
+    // when
     val result = filter.applyFilter(line, line.length + 100)
+
+    // then
     result!!.resultItems.size shouldBe 1
     (result.resultItems[0].hyperlinkInfo as OpenFileHyperlinkInfo).virtualFile?.toNioPath().toString() shouldContain "plugin-bsp"
     result.resultItems[0].highlightStartOffset shouldBe 100 + TEST_LINE_PREFIX.length
@@ -115,23 +162,60 @@ class BazelBuildTargetConsoleFilterTest : WorkspaceModelBaseTest() {
 
   @Test
   fun `BUILD_bazel has higher priority than BUILD`() {
+    // given
     createBazelFileInProject(".", "BUILD.bazel")
     createBazelFileInProject(".", "BUILD")
-
     val bazelTarget = "//:format"
     val line = "$TEST_LINE_PREFIX$bazelTarget$TEST_LINE_SUFFIX"
+
+    // when
     val result = filter.applyFilter(line, line.length + 100)
+
+    // then
     result!!.resultItems.size shouldBe 1
     result.resultItems[0].highlightStartOffset shouldBe 100 + TEST_LINE_PREFIX.length
     result.resultItems[0].highlightEndOffset shouldBe 100 + TEST_LINE_PREFIX.length + bazelTarget.length
     (result.resultItems[0].hyperlinkInfo as OpenFileHyperlinkInfo).virtualFile.toString().endsWith("BUILD.bazel") shouldBe true
   }
 
+  val testBazelFileContent = """
+proto_library(
+  name = "test_fixtures",
+  srcs = glob(["*.kt"]),
+)
+  """
+
+  @Test
+  fun `can jump to correct line`() {
+    // given
+    val path = createBazelFileInProject("plugin-bsp/src")
+    val tmp = path.readLines().joinToString("\n")
+    println(tmp)
+    val bazelTarget = "//plugin-bsp/src:test_fixtures"
+    val line = "$TEST_LINE_PREFIX$bazelTarget$TEST_LINE_SUFFIX"
+
+    // when
+    val result = filter.applyFilter(line, line.length + 100)
+
+    // then
+    result!!.resultItems.size shouldBe 1
+    val hyperLink = (result.resultItems[0].hyperlinkInfo as OpenFileHyperlinkInfo)
+    hyperLink.virtualFile?.toNioPath().toString() shouldContain "plugin-bsp"
+    hyperLink.descriptor?.offset shouldBe 16
+    result.resultItems[0].highlightStartOffset shouldBe 100 + TEST_LINE_PREFIX.length
+    result.resultItems[0].highlightEndOffset shouldBe 100 + TEST_LINE_PREFIX.length + bazelTarget.length
+  }
+
   private fun createBazelFileInProject(relativePath: String, buildFileName: String = "BUILD"): Path =
-    projectBasePath
-      .resolve(relativePath)
-      .createDirectories()
-      .resolve(buildFileName)
-      .createFile()
-      .also { LocalFileSystem.getInstance().refreshAndFindFileByNioFile(it) }
+    runWriteAction {
+      project.rootDir
+        .toNioPath()
+        .resolve(relativePath)
+        .createDirectories()
+        .resolve(buildFileName)
+        .findOrCreateFile()
+        .also {
+          it.writeText(testBazelFileContent)
+        }.also { LocalFileSystem.getInstance().refreshAndFindFileByNioFile(it) }
+    }
 }
