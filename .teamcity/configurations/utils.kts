4a1bcca917694d9208880d7ec227bc80a8e59486
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2024-12-26 11:11:11 +0400

    update config TC for remote cache 3

--- .teamcity/configurations/utils.kts
+++ .teamcity/configurations/utils.kts
@@ -17,7 +17,8 @@ object DockerParams {
 
 object CommonParams {
   val BazelTestlogsArtifactRules: String = "+:%system.teamcity.build.checkoutDir%/testlogs/** => testlogs.zip"
-  val BazelCiSpecificArgs: String = "--test_output=errors --announce_rc --show_progress_rate_limit=30 --curses=yes --terminal_columns=140"
+  val BazelCiSpecificArgs: String = " --config=nocacheupload --config:bes --test_output=errors --announce_rc --show_progress_rate_limit=30 --curses=yes --terminal_columns=140"
+  val BazelCiBuildSpecificArgs: String = "--config:bes --test_output=errors --announce_rc --show_progress_rate_limit=30 --curses=yes --terminal_columns=140"
 
   val DockerE2eImage: String = "registry.jetbrains.team/p/bazel/docker/hirschgarten-e2e:latest"
   val DockerQodanaImage: String = "registry.jetbrains.team/p/bazel/docker-private/hirschgarten-qodana:latest"
