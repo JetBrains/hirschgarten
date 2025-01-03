73a69d166bdc9e4c5b3d82a28e588623d1f0b8c9
Stanislaw Solarewicz <stanislaw.solarewicz@jetbrains.com>
2025-01-03 18:41:46 +0100

    parameter completion for java

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/completion/BazelNativeRulesArgumentCompletionContributor.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/completion/BazelNativeRulesArgumentCompletionContributor.kt
@@ -5,13 +5,13 @@ import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.CompletionType
-import com.intellij.codeInsight.completion.util.ParenthesesInsertHandler
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PatternCondition
 import com.intellij.patterns.PlatformPatterns.psiComment
 import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.patterns.PlatformPatterns.psiFile
+import com.intellij.psi.util.findParentOfType
 import com.intellij.util.PlatformIcons
 import com.intellij.util.ProcessingContext
 import org.jetbrains.bazel.languages.starlark.StarlarkLanguage
@@ -19,44 +19,59 @@ import org.jetbrains.bazel.languages.starlark.bazel.BazelFileType
 import org.jetbrains.bazel.languages.starlark.bazel.BazelNativeRules
 import org.jetbrains.bazel.languages.starlark.elements.StarlarkTokenTypes
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkFile
-import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkReferenceExpression
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkCallExpression
+import org.jetbrains.bazel.languages.starlark.psi.expressions.arguments.StarlarkArgumentExpression
 
-class BazelNativeRulesCompletionContributor : CompletionContributor() {
+class BazelNativeRulesArgumentCompletionContributor : CompletionContributor() {
   init {
     extend(
       CompletionType.BASIC,
       bazelBuildElement(),
-      BazelBuildNativeRulesCompletionProvider(),
+      BazelBuildNativeRulesArgumentCompletionProvider(),
     )
   }
 
   private fun bazelBuildElement() =
     psiElement()
       .withLanguage(StarlarkLanguage)
-      .withParent(StarlarkReferenceExpression::class.java)
-      .withSuperParent(3, StarlarkFile::class.java)
+      .inFile(psiFile(StarlarkFile::class.java).with(bazelFileTypeCondition()))
       .andNot(psiComment())
       .andNot(psiElement().afterLeaf(psiElement(StarlarkTokenTypes.DOT)))
-      .inFile(psiFile(StarlarkFile::class.java).with(bazelFileTypeCondition()))
+      .andNot(psiElement().afterLeaf(psiElement(StarlarkTokenTypes.INT)))
+      .inside(psiElement(StarlarkCallExpression::class.java))
+      .andOr(
+        psiElement().withSuperParent(2, StarlarkCallExpression::class.java),
+        psiElement()
+          .andOr(
+            psiElement().withSuperParent(2, StarlarkArgumentExpression::class.java),
+            psiElement().withParent(StarlarkArgumentExpression::class.java),
+          ).andNot(psiElement().afterLeaf(psiElement(StarlarkTokenTypes.EQ)))
+          .andNot(
+            psiElement()
+              .afterLeaf(psiElement(StarlarkTokenTypes.IDENTIFIER)),
+          ),
+      )
 
   private fun bazelFileTypeCondition() =
     object : PatternCondition<StarlarkFile>("withBazelFileType") {
       override fun accepts(file: StarlarkFile, context: ProcessingContext): Boolean = file.getBazelFileType() == BazelFileType.BUILD
     }
 
-  private class BazelBuildNativeRulesCompletionProvider : CompletionProvider<CompletionParameters>() {
+  private class BazelBuildNativeRulesArgumentCompletionProvider : CompletionProvider<CompletionParameters>() {
     override fun addCompletions(
       parameters: CompletionParameters,
       context: ProcessingContext,
       result: CompletionResultSet,
     ) {
-      BazelNativeRules.GetRuleNames().forEach { result.addElement(functionLookupElement(it)) }
+      val starlarkCallExpression = parameters.position.findParentOfType<StarlarkCallExpression>() ?: return
+      val functionName = starlarkCallExpression.firstChild.text
+      val arguments = BazelNativeRules.NATIVE_RULES_MAP[functionName] ?: return
+      arguments.forEach{result.addElement(functionLookupElement(it))}
     }
 
     private fun functionLookupElement(name: String): LookupElement =
       LookupElementBuilder
         .create(name)
-        .withInsertHandler(ParenthesesInsertHandler.NO_PARAMETERS)
-        .withIcon(PlatformIcons.FUNCTION_ICON)
+        .withIcon(PlatformIcons.PARAMETER_ICON)
   }
 }
