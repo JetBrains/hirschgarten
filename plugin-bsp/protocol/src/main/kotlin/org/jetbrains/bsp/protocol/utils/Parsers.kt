9ecaa8b6244013128dd7946dc32fcee06ee705bd
Lev Leontev <lev.leontev@jetbrains.com>
2025-01-07 16:25:11 +0000

    [fix] Optimize parseBspConnectionDetails a bit more | #BAZEL-1325 Done
    GitOrigin-RevId: c3f668492e7020bfaba5d7315fc632c0c40dd172

--- plugin-bsp/protocol/src/main/kotlin/org/jetbrains/bsp/protocol/utils/Parsers.kt
+++ plugin-bsp/protocol/src/main/kotlin/org/jetbrains/bsp/protocol/utils/Parsers.kt
@@ -5,14 +5,19 @@ import com.google.gson.Gson
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.isFile
-import com.intellij.openapi.vfs.readText
+import org.jetbrains.bsp.protocol.BSP_CONNECTION_DIR
+import kotlin.io.path.bufferedReader
 
-fun VirtualFile.parseBspConnectionDetails(): BspConnectionDetails? =
+fun VirtualFile.parseBspConnectionDetails(): BspConnectionDetails? {
   try {
-    if (isJsonFile()) Gson().fromJson(this.readText(), BspConnectionDetails::class.java) else null
+    if (!isBspConnectionFile()) return null
+    return this.toNioPath().bufferedReader().use { reader ->
+      Gson().fromJson(reader, BspConnectionDetails::class.java)
+    }
   } catch (e: Exception) {
     thisLogger().warn("Parsing file '$this' to BspConnectionDetails failed!", e)
-    null
+    return null
   }
+}
 
-private fun VirtualFile.isJsonFile() = isFile && extension == "json"
+private fun VirtualFile.isBspConnectionFile() = isFile && extension == "json" && parent?.name == BSP_CONNECTION_DIR
