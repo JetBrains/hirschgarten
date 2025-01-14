cc2c17c13507f74f5c9c0e816fa34dcc457137e9
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2025-01-14 11:43:53 +0000

    [feature] automatically add module dependency to `BUILD` file | #BAZEL-1259
    Caveat: For this first version, it only works when deps list is available in the rule declaration and is a simple literal list.
    
    GitOrigin-RevId: dbaea160feb57eb1067c9d8e07160f14e5bad8b0

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/modify/BazelProjectModelModifier.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/modify/BazelProjectModelModifier.kt
@@ -1,16 +1,28 @@
 package org.jetbrains.bazel.flow.modify
 
 import com.intellij.ide.util.EditorHelper
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.roots.DependencyScope
 import com.intellij.openapi.roots.ExternalLibraryDescriptor
 import com.intellij.openapi.roots.JavaProjectModelModifier
 import com.intellij.openapi.roots.libraries.Library
 import com.intellij.pom.java.LanguageLevel
+import org.jetbrains.bazel.commons.label.Label
+import org.jetbrains.bazel.config.BazelPluginBundle
+import org.jetbrains.bazel.languages.starlark.psi.StarlarkFile
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkListLiteralExpression
 import org.jetbrains.bazel.ui.widgets.findBuildFile
 import org.jetbrains.concurrency.Promise
 import org.jetbrains.concurrency.resolvedPromise
+import org.jetbrains.plugins.bsp.coroutines.BspCoroutineService
+import org.jetbrains.plugins.bsp.impl.flow.sync.PartialProjectSync
+import org.jetbrains.plugins.bsp.impl.flow.sync.ProjectSyncTask
 import org.jetbrains.plugins.bsp.target.temporaryTargetUtils
+import org.jetbrains.plugins.bsp.ui.notifications.BspBalloonNotifier
+
+private val log = logger<BazelProjectModelModifier>()
 
 class BazelProjectModelModifier : JavaProjectModelModifier() {
   override fun addModuleDependency(
@@ -19,8 +31,52 @@ class BazelProjectModelModifier : JavaProjectModelModifier() {
     scope: DependencyScope,
     exported: Boolean,
   ): Promise<Void>? {
-    from.jumpToBuildFile()
+    if (!tryAddingModuleDependencyToBuildFile(from, to)) {
+      from.jumpToBuildFile()
+      notifyAutomaticDependencyAdditionFailure()
+    }
+
     return resolvedPromise()
+  }
+
+  private fun tryAddingModuleDependencyToBuildFile(from: Module, to: Module): Boolean {
+    val fromBuildTargetInfo = from.project.temporaryTargetUtils.getBuildTargetInfoForModule(from) ?: return false
+    val toBuildTargetInfo = to.project.temporaryTargetUtils.getBuildTargetInfoForModule(to) ?: return false
+    val targetBuildFile = findBuildFile(from.project, fromBuildTargetInfo) as? StarlarkFile ?: return false
+    val targetRuleLabel = Label.parseOrNull(fromBuildTargetInfo.id.uri) ?: return false
+    val ruleTarget = targetBuildFile.findRuleTarget(targetRuleLabel.targetName) ?: return false
+    val depsList =
+      ruleTarget
+        .getArgumentList()
+        ?.getDepsArgument()
+        ?.children
+        ?.first { it is StarlarkListLiteralExpression } as? StarlarkListLiteralExpression
+        ?: return false
+    var insertSuccessful = false
+    try {
+      WriteCommandAction.runWriteCommandAction(from.project) {
+        depsList.insertString(toBuildTargetInfo.buildTargetName.trimStart('@'))
+        insertSuccessful = true
+      }
+    } catch (_: Exception) {
+      log.warn("Failed to write to BUILD file")
+    }
+
+    val syncScope = PartialProjectSync(targetsToSync = listOf(fromBuildTargetInfo.id))
+    if (insertSuccessful) {
+      BspCoroutineService.getInstance(from.project).start {
+        ProjectSyncTask(from.project).sync(syncScope = syncScope, buildProject = false)
+      }
+      return true
+    }
+    return false
+  }
+
+  private fun notifyAutomaticDependencyAdditionFailure() {
+    BspBalloonNotifier.warn(
+      BazelPluginBundle.message("balloon.add.target.dependency.to.build.file.failed.title"),
+      BazelPluginBundle.message("balloon.add.target.dependency.to.build.file.failed.message"),
+    )
   }
 
   override fun addLibraryDependency(
