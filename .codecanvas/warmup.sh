431f24900a49208c4c3e23bed9cc8f33f33e618f
Justin Kaeser <Justin.Kaeser@jetbrains.com>
2025-01-24 10:10:48 +0000

    disable cache in warmup?

--- .codecanvas/warmup.sh
+++ .codecanvas/warmup.sh
@@ -1,2 +1,5 @@
-bazel build //... --keep_going --config=remotecache --config=nocacheupload || true
-bazel test //... --keep_going --config=remotecache --config=nocacheupload || true
+#bazel build //... --keep_going --config=remotecache --config=nocacheupload || true
+#bazel test //... --keep_going --config=remotecache --config=nocacheupload || true
+
+bazel build //... --keep_going  || true
+bazel test //... --keep_going || true
