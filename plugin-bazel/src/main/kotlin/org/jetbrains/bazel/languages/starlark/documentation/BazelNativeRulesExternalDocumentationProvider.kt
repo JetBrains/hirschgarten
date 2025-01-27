f048f6ec9c316c707b0bbf75856707ff50c8512a
Stanislaw Solarewicz <stanislaw.solarewicz@jetbrains.com>
2025-01-27 15:38:18 +0100

    boilerplate documentation target

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/documentation/BazelNativeRulesExternalDocumentationProvider.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/documentation/BazelNativeRulesExternalDocumentationProvider.kt
@@ -1,12 +1,42 @@
 package org.jetbrains.bazel.languages.starlark.documentation
 
+import com.intellij.codeInsight.navigation.targetPresentation
+import com.intellij.model.Pointer
+import com.intellij.platform.backend.documentation.DocumentationResult
 import com.intellij.platform.backend.documentation.DocumentationTarget
 import com.intellij.platform.backend.documentation.PsiDocumentationTargetProvider
+import com.intellij.platform.backend.presentation.TargetPresentation
 import com.intellij.psi.PsiElement
+import com.intellij.psi.createSmartPointer
+import org.jetbrains.bazel.languages.bazelrc.BazelrcLanguage
+import org.jetbrains.bazel.languages.starlark.StarlarkLanguage
 
+class BazelNativeRulesDocumentationTarget(val element: PsiElement, private val originalElement: PsiElement?) : DocumentationTarget {
+  override fun createPointer(): Pointer<out DocumentationTarget> {
+    val elementPtr = element.createSmartPointer()
+    val originalElementPtr = originalElement?.createSmartPointer()
+    return Pointer {
+      val element = elementPtr.dereference() ?: return@Pointer null
+      BazelNativeRulesDocumentationTarget(element, originalElementPtr?.dereference())
+    }
+  }
+
+  override fun computePresentation(): TargetPresentation {
+    return targetPresentation(element)
+  }
+
+  override fun computeDocumentation(): DocumentationResult? {
+    val html = "TEST"
+    return DocumentationResult.documentation(html)
+  }
+}
 
 class BazelNativeRulesExternalDocumentationProvider : PsiDocumentationTargetProvider {
   override fun documentationTarget(element: PsiElement, originalElement: PsiElement?): DocumentationTarget? {
-    return super.documentationTarget(element, originalElement)
+    return if (element.language.`is`(StarlarkLanguage) || element.language.`is`(BazelrcLanguage)) {
+      BazelNativeRulesDocumentationTarget(element, originalElement)
+    } else {
+      null
+    }
   }
 }
