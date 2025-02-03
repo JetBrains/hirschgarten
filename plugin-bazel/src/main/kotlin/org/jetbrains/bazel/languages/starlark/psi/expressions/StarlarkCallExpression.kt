4b40af13f33b1b21693c898c32259b725ec80dce
Stanislaw Solarewicz <stanislaw.solarewicz@jetbrains.com>
2025-02-03 17:20:21 +0100

    implement external documentation

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/StarlarkCallExpression.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/StarlarkCallExpression.kt
@@ -1,14 +1,17 @@
 package org.jetbrains.bazel.languages.starlark.psi.expressions
 
 import com.intellij.lang.ASTNode
+import com.intellij.model.psi.PsiSymbolReference
 import com.intellij.psi.PsiReference
 import org.jetbrains.bazel.languages.starlark.elements.StarlarkElementTypes
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkBaseElement
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkElementVisitor
 import org.jetbrains.bazel.languages.starlark.psi.functions.StarlarkArgumentList
+import org.jetbrains.bazel.languages.starlark.references.BazelNativeRuleReference
 import org.jetbrains.bazel.languages.starlark.references.StarlarkFunctionCallReference
 import org.jetbrains.kotlin.idea.base.psi.relativeTo
 
+@Suppress("UnstableApiUsage")
 class StarlarkCallExpression(node: ASTNode) : StarlarkBaseElement(node) {
   override fun acceptVisitor(visitor: StarlarkElementVisitor) = visitor.visitCallExpression(this)
 
@@ -27,4 +30,7 @@ class StarlarkCallExpression(node: ASTNode) : StarlarkBaseElement(node) {
   fun getTargetName(): String? = getArgumentList()?.getNameArgumentValue()
 
   fun getArgumentList(): StarlarkArgumentList? = findChildrenByClass(StarlarkArgumentList::class.java).firstOrNull()
+
+  override fun getOwnReferences(): Collection<BazelNativeRuleReference> =
+    name?.let { listOf(BazelNativeRuleReference(this)) } ?: emptyList()
 }
