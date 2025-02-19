1c5215a68a83044e30d5b996ff2990743a02aab0
Lev Leontev <lev.leontev@jetbrains.com>
2025-02-19 12:12:41 +0100

    Last BSP version

--- plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/startup/BspStartupActivity.kt
+++ plugin-bsp/src/main/kotlin/org/jetbrains/plugins/bsp/startup/BspStartupActivity.kt
@@ -1,137 +1,36 @@
 package org.jetbrains.plugins.bsp.startup
 
-import com.intellij.build.events.impl.FailureResultImpl
-import com.intellij.openapi.application.AppUIExecutor
-import com.intellij.openapi.diagnostic.logger
+import com.intellij.ide.plugins.IdeaPluginDescriptor
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.ide.plugins.newui.MyPluginModel
+import com.intellij.notification.NotificationAction
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.impl.CloseProjectWindowHelper
-import org.jetbrains.plugins.bsp.config.BspFeatureFlags
-import org.jetbrains.plugins.bsp.config.BspPluginBundle
-import org.jetbrains.plugins.bsp.config.buildToolId
-import org.jetbrains.plugins.bsp.config.isBspProjectInitialized
-import org.jetbrains.plugins.bsp.config.isBspProjectLoaded
-import org.jetbrains.plugins.bsp.config.openedTimesSinceLastStartupResync
-import org.jetbrains.plugins.bsp.config.rootDir
-import org.jetbrains.plugins.bsp.config.workspaceModelLoadedFromCache
-import org.jetbrains.plugins.bsp.projectAware.BspWorkspace
-import org.jetbrains.plugins.bsp.server.connection.connectionDetailsProvider
-import org.jetbrains.plugins.bsp.sync.scope.SecondPhaseSync
-import org.jetbrains.plugins.bsp.sync.task.PhasedSync
-import org.jetbrains.plugins.bsp.sync.task.ProjectSyncTask
-import org.jetbrains.plugins.bsp.target.targetUtils
-import org.jetbrains.plugins.bsp.ui.console.BspConsoleService
-import org.jetbrains.plugins.bsp.ui.widgets.fileTargets.updateBspFileTargetsWidget
-import org.jetbrains.plugins.bsp.ui.widgets.tool.window.all.targets.registerBspToolWindow
-import org.jetbrains.plugins.bsp.utils.RunConfigurationProducersDisabler
-import java.util.Collections
-import java.util.WeakHashMap
+import com.intellij.openapi.startup.ProjectActivity
 
-private val log = logger<BspStartupActivity>()
+private const val NOTIFICATION_GROUP = "Disable BSP plugin"
 
-// Use WeakHashMap to avoid leaking the Project instance
-private val executedForProject: MutableSet<Project> =
-  Collections.synchronizedSet(Collections.newSetFromMap(WeakHashMap()))
-
-/**
- * Runs actions after the project has started up and the index is up-to-date.
- *
- * @see BspProjectOpenProcessor for additional actions that
- * may run when a project is being imported for the first time.
- */
-class BspStartupActivity : BspProjectActivity() {
-  override suspend fun Project.executeForBspProject() {
-    if (startupActivityExecutedAlready()) {
-      log.info("BSP startup activity executed already for project: $this")
-      return
-    }
-    log.info("Executing BSP startup activity for project: $this")
-    BspStartupActivityTracker.startConfigurationPhase(this)
-    executeEveryTime()
-
-    executeForNewProject()
-
-    resyncProjectIfNeeded()
-
-    updateProjectProperties()
-
-    BspStartupActivityTracker.stopConfigurationPhase(this)
+class BspStartupActivity : ProjectActivity {
+  override suspend fun execute(project: Project) {
+    val bspPlugin = PluginManagerCore.loadedPlugins.first { it.pluginId.idString == "org.jetbrains.bsp" }
+    NotificationGroupManager
+      .getInstance()
+      .getNotificationGroup(NOTIFICATION_GROUP)
+      .createNotification(
+        "BSP and Bazel plugins have been merged",
+        "Please use the Bazel (EAP) plugin instead from now on. The BSP plugin will no longer be updated.",
+        NotificationType.INFORMATION,
+      ).addAction(
+        NotificationAction.createExpiring("Disable the Build Server Procotol (BSP) plugin") { _, _ ->
+          disablePlugins(project, listOf(bspPlugin))
+        },
+      ).notify(project)
   }
 
-  /**
-   * Make sure calling [BazelBspOpenProjectProvider.performOpenBazelProjectViaBspPlugin]
-   * won't cause [BspStartupActivity] to execute twice.
-   */
-  private fun Project.startupActivityExecutedAlready(): Boolean = !executedForProject.add(this)
-
-  private suspend fun Project.executeEveryTime() {
-    log.debug("Executing BSP startup activities for every opening")
-    registerBspToolWindow(this)
-    updateBspFileTargetsWidget()
-    RunConfigurationProducersDisabler(this)
-    BspWorkspace.getInstance(this).initialize()
-  }
-
-  private suspend fun Project.executeForNewProject() {
-    log.debug("Executing BSP startup activities only for new project")
-    try {
-      if (!isBspProjectLoaded) {
-        runOnFirstOpening()
-      }
-    } catch (e: Exception) {
-      val bspSyncConsole = BspConsoleService.getInstance(this).bspSyncConsole
-      log.info("BSP sync has failed", e)
-      bspSyncConsole.startTask(
-        taskId = "bsp-pre-import",
-        title = BspPluginBundle.message("console.task.pre.import.title"),
-        message = BspPluginBundle.message("console.task.pre.import.in.progress"),
-      )
-      bspSyncConsole.finishTask(
-        taskId = "bsp-pre-import",
-        message = BspPluginBundle.message("console.task.pre.import.failed"),
-        result = FailureResultImpl(e),
-      )
-    }
-  }
-
-  private suspend fun Project.runOnFirstOpening() {
-    val wasFirstOpeningSuccessful = connectionDetailsProvider.onFirstOpening(this, rootDir)
-    log.debug("Was onFirstOpening successful: $wasFirstOpeningSuccessful")
-
-    if (!wasFirstOpeningSuccessful) {
-      handleFailedFirstOpening()
-    }
-  }
-
-  private fun Project.handleFailedFirstOpening() {
-    log.info("Cancelling BSP sync. Closing the project window")
-    // TODO https://youtrack.jetbrains.com/issue/BAZEL-623
-    AppUIExecutor.onUiThread().execute {
-      CloseProjectWindowHelper().windowClosing(this)
-    }
-  }
-
-  private suspend fun Project.resyncProjectIfNeeded() {
-    if (isProjectInIncompleteState()) {
-      // TODO: https://youtrack.jetbrains.com/issue/BAZEL-1555
-      if (BspFeatureFlags.isPhasedSync && buildToolId.id == "bazelbsp") {
-        log.info("Running BSP phased sync task")
-        PhasedSync(this).sync()
-      } else {
-        log.info("Running BSP sync task")
-        ProjectSyncTask(this).sync(
-          syncScope = SecondPhaseSync,
-          buildProject = BspFeatureFlags.isBuildProjectOnSyncEnabled,
-        )
-      }
-
-      openedTimesSinceLastStartupResync = 0
-    }
-  }
-
-  private fun Project.isProjectInIncompleteState() = targetUtils.allTargetIds().isEmpty() || !workspaceModelLoadedFromCache
-
-  private fun Project.updateProjectProperties() {
-    isBspProjectInitialized = true
-    openedTimesSinceLastStartupResync += 1
+  private fun disablePlugins(project: Project, plugins: List<IdeaPluginDescriptor>) {
+    val model = MyPluginModel(project)
+    model.disable(plugins)
+    model.apply(null)
   }
 }
