5d92e3613e8a7f57cbb59810ed981961913a3629
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2025-02-19 13:48:51 +0100

    Revert rules_intellij changes

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/intellij/CopyPluginToSandboxBeforeRunTaskProvider.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/intellij/CopyPluginToSandboxBeforeRunTaskProvider.kt
@@ -1,29 +1,30 @@
 package org.jetbrains.bazel.intellij
 
-import com.google.devtools.intellij.plugin.IntellijPluginDeployTargetInfo.IntellijPluginDeployInfo
-import com.google.protobuf.TextFormat
 import com.intellij.execution.BeforeRunTask
 import com.intellij.execution.BeforeRunTaskProvider
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.components.service
+import com.intellij.openapi.roots.OrderEnumerator
+import com.intellij.openapi.roots.OrderRootType
 import com.intellij.openapi.util.Key
 import org.jetbrains.bazel.config.BspPluginBundle
 import org.jetbrains.bazel.label.label
 import org.jetbrains.bazel.run.config.BspRunConfiguration
+import org.jetbrains.bazel.target.TargetUtils
+import org.jetbrains.bazel.target.getModule
 import org.jetbrains.bazel.ui.notifications.BspBalloonNotifier
 import java.io.IOException
-import java.io.InputStreamReader
-import java.nio.charset.StandardCharsets.UTF_8
-import java.nio.file.Files
+import java.net.URI
 import java.nio.file.Path
-import kotlin.io.path.Path
 import kotlin.io.path.copyTo
 import kotlin.io.path.createDirectories
 import kotlin.io.path.exists
+import kotlin.io.path.name
+import kotlin.io.path.toPath
 
 private val PROVIDER_ID = Key.create<CopyPluginToSandboxBeforeRunTaskProvider.Task>("CopyPluginToSandboxBeforeRunTaskProvider")
-private val DEPLOY_INFO_FILE_EXTENSION = ".intellij-plugin-debug-target-deploy-info"
 
 public class CopyPluginToSandboxBeforeRunTaskProvider : BeforeRunTaskProvider<CopyPluginToSandboxBeforeRunTaskProvider.Task>() {
   public class Task : BeforeRunTask<Task>(PROVIDER_ID)
@@ -52,57 +53,39 @@ public class CopyPluginToSandboxBeforeRunTaskProvider : BeforeRunTaskProvider<Co
         "INTELLIJ_PLUGIN_SANDBOX_KEY must be passed"
       }
 
-    val deployFiles = mutableMapOf<Path, Path>()
+    val pluginJars = mutableListOf<Path>()
 
-    val projectPath = Path(configuration.project.basePath!!)
-    val target = runConfiguration.targets.single()
-    val uriToPath =
-      target.uri
-        .removePrefix("@")
-        .removePrefix("/")
-        .removePrefix("/")
-        .replace(":", "/")
-
-    // convert uri to a project-local path pointing to the uri + DEPLOY_INFO_FILE_EXTENSION
-    val path = projectPath.resolve("bazel-bin").resolve(Path.of(uriToPath))
-    val jarPath = path.resolveSibling(path.fileName.toString() + DEPLOY_INFO_FILE_EXTENSION)
-
-    if (jarPath.exists()) {
-      val info =
-        Files.newInputStream(jarPath).use { inputStream ->
-          val builder = IntellijPluginDeployInfo.newBuilder()
-          val parser =
-            TextFormat.Parser
-              .newBuilder()
-              .setAllowUnknownFields(true)
-              .build()
-          parser.merge(InputStreamReader(inputStream, UTF_8), builder)
-          builder.build()
-        }
-      for (file in info.deployFilesList) {
-        val executionPath = Path.of(file.executionPath)
-        val deployLocation = Path.of(file.deployLocation)
-        deployFiles.put(projectPath.resolve(executionPath), pluginSandbox.resolve(deployLocation))
+    for (target in runConfiguration.targets) {
+      val targetInfo = configuration.project.service<TargetUtils>().getBuildTargetInfoForLabel(target.label())
+      val module = targetInfo?.getModule(environment.project) ?: continue
+      OrderEnumerator.orderEntries(module).librariesOnly().recursively().withoutSdk().forEachLibrary { library ->
+        // Use URLs directly because getFiles will be empty until VFS refresh.
+        library
+          .getUrls(OrderRootType.CLASSES)
+          .mapNotNull { "file://" + it.removePrefix("jar://").removeSuffix("!/") }
+          .map { URI.create(it).toPath() }
+          .forEach { pluginJars.add(it) }
+        true
       }
     }
 
-    if (deployFiles.isEmpty()) {
-      showError(jarPath.toString() + BspPluginBundle.message("console.task.exception.no.plugin.jars"))
+    if (pluginJars.isEmpty()) {
+      showError(BspPluginBundle.message("console.task.exception.no.plugin.jars"))
       return false
     }
-    for ((deployFile, target) in deployFiles) {
-      if (!deployFile.exists()) {
-        showError(BspPluginBundle.message("console.task.exception.plugin.jar.not.found", deployFile))
+    for (pluginJar in pluginJars) {
+      if (!pluginJar.exists()) {
+        showError(BspPluginBundle.message("console.task.exception.plugin.jar.not.found", pluginJar))
         return false
       }
       try {
         pluginSandbox.createDirectories()
-        deployFile.copyTo(target, overwrite = true)
+        pluginJar.copyTo(pluginSandbox.resolve(pluginJar.name), overwrite = true)
       } catch (e: IOException) {
         val errorMessage =
           BspPluginBundle.message(
             "console.task.exception.plugin.jar.could.not.copy",
-            deployFile,
+            pluginJar,
             pluginSandbox,
             e.message.orEmpty(),
           )
