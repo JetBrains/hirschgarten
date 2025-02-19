1a5a5909dce98a8450e86b92394d72e6644501ac
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2025-02-19 11:04:16 +0100

    Fix

--- server/server/src/main/kotlin/org/jetbrains/bsp/bazel/server/bsp/TelemetryContextPropagatingLauncherBuilder.kt
+++ server/server/src/main/kotlin/org/jetbrains/bsp/bazel/server/bsp/TelemetryContextPropagatingLauncherBuilder.kt
@@ -1,8 +1,8 @@
 package org.jetbrains.bsp.bazel.server.bsp
 
-import io.opentelemetry.api.incubator.propagation.ExtendedContextPropagators
 import io.opentelemetry.context.Context
 import io.opentelemetry.context.propagation.ContextPropagators
+import io.opentelemetry.context.propagation.TextMapGetter
 import org.eclipse.lsp4j.jsonrpc.Launcher
 import org.eclipse.lsp4j.jsonrpc.MessageIssueHandler
 import org.eclipse.lsp4j.jsonrpc.json.MessageJsonHandler
@@ -25,20 +25,28 @@ class TelemetryContextPropagatingLauncherBuilder<T> : Launcher.Builder<T>() {
   }
 }
 
-internal class CaseInsensitiveMap : HashMap<String?, String?> {
-  constructor(carrier: Map<String?, String?>?) {
-    if (carrier != null) {
-      this.putAll(carrier)
-    }
+val TEXT_MAP_GETTER: TextMapGetter<Map<String, String>> = object : TextMapGetter<Map<String, String>> {
+  override fun keys(carrier: Map<String, String>): Set<String> {
+    return carrier.keys
   }
 
-  override fun put(key: String?, value: String?): String? = super.put(getKeyLowerCase(key!!), value)
+  override fun get(carrier: Map<String, String>?, key: String): String? {
+    return carrier?.get(key)
+  }
+}
 
-  override fun putAll(m: Map<out String?, String?>) {
-    m.forEach { (key: String?, value: String?) -> this.put(key, value) }
+internal class CaseInsensitiveMap : HashMap<String, String> {
+  constructor(carrier: Map<String, String>) {
+    this.putAll(carrier)
   }
 
-  override fun get(key: String?): String? = super.get(getKeyLowerCase(key!!))
+  override fun put(key: String, value: String): String? = super.put(getKeyLowerCase(key), value)
+
+  override fun putAll(m: Map<out String, String>) {
+    m.forEach { (key: String, value: String) -> this.put(key, value) }
+  }
+
+  override fun get(key: String): String? = super.get(getKeyLowerCase(key))
 
   companion object {
     private fun getKeyLowerCase(key: String): String = key.lowercase()
@@ -58,17 +66,14 @@ private class TelemetryContextPropagatingStreamMessageProducer(
     super.parseHeader(line, headers)
   }
 
-  fun extractTextMapPropagationContext(carrier: Map<String?, String?>?, propagators: ContextPropagators): Context? {
+  fun extractTextMapPropagationContext(carrier: Map<String, String>, propagators: ContextPropagators): Context {
     val current = Context.current()
-    if (carrier == null) {
-      return current
-    }
     val caseInsensitiveMap = CaseInsensitiveMap(carrier)
     return propagators.textMapPropagator
-      .extract(current, caseInsensitiveMap, ExtendedContextPropagators.TEXT_MAP_GETTER)
+      .extract(current, caseInsensitiveMap, TEXT_MAP_GETTER)
   }
 
-  override fun handleMessage(input: InputStream?, headers: Headers?): Boolean {
+  override fun handleMessage(input: InputStream, headers: Headers): Boolean {
     if (!openTelemetryInitialized) {
       // The first call via BSP is build/initialize, at which point the telemetry isn't initialized yet.
       return super.handleMessage(input, headers)
