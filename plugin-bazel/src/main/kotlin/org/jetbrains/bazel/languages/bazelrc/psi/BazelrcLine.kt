93952735503d0d669fc19312d2ecdf16d8162342
Mihai Claudiu Toader <mtoader@gmail.com>
2025-02-24 04:40:31 -0800

    [feature] Add quick fixes to old and no-op flags | #BAZEL-1704 Done (#224)
    Add some quick fixes to deprecated and old flag names. With tests 
    
    This still uses the 7.4.1 flags db so it might be incorrect on newer
    bazel. I need to get access to the bazel version in order to use a
    accurate db.

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelrc/psi/BazelrcLine.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelrc/psi/BazelrcLine.kt
@@ -8,8 +8,14 @@ import org.jetbrains.bazel.languages.bazelrc.elements.BazelrcTokenTypes
 class BazelrcLine(node: ASTNode) : BazelrcBaseElement(node) {
   override fun acceptVisitor(visitor: BazelrcElementVisitor) = visitor.visitLine(this)
 
-  val config: String? = this.findChildByType<PsiElement>(BazelrcTokenTypes.CONFIG)?.text
-  val command: String = this.findChildByType<PsiElement>(COMMAND_TOKENS)?.text ?: ""
+  val config: String?
+    get() = this.findChildByType<PsiElement>(BazelrcTokenTypes.CONFIG)?.text
+
+  val command: String
+    get() = this.findChildByType<PsiElement>(COMMAND_TOKENS)?.text ?: ""
+
+  val flags: Array<BazelrcFlag>
+    get() = this.findChildrenByClass(BazelrcFlag::class.java)
 
   companion object {
     val COMMAND_TOKENS = TokenSet.create(BazelrcTokenTypes.COMMAND)
