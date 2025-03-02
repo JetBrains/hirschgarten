73146269c0fbc192a149742ae0bddcbe4aecd311
Robert Grigoryan <robert.grigoryan@jetbrains.com>
2025-03-02 11:09:23 +0100

    Add TODOs

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/section/ProjectViewListSection.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/section/ProjectViewListSection.kt
@@ -2,6 +2,7 @@ package org.jetbrains.bazel.languages.projectview.section
 
 data class ProjectViewListSection(val name: String) {
   companion object {
+    // TODO: Add the sections
     val KEYWORD_MAP = emptyMap<String, ProjectViewListSection>()
   }
 }
--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/section/ProjectViewScalarSection.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/section/ProjectViewScalarSection.kt
@@ -2,6 +2,7 @@ package org.jetbrains.bazel.languages.projectview.section
 
 data class ProjectViewScalarSection(val name: String, val isFollowedByColon: Boolean) {
   companion object {
+    // TODO: Add the sections
     val KEYWORD_MAP = emptyMap<String, ProjectViewScalarSection>()
   }
 }
