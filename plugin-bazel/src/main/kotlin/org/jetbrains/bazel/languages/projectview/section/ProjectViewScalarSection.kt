b620028d006034940de33f738663da5f80abb042
Robert Grigoryan <robert.grigoryan@jetbrains.com>
2025-03-02 11:04:37 +0100

    Fix formatting

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/section/ProjectViewScalarSection.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/section/ProjectViewScalarSection.kt
@@ -1,9 +1,6 @@
 package org.jetbrains.bazel.languages.projectview.section
 
-data class ProjectViewScalarSection(
-  val name: String,
-  val isFollowedByColon: Boolean
-) {
+data class ProjectViewScalarSection(val name: String, val isFollowedByColon: Boolean) {
   companion object {
     val KEYWORD_MAP = emptyMap<String, ProjectViewScalarSection>()
   }
