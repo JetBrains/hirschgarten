4aa876c13a8800e9e55b1694d29303bfa1da62e6
Jiaming Tang <jiaming.tang@jetbrains.com>
2025-03-03 19:06:51 +0000

    [feature] make bazel external targets in consoles navigatable | #BAZEL-1598 Done
    GitOrigin-RevId: 3cb4ae59466b4f2cddc778c67b3abad05c5c4544

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/console/bazel/BazelBuildTargetConsoleFilter.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/console/bazel/BazelBuildTargetConsoleFilter.kt
@@ -2,36 +2,25 @@ package org.jetbrains.bazel.ui.console
 
 import com.intellij.execution.filters.ConsoleFilterProvider
 import com.intellij.execution.filters.Filter
-import com.intellij.execution.filters.HyperlinkInfo
 import com.intellij.execution.filters.OpenFileHyperlinkInfo
-import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.findFile
-import com.intellij.openapi.vfs.findFileOrDirectory
-import com.intellij.openapi.vfs.findPsiFile
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.descendantsOfType
 import org.jetbrains.bazel.config.isBspProject
-import org.jetbrains.bazel.config.rootDir
 import org.jetbrains.bazel.label.Label
-import org.jetbrains.bazel.languages.starlark.psi.expressions.arguments.StarlarkNamedArgumentExpression
-import org.jetbrains.bazel.languages.starlark.references.BUILD_FILE_NAMES
+import org.jetbrains.bazel.label.ResolvedLabel
+import org.jetbrains.bazel.languages.starlark.references.BazelLabelReference
 
 class BazelBuildTargetConsoleFilter(private val project: Project) : Filter {
   private val highlightGroupName = "highlightGroup"
-  private val externalRepoGroupName = "externalRepoGroup"
-  private val pathGroupName = "pathGroup"
-  private val targetName = "targetName"
 
   // The set of characters allowed in labels is taken from https://bazel.build/concepts/labels#target-names
   private val bazelTargetRegex =
     """(^|\W)
         (?<$highlightGroupName>
-        ((@|@@)(?<$externalRepoGroupName>[a-zA-Z0-9!%\-^_"&'()*+,;<=>?\[\]{|}~/.\#]*))? #@ or @@ with potential external repo names
-        //(?<$pathGroupName>[a-zA-Z0-9!%\-@^_"&'()*+,;<=>?\[\]{|}~/.\#]*):      #path
-        (?<$targetName>[a-zA-Z0-9!%\-@^_"&'()*+,;<=>?\[\]{|}~/.\#]+)         #target
+        ((@|@@)([a-zA-Z0-9!%\-^_"&'()*+,;<=>?\[\]{|}~/.\#]*))? #@ or @@ with potential external repo names
+        //([a-zA-Z0-9!%\-@^_"&'()*+,;<=>?\[\]{|}~/.\#]*):      #path
+        ([a-zA-Z0-9!%\-@^_"&'()*+,;<=>?\[\]{|}~/.\#]+)         #target
         )
     """.trimMargin()
       .toRegex(setOf(RegexOption.COMMENTS, RegexOption.MULTILINE))
@@ -43,44 +32,13 @@ class BazelBuildTargetConsoleFilter(private val project: Project) : Filter {
 
   private fun MatchResult.toFilterResultOrNull(line: String, entireLength: Int): Filter.Result? {
     val highlightGroup = groups[highlightGroupName] ?: return null
-    val pathGroup = groups[pathGroupName] ?: return null
-    val externalRepoGroup = groups[externalRepoGroupName]
-    val target = groups[targetName] ?: return null
-    if (externalRepoGroup != null && externalRepoGroup.value.isNotEmpty()) {
-      // skip targets in external repo
-      return null
-    }
-    val hyperLinkInfo = getHyperLinkInfo(project, pathGroup.value, target.value) ?: return null
+    val label = Label.parseOrNull(highlightGroup.value) as? ResolvedLabel ?: return null
+    val psi = BazelLabelReference.resolveLabel(project, label, null, false) ?: return null
+    val containingFile = psi.containingFile?.virtualFile ?: return null
+    val hyperLinkInfo = OpenFileHyperlinkInfo(project, containingFile, psi.calculateLineNumber() ?: 0, 0)
     val highlightStartOffset = entireLength - line.length + highlightGroup.range.first
     val highlightEndOffset = entireLength - line.length + highlightGroup.range.last + 1
-
     return Filter.Result(highlightStartOffset, highlightEndOffset, hyperLinkInfo)
-  }
-
-  private fun getHyperLinkInfo(
-    project: Project,
-    buildFileName: String,
-    target: String,
-  ): HyperlinkInfo? {
-    val virtualFile = buildFileName.toBazelFileInProject() ?: return null
-
-    return runReadAction {
-      val psiElement =
-        virtualFile
-          .findPsiFile(project)
-          ?.descendantsOfType<StarlarkNamedArgumentExpression>()
-          ?.filter { it.isNameArgument() }
-          ?.firstOrNull {
-            it.getArgumentStringValue()?.let { name -> Label.parseOrNull(name)?.targetName } == target
-          }
-      OpenFileHyperlinkInfo(project, virtualFile, psiElement?.calculateLineNumber() ?: 0, 0)
-    }
-  }
-
-  private fun String.toBazelFileInProject(): VirtualFile? {
-    val vf = project.rootDir.findFileOrDirectory(this)
-    if (vf == null || !vf.isDirectory) return null
-    return BUILD_FILE_NAMES.mapNotNull { vf.findFile(it) }.firstOrNull()
   }
 
   private fun PsiElement.calculateLineNumber(): Int? =
