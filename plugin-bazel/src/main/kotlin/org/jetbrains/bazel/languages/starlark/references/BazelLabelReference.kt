952190c96bcd61b41f81e595b22d4a129ded65c2
Jiaming Tang <jiaming.tang@jetbrains.com>
2025-03-03 19:06:51 +0000

    [feature] make bazel external targets in consoles navigatable | #BAZEL-1598 Done

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/BazelLabelReference.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/BazelLabelReference.kt
@@ -33,11 +33,8 @@ class BazelLabelReference(element: StarlarkStringLiteralExpression, soft: Boolea
     val project = element.project
     if (!project.isBspProject || isInNameArgument()) return null
     val label = Label.parseOrNull(element.getStringContents()) ?: return null
-    val buildOrSource = resolveBuildFileOrSourceFile(label) ?: return null
-    return when (buildOrSource) {
-      is BuildFilePsi -> resolveRuleTarget(buildOrSource.file, label) ?: resolveFileTarget(element.project, buildOrSource.file, label)
-      is SourceFile -> PsiManager.getInstance(project).findFile(buildOrSource.file)
-    }
+
+    return resolveLabel(project, label, element.containingFile.originalFile.virtualFile, true)
   }
 
   private fun isInNameArgument(): Boolean {
@@ -51,84 +48,123 @@ class BazelLabelReference(element: StarlarkStringLiteralExpression, soft: Boolea
 
   private class SourceFile(val file: VirtualFile) : BuildFileOrSourceFile
 
-  private fun resolveBuildFileOrSourceFile(label: Label): BuildFileOrSourceFile? =
-    when (label) {
-      is ResolvedLabel -> resolveAbsolutePackage(label)?.let { BuildFilePsi(it) }
-      else -> resolveRelativePackageOrSourceFile(label)
+  companion object {
+    fun resolveLabel(
+      project: Project,
+      label: Label,
+      containingFile: VirtualFile?,
+      allowFallbackToRootDir: Boolean = true,
+    ): PsiElement? {
+      val buildOrSource = resolveBuildFileOrSourceFile(project, label, containingFile, allowFallbackToRootDir) ?: return null
+      return when (buildOrSource) {
+        is BuildFilePsi -> resolveRuleTarget(buildOrSource.file, label) ?: resolveFileTarget(project, buildOrSource.file, label)
+        is SourceFile -> PsiManager.getInstance(project).findFile(buildOrSource.file)
+      }
     }
 
-  private fun resolveAbsolutePackage(label: ResolvedLabel): StarlarkFile? {
-    val project = element.project
-    val containingFile = element.containingFile.originalFile.virtualFile ?: return null
-    val packageDir = findReferredAbsolutePackage(project, containingFile, label) ?: return null
-    val buildFile = findBuildFile(packageDir) ?: return null
-    return findBuildFilePsi(project, buildFile)
-  }
+    private fun resolveBuildFileOrSourceFile(
+      project: Project,
+      label: Label,
+      containingFile: VirtualFile?,
+      allowFallbackToRootDir: Boolean = true,
+    ): BuildFileOrSourceFile? =
+      when (label) {
+        is ResolvedLabel ->
+          resolveAbsolutePackage(
+            project,
+            label,
+            containingFile,
+            allowFallbackToRootDir,
+          )?.let { BuildFilePsi(it) }
 
-  private fun resolveRelativePackageOrSourceFile(label: Label): BuildFileOrSourceFile? {
-    val project = element.project
-    val containingFile = element.containingFile.originalFile.virtualFile ?: return null
-    val containingPackage = findContainingPackage(containingFile) ?: return null
-    val referredPackage = containingPackage.findFileByRelativePath(label.packagePath.toString()) ?: return null
-    if (referredPackage.isFile) {
-      if (label.target is AmbiguousEmptyTarget) return SourceFile(referredPackage)
-      return null
-    }
-    val buildFile = findBuildFile(referredPackage) ?: return null
-    return findBuildFilePsi(project, buildFile)?.let { BuildFilePsi(it) }
-  }
-
-  private fun findContainingPackage(directory: VirtualFile?): VirtualFile? =
-    directory?.let {
-      if (findBuildFile(directory) != null) directory else findContainingPackage(directory.parent)
-    }
-
-  private fun resolveRuleTarget(buildFilePsi: StarlarkFile, label: Label): StarlarkCallExpression? =
-    buildFilePsi.findRuleTarget(label.targetName)
-
-  private fun resolveFileTarget(
-    project: Project,
-    buildFilePsi: StarlarkFile,
-    label: Label,
-  ): PsiFile? {
-    // buildFilePsi may be excluded, in which case we can't get the parent PsiDirectory.
-    // Therefore get virtualFile then parent and not vice-versa
-    val targetFile = buildFilePsi.virtualFile?.parent?.findFileByRelativePath(label.targetName) ?: return null
-    return PsiManager.getInstance(project).findFile(targetFile)
-  }
-
-  private fun findReferredAbsolutePackage(
-    project: Project,
-    containingFile: VirtualFile,
-    label: ResolvedLabel,
-  ): VirtualFile? {
-    val foundRepoRoot =
-      if (label.repoName.isEmpty()) {
-        findContainingBazelRepo(project, containingFile)
-      } else if (label.isApparent) {
-        project.apparentRepoNameToCanonicalName[label.repoName]?.let { canonicalRepoName ->
-          project.canonicalRepoNameToPath[canonicalRepoName]
-        }
-      } else {
-        project.canonicalRepoNameToPath[label.repoName]
+        else -> resolveRelativePackageOrSourceFile(project, label, containingFile)
       }
 
-    val repoRoot =
-      foundRepoRoot?.let {
-        VirtualFileManager.getInstance().refreshAndFindFileByNioPath(foundRepoRoot)
-      } ?: project.rootDir
+    private fun resolveAbsolutePackage(
+      project: Project,
+      label: ResolvedLabel,
+      containingFile: VirtualFile?,
+      allowFallbackToRootDir: Boolean,
+    ): StarlarkFile? {
+      val packageDir = findReferredAbsolutePackage(project, containingFile, label, allowFallbackToRootDir) ?: return null
+      val buildFile = findBuildFile(packageDir) ?: return null
+      return findBuildFilePsi(project, buildFile)
+    }
 
-    return repoRoot.findFileByRelativePath(label.packagePath.toString())
+    private fun resolveRelativePackageOrSourceFile(
+      project: Project,
+      label: Label,
+      containingFile: VirtualFile?,
+    ): BuildFileOrSourceFile? {
+      val containingPackage = findContainingPackage(containingFile) ?: return null
+      val referredPackage = containingPackage.findFileByRelativePath(label.packagePath.toString()) ?: return null
+      if (referredPackage.isFile) {
+        if (label.target is AmbiguousEmptyTarget) return SourceFile(referredPackage)
+        return null
+      }
+      val buildFile = findBuildFile(referredPackage) ?: return null
+      return findBuildFilePsi(project, buildFile)?.let { BuildFilePsi(it) }
+    }
+
+    private fun findContainingPackage(directory: VirtualFile?): VirtualFile? =
+      directory?.let {
+        if (findBuildFile(directory) != null) directory else findContainingPackage(directory.parent)
+      }
+
+    private fun resolveRuleTarget(buildFilePsi: StarlarkFile, label: Label): StarlarkCallExpression? =
+      buildFilePsi.findRuleTarget(label.targetName)
+
+    private fun resolveFileTarget(
+      project: Project,
+      buildFilePsi: StarlarkFile,
+      label: Label,
+    ): PsiFile? {
+      // buildFilePsi may be excluded, in which case we can't get the parent PsiDirectory.
+      // Therefore get virtualFile then parent and not vice-versa
+      val targetFile = buildFilePsi.virtualFile?.parent?.findFileByRelativePath(label.targetName) ?: return null
+
+      return PsiManager.getInstance(project).findFile(targetFile)
+    }
+
+    private fun findReferredAbsolutePackage(
+      project: Project,
+      containingFile: VirtualFile?,
+      label: ResolvedLabel,
+      allowFallbackToRootDir: Boolean,
+    ): VirtualFile? {
+      val foundRepoRoot =
+        if (label.repoName.isEmpty() && containingFile != null) {
+          findContainingBazelRepo(project, containingFile)
+        } else if (label.isApparent) {
+          project.apparentRepoNameToCanonicalName[label.repoName]?.let { canonicalRepoName ->
+            project.canonicalRepoNameToPath[canonicalRepoName]
+          }
+        } else {
+          project.canonicalRepoNameToPath[label.repoName]
+        }
+
+      val repoRoot =
+        foundRepoRoot?.let {
+          VirtualFileManager.getInstance().refreshAndFindFileByNioPath(foundRepoRoot)
+        } ?: if (allowFallbackToRootDir) {
+          project.rootDir
+        } else {
+          return null
+        }
+
+      return repoRoot.findFileByRelativePath(label.packagePath.toString())
+    }
+
+    private fun findContainingBazelRepo(project: Project, file: VirtualFile): Path? {
+      val path = file.toNioPath()
+      val repositoryPaths = project.repositoryPaths
+      return path.allAncestorsSequence().firstOrNull { it in repositoryPaths }
+    }
+
+    private fun findBuildFile(packageDir: VirtualFile): VirtualFile? =
+      BUILD_FILE_NAMES.mapNotNull { packageDir.findChild(it) }.firstOrNull()
+
+    private fun findBuildFilePsi(project: Project, buildFile: VirtualFile): StarlarkFile? =
+      PsiManager.getInstance(project).findFile(buildFile) as? StarlarkFile
   }
-
-  private fun findContainingBazelRepo(project: Project, file: VirtualFile): Path? {
-    val path = file.toNioPath()
-    val repositoryPaths = project.repositoryPaths
-    return path.allAncestorsSequence().firstOrNull { it in repositoryPaths }
-  }
-
-  private fun findBuildFile(packageDir: VirtualFile): VirtualFile? = BUILD_FILE_NAMES.mapNotNull { packageDir.findChild(it) }.firstOrNull()
-
-  private fun findBuildFilePsi(project: Project, buildFile: VirtualFile): StarlarkFile? =
-    PsiManager.getInstance(project).findFile(buildFile) as? StarlarkFile
 }
