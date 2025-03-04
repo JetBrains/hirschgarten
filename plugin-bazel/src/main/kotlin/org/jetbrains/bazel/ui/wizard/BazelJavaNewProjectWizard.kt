41f57c8c302c7899a4176a6d34220d443a984fac
Andrzej Gluszak <Andrzej.Gluszak@jetbrains.com>
2025-03-04 18:40:35 +0000

    fixup! cosmetics

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/wizard/BazelJavaNewProjectWizard.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/wizard/BazelJavaNewProjectWizard.kt
@@ -39,7 +39,7 @@ class BazelJavaNewProjectWizard : BuildSystemJavaNewProjectWizard {
 
     companion object {
       const val BAZEL_VERSION = "8.1.1"
-      const val RULES_JAVA_VERSION = "7.11.1"
+      const val RULES_JAVA_VERSION = "8.10.0"
       const val RULES_JVM_EXTERNAL_VERSION = "6.7"
       const val JUNIT_VERSION = "4.13.2"
 
