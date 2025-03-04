ae0e4c2ecfdeb66d3bdad8b4133ab5be187fd961
Marta Hering-Zagrocka <mh448197@students.mimuw.edu.pl>
2025-03-04 18:15:03 +0100

    completions - add dependency on working directory in generateTargetCompletions function

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelquery/completion/TargetsCompletions.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelquery/completion/TargetsCompletions.kt
@@ -2,6 +2,8 @@ package org.jetbrains.bazel.languages.bazelquery.completion
 
 import com.intellij.openapi.project.ProjectManager
 import org.jetbrains.bazel.target.targetUtils
+import kotlin.text.removePrefix
+import kotlin.text.startsWith
 
 
 private val targets = ProjectManager.getInstance()
@@ -10,19 +12,26 @@ private val targets = ProjectManager.getInstance()
     it.toString().removePrefix("@")
   }
 
-// TODO: add current directory
-fun generateTargetCompletions(prefix: String): List<String> {
+fun generateTargetCompletions(prefix: String, directory: String = ""): List<String> {
+  // Note: project root dependent format - starts with "//",
+  // directory dependent format - starts with a letter or ":"
   val suggestions = mutableListOf<String>()
-  val allTargets =  targets?.toSet() ?: emptySet()
+  val projectDir = ProjectManager.getInstance().openProjects.firstOrNull()?.basePath ?: ""
+  if (!directory.startsWith(projectDir) && directory.isNotEmpty()) return suggestions
+  val currentDir = directory.removePrefix(projectDir).removePrefix("/")
+  val prefixPath = "//$currentDir"
+
+  val allTargets: MutableSet<String> =  targets?.toMutableSet() ?: mutableSetOf()
+  val allTargetsDirDepFormat =
+    if (currentDir.isNotEmpty())
+      allTargets
+        .filter { it.startsWith("$prefixPath/") }
+        .map { it.removePrefix("$prefixPath/") }
+    else
+      allTargets.map{ it.removePrefix("//") }
+  allTargets.addAll(allTargetsDirDepFormat)
 
   suggestions.addAll(allTargets.filter { it.startsWith(prefix) })
-  if (prefix.isNotEmpty() && prefix[0].isLetter()) {
-    suggestions.addAll(
-      allTargets
-        .filter { it.startsWith("//$prefix") }
-        .map { it.removePrefix("//") }
-    )
-  }
   suggestions.addAll(generateTargetPatterns(prefix, suggestions))
 
   if (prefix.startsWith("/") || prefix.isEmpty()) {
@@ -34,11 +43,30 @@ fun generateTargetCompletions(prefix: String): List<String> {
   if (prefix.startsWith(":") || prefix.isEmpty()) {
     // All rule targets in the working directory.
     suggestions.add(":all")
+    if(prefix.isNotEmpty())
+      // Targets in the working directory in directory dependent format (start with ":$prefix")
+      suggestions.addAll(
+        allTargets
+          .filter { it.startsWith("$prefixPath$prefix") }
+          .map { it.removePrefix(prefixPath) }
+      )
+    else
+      // Targets in the working directory in directory dependent format (start with ":")
+      suggestions.addAll(
+        allTargets
+          .filter { it.startsWith("$prefixPath:") }
+          .map { it.removePrefix(prefixPath) }
+      )
   }
   if (prefix.startsWith(".") || prefix.isEmpty()) {
     // All rule targets in all packages beneath the working directory.
     suggestions.add("...:all")
   }
+  // Note: The following are not supported in completions:
+  // bar/wiz - Equivalent to (assuming foo is current working directory):
+  //    foo/bar/wiz:wiz if foo/bar/wiz is a package
+  //    foo/bar:wiz if foo/bar is a package
+  //    foo:bar/wiz otherwise
 
   return suggestions.distinct()
 }
@@ -47,24 +75,30 @@ private fun generateTargetPatterns(prefix: String, suggestions: List<String>): L
   val patterns = mutableListOf<String>()
 
   val availablePathsSet = mutableSetOf<String>()
-  var prefixStartsWithBackslash = false
-  var prefixSegments: List<String> = emptyList()
+  val availableSuggestions = suggestions.toMutableList()
+  var isProjectRootDependentFormat = false
+  var prefixSegments = 0
 
-  if (prefix.isNotEmpty()){
-    prefixStartsWithBackslash = prefix[0] == '/'
-    val targetPrefix = prefix.removePrefix("/").removePrefix("/").split(":")[0]
-    prefixSegments = targetPrefix.split("/")
-  }
-  suggestions.forEach { s ->
-    val suggestionSegments = s.removePrefix("//").split(":")[0].split("/")
-    suggestionSegments.indices.reversed()
-      .filter { it + 1 >= prefixSegments.size }
-      .forEach { i ->
-        availablePathsSet.add(suggestionSegments.subList(0, i + 1).joinToString("/"))
-      }
+  if (prefix.isNotEmpty()) {
+    isProjectRootDependentFormat = prefix[0] == '/'
+    prefixSegments += prefix
+      .removePrefix("/").removePrefix("/").split(":")[0]
+      .split("/").size
   }
 
-  if (prefixStartsWithBackslash) {
+  // Add all subpaths to use with wildcards
+  availableSuggestions
+    .forEach { s ->
+      patterns.add(s)
+      val suggestionSegments = s.removePrefix("//").split(":")[0].split("/")
+      suggestionSegments.indices.reversed()
+        .filter { it + 1 >= prefixSegments }
+        .forEach { i ->
+          availablePathsSet.add(suggestionSegments.subList(0, i + 1).joinToString("/"))
+        }
+    }
+
+  if (isProjectRootDependentFormat) {
     availablePathsSet.forEach { path ->
       patterns.add("//$path:all")
       patterns.add("//$path/...:all-targets")
