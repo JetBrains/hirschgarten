e230ea91477a866f3ce0d4267b5551038c72cc30
Justin Kaeser <justin.kaeser@jetbrains.com>
2025-03-07 17:28:52 +0100

    fix Java wizard

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/wizard/NewProjectWizardConstants.kt
@@ -0,0 +1,11 @@
+package org.jetbrains.bazel.ui.wizard
+
+object NewProjectWizardConstants {
+  const val BAZEL_VERSION = "8.1.1"
+  const val RULES_JAVA_VERSION = "8.10.0"
+  const val RULES_JVM_EXTERNAL_VERSION = "6.7"
+  const val JUNIT_VERSION = "4.13.2"
+  const val RULES_KOTLIN_VERSION = "2.1.0"
+  const val KOTLIN_VERSION = "2.1.0"
+
+}
