78e36c3d88fccf536f5118a6a50d13f5d760b9c2
Jiaming Tang <jiaming.tang@jetbrains.com>
2025-03-13 15:19:25 +0100

    remove old code

--- server/server/src/main/kotlin/org/jetbrains/bazel/server/bep/Junit5TestVisualOutputParser.kt
+++ server/server/src/main/kotlin/org/jetbrains/bazel/server/bep/Junit5TestVisualOutputParser.kt
@@ -6,7 +6,6 @@ import org.jetbrains.bsp.protocol.JUnitStyleTestCaseData
 import org.jetbrains.bsp.protocol.TaskId
 import org.jetbrains.bsp.protocol.TestStatus
 import java.util.UUID
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 private class TestResultTreeNode(
@@ -32,7 +31,7 @@ private class TestResultTreeNode(
         taskId = taskId,
         status = status!!,
         message = fullMessage,
-        data = createTestCaseData(fullMessage),
+        data = createTestCaseData(fullMessage, time),
       )
     } else {
       bspClientTestNotifier.startTest(name, taskId)
@@ -150,97 +149,6 @@ class Junit5TestVisualOutputParser(private val bspClientTestNotifier: BspClientT
     notifyClient(tree)
   }
 
-  private fun processTestingEndingLine(
-    previousOutputLine: TestOutputLine?,
-    testingEndedMatcher: Matcher,
-    startedBuildTarget: StartedBuildTarget?,
-  ) {
-    previousOutputLine?.let { startAndFinishTest(it) }
-    while (startedSuites.isNotEmpty()) {
-      finishTopmostSuite()
-    }
-    val time = testingEndedMatcher.group("time").toLongOrNull() ?: 0
-  }
-
-  private fun processPreviousOutputLine(previousOutputLine: TestOutputLine?, currentOutputLine: TestOutputLine) {
-    if (previousOutputLine != null) {
-      if (currentOutputLine.indent > previousOutputLine.indent) {
-        startSuite(previousOutputLine)
-      } else {
-        startAndFinishTest(previousOutputLine)
-        removeAllFinishedSuites(currentOutputLine)
-      }
-    }
-  }
-
-  private fun removeAllFinishedSuites(currentOutputLine: TestOutputLine) {
-    while (startedSuites.isNotEmpty() && startedSuites.last().indent >= currentOutputLine.indent) {
-      finishTopmostSuite()
-    }
-  }
-
-  private fun getCurrentOutputLine(line: String): TestOutputLine? {
-    val cleanLine = line.removeFormat()
-
-    val currentLineMatcher = testLinePattern.matcher(cleanLine)
-    return if (currentLineMatcher.find()) {
-      TestOutputLine(
-        name = currentLineMatcher.group("name"),
-        status = currentLineMatcher.group("result").toTestStatus(),
-        message = currentLineMatcher.group("message"),
-        indent = currentLineMatcher.start("name"),
-        taskId = TaskId(testUUID()),
-      )
-    } else {
-      null
-    }
-  }
-
-  private fun parseStartedBuildTarget(line: String): StartedBuildTarget? {
-    val testingStartMatcher = testingStartPattern.matcher(line)
-    return if (testingStartMatcher.find()) {
-      StartedBuildTarget(testingStartMatcher.group("target"), TaskId(testUUID())).also {
-        beginTesting(it)
-      }
-    } else {
-      null
-    }
-  }
-
-  private fun beginTesting(startedBuildTarget: StartedBuildTarget) {
-    bspClientTestNotifier.beginTestTarget(Label.parse(startedBuildTarget.uri), startedBuildTarget.taskId)
-  }
-
-  private fun startSuite(suite: TestOutputLine) {
-    bspClientTestNotifier.startTest(suite.name, suite.taskId)
-    startedSuites.addLast(suite)
-  }
-
-  private fun finishTopmostSuite() {
-    with(startedSuites.removeLastOrNull() ?: return) {
-      bspClientTestNotifier.finishTest(
-        displayName = name,
-        taskId = taskId,
-        status = status,
-        message = message,
-      )
-    }
-  }
-
-  private fun startAndFinishTest(test: TestOutputLine) {
-    test.taskId.parents = generateParentList()
-    bspClientTestNotifier.startTest(test.name, test.taskId)
-    bspClientTestNotifier.finishTest(
-      displayName = test.name,
-      taskId = test.taskId,
-      status = test.status,
-      message = test.message,
-      data = createTestCaseData(test.message),
-    )
-  }
-
-  private fun generateParentList(): List<String> = startedSuites.toList().reversed().mapNotNull { it.taskId.id }
-
   private fun testUUID(): String = "test-" + UUID.randomUUID().toString()
 
   private fun String.toTestStatus(): TestStatus =
@@ -264,9 +172,9 @@ private val testingEndedPattern = Pattern.compile("^Test\\hrun\\hfinished\\hafte
 private fun String.removeFormat(): String =
   this.replace(Regex("[?\\u001b]\\[[;\\d]*m"), "") // '1B' symbol appears in console output and test logs, while '?' appears in test XMLs
 
-private fun createTestCaseData(message: String): JUnitStyleTestCaseData =
+private fun createTestCaseData(message: String, time: Long?): JUnitStyleTestCaseData =
   JUnitStyleTestCaseData(
-    time = null,
+    time = time as Double?,
     className = null,
     errorMessage = message,
     errorContent = null,
@@ -280,5 +188,3 @@ private data class TestOutputLine(
   val indent: Int,
   val taskId: TaskId,
 )
-
-private data class StartedBuildTarget(val uri: String, val taskId: TaskId)
