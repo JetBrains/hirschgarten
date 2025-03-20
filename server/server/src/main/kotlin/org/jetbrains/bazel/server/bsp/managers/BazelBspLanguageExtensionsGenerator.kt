ed9a55a42d1776d8d4f3c39e7076bcc771347ec3
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2025-03-20 13:42:38 +0000

    [maintenance] properly support templating scala aspects

--- server/server/src/main/kotlin/org/jetbrains/bazel/server/bsp/managers/BazelBspLanguageExtensionsGenerator.kt
+++ server/server/src/main/kotlin/org/jetbrains/bazel/server/bsp/managers/BazelBspLanguageExtensionsGenerator.kt
@@ -25,7 +25,7 @@ enum class Language(
     listOf("JavaInfo", "java_common", "JavaPluginInfo", "java_binary", "java_library"),
   ),
   Python("//aspects:rules/python/python_info.bzl", listOf("rules_python"), listOf("extract_python_info"), true, true, listOf("PyInfo")),
-  Scala("//aspects:rules/scala/scala_info.bzl", listOf("io_bazel_rules_scala", "rules_scala"), listOf("extract_scala_info"), false, false),
+  Scala("//aspects:rules/scala/scala_info.bzl", listOf("io_bazel_rules_scala", "rules_scala"), listOf("extract_scala_info"), true, false),
   Cpp("//aspects:rules/cpp/cpp_info.bzl", listOf("rules_cc"), listOf("extract_cpp_info", "extract_c_toolchain_info"), false, false),
   Kotlin("//aspects:rules/kt/kt_info.bzl", listOf("io_bazel_rules_kotlin", "rules_kotlin"), listOf("extract_kotlin_info"), true, false),
   Jvm(
