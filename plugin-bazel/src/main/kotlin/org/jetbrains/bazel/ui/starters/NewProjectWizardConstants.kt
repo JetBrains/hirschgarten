4432f01928cc0978bfb99457ebc96e05d7f19741
Justin Kaeser <justin.kaeser@jetbrains.com>
2025-03-24 09:41:32 +0000

    [feature] Project wizards for Java and Kotlin | #BAZEL-1766 done

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/starters/NewProjectWizardConstants.kt
@@ -0,0 +1,9 @@
+package org.jetbrains.bazel.ui.starters
+
+object NewProjectWizardConstants {
+  const val BAZEL_VERSION = "8.1.1"
+  const val RULES_JAVA_VERSION = "8.10.0"
+  const val RULES_JVM_EXTERNAL_VERSION = "6.7"
+  const val JUNIT_VERSION = "4.13.2"
+  const val RULES_KOTLIN_VERSION = "2.1.0"
+}
