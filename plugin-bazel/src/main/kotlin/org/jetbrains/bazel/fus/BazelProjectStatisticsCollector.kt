1105a8c2c9db8752e7a743dda5b45626b24f37da
Justin Kaeser <justin.kaeser@jetbrains.com>
2025-03-26 12:07:00 +0000

    [feature] some very basic Feature Usage Statistics (FUS) | #BAZEL-464
    GitOrigin-RevId: 8af493acf8a7934b4ee99ee04f49b6e80df1c0a0

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/fus/BazelProjectStatisticsCollector.kt
@@ -0,0 +1,44 @@
+@file:Suppress("UnstableApiUsage")
+
+package org.jetbrains.plugins.bazel.fus
+
+import com.intellij.internal.statistic.beans.MetricEvent
+import com.intellij.internal.statistic.eventLog.EventLogGroup
+import com.intellij.internal.statistic.eventLog.events.EventFields
+import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
+import com.intellij.openapi.project.Project
+import org.jetbrains.bazel.config.bazelProjectProperties
+import org.jetbrains.bazel.target.targetUtils
+
+class BazelProjectStatisticsCollector : ProjectUsagesCollector() {
+  override fun getGroup(): EventLogGroup = Const.GROUP
+
+  override fun getMetrics(project: Project): Set<MetricEvent> {
+    val targetUtils = project.targetUtils
+    val projectProperties = project.bazelProjectProperties
+
+    return if (projectProperties.isBazelProject) {
+      setOf(
+        Const.COUNT_TARGETS.metric(targetUtils.labelToTargetInfo.size),
+        Const.COUNT_FILES.metric(targetUtils.fileToTarget.size),
+      )
+    } else {
+      emptySet()
+    }
+  }
+
+  object Const {
+    internal val GROUP = EventLogGroup("bazel.project.statistics", 0, "FUS", "General statistics about Bazel projects")
+
+    internal val COUNT_TARGETS =
+      GROUP.registerEvent(
+        "count.targets",
+        EventFields.LogarithmicInt("count_targets", "number of targets synced"),
+      )
+    internal val COUNT_FILES =
+      GROUP.registerEvent(
+        "count.files",
+        EventFields.LogarithmicInt("count_files", "number of files synced"),
+      )
+  }
+}
