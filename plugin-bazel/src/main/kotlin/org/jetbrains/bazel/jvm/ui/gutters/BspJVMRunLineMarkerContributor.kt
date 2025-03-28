6418683eb93164a930cb68135fe40286d8e7d173
Jiaming Tang <jiaming.tang@jetbrains.com>
2025-03-28 16:34:54 +0100

    add a runLineMarkerContributor without kotlin plugin references

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/jvm/ui/gutters/BspJVMRunLineMarkerContributor.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/jvm/ui/gutters/BspJVMRunLineMarkerContributor.kt
@@ -1,50 +1,18 @@
 package org.jetbrains.bazel.jvm.ui.gutters
 
-import com.intellij.openapi.vfs.JarFileSystem
 import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiNameIdentifierOwner
-import org.jetbrains.bazel.ui.gutters.BspRunLineMarkerContributor
 import org.jetbrains.kotlin.psi.KtClassOrObject
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
 
-class BspJVMRunLineMarkerContributor : BspRunLineMarkerContributor() {
-  override fun isDumbAware(): Boolean = true
-
-  override fun PsiElement.shouldAddMarker(): Boolean =
-    !isInsideJar() &&
-      getStrictParentOfType<PsiNameIdentifierOwner>()
-        ?.isClassOrMethod() ?: false
-
-  private fun PsiElement.isInsideJar() = containingFile.virtualFile?.fileSystem is JarFileSystem
-
-  // TODO: https://youtrack.jetbrains.com/issue/BAZEL-1316
-  override fun getSingleTestFilter(element: PsiElement): String? {
-    val psiIdentifier = element.getStrictParentOfType<PsiNameIdentifierOwner>()
-    val functionName = psiIdentifier?.getFunctionName()
-    if (psiIdentifier?.isMethod() == true) {
-      val className = psiIdentifier.getClassName() ?: return functionName
-      return "$className.$functionName"
-    }
-    return "$functionName"
-  }
-
-  private fun PsiNameIdentifierOwner.getFunctionName(): String? = if (this.isClassOrMethod()) tryGetFQN() else null
-
-  private fun PsiNameIdentifierOwner.getClassName(): String? =
+class BspJVMRunLineMarkerContributor : BspJavaRunLineMarkerContributor() {
+  override fun PsiNameIdentifierOwner.getClassName(): String? =
     this.getStrictParentOfType<KtClassOrObject>()?.name ?: this.getStrictParentOfType<PsiClass>()?.name
 
-  private fun PsiNameIdentifierOwner.tryGetFQN(): String? =
-    if (this is PsiClass) {
-      qualifiedName
-    } else {
-      name
-    }
-
-  private fun PsiNameIdentifierOwner.isClassOrMethod(): Boolean =
+  override fun PsiNameIdentifierOwner.isClassOrMethod(): Boolean =
     this is KtClassOrObject || this is KtNamedFunction || this is PsiClass || this is PsiMethod
 
-  private fun PsiNameIdentifierOwner.isMethod(): Boolean = this is KtNamedFunction || this is PsiMethod
+  override fun PsiNameIdentifierOwner.isMethod(): Boolean = this is KtNamedFunction || this is PsiMethod
 }
