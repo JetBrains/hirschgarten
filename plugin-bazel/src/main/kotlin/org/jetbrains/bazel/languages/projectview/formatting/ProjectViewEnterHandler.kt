048d98e4110b2a7cdb5035124ddf553dd49798f1
Andrzej Jabłoński <andrzej.jablonski@jetbrains.com>
2025-04-02 18:58:42 +0200

    fix blank line check

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/formatting/ProjectViewEnterHandler.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/formatting/ProjectViewEnterHandler.kt
@@ -16,9 +16,10 @@ import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.impl.source.tree.injected.InjectedLanguageEditorUtil
-import com.intellij.psi.util.elementType
 import org.jetbrains.bazel.languages.projectview.lexer.ProjectViewTokenType
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewPsiFile
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.util.TextRange
 
 class ProjectViewEnterHandler : EnterHandlerDelegateAdapter() {
   companion object {
@@ -49,31 +50,26 @@ class ProjectViewEnterHandler : EnterHandlerDelegateAdapter() {
     return prev?.elementType == ProjectViewTokenType.SECTION_KEYWORD
   }
 
-  fun isBlankLine(file: PsiFile, caretOffset: Int): Boolean {
-    val start = file.findElementAt(caretOffset - 1)
-    var left = start
-    while (left != null && left is PsiWhiteSpace) {
-      left = left.prevSibling
-    }
+  fun isBlankLine(file: PsiFile, caretOffset: Int, document: Document): Boolean {
+    val lineNumber = document.getLineNumber(caretOffset)
 
-    var right = file.findElementAt(caretOffset)
-    while (right != null && right is PsiWhiteSpace) {
-      right = right.nextSibling
-    }
+    val lineStart = document.getLineStartOffset(lineNumber)
+    val lineEnd = document.getLineEndOffset(lineNumber)
 
-    return (left == null || left.elementType == ProjectViewTokenType.NEWLINE) &&
-      (right == null || right.elementType == ProjectViewTokenType.NEWLINE)
+    val textInLine = document.getText(TextRange(lineStart, lineEnd)).trim()
+
+    return textInLine.isEmpty()
   }
 
   override fun preprocessEnter(
     file: PsiFile,
     editor: Editor,
-    caretOffset: Ref<Int?>,
-    caretAdvance: Ref<Int?>,
+    caretOffset: Ref<Int>,
+    caretAdvance: Ref<Int>,
     dataContext: DataContext,
     originalHandler: EditorActionHandler?,
-  ): EnterHandlerDelegate.Result? {
-    var offset = caretOffset.get() ?: return EnterHandlerDelegate.Result.Continue
+  ): EnterHandlerDelegate.Result {
+    var offset = caretOffset.get()
     var file = file // now it's mutable
     var editor = editor
 
@@ -86,8 +82,12 @@ class ProjectViewEnterHandler : EnterHandlerDelegateAdapter() {
     if (!isApplicable(file, dataContext)) {
       return EnterHandlerDelegate.Result.Default
     }
+    val document = editor.document
+    PsiDocumentManager.getInstance(file.project).commitDocument(document)
+    editor.caretModel.moveToOffset(offset)
+
     // remove indent if enter is pressed on the blank line
-    if (isBlankLine(file, offset)) {
+    if (isBlankLine(file, offset, document)) {
       editor.document.insertString(offset, "\n")
       editor.caretModel.moveToOffset(offset + 1)
       return EnterHandlerDelegate.Result.Stop
@@ -96,15 +96,12 @@ class ProjectViewEnterHandler : EnterHandlerDelegateAdapter() {
       return EnterHandlerDelegate.Result.Default
     }
 
-    editor.caretModel.moveToOffset(offset)
-    val doc = editor.document
-    PsiDocumentManager.getInstance(file.project).commitDocument(doc)
 
     originalHandler?.execute(editor, editor.caretModel.currentCaret, dataContext)
     val position = editor.caretModel.logicalPosition
     if (position.column < INDENT) {
       val spacesPadding = StringUtil.repeat(" ", INDENT - position.column)
-      doc.insertString(editor.caretModel.offset, spacesPadding)
+      document.insertString(editor.caretModel.offset, spacesPadding)
     }
     editor.caretModel.moveToLogicalPosition(LogicalPosition(position.line, INDENT))
     return EnterHandlerDelegate.Result.Stop
