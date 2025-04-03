f8e008171abd50ea40ca3d9a7ab176555be1be73
Lev Leontev <lev.leontev@jetbrains.com>
2025-04-03 10:24:21 +0000

    [fix] Fix navigating to imports of native rules in Starlark | #BAZEL-1822 Done
    GitOrigin-RevId: 5a84cac485b47504cb32f13434bfbe0a26989ca7

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/StarlarkCallExpression.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/StarlarkCallExpression.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.bazel.languages.starlark.psi.expressions
 
 import com.intellij.lang.ASTNode
+import com.intellij.model.psi.PsiSymbolReference
+import com.intellij.model.psi.PsiSymbolService
 import com.intellij.psi.PsiReference
 import org.jetbrains.bazel.languages.starlark.bazel.BazelNativeRules
 import org.jetbrains.bazel.languages.starlark.elements.StarlarkElementTypes
@@ -31,9 +33,12 @@ class StarlarkCallExpression(node: ASTNode) : StarlarkBaseElement(node) {
 
   fun getArgumentList(): StarlarkArgumentList? = findChildrenByClass(StarlarkArgumentList::class.java).firstOrNull()
 
-  override fun getOwnReferences(): Collection<BazelNativeRuleReference> {
+  override fun getOwnReferences(): Collection<PsiSymbolReference> {
     val name = name ?: return emptyList()
     val nativeRule = BazelNativeRules.getRuleByName(name) ?: return emptyList()
-    return listOf(BazelNativeRuleReference(this, nativeRule))
+    return listOfNotNull(
+      BazelNativeRuleReference(this, nativeRule),
+      reference?.let { PsiSymbolService.getInstance().asSymbolReference(it) },
+    )
   }
 }
