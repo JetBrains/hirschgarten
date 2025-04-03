3ed285099401bac24fbc52a47ece84efc9551b97
Karolus314 <trener199951@gmail.com>
2025-04-04 00:25:55 +0200

    delete outdated tests

--- plugin-bazel/src/test/kotlin/org/jetbrains/bazel/languages/bazelquery/parser/BazelqueryParsingTest.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.bazel.languages.bazelquery.parser
-
-import org.jetbrains.bazel.languages.bazelquery.fixtures.BazelqueryParsingTestCase
-
-class BazelqueryParsingTest : BazelqueryParsingTestCase("") {
-  fun testExpr() = doTest(true, true)
-
-  fun testFlag() = doTest(true, true)
-}
