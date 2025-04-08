a647b0a6376588e26d8d61bcfcb92718aed1d283
Lev Leontev <lev.leontev@jetbrains.com>
2025-04-08 21:32:23 +0000

    [feature] Parse multi-line exception messages for JUnit 5 tests | #BAZEL-1872 Done
    GitOrigin-RevId: 36d917aee42e627c9247bdba877423cf5d0b105e

--- server/server/src/main/kotlin/org/jetbrains/bazel/server/bep/Junit5TestVisualOutputParser.kt
+++ server/server/src/main/kotlin/org/jetbrains/bazel/server/bep/Junit5TestVisualOutputParser.kt
@@ -35,46 +35,51 @@ class Junit5TestVisualOutputParser(private val bspClientTestNotifier: BspClientT
     val lines = output.lines()
     val targetToTestResultTree = mutableMapOf<String, TestResultTreeNode>()
     var treeRootForCurrentTarget: TestResultTreeNode? = null
-    var previousNode: TestResultTreeNode? = null
+    var currentNode: TestResultTreeNode? = null
+    var currentStackTraceNode: TestResultTreeNode? = null
     var i = 0
     while (i < lines.size) {
       val line = lines[i]
       val cleanLine = line.removeFormat()
       val testEndedMatcher = testingEndedPattern.matcher(line)
       val testLineMatcher = testLinePattern.matcher(cleanLine)
+      val failuresCountMatcher = failuresCountPattern.matcher(cleanLine)
       if (treeRootForCurrentTarget == null) {
         parseBuildTargetName(line)?.let {
-          treeRootForCurrentTarget = TestResultTreeNode(it, TaskId(testUUID()), null, "", -1)
+          treeRootForCurrentTarget = TestResultTreeNode(it, TaskId(testUUID()), null, mutableListOf(), -1)
           targetToTestResultTree[it] = treeRootForCurrentTarget
-          previousNode = treeRootForCurrentTarget
+          currentNode = treeRootForCurrentTarget
         }
       } else if (testEndedMatcher.find()) {
         val time = testEndedMatcher.group("time").toLongOrNull()
         treeRootForCurrentTarget.time = time
-        previousNode = null
+        currentNode = null
         treeRootForCurrentTarget = null
+        currentStackTraceNode = null
+      } else if (failuresCountMatcher.find()) {
+        currentNode = null
       } else if (testLineMatcher.find()) {
         val indent = testLineMatcher.start("name")
-        val parent = previousNode?.let { findParentByIndent(it, indent) }
+        val parent = currentNode?.let { findParentByIndent(it, indent) }
         val parentId = if (parent?.isRootNode() == true) null else parent?.taskId?.id
         val newNode =
           TestResultTreeNode(
             name = testLineMatcher.group("name"),
             taskId = TaskId(testUUID(), parents = listOfNotNull(parentId)),
             status = testLineMatcher.group("result").toTestStatus(),
-            message = testLineMatcher.group("message"),
+            messageLines = mutableListOf(testLineMatcher.group("message")),
             indent = indent,
             parent = parent,
           )
         parent?.children?.put(newNode.name, newNode)
-        previousNode = newNode
+        currentNode = newNode
       } else if (isStackTraceStartingLine(cleanLine)) {
-        val testNode = findNodeByPath(cleanLine, treeRootForCurrentTarget)
-        if (testNode != null) {
-          while (i + 1 < lines.size && lines[i + 1].isNotEmpty() && !isStackTraceStartingLine(lines[i + 1].removeFormat())) {
-            testNode.stacktrace.add(lines[++i].removeFormat())
-          }
-        }
+        currentStackTraceNode = findNodeByPath(cleanLine, treeRootForCurrentTarget)
+        currentNode = null
+      } else if (currentNode != null && currentNode.messageLines.isNotEmpty()) {
+        currentNode.messageLines.add(parseMultilineMessageContinuation(line, cleanLine))
+      } else if (currentStackTraceNode != null) {
+        currentStackTraceNode.stacktrace.add(cleanLine.substringAfter("    => "))
       }
       i++
     }
@@ -109,7 +114,24 @@ class Junit5TestVisualOutputParser(private val bspClientTestNotifier: BspClientT
       }
     }
 
-  private fun isStackTraceStartingLine(line: String) = line.trim().startsWith("JUnit Jupiter:")
+  private fun isStackTraceStartingLine(line: String) =
+    line.trim().let {
+      it.startsWith("JUnit Jupiter:") || it.startsWith("JUnit Platform Suite:")
+    }
+
+  private fun parseMultilineMessageContinuation(rawLine: String, cleanLine: String): String {
+    // We want to preserve leading whitespaces, where possible. We need to find the starting offset for that.
+    // Some versions of JUnit 5 include the vertical bar "│" before multiline error messages, some don't.
+    // For an example of a test that doesn't have the vertical bar, try to break any test using intellij_integration_test_suite.
+    // We can also try to use the color information to find the message offset, but it may be different between operating systems.
+    return cleanLine.substringAfterOrNull("│        ") ?: rawLine.substringAfterOrNull("?[0m?[31m   ")?.removeFormat()
+      ?: cleanLine.trimStart()
+  }
+
+  private fun String.substringAfterOrNull(delimiter: String): String? {
+    val index = indexOf(delimiter)
+    return if (index == -1) null else substring(index + delimiter.length, length)
+  }
 
   private fun findNodeByPath(path: String, root: TestResultTreeNode): TestResultTreeNode? {
     val segment = path.trim().split(":")
@@ -137,8 +159,9 @@ class Junit5TestVisualOutputParser(private val bspClientTestNotifier: BspClientT
 
 private val testingStartPattern = Pattern.compile("^Executing\\htests\\hfrom\\h(?<target>[^:]*:[^:]+)")
 private val testLinePattern =
-  Pattern.compile("^(?:[\\h└├│]{3})+[└├│]─\\h(?<name>.+)\\h(?<result>[✔✘↷])\\h?(?<message>.*)\$")
+  Pattern.compile("^(?:[\\h└├│]{3})+[└├│]─\\h(?<name>.+)\\h(?<result>[✔✘↷])\\h?(?<message>.*)$")
 private val testingEndedPattern = Pattern.compile("^Test\\hrun\\hfinished\\hafter\\h(?<time>\\d+)\\hms")
+private val failuresCountPattern = Pattern.compile("^Failures \\(\\d+\\):$")
 
 private fun String.removeFormat(): String =
   this.replace(Regex("[?\\u001b]\\[[;\\d]*m"), "") // '1B' symbol appears in console output and test logs, while '?' appears in test XMLs
@@ -156,7 +179,7 @@ private class TestResultTreeNode(
   val name: String,
   val taskId: TaskId,
   val status: TestStatus?,
-  val message: String,
+  val messageLines: MutableList<String>,
   val indent: Int,
   val parent: TestResultTreeNode? = null,
   var time: Long? = null,
@@ -196,10 +219,20 @@ private class TestResultTreeNode(
           status
             ?: return,
         // this should never be null, because every node except root nodes should have status. Just for compiling
-        message = message,
+        message = messageLines.joinLinesIgnoringLastEmpty(),
       )
     }
   }
 
-  private fun generateMessage(): String = message + "\n" + stacktrace.joinToString("\n")
+  private fun generateMessage(): String = messageLines.joinLinesIgnoringLastEmpty() + "\n" + stacktrace.joinLinesIgnoringLastEmpty()
+
+  private fun List<String>.joinLinesIgnoringLastEmpty(): String {
+    val linesToJoin =
+      if (this.lastOrNull()?.isEmpty() == true) {
+        this.dropLast(1)
+      } else {
+        this
+      }
+    return linesToJoin.joinToString("\n")
+  }
 }
