86a0cdfc75eb16825668ee02ed99ae21a91b5dc3
Lev Leontev <lev.leontev@jetbrains.com>
2025-04-08 15:04:50 +0000

    [fix] Make "New Bazel package" UI more idiomatic | #BAZEL-177

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/action/registered/NewBazelPackageAction.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/action/registered/NewBazelPackageAction.kt
@@ -2,21 +2,24 @@ package org.jetbrains.bazel.action.registered
 import com.intellij.ide.IdeView
 import com.intellij.ide.actions.CreateDirectoryOrPackageHandler
 import com.intellij.ide.actions.OpenFileAction
+import com.intellij.ide.ui.newItemPopup.NewItemPopupUtil
+import com.intellij.ide.ui.newItemPopup.NewItemSimplePopupPanel
 import com.intellij.ide.util.DirectoryChooserUtil
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.ThrowableComputable
 import com.intellij.psi.PsiDirectory
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiFileFactory
-import com.intellij.psi.PsiManager
+import com.intellij.ui.DocumentAdapter
+import com.intellij.util.Consumer
 import org.jetbrains.bazel.assets.BazelPluginIcons
 import org.jetbrains.bazel.config.BazelPluginConstants
-import org.jetbrains.bazel.languages.starlark.StarlarkLanguage
+import javax.swing.SwingUtilities
+import javax.swing.event.DocumentEvent
 
 class NewBazelPackageAction : DumbAwareAction() {
   override fun update(e: AnActionEvent) {
@@ -36,53 +39,67 @@ class NewBazelPackageAction : DumbAwareAction() {
 
   override fun actionPerformed(event: AnActionEvent) {
     val project = event.project ?: return
-    val view = event.getData<IdeView>(LangDataKeys.IDE_VIEW) ?: return
-    val directory = DirectoryChooserUtil.getOrChooseDirectory(view) ?: return
+    val ideView = event.getData(LangDataKeys.IDE_VIEW) ?: return
+    val directory = DirectoryChooserUtil.getOrChooseDirectory(ideView) ?: return
     val validator: CreateDirectoryOrPackageHandler =
       object : CreateDirectoryOrPackageHandler(project, directory, false, ".") {
         override fun createDirectories(subDirName: String) {
           super.createDirectories(subDirName)
           val element = createdElement
           if (element is PsiDirectory) {
-            createBuildFile(project, element)
+            createBuildFile(project, element, ideView)
           }
         }
       }
-    Messages.showInputDialog(
-      project,
-      "Enter new package name:",
-      String.format("New %s Package", BazelPluginConstants.BAZEL_DISPLAY_NAME),
-      Messages.getQuestionIcon(),
-      "",
-      validator,
+
+    // UI code based on CreateDirectoryOrPackageAction
+    val contentPanel = NewItemSimplePopupPanel()
+    val nameField = contentPanel.textField
+    nameField.document.addDocumentListener(
+      object : DocumentAdapter() {
+        override fun textChanged(event: DocumentEvent) {
+          val name = nameField.text
+          validator.checkInput(name)
+          val errorText = validator.getErrorText(name) ?: validator.getWarningText(name)
+          // invokeLater here to make sure that the text update in the UI does not interfere with showing the error popups
+          SwingUtilities.invokeLater {
+            contentPanel.setError(errorText)
+          }
+        }
+      },
     )
-    val newDir = validator.createdElement as PsiDirectory?
-    if (newDir != null) {
-      val buildFile = findBuildFile(project, newDir)
-      if (buildFile != null) {
-        view.selectElement(buildFile)
-        OpenFileAction.openFile(buildFile.viewProvider.virtualFile, project)
+
+    val popup =
+      NewItemPopupUtil.createNewItemPopup(
+        "New Bazel Package",
+        contentPanel,
+        nameField,
+      )
+    contentPanel.applyAction =
+      Consumer { event ->
+        val name = nameField.getText()
+        validator.checkInput(name)
+        if (validator.getErrorText(name) == null && validator.getWarningText(name) == null && validator.canClose(name)) {
+          popup.closeOk(event)
+        }
       }
-    }
+    popup.showCenteredInCurrentWindow(project)
   }
 
-  private fun findBuildFile(project: Project, parent: PsiDirectory): PsiFile? {
-    for (filename in BazelPluginConstants.BUILD_FILE_NAMES) {
-      val vf = parent.virtualFile.findChild(filename)
-      if (vf != null) {
-        return PsiManager.getInstance(project).findFile(vf)
-      }
-    }
-    return null
-  }
-
-  private fun createBuildFile(project: Project, parent: PsiDirectory) {
+  private fun createBuildFile(
+    project: Project,
+    parent: PsiDirectory,
+    ideView: IdeView,
+  ) {
     val filename = BazelPluginConstants.BUILD_FILE_NAMES[0]
-    val file =
-      PsiFileFactory
-        .getInstance(project)
-        .createFileFromText(filename, StarlarkLanguage, "")
-    parent.add(file)
+    val buildFile =
+      WriteCommandAction.writeCommandAction(project).compute(
+        ThrowableComputable {
+          parent.createFile(filename)
+        },
+      )
+    ideView.selectElement(buildFile)
+    OpenFileAction.openFile(buildFile.viewProvider.virtualFile, project)
   }
 
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
