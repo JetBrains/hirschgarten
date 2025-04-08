1dc40a330bbcced9701615ca17e13d2f0225d3d8
Justin Kaeser <justin.kaeser@jetbrains.com>
2025-04-08 14:37:07 +0000

    [fix] kill process in BazelRunner OS-independently | #BAZEL-1847 fixed
    this should fix java.io.IOException: Cannot run program "kill": CreateProcess error=2, The system cannot find the file specified seen in BAZEL-1847. The other exception looks like it is already obsolete
    
    GitOrigin-RevId: 9f2c26fe3f0628017ea987d8525219b63c39b3c7

--- server/bazelrunner/src/main/kotlin/org/jetbrains/bazel/bazelrunner/outputs/OutputProcessor.kt
+++ server/bazelrunner/src/main/kotlin/org/jetbrains/bazel/bazelrunner/outputs/OutputProcessor.kt
@@ -2,11 +2,10 @@ package org.jetbrains.bazel.bazelrunner.outputs
 
 import com.intellij.execution.process.OSProcessUtil
 import com.intellij.util.io.awaitExit
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.future.await
 import kotlinx.coroutines.isActive
-import kotlinx.coroutines.withTimeoutOrNull
-import org.jetbrains.bazel.logger.BspClientLogger
+import kotlinx.coroutines.withContext
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
@@ -15,6 +14,7 @@ import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
+import kotlin.coroutines.cancellation.CancellationException
 
 abstract class OutputProcessor(private val process: Process, vararg loggers: OutputHandler) {
   val stdoutCollector = OutputCollector()
@@ -61,25 +61,24 @@ abstract class OutputProcessor(private val process: Process, vararg loggers: Out
     executorService.submit(runnable).also { runningProcessors.add(it) }
   }
 
-  suspend fun waitForExit(serverPidFuture: CompletableFuture<Long>?, logger: BspClientLogger?): Int =
+  suspend fun waitForExit(serverPidFuture: CompletableFuture<Long>?): Int =
     coroutineScope {
-      var isFinished = false
-      while (!isFinished) {
-        isFinished = process.waitFor(500, TimeUnit.MILLISECONDS)
-        if (!isActive) {
-          process.destroy()
-          withTimeoutOrNull(2000) {
-            serverPidFuture
-              ?.await() // we don't want to wait forever if server never gave us its PID
-              ?.let { pid -> OSProcessUtil.killProcess(pid.toInt()) }
-              ?: logger?.error("Could not cancel the task. Bazel server needs to be interrupted manually.")
-          }
+      try {
+        var isFinished = false
+        while (!isFinished && isActive) {
+          isFinished = withContext(Dispatchers.IO) { process.waitFor(500, TimeUnit.MILLISECONDS) }
         }
+        return@coroutineScope process.awaitExit()
+      } catch (e: CancellationException) {
+        OSProcessUtil.killProcessTree(process)
+        OSProcessUtil.killProcess(process)
+        if (serverPidFuture?.isDone == true) {
+          val pid = serverPidFuture.get()
+          OSProcessUtil.killProcess(pid.toInt())
+        }
+        throw e
+      } finally {
+        shutdown()
       }
-      // Return values of waitFor() and waitFor(long, TimeUnit) differ
-      // so we can't just return value from waitFor(long, TimeUnit) here
-      val exitCode = process.awaitExit()
-      shutdown()
-      return@coroutineScope exitCode
     }
 }
