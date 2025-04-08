2efb7d817c23055a082da1702f6a8929bc1d12ca
Andrzej Jabłoński <andrzej.jablonski@jetbrains.com>
2025-04-08 09:34:49 +0000

    [feature] Implement glob expansion in Starlark | #BAZEL-1581 Done
    Glob expasion works! 
    Tests will be added in another feature.
    
    GitOrigin-RevId: 19c71c3f0d896bb6182b5a57ed89883e5ae81f96

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/arguments/StarlarkArgumentElement.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/arguments/StarlarkArgumentElement.kt
@@ -1,5 +1,22 @@
 package org.jetbrains.bazel.languages.starlark.psi.expressions.arguments
 
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import org.jetbrains.bazel.languages.starlark.elements.StarlarkElementTypes
+import org.jetbrains.bazel.languages.starlark.elements.StarlarkTokenTypes
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkElement
 
-interface StarlarkArgumentElement : StarlarkElement
+interface StarlarkArgumentElement : StarlarkElement {
+  fun getValue(): PsiElement? {
+    var childNode: ASTNode? = node.lastChildNode
+    while (childNode != null) {
+      val type = childNode.elementType
+      if (StarlarkElementTypes.EXPRESSIONS.contains(type)) {
+        return childNode.psi
+      }
+      if (type == StarlarkTokenTypes.EQ) break
+      childNode = childNode.treePrev
+    }
+    return null
+  }
+}
