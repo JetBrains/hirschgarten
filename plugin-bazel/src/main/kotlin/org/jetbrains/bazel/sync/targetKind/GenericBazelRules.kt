7608694a02bf3ed3f852fcd298f9c3b51874366b
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2025-04-09 17:32:14 +0200

    wip

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/targetKind/GenericBazelRules.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/targetKind/GenericBazelRules.kt
@@ -1,22 +1,11 @@
-/*
- * Copyright 2018 The Bazel Authors. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jetbrains.bazel.commons
+package org.jetbrains.bazel.sync
+
+import org.jetbrains.bazel.commons.LanguageClass
+import org.jetbrains.bazel.commons.RuleType
+import org.jetbrains.bazel.commons.TargetKind
 
 /** Contributes generic rules to [TargetKind].  */
-class GenericBazelRules : TargetKind.Provider {
+class GenericBazelRules : TargetKindProvider {
   /** Generic bazel rule types.  */
   enum class RuleTypes(
     val ruleName: String,
