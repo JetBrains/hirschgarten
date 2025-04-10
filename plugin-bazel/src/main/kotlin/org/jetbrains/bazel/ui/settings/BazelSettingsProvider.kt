3aebdaabf43b4ac3b3544d5e855f30d5dec1182b
Jiaming Tang <jiaming.tang@jetbrains.com>
2025-04-10 16:36:04 +0200

    add comments for extension points

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/settings/BazelSettingsProvider.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/settings/BazelSettingsProvider.kt
@@ -5,12 +5,33 @@ import com.intellij.openapi.project.Project
 import com.intellij.ui.dsl.builder.Panel
 
 interface BazelSettingsProvider {
+  /**
+   * This function returns a function which will be called in BazelSettingsPanel.createComponent
+   * and BazelExperimentalProjectSettingsPanel.createComponent.
+   * The returned function should be able to add the new setting ui components into the bazel setting panel.
+   * See BazelSettingsPanel.createComponent for more details.
+   * */
   fun addGeneralSettings(): Panel.() -> Unit
 
+  /**
+   * This function returns a function which will be called in BazelSettingsPanel.createComponent
+   * and BazelExperimentalProjectSettingsPanel.createComponent.
+   * The returned function should be able to add the new setting ui components
+   * into the bazel experimental settings panel.
+   * See BazelSettingsPanel.createComponent for more details.
+   * */
   fun addExperimentalSettings(): Panel.() -> Unit
 
+  /**
+   * This function is called in BazelSettingsPanel.isModified and BazelExperimentalProjectSettingsPanel.isModified.
+   * It should be able to indicate whether there are any changes in the settings injected by the provider
+   * */
   fun isModified(): Boolean
 
+  /**
+   * This function is called in BazelSettingsPanel.apply and BazelExperimentalProjectSettingsPanel.apply
+   * It should perform the necessary operations to make the change go into effect(e.g. resync ...)
+   * */
   fun apply()
 
   companion object {
