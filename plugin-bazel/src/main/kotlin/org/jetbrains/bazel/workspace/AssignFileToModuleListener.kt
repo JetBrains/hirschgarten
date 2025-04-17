f429e562b7a79057c69a889e7c517ea4e9d96f7b
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2025-04-17 12:32:45 +0200

    fix assign file listener

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/workspace/AssignFileToModuleListener.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/workspace/AssignFileToModuleListener.kt
@@ -49,7 +49,6 @@ import org.jetbrains.bazel.workspacemodel.entities.BspDummyEntitySource
 import org.jetbrains.bsp.protocol.InverseSourcesParams
 import org.jetbrains.bsp.protocol.InverseSourcesResult
 import org.jetbrains.bsp.protocol.TextDocumentIdentifier
-import org.jetbrains.kotlin.config.KOTLIN_SOURCE_ROOT_TYPE_ID
 
 class AssignFileToModuleListener : BulkFileListener {
   private val pendingEvents = mutableMapOf<Project, MutableList<VFileEvent>>()
@@ -249,7 +248,7 @@ private suspend fun VirtualFileUrl.addToModule(
   val sourceRootType =
     when (extension) {
       "java" -> JAVA_SOURCE_ROOT_ENTITY_TYPE_ID
-      "kt" -> SourceRootTypeId(KOTLIN_SOURCE_ROOT_TYPE_ID)
+      "kt" -> SourceRootTypeId("kotlin-source")
       "py" -> SourceRootTypeId("python-source")
       else -> {
         logger.warn("Bazel recognised a file as a source, but we failed to parse its extension: .$extension")
