68f729e32498e2e81dcaf6f9db608c819c5660dd
Jiaming Tang <jiaming.tang@jetbrains.com>
2025-04-17 09:09:40 +0000

    [fix] seperate kotlin run line marker | #BAZEL-750
    GitOrigin-RevId: ed9a7b8698bd0110f9d3e7dd37e318fb92435997

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/kotlin/ui/gutters/BazelKotlinRunLineMarkerContributor.kt
@@ -0,0 +1,15 @@
+package org.jetbrains.bazel.kotlin.ui.gutters
+
+import com.intellij.psi.PsiNameIdentifierOwner
+import org.jetbrains.bazel.jvm.ui.gutters.BazelJavaRunLineMarkerContributor
+import org.jetbrains.kotlin.psi.KtClassOrObject
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
+
+class BazelKotlinRunLineMarkerContributor : BazelJavaRunLineMarkerContributor() {
+  override fun PsiNameIdentifierOwner.getClassName(): String? = this.getStrictParentOfType<KtClassOrObject>()?.name
+
+  override fun PsiNameIdentifierOwner.isClassOrMethod(): Boolean = this is KtClassOrObject || this is KtNamedFunction
+
+  override fun PsiNameIdentifierOwner.isMethod(): Boolean = this is KtNamedFunction
+}
