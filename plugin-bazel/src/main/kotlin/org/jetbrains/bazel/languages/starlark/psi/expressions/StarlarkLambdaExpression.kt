abd03c4778d1fe300adcddfb3aa57c58b09d0bf2
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-04-17 18:04:52 +0200

    [fix] Don't report lambda parameters as unused

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/StarlarkLambdaExpression.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/StarlarkLambdaExpression.kt
@@ -3,7 +3,17 @@ package org.jetbrains.bazel.languages.starlark.psi.expressions
 import com.intellij.lang.ASTNode
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkBaseElement
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkElementVisitor
+import org.jetbrains.bazel.languages.starlark.psi.functions.StarlarkCallable
+import org.jetbrains.bazel.languages.starlark.psi.functions.StarlarkMandatoryParameter
+import org.jetbrains.bazel.languages.starlark.psi.functions.StarlarkOptionalParameter
+import org.jetbrains.bazel.languages.starlark.psi.functions.StarlarkParameter
+import org.jetbrains.bazel.languages.starlark.psi.functions.StarlarkParameterList
 
-class StarlarkLambdaExpression(node: ASTNode) : StarlarkBaseElement(node) {
+class StarlarkLambdaExpression(node: ASTNode) : StarlarkBaseElement(node), StarlarkCallable {
   override fun acceptVisitor(visitor: StarlarkElementVisitor) = visitor.visitLambdaExpression(this)
+
+  override fun getNamedParameters(): List<StarlarkParameter> =
+    getParameters().filter { it is StarlarkMandatoryParameter || it is StarlarkOptionalParameter }
+
+  override fun getParameters(): List<StarlarkParameter> = findChildByClass(StarlarkParameterList::class.java)?.getParameters().orEmpty()
 }
