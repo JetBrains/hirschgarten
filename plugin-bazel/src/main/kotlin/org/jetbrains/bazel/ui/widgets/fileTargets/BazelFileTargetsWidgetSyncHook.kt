9d1c3066b1abb4d8f948273b950387332cbebae4
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2025-04-18 12:16:54 +0000

    [maintenance] move `BazelFileTargetsWidgetSyncHook` to post sync hook
    GitOrigin-RevId: c6d62a0176053b560d4a1effc563f6560d6622fa

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/widgets/fileTargets/BazelFileTargetsWidgetSyncHook.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/widgets/fileTargets/BazelFileTargetsWidgetSyncHook.kt
@@ -1,17 +1,13 @@
 package org.jetbrains.bazel.ui.widgets.fileTargets
 
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.project.Project
-import org.jetbrains.bazel.sync.ProjectSyncHook
-import org.jetbrains.bazel.sync.ProjectSyncHook.ProjectSyncHookEnvironment
+import org.jetbrains.bazel.sync.ProjectPostSyncHook
 
 /**
  * A sync hook that updates the BazelFileTargetsWidget when a sync event occurs.
  */
-class BazelFileTargetsWidgetSyncHook : ProjectSyncHook {
-  override fun isEnabled(project: Project): Boolean = true
-
-  override suspend fun onSync(environment: ProjectSyncHookEnvironment) {
+class BazelFileTargetsWidgetSyncHook : ProjectPostSyncHook {
+  override suspend fun onPostSync(environment: ProjectPostSyncHook.ProjectPostSyncHookEnvironment) {
     val project = environment.project
     ApplicationManager.getApplication().invokeLater {
       project.updateBazelFileTargetsWidget()
