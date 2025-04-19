afb153e193e0ab5f17409dba4b944a3d044ee56b
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-04-19 08:02:42 +0200

    [fix] Track references to list comprehension variables
    # Conflicts:
    #	plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/SearchUtils.kt
    #	plugin-bazel/src/test/testData/starlark/annotation/UnusedDeclarationTestData.bzl

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/StarlarkListCompExpression.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/StarlarkListCompExpression.kt
@@ -6,4 +6,13 @@ import org.jetbrains.bazel.languages.starlark.psi.StarlarkElementVisitor
 
 class StarlarkListCompExpression(node: ASTNode) : StarlarkBaseElement(node) {
   override fun acceptVisitor(visitor: StarlarkElementVisitor) = visitor.visitListCompExpression(this)
+
+  fun getCompVariables(): List<StarlarkTargetExpression> =
+    getCompVariable()?.let(::listOf) ?: getCompTuple()?.getTargetExpressions() ?: emptyList()
+
+  private fun getCompVariable(): StarlarkTargetExpression? = findChildByClass(StarlarkTargetExpression::class.java)
+
+  private fun getCompTuple(): StarlarkTupleExpression? =
+    findChildByClass(StarlarkTupleExpression::class.java)
+      ?: findChildByClass(StarlarkParenthesizedExpression::class.java)?.getTuple()
 }
--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/SearchUtils.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/SearchUtils.kt
@@ -5,6 +5,7 @@ import com.intellij.psi.PsiFileSystemItem
 import com.intellij.util.Processor
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkElement
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkFile
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkListCompExpression
 import org.jetbrains.bazel.languages.starlark.psi.functions.StarlarkCallable
 import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkForStatement
 import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkStatementList
@@ -36,6 +37,7 @@ object SearchUtils {
       is StarlarkCallable -> parent.searchInParameters(processor)
       is StarlarkForStatement -> parent.searchInLoopVariables(processor)
       is StarlarkStatementList -> parent.searchInAssignmentsAndFunctionDeclarations(processor)
+      is StarlarkListCompExpression -> parent.searchInListComprehension(processor)
       else -> true
     }
 
@@ -48,4 +50,7 @@ object SearchUtils {
 
   private fun StarlarkForStatement.searchInLoopVariables(processor: Processor<StarlarkElement>): Boolean =
     getLoopVariables().all { processor.process(it) }
+
+  private fun StarlarkListCompExpression.searchInListComprehension(processor: Processor<StarlarkElement>): Boolean =
+    getCompVariables().all { processor.process(it) }
 }
