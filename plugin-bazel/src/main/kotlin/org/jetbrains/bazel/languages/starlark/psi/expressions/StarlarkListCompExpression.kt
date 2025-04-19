d17cf65ae3a81195bc73e760b7832a41a6491a49
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-04-19 22:57:05 +0200

    Generalize to cover dict comprehensions

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/StarlarkListCompExpression.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/psi/expressions/StarlarkListCompExpression.kt
@@ -1,18 +1,8 @@
 package org.jetbrains.bazel.languages.starlark.psi.expressions
 
 import com.intellij.lang.ASTNode
-import org.jetbrains.bazel.languages.starlark.psi.StarlarkBaseElement
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkElementVisitor
 
-class StarlarkListCompExpression(node: ASTNode) : StarlarkBaseElement(node) {
+class StarlarkListCompExpression(node: ASTNode) : StarlarkCompExpression(node) {
   override fun acceptVisitor(visitor: StarlarkElementVisitor) = visitor.visitListCompExpression(this)
-
-  fun getCompVariables(): List<StarlarkTargetExpression> =
-    getCompVariable()?.let(::listOf) ?: getCompTuple()?.getTargetExpressions() ?: emptyList()
-
-  private fun getCompVariable(): StarlarkTargetExpression? = findChildByClass(StarlarkTargetExpression::class.java)
-
-  private fun getCompTuple(): StarlarkTupleExpression? =
-    findChildByClass(StarlarkTupleExpression::class.java)
-      ?: findChildByClass(StarlarkParenthesizedExpression::class.java)?.getTuple()
 }
--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/SearchUtils.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/SearchUtils.kt
@@ -5,7 +5,7 @@ import com.intellij.psi.PsiFileSystemItem
 import com.intellij.util.Processor
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkElement
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkFile
-import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkListCompExpression
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkCompExpression
 import org.jetbrains.bazel.languages.starlark.psi.functions.StarlarkCallable
 import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkForStatement
 import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkStatementList
@@ -37,7 +37,7 @@ object SearchUtils {
       is StarlarkCallable -> parent.searchInParameters(processor)
       is StarlarkForStatement -> parent.searchInLoopVariables(processor)
       is StarlarkStatementList -> parent.searchInAssignmentsAndFunctionDeclarations(processor)
-      is StarlarkListCompExpression -> parent.searchInListComprehension(processor)
+      is StarlarkCompExpression -> parent.searchInComprehension(processor)
       else -> true
     }
 
@@ -51,6 +51,6 @@ object SearchUtils {
   private fun StarlarkForStatement.searchInLoopVariables(processor: Processor<StarlarkElement>): Boolean =
     getLoopVariables().all { processor.process(it) }
 
-  private fun StarlarkListCompExpression.searchInListComprehension(processor: Processor<StarlarkElement>): Boolean =
+  private fun StarlarkCompExpression.searchInComprehension(processor: Processor<StarlarkElement>): Boolean =
     getCompVariables().all { processor.process(it) }
 }
