60b57c55459b3e19b592fa38eef0af024c59fa0b
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2025-04-23 10:41:40 +0000

    [bazel] Prepare for moving Bazel plugin into monorepo
    Co-authored-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    Merge-request: IJ-MR-155857
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: 69f876a1d8db4a1785e04225e3f9b6ee994c29ce

---
+++ plugin-bazel/src/main/gen/com/intellij/bazel/plugin/icons/BazelPluginIcons.java
@@ -0,0 +1,27 @@
+package com.intellij.bazel.plugin.icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class BazelPluginIcons {
+  private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, BazelPluginIcons.class.getClassLoader(), cacheKey, flags);
+  }
+  /** 16x16 */ public static final @NotNull Icon Bazel = load("icons/bazel.svg", 1576337156, 2);
+  /** 16x16 */ public static final @NotNull Icon BazelConfig = load("icons/bazelConfig.svg", -1748716579, 2);
+  /** 16x16 */ public static final @NotNull Icon BazelDirectory = load("icons/bazelDirectory.svg", 827159797, 2);
+  /** 16x16 */ public static final @NotNull Icon BazelError = load("icons/bazelError.svg", -1702626701, 2);
+  /** 16x16 */ public static final @NotNull Icon BazelReload = load("icons/bazelReload.svg", -2014973098, 2);
+  /** 16x16 */ public static final @NotNull Icon BazelWarning = load("icons/bazelWarning.svg", 1467049691, 2);
+  /** 16x16 */ public static final @NotNull Icon BuildAndResync = load("icons/buildAndResync.svg", 1469339990, 2);
+  /** 16x16 */ public static final @NotNull Icon RedBazel = load("icons/redBazel.svg", -105887228, 0);
+  /** 16x16 */ public static final @NotNull Icon Resync = load("icons/resync.svg", -1179655148, 2);
+  /** 16x16 */ public static final @NotNull Icon ToolWindowBazel = load("icons/toolWindowBazel.svg", -617285515, 2);
+  /** 20x20 */ public static final @NotNull Icon ToolWindowBazel_20x20 = load("icons/toolWindowBazel@20x20.svg", -1095987536, 2);
+}
