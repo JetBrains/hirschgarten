de25f11d034153d2017abab88a4ee8e89c0becf9
Lev Leontev <lev.leontev@jetbrains.com>
2025-04-30 13:08:56 +0000

    BAZEL-1973 [fix] Avoid a read lock in LabelWithPreview#getTargetElement to avoid deadlock due to thread starvation
    Merge-request: IJ-MR-161700
    Merged-by: Lev Leontev <lev.leontev@jetbrains.com>
    
    GitOrigin-RevId: 69b627408fd32181464054e8dd6cce8d9cbed218

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/widgets/LabelSearchEverywhereContributor.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/widgets/LabelSearchEverywhereContributor.kt
@@ -5,9 +5,11 @@ import com.intellij.ide.actions.searcheverywhere.SearchEverywhereContributor
 import com.intellij.ide.actions.searcheverywhere.SearchEverywhereContributorFactory
 import com.intellij.ide.actions.searcheverywhere.SearchEverywherePreviewProvider
 import com.intellij.ide.actions.searcheverywhere.WeightedSearchEverywhereContributor
+import com.intellij.ide.util.EditorHelper
 import com.intellij.navigation.ItemPresentation
 import com.intellij.navigation.PsiElementNavigationItem
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
@@ -15,11 +17,12 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.codeStyle.NameUtil
 import com.intellij.ui.SimpleListCellRenderer
 import com.intellij.util.Processor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.bazel.assets.BazelPluginIcons
 import org.jetbrains.bazel.config.BazelPluginConstants
 import org.jetbrains.bazel.config.isBazelProject
 import org.jetbrains.bazel.coroutines.BazelCoroutineService
-import org.jetbrains.bazel.label.Label
 import org.jetbrains.bazel.languages.starlark.references.resolveLabel
 import org.jetbrains.bazel.languages.starlark.repomapping.toShortString
 import org.jetbrains.bazel.target.targetUtils
@@ -30,7 +33,7 @@ class LabelSearchEverywhereContributor(private val project: Project) :
   SearchEverywherePreviewProvider {
   private val listCellRenderer: ListCellRenderer<LabelWithPreview> =
     SimpleListCellRenderer.create { label, value, index ->
-      label.text = value.label.toShortString(project)
+      label.text = value.displayName
       label.icon = BazelPluginIcons.bazel
     }
 
@@ -59,7 +62,9 @@ class LabelSearchEverywhereContributor(private val project: Project) :
       if (progressIndicator.isCanceled) break
       val fullString = label.toString()
       if (matcher.matches(fullString)) {
-        val foundItemDescriptor = FoundItemDescriptor(LabelWithPreview(label, project), matcher.matchingDegree(fullString))
+        val targetElement = runReadAction { resolveLabel(project, label) } ?: continue
+        val displayName = label.toShortString(project)
+        val foundItemDescriptor = FoundItemDescriptor(LabelWithPreview(displayName, targetElement), matcher.matchingDegree(fullString))
         if (!runReadAction { consumer.process(foundItemDescriptor) }) break
       }
     }
@@ -71,15 +76,17 @@ class LabelSearchEverywhereContributor(private val project: Project) :
     searchText: String,
   ): Boolean {
     BazelCoroutineService.getInstance(project).start {
-      jumpToBuildFile(project, selected.label)
+      withContext(Dispatchers.EDT) {
+        EditorHelper.openInEditor(selected.element, true, true)
+      }
     }
     return true
   }
 
   override fun getElementsRenderer(): ListCellRenderer<in LabelWithPreview> = listCellRenderer
 
-  class LabelWithPreview(val label: Label, private val project: Project) : PsiElementNavigationItem {
-    override fun getTargetElement(): PsiElement? = runReadAction { resolveLabel(project, label) }
+  class LabelWithPreview(val displayName: String, val element: PsiElement) : PsiElementNavigationItem {
+    override fun getTargetElement(): PsiElement = element
 
     override fun getName(): String? = null
 
