62a9966f2d662293e754fa8f7d1ca8bd6bf38f6c
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2025-05-05 15:01:13 +0000

    [bazel][maintenance] add IDE Starter Test to make sure Bazel projects don't resync on reopening.
    - Currently, this test only prevents the regression error on a cleanly imported project.
    
    
    Merge-request: IJ-MR-162047
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: e97e3f778f9280e69c1f9438a1a96544ecc8b552

---
+++ plugin-bazel/src/test/kotlin/org/jetbrains/bazel/reopen/noResync/ReopenWithoutResync.kt
@@ -0,0 +1,67 @@
+package org.jetbrains.bazel.reopen.noResync
+
+import com.intellij.driver.sdk.WaitForException
+import com.intellij.driver.sdk.invokeAction
+import com.intellij.driver.sdk.ui.components.common.ideFrame
+import com.intellij.driver.sdk.ui.components.common.welcomeScreen
+import com.intellij.driver.sdk.wait
+import com.intellij.driver.sdk.waitForIndicators
+import com.intellij.ide.starter.driver.engine.runIdeWithDriver
+import com.intellij.ide.starter.project.ProjectInfoSpec
+import com.intellij.tools.ide.performanceTesting.commands.CommandChain
+import com.intellij.tools.ide.performanceTesting.commands.takeScreenshot
+import org.jetbrains.bazel.config.BazelPluginBundle
+import org.jetbrains.bazel.ideStarter.IdeStarterBaseProjectTest
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+
+/**
+ * The current state of the test only prevents the regression error on a cleanly imported project.
+ *
+ * However, this issue may also happen when migrating the workspace data (in `.idea/workspace.xml`) from one version of the plugin to another.
+ * Refer to [BAZEL-1967](https://youtrack.jetbrains.com/issue/BAZEL-1967) for more details.
+ * A more complex version of this test should be introduced to capture this case. e.g., importing the project with an older plugin version,
+ * and reopening such a project with the current plugin version.
+ */
+class ReopenWithoutResync : IdeStarterBaseProjectTest() {
+  override val projectInfo: ProjectInfoSpec
+    get() =
+      com.intellij.ide.starter.project.GitProjectInfo(
+        repositoryUrl = "https://github.com/JetBrainsBazelBot/simpleBazelProjectsForTesting.git",
+        commitHash = "c88d3cb84fae0c8a42cd8ddd78306a40595ff764",
+        branchName = "main",
+        projectHomeRelativePath = { it.resolve("simpleKotlinTest") },
+        isReusable = false,
+        configureProjectBeforeUse = ::configureProjectBeforeUse,
+      )
+
+  @org.junit.jupiter.api.Test
+  fun openProject() {
+    val commands =
+      CommandChain()
+        .takeScreenshot("startSync")
+    createContext()
+      .runIdeWithDriver(runTimeout = timeout, commands = commands)
+      .useDriverAndCloseIde {
+        ideFrame {
+          val buildView = x { byType("com.intellij.build.BuildView") }
+          assert(buildView.getAllTexts().any { it.text.contains(BazelPluginBundle.message("console.task.sync.in.progress")) })
+        }
+        takeScreenshot("whileSyncing")
+        waitForIndicators(10.minutes)
+        invokeAction("CloseProject")
+        // simulating reopening project
+        welcomeScreen { clickRecentProject("simpleKotlinTest") }
+        ideFrame {
+          wait(3.seconds)
+          takeScreenshot("afterReopeningProject")
+          try {
+            val buildView = x { byType("com.intellij.build.BuildView") }
+            assert(!buildView.getAllTexts().any { it.text.contains(BazelPluginBundle.message("console.task.sync.in.progress")) })
+          } catch (e: Exception) {
+            assert(e is WaitForException)
+          }
+        }
+      }
+  }
+}
