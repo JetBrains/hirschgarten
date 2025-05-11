54d8138d94b5719a27eca0137ef56ffad4fc2171
Robert Grigoryan <robert.grigoryan@jetbrains.com>
2025-05-11 15:59:52 +0200

    BAZEL-1506 [feature] Import ProjectViewSection Parser in ProjectViewSection.kt
    GitOrigin-RevId: a97b6d44dfce7e850c2113a3e0479db50d19a0ed

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/language/ProjectViewSection.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/language/ProjectViewSection.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.bazel.languages.projectview.language
 
 import com.intellij.openapi.editor.colors.TextAttributesKey
+import org.jetbrains.bazel.languages.projectview.language.ProjectViewSectionParser.ListSectionParser
+import org.jetbrains.bazel.languages.projectview.language.ProjectViewSectionParser.ScalarSectionParser
 
 sealed class ProjectViewSection(open val textAttributesKey: TextAttributesKey?) {
   data class Scalar<out T>(val item: T, override val textAttributesKey: TextAttributesKey?) : ProjectViewSection(textAttributesKey)
@@ -10,27 +12,27 @@ sealed class ProjectViewSection(open val textAttributesKey: TextAttributesKey?) 
 
   companion object {
     /** A map of registered section keywords. */
-    val KEYWORD_MAP: Map<ProjectViewSyntaxKey, ProjectViewSectionParser> =
-      mapOf(
-        "allow_manual_targets_sync" to ProjectViewSectionParser.ScalarSectionParser.Boolean,
-        "android_min_sdk" to ProjectViewSectionParser.ScalarSectionParser.Int,
-        "bazel_binary" to ProjectViewSectionParser.ScalarSectionParser.Path,
-        "build_flags" to ProjectViewSectionParser.ListSectionParser.Identifiers,
-        "derive_targets_from_directories" to ProjectViewSectionParser.ScalarSectionParser.Boolean,
-        "directories" to ProjectViewSectionParser.ListSectionParser.Paths,
-        "enable_native_android_rules" to ProjectViewSectionParser.ScalarSectionParser.Boolean,
-        "enabled_rules" to ProjectViewSectionParser.ListSectionParser.Identifiers,
-        "experimental_add_transitive_compile_time_jars" to ProjectViewSectionParser.ScalarSectionParser.Boolean,
-        "experimental_no_prune_transitive_compile_time_jars_patterns" to ProjectViewSectionParser.ListSectionParser.Identifiers,
-        "experimental_transitive_compile_time_jars_target_kinds" to ProjectViewSectionParser.ListSectionParser.Identifiers,
-        "ide_java_home_override" to ProjectViewSectionParser.ScalarSectionParser.Path,
-        "import_depth" to ProjectViewSectionParser.ScalarSectionParser.Int,
-        "shard_approach" to ProjectViewSectionParser.ScalarSectionParser.Identifier,
-        "shard_sync" to ProjectViewSectionParser.ScalarSectionParser.Boolean,
-        "sync_flags" to ProjectViewSectionParser.ListSectionParser.Identifiers,
-        "target_shard_size" to ProjectViewSectionParser.ScalarSectionParser.Int,
-        "targets" to ProjectViewSectionParser.ListSectionParser.Paths,
-        "import_run_configurations" to ProjectViewSectionParser.ListSectionParser.Identifiers,
+    val KEYWORD_MAP =
+      mapOf<ProjectViewSyntaxKey, ProjectViewSectionParser>(
+        "allow_manual_targets_sync" to ScalarSectionParser.Boolean,
+        "android_min_sdk" to ScalarSectionParser.Int,
+        "bazel_binary" to ScalarSectionParser.Path,
+        "build_flags" to ListSectionParser.Identifiers,
+        "derive_targets_from_directories" to ScalarSectionParser.Boolean,
+        "directories" to ListSectionParser.Paths,
+        "enable_native_android_rules" to ScalarSectionParser.Boolean,
+        "enabled_rules" to ListSectionParser.Identifiers,
+        "experimental_add_transitive_compile_time_jars" to ScalarSectionParser.Boolean,
+        "experimental_no_prune_transitive_compile_time_jars_patterns" to ListSectionParser.Identifiers,
+        "experimental_transitive_compile_time_jars_target_kinds" to ListSectionParser.Identifiers,
+        "ide_java_home_override" to ScalarSectionParser.Path,
+        "import_depth" to ScalarSectionParser.Int,
+        "shard_approach" to ScalarSectionParser.Identifier,
+        "shard_sync" to ScalarSectionParser.Boolean,
+        "sync_flags" to ListSectionParser.Identifiers,
+        "target_shard_size" to ScalarSectionParser.Int,
+        "targets" to ListSectionParser.Paths,
+        "import_run_configurations" to ListSectionParser.Identifiers,
       )
   }
 }
