f026659f15a55c8845bbdb84fed5ed09e15c62cc
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2025-05-14 15:53:36 +0200

    format all the xml files
    GitOrigin-RevId: 4125d49cbffc80f146470d4eb55ec2ff9fb08577

--- tools/format/prettier.config.cjs
+++ tools/format/prettier.config.cjs
@@ -1,6 +1,9 @@
 const config = {
   tabWidth: 2,
   printWidth: 80,
+  xmlWhitespaceSensitivity: "ignore",
+  bracketSameLine: true,
+  singleAttributePerLine: true,
   plugins: [
     require("@prettier/plugin-xml"),
   ],
