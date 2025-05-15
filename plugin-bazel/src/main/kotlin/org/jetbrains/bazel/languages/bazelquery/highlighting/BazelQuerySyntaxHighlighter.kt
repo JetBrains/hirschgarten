23e20748fabbf52fcbd0ae59ca4746c141cc42d2
Marta Hering-Zagrocka <mh448197@students.mimuw.edu.pl>
2025-05-15 13:35:42 +0000

    BAZEL-1542 [bazel] Bazel Query - language, highlighting, completions, documentation
    Bazel Query:
    - language (register in plugin.xml)
    - highlighting (BAZEL-2027)
    - completions (BAZEL-2024)
    - documentation (BAZEL-2025)
    
    Merge-request: IJ-MR-161766
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: 0542c263b1f0ff9aa32b9c2df4620002c8c30b8a

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelquery/highlighting/BazelQuerySyntaxHighlighter.kt
@@ -0,0 +1,40 @@
+package org.jetbrains.bazel.languages.bazelquery.highlighting
+
+import com.intellij.lexer.Lexer
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.fileTypes.SyntaxHighlighter
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.tree.IElementType
+import org.jetbrains.bazel.languages.bazelquery.elements.BazelQueryTokenSets
+import org.jetbrains.bazel.languages.bazelquery.elements.BazelQueryTokenTypes
+import org.jetbrains.bazel.languages.bazelquery.lexer.BazelQueryLexer
+
+object BazelQuerySyntaxHighlighter : SyntaxHighlighterBase() {
+  private val keys =
+    mapOf(
+      *BazelQueryTokenSets.WORDS.types
+        .map { it to BazelQueryHighlightingColors.WORD }
+        .toTypedArray(),
+      BazelQueryTokenTypes.INTEGER to BazelQueryHighlightingColors.INTEGER,
+      *BazelQueryTokenSets.OPERATIONS.types
+        .map { it to BazelQueryHighlightingColors.OPERATION }
+        .toTypedArray(),
+      *BazelQueryTokenSets.FLAGS.types
+        .map { it to BazelQueryHighlightingColors.FLAG }
+        .toTypedArray(),
+      *BazelQueryTokenSets.FLAG_VALS.types
+        .map { it to BazelQueryHighlightingColors.FLAG_VAL }
+        .toTypedArray(),
+    )
+
+  override fun getHighlightingLexer(): Lexer = BazelQueryLexer()
+
+  override fun getTokenHighlights(tokenType: IElementType): Array<TextAttributesKey> = pack(keys[tokenType])
+}
+
+class BazelQuerySyntaxHighlighterFactory : SyntaxHighlighterFactory() {
+  override fun getSyntaxHighlighter(project: Project?, virtualFile: VirtualFile?): SyntaxHighlighter = BazelQuerySyntaxHighlighter
+}
