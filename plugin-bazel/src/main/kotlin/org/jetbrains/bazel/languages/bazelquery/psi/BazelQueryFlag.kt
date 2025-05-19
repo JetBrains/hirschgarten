94c5d58ed79cd9b48b1cda94c64135400c6c6761
Marta Hering-Zagrocka <mh448197@students.mimuw.edu.pl>
2025-05-19 23:55:53 +0200

    [bazel] [fix] add swingx to intellij.bazel.plugin.iml and fix documentation for functions and flags
    GitOrigin-RevId: 40265129c5495e8af8bcb4f154eacf203a8ec36b

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelquery/psi/BazelQueryFlag.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelquery/psi/BazelQueryFlag.kt
@@ -3,10 +3,14 @@ package org.jetbrains.bazel.languages.bazelquery.psi
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import org.jetbrains.bazel.languages.bazelquery.elements.BazelQueryTokenTypes
+import org.jetbrains.bazel.languages.bazelquery.references.BazelQueryFlagNameReference
 
 class BazelQueryFlag(node: ASTNode) : BazelQueryBaseElement(node) {
   override fun acceptVisitor(visitor: BazelQueryElementVisitor) = visitor.visitFlag(this)
 
   val name: PsiElement?
     get() = findChildByType(BazelQueryTokenTypes.FLAG)
+
+  override fun getOwnReferences(): Collection<BazelQueryFlagNameReference> =
+    name?.let { listOf(BazelQueryFlagNameReference(this)) } ?: emptyList()
 }
