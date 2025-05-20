0094f0532b8aa964c222706356716f8584875eac
Piotr Pływacz <piotr.plywacz@jetbrains.com>
2025-05-20 12:13:21 +0000

    BAZEL-1888 [fix] Remove duplicate quote at the end of completion
    Correct completions of visibility, Bazel targtes, and source files so that after pressing ENTER on a selected option, there is no additional quote added at the end of the line.
    
    Merge-request: IJ-MR-162934
    Merged-by: Piotr Pływacz <piotr.plywacz@jetbrains.com>
    
    GitOrigin-RevId: 4244361693c91006daa7bc36b054db5c91aacd5c

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/StarlarkVisibilityReference.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/StarlarkVisibilityReference.kt
@@ -1,14 +1,13 @@
 package org.jetbrains.bazel.languages.starlark.references
 
-import com.intellij.codeInsight.completion.PrioritizedLookupElement
 import com.intellij.codeInsight.lookup.LookupElement
-import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReferenceBase
 import com.intellij.util.PlatformIcons
 import org.jetbrains.bazel.config.isBazelProject
 import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkStringLiteralExpression
+import org.jetbrains.bazel.languages.starlark.psi.expressions.getCompletionLookupElemenent
 import org.jetbrains.bazel.target.targetUtils
 
 class StarlarkVisibilityReference(element: StarlarkStringLiteralExpression) :
@@ -18,26 +17,17 @@ class StarlarkVisibilityReference(element: StarlarkStringLiteralExpression) :
   override fun getVariants(): Array<LookupElement> {
     val project = element.project
     if (!project.isBazelProject) return emptyArray()
-
+    val icon = PlatformIcons.VARIABLE_ICON
     val shortTargets = project.targetUtils.allTargetsAndLibrariesLabels
-    val targetVisibilities = shortTargets.map { visibilityLookupElement(it) }.toTypedArray()
-    val pkgVisibilities = shortTargets.map { visibilityLookupElement("$it:__pkg__") }.toTypedArray()
-    val subpkgVisibilities = shortTargets.map { visibilityLookupElement("$it:__subpackages__") }.toTypedArray()
+    val targetVisibilities = shortTargets.map { getCompletionLookupElemenent(it, icon) }.toTypedArray()
+    val pkgVisibilities = shortTargets.map { getCompletionLookupElemenent("$it:__pkg__", icon) }.toTypedArray()
+    val subpkgVisibilities = shortTargets.map { getCompletionLookupElemenent("$it:__subpackages__", icon) }.toTypedArray()
 
     val predefined =
       listOf(
-        visibilityLookupElement("//visibility:public", 1.0),
-        visibilityLookupElement("//visibility:private", 1.0),
+        getCompletionLookupElemenent("//visibility:public", icon, 1.0),
+        getCompletionLookupElemenent("//visibility:private", icon, 1.0),
       )
     return pkgVisibilities + subpkgVisibilities + targetVisibilities + predefined
   }
-
-  private fun visibilityLookupElement(name: String, priority: Double = 0.0): LookupElement =
-    PrioritizedLookupElement.withPriority(
-      LookupElementBuilder
-        .create("\"" + name + "\"")
-        .withIcon(PlatformIcons.VARIABLE_ICON)
-        .withPresentableText(name),
-      priority,
-    )
 }
