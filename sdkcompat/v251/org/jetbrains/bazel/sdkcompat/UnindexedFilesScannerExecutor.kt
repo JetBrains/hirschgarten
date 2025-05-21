70b6549b87eb6d03427c1f0da9d08727191cca3b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-05-21 20:09:26 +0200

    ProjectSyncTask - get rid of runBlocking and disable auto save (and sync)
    ProjectSyncTask - get rid of runBlocking
    
    GitOrigin-RevId: 2d50304d6ca5b52891c482d3d2f79710df8477af

---
+++ sdkcompat/v251/org/jetbrains/bazel/sdkcompat/UnindexedFilesScannerExecutor.kt
@@ -0,0 +1,21 @@
+package org.jetbrains.bazel.sdkcompat
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.UnindexedFilesScannerExecutor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+suspend fun suspendScanningAndIndexingThenExecute(activityName: String, project: Project, activity: suspend () -> Unit) {
+  coroutineScope {
+    // Use Dispatchers.IO to wait for the blocking call
+    withContext(Dispatchers.IO) {
+      UnindexedFilesScannerExecutor.getInstance(project).suspendScanningAndIndexingThenRun(activityName) {
+        runBlocking(coroutineContext) {
+          activity()
+        }
+      }
+    }
+  }
+}
