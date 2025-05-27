8de45effb2086118e66dbd0760957204f37d2c64
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-05-27 13:37:03 +0200

    IJ-CR-163718 change `suspend () -> Unit` to `suspend CoroutineScope.() -> Unit` because "otherwise you would have nasty problems with launch executed in some unrelated scope"
    GitOrigin-RevId: ae4a6fe0b253afec5738ab5e4617782833b11651

--- sdkcompat/v243/org/jetbrains/bazel/sdkcompat/UnindexedFilesScannerExecutor.kt
+++ sdkcompat/v243/org/jetbrains/bazel/sdkcompat/UnindexedFilesScannerExecutor.kt
@@ -2,6 +2,7 @@ package org.jetbrains.bazel.sdkcompat
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.UnindexedFilesScannerExecutor
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
@@ -10,7 +11,7 @@ import kotlinx.coroutines.withContext
 suspend fun suspendScanningAndIndexingThenExecute(
   activityName: String,
   project: Project,
-  activity: suspend () -> Unit,
+  activity: suspend CoroutineScope.() -> Unit,
 ) {
   coroutineScope {
     // Use Dispatchers.IO to wait for the blocking call
