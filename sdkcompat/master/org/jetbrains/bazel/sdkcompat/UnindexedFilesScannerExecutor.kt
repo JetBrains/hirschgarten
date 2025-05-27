2c812987cc827233d420beee19a23e12aaa4cbe6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-05-27 13:37:03 +0200

    IJ-CR-163718 change `suspend () -> Unit` to `suspend CoroutineScope.() -> Unit` because "otherwise you would have nasty problems with launch executed in some unrelated scope"
    GitOrigin-RevId: ae4a6fe0b253afec5738ab5e4617782833b11651

--- sdkcompat/master/org/jetbrains/bazel/sdkcompat/UnindexedFilesScannerExecutor.kt
+++ sdkcompat/master/org/jetbrains/bazel/sdkcompat/UnindexedFilesScannerExecutor.kt
@@ -5,11 +5,12 @@ import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.UnindexedFilesScannerExecutor
 import com.intellij.openapi.util.NlsContexts
+import kotlinx.coroutines.CoroutineScope
 
 suspend fun suspendScanningAndIndexingThenExecute(
   @NlsContexts.ProgressText activityName: String,
   project: Project,
-  activity: suspend () -> Unit,
+  activity: suspend CoroutineScope.() -> Unit,
 ) {
   UnindexedFilesScannerExecutor.getInstance(project).suspendScanningAndIndexingThenExecute(activityName) {
     serviceAsync<SaveAndSyncHandler>().disableAutoSave().use {
