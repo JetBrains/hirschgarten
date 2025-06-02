26ad79fb44ae171c836c3a0b5d511b5e57eeb03d
Lev Leontev <lev.leontev@jetbrains.com>
2025-06-02 19:05:10 +0200

    IDEA-372530 [bazel]: Use registerNonRecursiveFileSet for file-based source root support
    GitOrigin-RevId: 292c6bf6d68d4f80c4f867987a3cfa3f24604ea8

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/sync/DirectoriesSyncHook.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/sync/DirectoriesSyncHook.kt
@@ -1,12 +1,6 @@
 package org.jetbrains.bazel.flow.sync
 
-import com.intellij.openapi.application.readAction
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ProjectFileIndex
-import com.intellij.openapi.vcs.ProjectLevelVcsManager
-import com.intellij.openapi.vcs.VcsDirectoryMapping
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.platform.backend.workspace.WorkspaceModel
 import com.intellij.platform.backend.workspace.toVirtualFileUrl
 import com.intellij.platform.workspace.storage.impl.url.toVirtualFileUrl
@@ -31,10 +25,6 @@ class DirectoriesSyncHook : ProjectSyncHook {
 
       environment.diff.workspaceModelDiff.mutableEntityStorage
         .addEntity(entity)
-
-      environment.diff.workspaceModelDiff.addPostApplyAction {
-        removeExcludedVcsMappings(environment.project)
-      }
     }
   }
 
@@ -56,28 +46,5 @@ class DirectoriesSyncHook : ProjectSyncHook {
       excludedRoots = excludedRoots,
       entitySource = BazelProjectEntitySource,
     )
-  }
-
-  /**
-   * https://youtrack.jetbrains.com/issue/BAZEL-948
-   */
-  private suspend fun removeExcludedVcsMappings(project: Project) {
-    val manager = ProjectLevelVcsManager.getInstance(project)
-    val directoryMappings = manager.directoryMappings
-    val directoryMappingsWithoutExcludes = manager.directoryMappings.filter { mapping -> !isExcludedPath(project, mapping) }
-    if (directoryMappingsWithoutExcludes.size == directoryMappings.size) return
-    manager.directoryMappings = directoryMappingsWithoutExcludes
-  }
-
-  private suspend fun isExcludedPath(project: Project, mapping: VcsDirectoryMapping): Boolean {
-    if (mapping.isDefaultMapping) return false
-    val file = LocalFileSystem.getInstance().findFileByPath(mapping.directory) ?: return false
-    // If the project root is a child of the VCS root, then keep it
-    if (VfsUtilCore.isAncestor(file, project.rootDir, false)) return false
-    // Otherwise, if the VCS root is somewhere else (not inside our project directory), then remove it.
-    // Usually it is something like /private/var/tmp/_bazel_...
-    if (!VfsUtilCore.isAncestor(project.rootDir, file, false)) return true
-    val projectFileIndex = ProjectFileIndex.getInstance(project)
-    return readAction { projectFileIndex.isExcluded(file) }
   }
 }
