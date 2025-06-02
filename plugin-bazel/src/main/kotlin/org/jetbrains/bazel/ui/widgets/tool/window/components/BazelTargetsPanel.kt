bcc8b2904e4c34a2860566376528650f9ee54f8f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-06-02 08:32:22 +0200

    reduce visibility, make clear that update must be executed in EDT
    GitOrigin-RevId: acedb4bb8124d479e7ff9a0e073bae54b8ff54fa

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/widgets/tool/window/components/BazelTargetsPanel.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/widgets/tool/window/components/BazelTargetsPanel.kt
@@ -1,10 +1,10 @@
 package org.jetbrains.bazel.ui.widgets.tool.window.components
 
-import com.intellij.openapi.application.runInEdt
 import com.intellij.openapi.project.Project
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.components.JBScrollPane
+import com.intellij.util.concurrency.annotations.RequiresEdt
 import org.jetbrains.bazel.config.BazelPluginBundle
 import org.jetbrains.bazel.ui.widgets.tool.window.utils.BspShortcuts
 import org.jetbrains.bazel.ui.widgets.tool.window.utils.SimpleAction
@@ -13,13 +13,9 @@ import javax.swing.SwingConstants
 
 class BazelTargetsPanel(private val project: Project, private val model: BazelTargetsPanelModel) :
   JBPanel<BazelTargetsPanel>(BorderLayout()) {
-  val searchBarPanel = SearchBarPanel(model)
-  val targetTree =
-    BuildTargetTree(
-      project = project,
-      model = model,
-    )
-  val scrollPane = JBScrollPane(targetTree)
+  private val searchBarPanel = SearchBarPanel(model)
+  private val targetTree = BuildTargetTree(project = project, model = model)
+  private val scrollPane = JBScrollPane(targetTree)
 
   private val message =
     JBLabel(
@@ -56,36 +52,36 @@ class BazelTargetsPanel(private val project: Project, private val model: BazelTa
     remove(message)
   }
 
-  fun update() =
-    runInEdt {
-      // Update the tree highlighter to highlight search matches
-      val searchRegex = model.searchRegex
-      if (searchRegex != null) {
-        targetTree.cellRenderer = TargetTreeCellRenderer { QueryHighlighter.highlight(it, searchRegex) }
-      } else {
-        targetTree.cellRenderer = TargetTreeCellRenderer { it }
-      }
-
-      // Update visibility of components based on search results
-      if (!model.hasAnyTargets) {
-        message.text = BazelPluginBundle.message("widget.no.targets.message")
-        hideTargetTree()
-        showMessage()
-      } else if (model.isSearchActive && model.visibleTargets.isEmpty()) {
-        message.text = BazelPluginBundle.message("widget.target.search.no.results")
-        hideTargetTree()
-        showMessage()
-      } else {
-        hideMessage()
-        showTargetTree()
-      }
-
-      targetTree.updateTree()
-
-      // Refresh the panel
-      revalidate()
-      repaint()
+  @RequiresEdt
+  fun update() {
+    // Update the tree highlighter to highlight search matches
+    val searchRegex = model.searchRegex
+    if (searchRegex == null) {
+      targetTree.cellRenderer = TargetTreeCellRenderer { it }
+    } else {
+      targetTree.cellRenderer = TargetTreeCellRenderer { QueryHighlighter.highlight(it, searchRegex) }
     }
+
+    // Update visibility of components based on search results
+    if (!model.hasAnyTargets) {
+      message.text = BazelPluginBundle.message("widget.no.targets.message")
+      hideTargetTree()
+      showMessage()
+    } else if (model.isSearchActive && model.visibleTargets.isEmpty()) {
+      message.text = BazelPluginBundle.message("widget.target.search.no.results")
+      hideTargetTree()
+      showMessage()
+    } else {
+      hideMessage()
+      showTargetTree()
+    }
+
+    targetTree.updateTree()
+
+    // Refresh the panel
+    revalidate()
+    repaint()
+  }
 
   private fun registerMoveDownShortcut(searchBarPanel: SearchBarPanel) {
     val action =
