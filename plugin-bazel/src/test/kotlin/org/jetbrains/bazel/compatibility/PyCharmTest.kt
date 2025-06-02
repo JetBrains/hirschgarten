9ebdd8b9a1d59230371fc6c7d324fb1ef2cd281f
Jiaming Tang <jiaming.tang@jetbrains.com>
2025-06-02 10:53:50 +0000

    [bazel] [feature] add python gutters support for bazel plugin | #BAZEL-2023
    Merge-request: IJ-MR-162849
    Merged-by: Jiaming Tang <jiaming.tang@jetbrains.com>
    
    GitOrigin-RevId: 70918f4143a3fc4f9350c8b46139cd2b4c43f334

--- plugin-bazel/src/test/kotlin/org/jetbrains/bazel/compatibility/PyCharmTest.kt
+++ plugin-bazel/src/test/kotlin/org/jetbrains/bazel/compatibility/PyCharmTest.kt
@@ -1,15 +1,25 @@
 package org.jetbrains.bazel.compatibility
 
+import com.intellij.driver.client.Driver
+import com.intellij.driver.sdk.openFile
+import com.intellij.driver.sdk.ui.components.common.editorTabs
+import com.intellij.driver.sdk.ui.components.common.gutter
+import com.intellij.driver.sdk.ui.components.common.ideFrame
+import com.intellij.driver.sdk.ui.components.elements.popup
+import com.intellij.driver.sdk.ui.xQuery
+import com.intellij.driver.sdk.waitForIndicators
+import com.intellij.ide.starter.driver.engine.runIdeWithDriver
 import com.intellij.ide.starter.project.GitProjectInfo
 import com.intellij.ide.starter.project.ProjectInfoSpec
 import com.intellij.openapi.ui.playback.commands.AbstractCommand.CMD_PREFIX
 import com.intellij.tools.ide.performanceTesting.commands.CommandChain
-import com.intellij.tools.ide.performanceTesting.commands.exitApp
+import com.intellij.tools.ide.performanceTesting.commands.openFile
 import com.intellij.tools.ide.performanceTesting.commands.takeScreenshot
 import com.intellij.tools.ide.performanceTesting.commands.waitForSmartMode
 import org.jetbrains.bazel.ideStarter.IdeStarterBaseProjectTest
 import org.jetbrains.bazel.ideStarter.waitForBazelSync
 import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.minutes
 
 /**
  * ```sh
@@ -21,7 +31,7 @@ class PyCharmTest : IdeStarterBaseProjectTest() {
     get() =
       GitProjectInfo(
         repositoryUrl = "https://github.com/JetBrainsBazelBot/simpleBazelProjectsForTesting.git",
-        commitHash = "fbdb88d9d08d46ae8db57e23dd4034862b4075df",
+        commitHash = "a992706c09a5ec4ba50c86020b4ca67d13974eb8",
         branchName = "main",
         projectHomeRelativePath = { it.resolve("simpleMultiLanguageTest") },
         isReusable = false,
@@ -36,9 +46,44 @@ class PyCharmTest : IdeStarterBaseProjectTest() {
         .waitForBazelSync()
         .waitForSmartMode()
         .checkImportedModules()
-        .exitApp()
+        .openFile("python/bin.py")
 
-    createContext().runIDE(commands = commands, runTimeout = timeout)
+    createContext()
+      .runIdeWithDriver(commands = commands, runTimeout = timeout)
+      .useDriverAndCloseIde {
+        verifyRunLineMarkerText(listOf("Run", "Debug run"))
+      }
+  }
+
+  @Test
+  fun openBazelProjectWithTestFile() {
+    val commands =
+      CommandChain()
+        .takeScreenshot("startSync")
+        .waitForBazelSync()
+        .waitForSmartMode()
+        .checkImportedModules()
+        .openFile("python/test.py")
+
+    createContext()
+      .runIdeWithDriver(commands = commands, runTimeout = timeout)
+      .useDriverAndCloseIde {
+        verifyRunLineMarkerText(listOf("Test", "Debug test", "Run with Coverage"))
+      }
+  }
+
+  private fun Driver.verifyRunLineMarkerText(expectedTexts: List<String>) {
+    ideFrame {
+      waitForIndicators(5.minutes)
+      val gutterIcons = editorTabs().gutter().getGutterIcons()
+      val selectedGutterIcon = gutterIcons.first()
+      selectedGutterIcon.click()
+      val heavyWeightWindow = popup(xQuery { byClass("HeavyWeightWindow") })
+      takeScreenshot("afterClickingOnRunLineMarker")
+      val texts = heavyWeightWindow.getAllTexts()
+      assert(texts.size == expectedTexts.size)
+      expectedTexts.forEach { expected -> assert(texts.any { actual -> actual.text.contains(expected) }) }
+    }
   }
 
   private fun <T : CommandChain> T.checkImportedModules(): T = also { addCommand("${CMD_PREFIX}PyCharmCheckImportedModules") }
