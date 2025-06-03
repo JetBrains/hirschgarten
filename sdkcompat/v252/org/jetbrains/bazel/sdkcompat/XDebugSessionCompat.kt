e2f0ebafb3553a0af03aef7736ef34db6266591b
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2025-06-03 08:33:34 +0200

    [bazel][maintenance/fix] update IJ platform to `252.18003.27`
    GitOrigin-RevId: 0db9d92619445002bf32c5c06d549408b476be19

--- sdkcompat/v252/org/jetbrains/bazel/sdkcompat/XDebugSessionCompat.kt
+++ sdkcompat/v252/org/jetbrains/bazel/sdkcompat/XDebugSessionCompat.kt
@@ -1,9 +1,12 @@
 package org.jetbrains.bazel.sdkcompat
 
+import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.xdebugger.XDebugSession
 
 abstract class XDebugSessionCompat : XDebugSession {
   abstract fun isMixedModeCompat(): Boolean
 
   override fun isMixedMode(): Boolean = isMixedModeCompat()
+
+  override fun getExecutionEnvironment(): ExecutionEnvironment? = null
 }
