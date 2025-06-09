17020ebe013a64b398719fabdd21282e5f0d6f10
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2025-06-09 16:17:44 +0000

    [bazel][maintenance/fix] bump IJ Platform to `252.19874.12` | #BAZEL-2113 Done
    Merge-request: IJ-MR-165162
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: c68b6849df03f8b0762559be44a1478fa2d70483

--- sdkcompat/v252/org/jetbrains/bazel/sdkcompat/WorkspaceFileIndexCompat.kt
+++ sdkcompat/v252/org/jetbrains/bazel/sdkcompat/WorkspaceFileIndexCompat.kt
@@ -3,12 +3,4 @@ package org.jetbrains.bazel.sdkcompat
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndex
 
-fun WorkspaceFileIndex.isIndexableCompat(file: VirtualFile): Boolean =
-  findFileSet(
-    file,
-    true,
-    true,
-    true,
-    true,
-    true,
-  ) != null
+fun WorkspaceFileIndex.isIndexableCompat(file: VirtualFile): Boolean = isIndexable(file)
