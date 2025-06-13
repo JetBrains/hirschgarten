5485b2ba9f736652fcd6290890790fcdaca5f185
Maciej Gryz <maciej.gryz@jetbrains.com>
2025-06-13 07:53:37 +0000

    BAZEL-1543 [bazel] [feature] Bazel Query UI/UX
    Co-authored-by: Karolus314 <trener199951@gmail.com>
    Co-authored-by: Marta Hering-Zagrocka <mh448197@students.mimuw.edu.pl>
    
    Merge-request: IJ-MR-163511
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: e762d829a902299dd0e42864d0748b6c56f69855

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/widgets/tool/window/components/BazelToolWindowPanel.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/widgets/tool/window/components/BazelToolWindowPanel.kt
@@ -8,29 +8,29 @@ import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.application.EDT
 import com.intellij.openapi.application.writeIntentReadAction
 import com.intellij.openapi.components.service
-import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.SimpleToolWindowPanel
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
+import com.intellij.ui.components.JBTabbedPane
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jetbrains.bazel.action.SuspendableAction
 import org.jetbrains.bazel.config.BazelPluginBundle
+import org.jetbrains.bazel.config.FeatureFlagsProvider
 import org.jetbrains.bazel.settings.bazel.bazelProjectSettings
+import org.jetbrains.bazel.ui.queryTab.BazelQueryTab
 import java.nio.file.Path
 import javax.swing.SwingConstants
 
-internal class BazelToolWindowPanel(project: Project) : SimpleToolWindowPanel(true, true) {
-  private val panel: BazelTargetsPanel
+class BazelToolWindowPanel(val project: Project) : SimpleToolWindowPanel(true, true) {
+  private val model = project.service<BazelTargetsPanelModel>()
 
   init {
-    val model = project.service<BazelTargetsPanelModel>()
-    panel = BazelTargetsPanel(project, model)
-
     val actionManager = ActionManager.getInstance()
+
     val defaultActions = actionManager.getAction("Bazel.ActionsToolbar")
     val actionGroup =
       DefaultActionGroup().apply {
@@ -48,9 +48,25 @@ internal class BazelToolWindowPanel(project: Project) : SimpleToolWindowPanel(tr
         targetComponent = this@BazelToolWindowPanel.component
         orientation = SwingConstants.HORIZONTAL
       }
+    toolbar = actionToolbar.component
 
-    this.toolbar = actionToolbar.component
-    setContent(panel)
+    updateTabs()
+    updateUI()
+  }
+
+  fun updateTabs() {
+    val panel = BazelTargetsPanel(project, this.model)
+    setContent(
+      if (FeatureFlagsProvider.getFeatureFlags(project).isBazelQueryTabEnabled) {
+        JBTabbedPane().apply {
+          addTab("Loaded Targets", panel)
+          addTab("Bazel Query", BazelQueryTab(project))
+        }
+      } else {
+        panel
+      },
+    )
+    panel.update()
   }
 }
 
@@ -60,7 +76,7 @@ private class BazelToolWindowSettingsAction(private val settingsDisplayName: Str
     AllIcons.General.Settings,
   ) {
   override suspend fun actionPerformed(project: Project, e: AnActionEvent) {
-    val showSettingsUtil = serviceAsync<ShowSettingsUtil>()
+    val showSettingsUtil = ShowSettingsUtil.getInstance()
     withContext(Dispatchers.EDT) {
       writeIntentReadAction {
         showSettingsUtil.showSettingsDialog(project, settingsDisplayName)
@@ -78,15 +94,15 @@ private class BazelToolWindowConfigFileOpenAction :
     val configFile = project.bazelProjectSettings.projectViewPath
     e.presentation.isEnabled = configFile != null
     withContext(Dispatchers.EDT) {
-      project.serviceAsync<ProjectView>().refresh()
-      if (configFile != null) {
-        getPsiFile(configFile, project)?.navigate(true)
-      }
+      ProjectView.getInstance(project).refresh()
+      configFile?.getPsiFile(project)?.navigate(true)
     }
   }
 }
 
-private suspend fun getPsiFile(file: Path, project: Project): PsiFile? {
-  val virtualFile = VirtualFileManager.getInstance().findFileByNioPath(file) ?: return null
-  return project.serviceAsync<PsiManager>().findFile(virtualFile)
+private fun Path.getPsiFile(project: Project): PsiFile? {
+  val virtualFileManager = VirtualFileManager.getInstance()
+  val virtualFile =
+    virtualFileManager.findFileByNioPath(this) ?: return null
+  return PsiManager.getInstance(project).findFile(virtualFile)
 }
--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/utils/BazelWorkingDirectoryManager.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/utils/BazelWorkingDirectoryManager.kt
@@ -1,6 +1,10 @@
 package org.jetbrains.bazel.utils
 
-object BazelWorkingDirectoryManager {
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.project.Project
+
+@Service(Service.Level.PROJECT)
+class BazelWorkingDirectoryManager {
   @Volatile
   private var workingDirectory: String = ""
 
@@ -8,5 +12,10 @@ object BazelWorkingDirectoryManager {
 
   fun setWorkingDirectory(directory: String) {
     workingDirectory = directory
+  }
+
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project): BazelWorkingDirectoryManager = project.getService(BazelWorkingDirectoryManager::class.java)
   }
 }
