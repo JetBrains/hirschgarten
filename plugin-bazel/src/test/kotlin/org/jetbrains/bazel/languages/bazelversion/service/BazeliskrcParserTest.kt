2e0722f2a9618984f6871892234160005a3587ca
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-06-24 08:46:35 +0200

    BAZEL-2102 add tests for current workspace bazel version resolver
    GitOrigin-RevId: 9f7f4cfab803d8c69b82153830c7f4c1f7db50da

---
+++ plugin-bazel/src/test/kotlin/org/jetbrains/bazel/languages/bazelversion/service/BazeliskrcParserTest.kt
@@ -0,0 +1,35 @@
+package org.jetbrains.bazel.languages.bazelversion.service
+
+import io.kotest.matchers.equals.shouldBeEqual
+import org.junit.jupiter.api.Test
+
+class BazeliskrcParserTest {
+
+  @Test
+  fun `test simple case2`() {
+    val properties = BazeliskrcParser.parse("""
+      # comment
+      KEY1=VALUE1
+      KEY2= VALUE2 
+    """.trimIndent())
+
+    properties shouldBeEqual mapOf(
+      "KEY1" to "VALUE1",
+      "KEY2" to "VALUE2",
+    )
+  }
+
+  @Test
+  fun `test comment after`() {
+    val properties = BazeliskrcParser.parse("""
+      # comment
+      KEY1=VALUE1
+      KEY2= VALUE2 # THIS IS NOT COMMENT
+    """.trimIndent())
+
+    properties shouldBeEqual mapOf(
+      "KEY1" to "VALUE1",
+      "KEY2" to "VALUE2 # THIS IS NOT COMMENT",
+    )
+  }
+}
