5fea299b2e636b01a29faec3ee5ec704f0259982
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-06-25 12:48:02 +0200

    BAZEL-2102 BazelVersionWorkspaceResolverTest test cleanup
    GitOrigin-RevId: 2855dc5737d918fa9c41ac4b2d9723eab628b191

--- plugin-bazel/src/test/kotlin/org/jetbrains/bazel/languages/bazelversion/service/BazelVersionWorkspaceResolverTest.kt
+++ plugin-bazel/src/test/kotlin/org/jetbrains/bazel/languages/bazelversion/service/BazelVersionWorkspaceResolverTest.kt
@@ -5,22 +5,20 @@ import com.intellij.util.text.SemVer
 import io.kotest.matchers.equals.shouldBeEqual
 import org.jetbrains.bazel.languages.bazelversion.psi.BazelVersionLiteral
 import org.junit.jupiter.api.Test
-import java.nio.file.Path
-import kotlin.io.path.pathString
 
 class BazelVersionWorkspaceResolverTest {
 
   @Test
   fun `test workspace version resolver bazelisk`() {
     val path = Runfiles.runfilesPath("/plugin-bazel/src/test/testData/bazelversion/version_resolve_bazelisk")
-    val version = BazelVersionWorkspaceResolver.resolveBazelVersionFromWorkspace(Path.of(path.pathString))
+    val version = BazelVersionWorkspaceResolver.resolveBazelVersionFromWorkspace(path)
     version!! shouldBeEqual BazelVersionLiteral.Specific(SemVer.parseFromText("123.4.10")!!)
   }
 
   @Test
   fun `test workspace version resolver bazelversion`() {
     val path = Runfiles.runfilesPath("/plugin-bazel/src/test/testData/bazelversion/version_resolve_bazelversion")
-    val version = BazelVersionWorkspaceResolver.resolveBazelVersionFromWorkspace(Path.of(path.pathString))
+    val version = BazelVersionWorkspaceResolver.resolveBazelVersionFromWorkspace(path)
     version!! shouldBeEqual BazelVersionLiteral.Specific(SemVer.parseFromText("123.4.5")!!)
   }
 }
