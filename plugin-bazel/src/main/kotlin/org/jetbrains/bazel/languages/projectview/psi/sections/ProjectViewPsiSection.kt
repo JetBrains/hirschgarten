8840c72f0bb66b9a98eabca6120da804957260f5
Robert Grigoryan <robert.grigoryan@jetbrains.com>
2025-06-26 10:34:50 +0200

    Add utility methods to Project View Psi elements
    GitOrigin-RevId: 497a83c87b9ca7c55613ad0c72475c2dd48f88ce

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/psi/sections/ProjectViewPsiSection.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/psi/sections/ProjectViewPsiSection.kt
@@ -1,11 +1,21 @@
 package org.jetbrains.bazel.languages.projectview.psi.sections
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
+import org.jetbrains.bazel.languages.projectview.lexer.ProjectViewTokenType
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewBaseElement
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewElementVisitor
+import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
 
 class ProjectViewPsiSection(node: ASTNode) : ProjectViewBaseElement(node) {
   override fun acceptVisitor(visitor: ProjectViewElementVisitor) {
     visitor.visitSection(this)
   }
+
+  fun getKeyword(): PsiElement = firstChild
+
+  fun getItems(): Array<ProjectViewPsiSectionItem> = getChildrenOfType<ProjectViewPsiSectionItem>()
+
+  fun getColon(): PsiElement? = getChildrenOfType<PsiElement>().find { it.elementType == ProjectViewTokenType.COLON }
 }
