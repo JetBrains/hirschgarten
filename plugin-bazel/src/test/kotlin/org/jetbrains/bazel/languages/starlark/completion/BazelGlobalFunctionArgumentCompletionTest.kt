a572f933cdbfa634ad5c7830720d15546c1ed29b
Stanislaw Solarewicz <stanislaw.solarewicz@jetbrains.com>
2025-06-28 19:23:19 +0200

    test with handpicked examples
    GitOrigin-RevId: af8acca9153674b02a9bd670ba6737dea59c3ba1

--- plugin-bazel/src/test/kotlin/org/jetbrains/bazel/languages/starlark/completion/BazelGlobalFunctionArgumentCompletionTest.kt
+++ plugin-bazel/src/test/kotlin/org/jetbrains/bazel/languages/starlark/completion/BazelGlobalFunctionArgumentCompletionTest.kt
@@ -2,41 +2,54 @@ package org.jetbrains.bazel.languages.starlark.completion
 
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import io.kotest.matchers.collections.shouldContainAll
-import org.jetbrains.bazel.languages.starlark.bazel.BazelGlobalFunction
 import org.jetbrains.bazel.languages.starlark.bazel.BazelGlobalFunctions
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
+import org.junit.runners.JUnit4
 
-@RunWith(Parameterized::class)
-class BazelGlobalFunctionArgumentCompletionTest(val function: BazelGlobalFunction) : BasePlatformTestCase() {
-  companion object {
-    @JvmStatic
-    @Parameterized.Parameters
-    fun data(): Array<BazelGlobalFunction> = BazelGlobalFunctions.MODULE_FUNCTIONS.values.toTypedArray()
-  }
-
+@RunWith(JUnit4::class)
+class BazelGlobalFunctionArgumentCompletionTest : BasePlatformTestCase() {
   @Test
-  fun `should complete arguments for global MODULE functions`() {
+  fun `should complete module function`() {
     // given
-    myFixture.configureByText("MODULE.bazel", "${function.name}(<caret>)")
+    myFixture.configureByText("MODULE.bazel", "module(<caret>)")
     myFixture.type("a")
 
     // when
     val lookups = myFixture.completeBasic().flatMap { it.allLookupStrings }
 
     // then
-    val argNames = function.params.map { it.name }
-    lookups shouldContainAll argNames.filter { it.contains("a") }
+    val expectedArgNames =
+      BazelGlobalFunctions.MODULE_FUNCTIONS["module"]!!
+        .params
+        .filter { it.name.contains('a') }
+        .map { it.name }
+    lookups shouldContainAll expectedArgNames
   }
 
   @Test
-  fun `should use insert handler`() {
-    val firstParam = function.params.filter { !it.positional }.getOrNull(0) ?: return
-
+  fun `should complete on subsequent parameters`() {
     // given
-    myFixture.configureByText("MODULE.bazel", "${function.name}(<caret>)")
-    myFixture.type(firstParam.name)
+    myFixture.configureByText("MODULE.bazel", "module(name = 'testName', <caret>)")
+    myFixture.type("a")
+
+    // when
+    val lookups = myFixture.completeBasic().flatMap { it.allLookupStrings }
+
+    // then
+    val expectedArgNames =
+      BazelGlobalFunctions.MODULE_FUNCTIONS["module"]!!
+        .params
+        .filter { it.name.contains('a') }
+        .map { it.name }
+    lookups shouldContainAll expectedArgNames
+  }
+
+  @Test
+  fun `should complete with empty quotes`() {
+    // given
+    myFixture.configureByText("MODULE.bazel", "module(<caret>)")
+    myFixture.type("name")
 
     // when
     val lookupElements = myFixture.completeBasic()
@@ -48,9 +61,25 @@ class BazelGlobalFunctionArgumentCompletionTest(val function: BazelGlobalFunctio
     }
 
     // then
-    val argName = firstParam.name
-    val defaultValue = firstParam.default
+    myFixture.checkResult("""module(name = '<caret>',)""")
+  }
 
-    myFixture.checkResult("""${function.name}($argName = $defaultValue,)""")
+  @Test
+  fun `should complete with default value and selection`() {
+    // given
+    myFixture.configureByText("MODULE.bazel", "module(<caret>)")
+    myFixture.type("compatibility_level")
+
+    // when
+    val lookupElements = myFixture.completeBasic()
+
+    // Select first lookup element and simulate pressing Tab key to trigger insert handler.
+    if (lookupElements != null && lookupElements.isNotEmpty()) {
+      myFixture.lookup?.currentItem = lookupElements[0]
+      myFixture.type('\t')
+    }
+
+    // then
+    myFixture.checkResult("""module(compatibility_level = <selection>0<caret></selection>,)""")
   }
 }
