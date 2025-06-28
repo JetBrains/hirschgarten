3ac60f5961d4120fccb65838a391bb67201f02d8
Robert Grigoryan <robert.grigoryan@jetbrains.com>
2025-06-28 22:59:36 +0200

    Add ExcludeFromProjectViewDirectoriesAction
    New features:
      * Exclude directories from the project view file directories section
      * Create the directories section if it doesn't exist already
      * Notify the user when they try to include a directory that has been already included/excluded.  Do the same when they try to exclude a directory.
    
    Perhaps the last bullet point is less preferable to simply overwriting the previous entry in the directories section and notifying the user?
    
    GitOrigin-RevId: f243e5de8955387227cf18ec1387214627c95196

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/action/registered/projectViewDirectories/ExcludeFromProjectViewDirectoriesAction.kt
@@ -0,0 +1,32 @@
+package org.jetbrains.bazel.action.registered.projectViewDirectories
+
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ExcludeFromProjectViewDirectoriesAction : AnAction() {
+  val action: ProjectViewDirectoriesAction = ProjectViewDirectoriesAction(
+    ProjectViewDirectoriesAction.NotificationFactory(
+      "Directory already excluded",
+      {"Directory \'$it\' is already excluded from the project view file directories section."},
+      NotificationType.INFORMATION
+    ),
+    ProjectViewDirectoriesAction.NotificationFactory(
+      "Excluding included directory",
+      {"Attempted to exclude directory \'$it\' which has been already included in the project view file directories section."},
+      NotificationType.WARNING
+    ),
+  )
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
+    val selectedDirectoryRelativePath = action.getSelectedDirectoryRelativePath(project, e) ?: return
+    val item = "-$selectedDirectoryRelativePath"
+    action.addItemToProjectView(item, e)
+  }
+
+  override fun update(e: AnActionEvent) = action.update(e)
+
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+}
