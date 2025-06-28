d36ac39cfd56a4d269d7676af6c996c339a99bb3
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2025-06-28 10:21:07 +0000

    [bazel][maintenance] IDE-starter tests maintenance; 1) fix PyCharm test by making the project reusable 2) fix BazelProjectModelModifierTest by changing expected BUILD file to include trailing comma 3) make most of the IDE-starter test use ideFrame and step pattern instead of CommandChain() - command chain is evaluated before the test and you can set breakpoints during execution.
    Created by Danil Tereshchenko (Danil.Tereshchenko) via Patronus
    
    Merge-request: IJ-MR-167310
    Merged-by: Danil Tereshchenko <Danil.Tereshchenko@jetbrains.com>
    
    GitOrigin-RevId: cebf2d4fd495bee200fd00c9ac5ceb0695a53519

--- plugin-bazel/src/test/kotlin/org/jetbrains/bazel/compatibility/DisabledKotlinPluginTest.kt
+++ plugin-bazel/src/test/kotlin/org/jetbrains/bazel/compatibility/DisabledKotlinPluginTest.kt
@@ -1,16 +1,21 @@
 package org.jetbrains.bazel.compatibility
 
+import com.intellij.driver.sdk.step
+import com.intellij.driver.sdk.ui.components.common.ideFrame
+import com.intellij.driver.sdk.waitForIndicators
+import com.intellij.ide.starter.driver.engine.runIdeWithDriver
 import com.intellij.ide.starter.ide.IDETestContext
 import com.intellij.ide.starter.project.GitProjectInfo
 import com.intellij.ide.starter.project.ProjectInfoSpec
 import com.intellij.openapi.ui.playback.commands.AbstractCommand.CMD_PREFIX
 import com.intellij.tools.ide.performanceTesting.commands.CommandChain
-import com.intellij.tools.ide.performanceTesting.commands.exitApp
 import com.intellij.tools.ide.performanceTesting.commands.takeScreenshot
 import com.intellij.tools.ide.performanceTesting.commands.waitForSmartMode
 import org.jetbrains.bazel.ideStarter.IdeStarterBaseProjectTest
+import org.jetbrains.bazel.ideStarter.execute
 import org.jetbrains.bazel.ideStarter.waitForBazelSync
 import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.minutes
 
 /**
  * ```sh
@@ -36,13 +41,25 @@ class DisabledKotlinPluginTest : IdeStarterBaseProjectTest() {
         .takeScreenshot("startSync")
         .waitForBazelSync()
         .waitForSmartMode()
-        .checkImportedModules()
-        .checkTargetsInTargetWidget()
-        .exitApp()
 
     createContext()
       .withDisabledPlugins(setOf("org.jetbrains.kotlin"))
-      .runIDE(commands = commands, runTimeout = timeout)
+      .runIdeWithDriver(commands = commands, runTimeout = timeout)
+      .useDriverAndCloseIde {
+        ideFrame {
+          waitForIndicators(5.minutes)
+
+          step("Check imported modules") {
+            execute { checkImportedModules() }
+            takeScreenshot("afterCheckImportedModules")
+          }
+
+          step("Check targets in target widget") {
+            execute { checkTargetsInTargetWidget() }
+            takeScreenshot("afterCheckTargetsInTargetWidget")
+          }
+        }
+      }
   }
 
   private fun <T : CommandChain> T.checkImportedModules(): T = also { addCommand("${CMD_PREFIX}checkImportedModules") }
