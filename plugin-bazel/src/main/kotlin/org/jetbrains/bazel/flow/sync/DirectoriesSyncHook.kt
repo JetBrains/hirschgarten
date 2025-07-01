ef06d534e59099fdd98f2410142283f3479f1e8d
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-07-01 15:59:38 +0200

    BAZEL-2150 add temporary fix for VirtualFileUrlManager#getOrCreateFromUrl not working on windows
    GitOrigin-RevId: d96dbb73bfb3444db2a47204fc7ce3f51767afa6

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/sync/DirectoriesSyncHook.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/sync/DirectoriesSyncHook.kt
@@ -5,6 +5,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.platform.backend.workspace.WorkspaceModel
 import com.intellij.platform.backend.workspace.toVirtualFileUrl
 import com.intellij.platform.workspace.storage.impl.url.toVirtualFileUrl
+import com.intellij.platform.workspace.storage.url.VirtualFileUrl
+import com.intellij.platform.workspace.storage.url.VirtualFileUrlManager
 import org.jetbrains.bazel.config.rootDir
 import org.jetbrains.bazel.flow.open.exclude.BazelSymlinkExcludeService
 import org.jetbrains.bazel.sdkcompat.workspacemodel.entities.BazelProjectDirectoriesEntity
@@ -15,6 +17,7 @@ import org.jetbrains.bazel.sync.projectStructure.workspaceModel.workspaceModelDi
 import org.jetbrains.bazel.sync.task.query
 import org.jetbrains.bazel.sync.withSubtask
 import org.jetbrains.bsp.protocol.WorkspaceDirectoriesResult
+import java.net.URI
 import java.nio.file.Path
 
 private class DirectoriesSyncHook : ProjectSyncHook {
@@ -35,6 +38,24 @@ private class DirectoriesSyncHook : ProjectSyncHook {
     }
   }
 
+  private fun String.toVirtualFileUrl(virtualFileUrlManager: VirtualFileUrlManager): VirtualFileUrl {
+    return if (this.startsWith("file://")) {
+      /*
+        Apparently on some operating systems(windows)
+        VirtualFileUrlManager is unable to decode uri obtained from Path#toUri correctly
+        on unix-like operating system uri path will look something like that:
+          - file:///home/user/something - valid
+        on windows path still contains root directory '/' slash
+          - file:///C:/Users/user/something - valid
+        VirtualFileUrlManager does not handle that and on windows returns path from root
+           /C:/Users/user/something - invalid
+       */
+      virtualFileUrlManager.fromPath(Path.of(URI.create(this)).toAbsolutePath().toString())
+    } else {
+      virtualFileUrlManager.getOrCreateFromUrl(this)
+    }
+  }
+
   private suspend fun createEntity(
     project: Project,
     directories: WorkspaceDirectoriesResult,
@@ -43,9 +64,9 @@ private class DirectoriesSyncHook : ProjectSyncHook {
   ): BazelProjectDirectoriesEntity.Builder {
     val virtualFileUrlManager = project.serviceAsync<WorkspaceModel>().getVirtualFileUrlManager()
 
-    val includedRoots = directories.includedDirectories.map { it.uri }.map { virtualFileUrlManager.getOrCreateFromUrl(it) }
+    val includedRoots = directories.includedDirectories.map { it.uri.toVirtualFileUrl(virtualFileUrlManager) }
     val excludedRoots =
-      directories.excludedDirectories.map { it.uri }.map { virtualFileUrlManager.getOrCreateFromUrl(it) } +
+      directories.excludedDirectories.map { it.uri.toVirtualFileUrl(virtualFileUrlManager) } +
         additionalExcludes.map { it.toVirtualFileUrl(virtualFileUrlManager) }
 
     return BazelProjectDirectoriesEntity(
