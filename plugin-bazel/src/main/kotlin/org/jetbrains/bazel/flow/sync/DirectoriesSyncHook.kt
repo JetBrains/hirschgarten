35d338f10914d9a413a5e336cfa5abd6e8668809
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-07-02 09:10:42 +0000

    BAZEL-2150 add temporary fix for VirtualFileUrlManager#getOrCreateFromUrl not working on windows
    Merge-request: IJ-MR-167681
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: 8c4c735ea2cf96a7c0862e64a3f56f892ef4be8e

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/sync/DirectoriesSyncHook.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/sync/DirectoriesSyncHook.kt
@@ -5,6 +5,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.platform.backend.workspace.WorkspaceModel
 import com.intellij.platform.backend.workspace.toVirtualFileUrl
 import com.intellij.platform.workspace.storage.impl.url.toVirtualFileUrl
+import com.intellij.platform.workspace.storage.url.VirtualFileUrl
+import com.intellij.platform.workspace.storage.url.VirtualFileUrlManager
 import org.jetbrains.bazel.config.rootDir
 import org.jetbrains.bazel.flow.open.exclude.BazelSymlinkExcludeService
 import org.jetbrains.bazel.sdkcompat.workspacemodel.entities.BazelProjectDirectoriesEntity
@@ -15,6 +17,7 @@ import org.jetbrains.bazel.sync.projectStructure.workspaceModel.workspaceModelDi
 import org.jetbrains.bazel.sync.task.query
 import org.jetbrains.bazel.sync.withSubtask
 import org.jetbrains.bsp.protocol.WorkspaceDirectoriesResult
+import java.net.URI
 import java.nio.file.Path
 
 private class DirectoriesSyncHook : ProjectSyncHook {
@@ -43,9 +46,9 @@ private class DirectoriesSyncHook : ProjectSyncHook {
   ): BazelProjectDirectoriesEntity.Builder {
     val virtualFileUrlManager = project.serviceAsync<WorkspaceModel>().getVirtualFileUrlManager()
 
-    val includedRoots = directories.includedDirectories.map { it.uri }.map { virtualFileUrlManager.getOrCreateFromUrl(it) }
+    val includedRoots = directories.includedDirectories.map { IdeaVFSUtil.toVirtualFileUrl(it.uri, virtualFileUrlManager) }
     val excludedRoots =
-      directories.excludedDirectories.map { it.uri }.map { virtualFileUrlManager.getOrCreateFromUrl(it) } +
+      directories.excludedDirectories.map { IdeaVFSUtil.toVirtualFileUrl(it.uri, virtualFileUrlManager) } +
         additionalExcludes.map { it.toVirtualFileUrl(virtualFileUrlManager) }
 
     return BazelProjectDirectoriesEntity(
