f657cc5c256768de38ec941fde6c2d338e2589b3
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-07-04 12:47:55 +0200

    BAZEL-164 add safe delete for target rules
    GitOrigin-RevId: 11d9a9c168eabf4f9ff7dc447dee1f09e3866274

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/refactoring/StarlarkTargetSafeDeleteProcessor.kt
@@ -0,0 +1,63 @@
+package org.jetbrains.bazel.languages.starlark.refactoring
+
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.refactoring.RefactoringSettings
+import com.intellij.refactoring.safeDelete.NonCodeUsageSearchInfo
+import com.intellij.refactoring.safeDelete.SafeDeleteProcessor
+import com.intellij.refactoring.safeDelete.SafeDeleteProcessorDelegateBase
+import com.intellij.usageView.UsageInfo
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkReferenceExpression
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkTargetExpression
+import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkAssignmentStatement
+import org.jetbrains.kotlin.analysis.utils.printer.parentOfType
+
+class StarlarkTargetSafeDeleteProcessor : SafeDeleteProcessorDelegateBase() {
+  override fun getElementsToSearch(
+    element: PsiElement,
+    module: Module?,
+    allElementsToDelete: Collection<PsiElement?>,
+  ): Collection<PsiElement> = listOf(element)
+
+  override fun handlesElement(element: PsiElement?): Boolean = element is StarlarkTargetExpression || element is StarlarkReferenceExpression
+
+  override fun findUsages(
+    element: PsiElement,
+    allElementsToDelete: Array<out PsiElement?>,
+    result: MutableList<in UsageInfo>,
+  ): NonCodeUsageSearchInfo {
+    SafeDeleteProcessor.findGenericElementUsages(
+      element,
+      result,
+      allElementsToDelete,
+      GlobalSearchScope.projectScope(element.project),
+    )
+    return NonCodeUsageSearchInfo(SafeDeleteProcessor.getDefaultInsideDeletedCondition(allElementsToDelete), listOf(element))
+  }
+
+  override fun getAdditionalElementsToDelete(
+    element: PsiElement,
+    allElementsToDelete: Collection<PsiElement?>,
+    askUser: Boolean,
+  ): Collection<PsiElement> = listOfNotNull(element.parentOfType<StarlarkAssignmentStatement>())
+
+  override fun preprocessUsages(project: Project, usages: Array<out UsageInfo?>): Array<out UsageInfo?> = usages
+
+  override fun prepareForDeletion(element: PsiElement) {
+  }
+
+  override fun isToSearchInComments(element: PsiElement?): Boolean = RefactoringSettings.getInstance().SAFE_DELETE_SEARCH_IN_COMMENTS
+
+  override fun setToSearchInComments(element: PsiElement?, enabled: Boolean) {
+    RefactoringSettings.getInstance().SAFE_DELETE_SEARCH_IN_COMMENTS = enabled
+  }
+
+  override fun isToSearchForTextOccurrences(element: PsiElement?): Boolean =
+    RefactoringSettings.getInstance().SAFE_DELETE_SEARCH_IN_NON_JAVA
+
+  override fun setToSearchForTextOccurrences(element: PsiElement?, enabled: Boolean) {
+    RefactoringSettings.getInstance().SAFE_DELETE_SEARCH_IN_NON_JAVA = enabled
+  }
+}
