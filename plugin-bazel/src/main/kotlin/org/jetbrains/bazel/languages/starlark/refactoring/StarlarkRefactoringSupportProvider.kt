1ed01660ee0284af4743d5f98ccc63f3880d88fb
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-07-04 12:48:43 +0200

    BAZEL-164 support safe delete only for selected elements
    GitOrigin-RevId: 36f879e905e2eebd432820cac143fb2cdc51d95f

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/refactoring/StarlarkRefactoringSupportProvider.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/refactoring/StarlarkRefactoringSupportProvider.kt
@@ -2,7 +2,15 @@ package org.jetbrains.bazel.languages.starlark.refactoring
 
 import com.intellij.lang.refactoring.RefactoringSupportProvider
 import com.intellij.psi.PsiElement
+import org.jetbrains.bazel.languages.starlark.psi.StarlarkFile
+import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkTargetExpression
+import org.jetbrains.bazel.languages.starlark.psi.functions.StarlarkFunctionDeclaration
+import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkAssignmentStatement
 
 class StarlarkRefactoringSupportProvider : RefactoringSupportProvider() {
-  override fun isSafeDeleteAvailable(element: PsiElement): Boolean = true
+  override fun isSafeDeleteAvailable(element: PsiElement): Boolean =
+    element is StarlarkFile
+      || element is StarlarkTargetExpression
+      || element is StarlarkFunctionDeclaration
+      || element is StarlarkAssignmentStatement
 }
