e3eb2a721b099da08f76ec5c2c29b632b7876348
Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
2025-07-10 16:48:06 +0000

    [bazel][maintenance/fix] improve bazel query lexer | (#BAZEL-2183, #BAZEL-2184,  #BAZEL-2185) Done
    update branch
    
    [bazel] fix bazel query completion and tests after lexer updates
    
    [bazel] apply suggestions regarding comments and run format
    
    [bazel] use BazelPluginBundle for user-visible strings
    
    update branch and resolve conflicts
    
    [bazel] add checking errors in target names
    
    [bazel] handle patterns in bazel query functions arguments (kind, filter, attr)
    
    [bazel] remove old lexer backup
    
    [bazel] add bazel query flags tests and fix unnecessary parens handling
    
    [bazel][fix] add bazel query tab
    
    
    Merge-request: IJ-MR-168305
    Merged-by: Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
    
    GitOrigin-RevId: 8cf14bc89cd32ea7c10558684269500f7a06df08

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelquery/elements/BazelQueryElementType.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelquery/elements/BazelQueryElementType.kt
@@ -1,6 +1,9 @@
 package org.jetbrains.bazel.languages.bazelquery.elements
 
 import com.intellij.psi.tree.IElementType
+import org.jetbrains.bazel.languages.bazelquery.BazelQueryFlagsLanguage
 import org.jetbrains.bazel.languages.bazelquery.BazelQueryLanguage
 
 class BazelQueryElementType(debugName: String) : IElementType(debugName, BazelQueryLanguage)
+
+class BazelQueryFlagsElementType(debugName: String) : IElementType(debugName, BazelQueryFlagsLanguage)
