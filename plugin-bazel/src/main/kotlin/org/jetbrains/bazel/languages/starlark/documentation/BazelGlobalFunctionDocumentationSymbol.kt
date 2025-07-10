a72dd761868f20ba4a7f6fb27ef8468e80bedd06
Stanislaw Solarewicz <stanislaw.solarewicz@jetbrains.com>
2025-07-10 12:40:41 +0000

    BAZEL-2163 [maintenance] Refactor `BazelNativeRules` into `BazelGlobalFunctions`
    Merge-request: IJ-MR-167679
    Merged-by: Stanisław Solarewicz <stanislaw.solarewicz@jetbrains.com>
    
    GitOrigin-RevId: 8e550c082a51753ed25757f211885c33667837a2

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/documentation/BazelGlobalFunctionDocumentationSymbol.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/documentation/BazelGlobalFunctionDocumentationSymbol.kt
@@ -3,15 +3,15 @@ package org.jetbrains.bazel.languages.starlark.documentation
 import com.intellij.model.Pointer
 import com.intellij.openapi.project.Project
 import com.intellij.platform.backend.documentation.DocumentationSymbol
-import org.jetbrains.bazel.languages.starlark.bazel.BazelNativeRule
+import org.jetbrains.bazel.languages.starlark.bazel.BazelGlobalFunction
 
 @Suppress("UnstableApiUsage")
-class BazelNativeRuleDocumentationSymbol(val nativeRule: BazelNativeRule, val project: Project) :
+class BazelGlobalFunctionDocumentationSymbol(val nativeRule: BazelGlobalFunction, val project: Project) :
   DocumentationSymbol,
-  Pointer<BazelNativeRuleDocumentationSymbol> {
+  Pointer<BazelGlobalFunctionDocumentationSymbol> {
   override fun createPointer() = this
 
   override fun dereference() = this
 
-  override fun getDocumentationTarget() = BazelNativeRulesDocumentationTarget(this)
+  override fun getDocumentationTarget() = BazelGlobalFunctionDocumentationTarget(this)
 }
