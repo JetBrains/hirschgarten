1ee804aee07a6a1a1ecab371eda0b4c308403e45
Marcin Abramowicz <marcin.abramowicz@jetbrains.com>
2025-07-12 10:19:07 +0000

    [bazel] [maintenance] remove ExecutionContext things
    Co-authored-by: Marcin Kocot <Marcin.Kocot@jetbrains.com>
    Co-authored-by: Marcin Abramowicz
    
    Merge-request: IJ-MR-164162
    Merged-by: Marcin Kocot <Marcin.Kocot@jetbrains.com>
    
    GitOrigin-RevId: 1b5f5886d619c3f9d109826950ed4af4c3267b9a

--- commons/src/main/kotlin/org/jetbrains/bazel/workspacecontext/provider/ImportIjarsSpecExtractor.kt
+++ commons/src/main/kotlin/org/jetbrains/bazel/workspacecontext/provider/ImportIjarsSpecExtractor.kt
@@ -1,9 +1,7 @@
 package org.jetbrains.bazel.workspacecontext.provider
-
-import org.jetbrains.bazel.executioncontext.api.ExecutionContextEntityExtractor
 import org.jetbrains.bazel.projectview.model.ProjectView
 import org.jetbrains.bazel.workspacecontext.ImportIjarsSpec
 
-internal object ImportIjarsSpecExtractor : ExecutionContextEntityExtractor<ImportIjarsSpec> {
+internal object ImportIjarsSpecExtractor : WorkspaceContextEntityExtractor<ImportIjarsSpec> {
   override fun fromProjectView(projectView: ProjectView): ImportIjarsSpec = ImportIjarsSpec(projectView.importIjars?.value ?: false)
 }
