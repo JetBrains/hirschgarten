85af78a5fe383d0da59dde3455bf4181aac782fa
Lev Leontev <lev.leontev@jetbrains.com>
2025-07-17 18:58:30 +0200

    BAZEL-2187 [fix] Don't visit excluded files in BazelBzlFileService
    GitOrigin-RevId: f408e6aae1e9b2695727e3948b4f1e034202c67c

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/BazelBzlFileService.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/references/BazelBzlFileService.kt
@@ -1,13 +1,15 @@
 package org.jetbrains.bazel.languages.starlark.references
 
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.openapi.vfs.isFile
+import com.intellij.openapi.vfs.VirtualFileVisitor
 import org.jetbrains.annotations.TestOnly
+import org.jetbrains.bazel.coroutines.BazelCoroutineService
 import org.jetbrains.bazel.label.Canonical
 import org.jetbrains.bazel.label.Label
 import org.jetbrains.bazel.label.Package
@@ -16,10 +18,6 @@ import org.jetbrains.bazel.label.SingleTarget
 import org.jetbrains.bazel.languages.starlark.repomapping.canonicalRepoNameToPath
 import org.jetbrains.bazel.startup.utils.BazelProjectActivity
 import org.jetbrains.bazel.sync.status.SyncStatusListener
-import java.nio.file.Path
-import kotlin.collections.component1
-import kotlin.collections.component2
-import kotlin.collections.iterator
 import kotlin.io.path.relativeToOrNull
 
 @Service(Service.Level.PROJECT)
@@ -28,7 +26,7 @@ class BazelBzlFileService(private val project: Project) {
   var canonicalRepoNameToBzlFiles: Map<String, List<ResolvedLabel>> = emptyMap()
 
   init {
-    ApplicationManager.getApplication().executeOnPooledThread { updateCache() }
+    BazelCoroutineService.getInstance(project).start { updateCache() }
 
     project.messageBus.connect().subscribe(
       SyncStatusListener.TOPIC,
@@ -37,41 +35,47 @@ class BazelBzlFileService(private val project: Project) {
 
         override fun syncFinished(canceled: Boolean) {
           if (canceled) return
-          ApplicationManager.getApplication().executeOnPooledThread { updateCache() }
+          BazelCoroutineService.getInstance(project).start { updateCache() }
         }
       },
     )
   }
 
-  private class FileVisitor(
-    private val canonicalName: String,
-    private val repoPath: Path,
-    private val map: MutableMap<String, List<ResolvedLabel>>,
-  ) {
-    fun visitFile(file: VirtualFile): Boolean {
-      if (file.isFile && file.name.endsWith(".bzl")) {
-        val targetName = file.name
-        val targetBaseDirectory = file.parent ?: return true
-        val relativeTargetBaseDirectory = targetBaseDirectory.toNioPath().relativeToOrNull(repoPath) ?: return true
-
-        val label =
-          ResolvedLabel(
-            repo = Canonical.createCanonicalOrMain(canonicalName),
-            packagePath = Package(relativeTargetBaseDirectory.toString().split("/")),
-            target = SingleTarget(targetName),
-          )
-        map[canonicalName] = map.getOrDefault(canonicalName, emptyList()) + label
-      }
-      return true
-    }
-  }
-
   private fun updateCache() {
     val newMap = mutableMapOf<String, List<ResolvedLabel>>()
+    val projectFileIndex = ProjectFileIndex.getInstance(project)
+
+    val canonicalRepoPaths =
+      project.canonicalRepoNameToPath.values
+        .mapNotNull {
+          VirtualFileManager.getInstance().findFileByNioPath(it)
+        }.toSet()
+
     for ((canonicalName, repoPath) in project.canonicalRepoNameToPath) {
       val root = VirtualFileManager.getInstance().findFileByNioPath(repoPath) ?: continue
-      val visitor = FileVisitor(canonicalName, repoPath, newMap)
-      VfsUtilCore.processFilesRecursively(root, visitor::visitFile)
+      VfsUtilCore.visitChildrenRecursively(
+        root,
+        object : VirtualFileVisitor<Unit>() {
+          override fun visitFileEx(file: VirtualFile): Result {
+            if (runReadAction { projectFileIndex.isExcluded(file) }) return SKIP_CHILDREN
+            if (file != root && file in canonicalRepoPaths) return SKIP_CHILDREN
+            if (file.isDirectory || file.extension != "bzl") return CONTINUE
+
+            val targetName = file.name
+            val targetBaseDirectory = file.parent ?: return CONTINUE
+            val relativeTargetBaseDirectory = targetBaseDirectory.toNioPath().relativeToOrNull(repoPath) ?: return CONTINUE
+
+            val label =
+              ResolvedLabel(
+                repo = Canonical.createCanonicalOrMain(canonicalName),
+                packagePath = Package(relativeTargetBaseDirectory.toString().split("/")),
+                target = SingleTarget(targetName),
+              )
+            newMap[canonicalName] = newMap.getOrDefault(canonicalName, emptyList()) + label
+            return CONTINUE
+          }
+        },
+      )
     }
     canonicalRepoNameToBzlFiles = newMap
   }
