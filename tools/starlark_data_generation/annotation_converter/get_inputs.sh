940d6ec4c6329ea324dec02815fd134fd62a9da4
Stanislaw Solarewicz <stanislaw.solarewicz@jetbrains.com>
2025-07-25 12:45:28 +0200

    add a comment to the bash script
    GitOrigin-RevId: 8e6c7c179e5342c757c3ce3da2bcf511dd60821e

--- tools/starlark_data_generation/annotation_converter/get_inputs.sh
+++ tools/starlark_data_generation/annotation_converter/get_inputs.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Usage: bash get_inputs.sh $PATH_TO_BAZEL_REPO
+
+# This script finds all files in the bazel repo that contain the annotation @GlobalMethods,
+# which means it contains @StarlarkMethod annotations that can be extracted.
+# Located files then can be fed to the annotation_converter to generate documentation data.
+
+# WARNING! Not all fils that contain @StarlarkMethod also contain @GlobalMethod,
+# these exceptions are included in the inputs file, so overwrite that carefully.
+
+# We don't look for all files containing @StarlarkMethod because that would also include
+# native build rules which we handle separately.
+
 if [ $# -eq 0 ]; then
     echo "Please provide repository path as an argument"
     exit 1
