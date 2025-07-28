652e4aef16ef68055e701b42655d37c4ef7c9974
Stanislaw Solarewicz <stanislaw.solarewicz@jetbrains.com>
2025-07-28 14:56:49 +0200

    getElement returns ProjectViewPsiSectionItem
    GitOrigin-RevId: f43d84b4e1262bfe83febad0a6cbba22635785cb

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/references/ProjectViewFlagReference.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/references/ProjectViewFlagReference.kt
@@ -3,14 +3,13 @@ package org.jetbrains.bazel.languages.projectview.references
 import com.intellij.model.Symbol
 import com.intellij.model.psi.PsiSymbolReference
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.PsiElement
 import org.jetbrains.bazel.languages.bazelrc.flags.BazelFlagSymbol
 import org.jetbrains.bazel.languages.bazelrc.flags.Flag
 import org.jetbrains.bazel.languages.projectview.psi.sections.ProjectViewPsiSectionItem
 
 @Suppress("UnstableApiUsage")
 class ProjectViewFlagReference(private val element: ProjectViewPsiSectionItem) : PsiSymbolReference {
-  override fun getElement(): PsiElement = element
+  override fun getElement(): ProjectViewPsiSectionItem = element
 
   override fun getRangeInElement(): TextRange = element.firstChild.textRangeInParent
 
