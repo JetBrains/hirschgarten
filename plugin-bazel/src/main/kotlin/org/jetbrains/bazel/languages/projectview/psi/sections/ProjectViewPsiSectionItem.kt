4ab903219a56a95e68bffcc4b9ac8bdb46ea57ff
Stanislaw Solarewicz <stanislaw.solarewicz@jetbrains.com>
2025-07-29 11:17:34 +0200

    make checking more strict
    GitOrigin-RevId: cd4877495b1afd5d3d74580bf029b645753848f5

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/psi/sections/ProjectViewPsiSectionItem.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/psi/sections/ProjectViewPsiSectionItem.kt
@@ -1,6 +1,9 @@
 package org.jetbrains.bazel.languages.projectview.psi.sections
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.util.elementType
+import org.jetbrains.bazel.languages.projectview.language.ProjectViewSection
+import org.jetbrains.bazel.languages.projectview.lexer.ProjectViewTokenType
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewBaseElement
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewElementVisitor
 import org.jetbrains.bazel.languages.projectview.references.ProjectViewFlagReference
@@ -11,9 +14,11 @@ class ProjectViewPsiSectionItem(node: ASTNode) : ProjectViewBaseElement(node) {
     visitor.visitSectionItem(this)
   }
 
+  private fun isLeaf(): Boolean = firstChild != null && firstChild == lastChild && firstChild.elementType == ProjectViewTokenType.IDENTIFIER
+
   override fun getOwnReferences(): Collection<ProjectViewFlagReference> {
-    val sectionName = (parent as? ProjectViewPsiSection)?.getKeyword() ?: return listOf()
-    if (sectionName.text.contains("flag")) {
+    val sectionName = (parent as? ProjectViewPsiSection)?.getKeyword()?.text ?: return listOf()
+    if (sectionName.contains("flag") && ProjectViewSection.KEYWORD_MAP.contains(sectionName) && isLeaf()) {
       return listOf(ProjectViewFlagReference(this))
     }
     return listOf()
