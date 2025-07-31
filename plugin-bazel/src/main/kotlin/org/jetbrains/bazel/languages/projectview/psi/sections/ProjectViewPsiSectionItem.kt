d6a160706a9d982cb7cf64f3e8382429e6dbd1c8
Stanislaw Solarewicz <stanislaw.solarewicz@jetbrains.com>
2025-07-31 17:51:59 +0200

    use DocumentationTargetProvider instead of PsiDocumentationTargetProvider
    GitOrigin-RevId: 113d4c23e267b7d80948c7021238295edc6702eb

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/psi/sections/ProjectViewPsiSectionItem.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/psi/sections/ProjectViewPsiSectionItem.kt
@@ -1,10 +1,10 @@
 package org.jetbrains.bazel.languages.projectview.psi.sections
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiReference
 import com.intellij.psi.util.elementType
 import org.jetbrains.bazel.languages.projectview.language.ProjectViewSection
 import org.jetbrains.bazel.languages.projectview.lexer.ProjectViewTokenType
-import com.intellij.psi.PsiReference
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewBaseElement
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewElementVisitor
 import org.jetbrains.bazel.languages.projectview.references.ProjectViewFlagReference
@@ -19,13 +19,13 @@ class ProjectViewPsiSectionItem(node: ASTNode) : ProjectViewBaseElement(node) {
   fun getSection(): ProjectViewPsiSection? = parent as? ProjectViewPsiSection
 
   override fun getOwnReferences(): Collection<ProjectViewFlagReference> {
-    //if (!(firstChild != null && firstChild == lastChild && firstChild.elementType == ProjectViewTokenType.IDENTIFIER)) {
+    // if (!(firstChild != null && firstChild == lastChild && firstChild.elementType == ProjectViewTokenType.IDENTIFIER)) {
     //  return emptyList()
-    //}
-    //val sectionName = getSection()?.getKeyword()?.text ?: return emptyList()
-    //if (sectionName.contains("flag") && ProjectViewSection.KEYWORD_MAP.contains(sectionName)) {
+    // }
+    // val sectionName = getSection()?.getKeyword()?.text ?: return emptyList()
+    // if (sectionName.contains("flag") && ProjectViewSection.KEYWORD_MAP.contains(sectionName)) {
     //  return listOf(ProjectViewFlagReference(this))
-    //}
+    // }
     return emptyList()
   }
 
