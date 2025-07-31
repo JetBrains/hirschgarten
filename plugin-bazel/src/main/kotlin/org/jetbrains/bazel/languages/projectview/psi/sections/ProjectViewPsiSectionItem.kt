a8bb7ff7fe5d60fca8a263864eda03b67f2f5c72
Stanislaw Solarewicz <stanislaw.solarewicz@jetbrains.com>
2025-07-31 17:54:24 +0200

    get rid of FlagReference
    GitOrigin-RevId: f095b34ac981050441d53d51ac34dddc26900271

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/psi/sections/ProjectViewPsiSectionItem.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/psi/sections/ProjectViewPsiSectionItem.kt
@@ -2,32 +2,16 @@ package org.jetbrains.bazel.languages.projectview.psi.sections
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiReference
-import com.intellij.psi.util.elementType
-import org.jetbrains.bazel.languages.projectview.language.ProjectViewSection
-import org.jetbrains.bazel.languages.projectview.lexer.ProjectViewTokenType
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewBaseElement
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewElementVisitor
-import org.jetbrains.bazel.languages.projectview.references.ProjectViewFlagReference
 import org.jetbrains.bazel.languages.projectview.references.ProjectViewLabelReference
 
-@Suppress("UnstableApiUsage")
 class ProjectViewPsiSectionItem(node: ASTNode) : ProjectViewBaseElement(node) {
   override fun acceptVisitor(visitor: ProjectViewElementVisitor) {
     visitor.visitSectionItem(this)
   }
 
   fun getSection(): ProjectViewPsiSection? = parent as? ProjectViewPsiSection
-
-  override fun getOwnReferences(): Collection<ProjectViewFlagReference> {
-    // if (!(firstChild != null && firstChild == lastChild && firstChild.elementType == ProjectViewTokenType.IDENTIFIER)) {
-    //  return emptyList()
-    // }
-    // val sectionName = getSection()?.getKeyword()?.text ?: return emptyList()
-    // if (sectionName.contains("flag") && ProjectViewSection.KEYWORD_MAP.contains(sectionName)) {
-    //  return listOf(ProjectViewFlagReference(this))
-    // }
-    return emptyList()
-  }
 
   override fun getReference(): PsiReference? {
     val sectionKeyword = getSection()?.getKeyword()?.text ?: return ProjectViewLabelReference(this)
