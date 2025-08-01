95f025d3376a403c3e84e356f65bd6816b9e51a7
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-08-01 12:48:16 +0200

    open BspInfo for tests
    GitOrigin-RevId: 39c56b28542f7488484bd7c93d8198f1a927e719

--- server/server/src/main/kotlin/org/jetbrains/bazel/server/bsp/info/BspInfo.kt
+++ server/server/src/main/kotlin/org/jetbrains/bazel/server/bsp/info/BspInfo.kt
@@ -2,6 +2,6 @@ package org.jetbrains.bazel.server.bsp.info
 
 import java.nio.file.Path
 
-class BspInfo(val bspProjectRoot: Path) {
-  val bazelBspDir: Path = bspProjectRoot.resolve(".bazelbsp")
+open class BspInfo(val bspProjectRoot: Path) {
+  open val bazelBspDir: Path = bspProjectRoot.resolve(".bazelbsp")
 }
