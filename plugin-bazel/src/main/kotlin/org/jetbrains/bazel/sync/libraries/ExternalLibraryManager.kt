79df34b26f4165ec6ce37ef7c5873c3fd2dd2564
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2025-08-01 18:30:46 +0200

    BAZEL-2265 [fix] additional fix
    GitOrigin-RevId: 5872c402b996540f1dc8c939828563caf6e40a4b

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/libraries/ExternalLibraryManager.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/libraries/ExternalLibraryManager.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.AdditionalLibraryRootsProvider
 import kotlinx.coroutines.CoroutineScope
 import org.jetbrains.bazel.config.isBazelProject
+import org.jetbrains.bazel.coroutines.BazelCoroutineService
 import org.jetbrains.bazel.sync.status.SyncStatusListener
 import org.jetbrains.bazel.utils.refreshAndFindVirtualFile
 import java.util.concurrent.atomic.AtomicBoolean
@@ -24,19 +25,24 @@ class ExternalLibraryManager(private val project: Project, private val cs: Corou
     }
   }
 
+  @Synchronized
   private fun initializeVariables() {
-    this.libraries =
-      AdditionalLibraryRootsProvider.EP_NAME
-        .extensionList
-        .mapNotNull { it as? BazelExternalLibraryProvider }
-        .mapNotNull { provider ->
-          val files = provider.getLibraryFiles(project).mapNotNull { it.refreshAndFindVirtualFile() }
-          if (files.isNotEmpty()) {
-            provider.javaClass to BazelExternalSyntheticLibrary(provider.libraryName, files)
-          } else {
-            null
-          }
-        }.toMap()
+    BazelCoroutineService.getInstance(project).start {
+      underLibraryUpdate.set(true)
+      libraries =
+        AdditionalLibraryRootsProvider.EP_NAME
+          .extensionList
+          .mapNotNull { it as? BazelExternalLibraryProvider }
+          .mapNotNull { provider ->
+            val files = provider.getLibraryFiles(project).mapNotNull { it.refreshAndFindVirtualFile() }
+            if (files.isNotEmpty()) {
+              provider.javaClass to BazelExternalSyntheticLibrary(provider.libraryName, files)
+            } else {
+              null
+            }
+          }.toMap()
+      underLibraryUpdate.set(false)
+    }
   }
 
   private fun initializeListeners() {
@@ -45,7 +51,6 @@ class ExternalLibraryManager(private val project: Project, private val cs: Corou
       object : SyncStatusListener {
         override fun syncStarted() {
           duringSync.set(true)
-          underLibraryUpdate.set(false)
         }
 
         override fun targetUtilAvailable() {
@@ -53,10 +58,9 @@ class ExternalLibraryManager(private val project: Project, private val cs: Corou
         }
 
         private fun handleLibraryUpdate() {
-          if (duringSync.get() && underLibraryUpdate.compareAndSet(false, true)) {
+          if (duringSync.get()) {
             initializeVariables()
             duringSync.set(false)
-            underLibraryUpdate.set(false)
           }
         }
 
@@ -69,7 +73,7 @@ class ExternalLibraryManager(private val project: Project, private val cs: Corou
 
   @Synchronized
   fun getLibrary(providerClass: Class<out BazelExternalLibraryProvider>): BazelExternalSyntheticLibrary? =
-    if (duringSync.get()) null else libraries[providerClass]
+    if (duringSync.get() || underLibraryUpdate.get()) null else libraries[providerClass]
 
   companion object {
     @JvmStatic
