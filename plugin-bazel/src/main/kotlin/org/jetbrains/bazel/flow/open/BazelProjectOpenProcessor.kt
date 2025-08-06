daae758c686af9bcd9500ce960ffac5c054b5e66
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2025-08-06 17:40:58 +0200

    [bazel][maintenance] open Bazel projects asynchronously to avoid SlowOperations exceptions | #BAZEL-1807 Done
    GitOrigin-RevId: c1ce0e169d6248d2d534bf22dbaa443fdc0a3867

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/BazelProjectOpenProcessor.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/BazelProjectOpenProcessor.kt
@@ -1,16 +1,25 @@
 package org.jetbrains.bazel.flow.open
 
+import com.intellij.ide.impl.OpenProjectTask
+import com.intellij.ide.impl.ProjectUtilCore
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ex.ProjectManagerEx
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.isFile
+import com.intellij.projectImport.ProjectOpenProcessor
 import org.jetbrains.bazel.assets.BazelPluginIcons
 import org.jetbrains.bazel.commons.constants.Constants
 import org.jetbrains.bazel.config.BazelFeatureFlags
 import org.jetbrains.bazel.config.BazelPluginConstants
+import org.jetbrains.bazel.config.isBazelProject
+import org.jetbrains.bazel.config.rootDir
 import org.jetbrains.bazel.settings.bazel.setProjectViewPath
 import org.jetbrains.bazel.utils.refreshAndFindVirtualFile
 import java.io.IOException
+import java.nio.file.Path
 import javax.swing.Icon
 import kotlin.io.path.isRegularFile
 import kotlin.io.path.listDirectoryEntries
@@ -22,8 +31,74 @@ internal val BUILD_FILE_GLOB = "{${Constants.BUILD_FILE_NAMES.joinToString(",")}
 /**
  * Refrain from using [VirtualFile.getChildren] as it causes performance issues in large projects, such as [BAZEL-1717](https://youtrack.jetbrains.com/issue/BAZEL-1717)
  */
-internal class BazelProjectOpenProcessor : BaseProjectOpenProcessor() {
-  override fun calculateProjectFolderToOpen(virtualFile: VirtualFile): VirtualFile =
+internal class BazelProjectOpenProcessor : ProjectOpenProcessor() {
+  override fun doOpenProject(
+    virtualFile: VirtualFile,
+    projectToClose: Project?,
+    forceOpenInNewFrame: Boolean,
+  ): Project? {
+    log.info("Opening project :$virtualFile")
+    val vFileToOpen = calculateProjectFolderToOpen(virtualFile)
+    val projectPath = vFileToOpen.toNioPath()
+    val openProjectTask =
+      calculateOpenProjectTask(
+        projectPath = projectPath,
+        forceOpenInNewFrame = forceOpenInNewFrame,
+        projectToClose = projectToClose,
+        vFileToOpen = vFileToOpen,
+        originalVFile = virtualFile,
+      )
+
+    return ProjectManagerEx.getInstanceEx().openProject(projectPath, openProjectTask)
+  }
+
+  override suspend fun openProjectAsync(
+    virtualFile: VirtualFile,
+    projectToClose: Project?,
+    forceOpenInNewFrame: Boolean,
+  ): Project? {
+    log.info("Opening asynchronously project :$virtualFile")
+    val vFileToOpen = calculateProjectFolderToOpen(virtualFile)
+    val projectPath = vFileToOpen.toNioPath()
+    val openProjectTask =
+      calculateOpenProjectTask(
+        projectPath = projectPath,
+        forceOpenInNewFrame = forceOpenInNewFrame,
+        projectToClose = projectToClose,
+        vFileToOpen = vFileToOpen,
+        originalVFile = virtualFile,
+      )
+
+    return ProjectManagerEx.getInstanceEx().openProjectAsync(projectPath, openProjectTask)
+  }
+
+  private fun calculateOpenProjectTask(
+    projectPath: Path,
+    forceOpenInNewFrame: Boolean,
+    projectToClose: Project?,
+    vFileToOpen: VirtualFile,
+    originalVFile: VirtualFile,
+  ): OpenProjectTask =
+    OpenProjectTask {
+      runConfigurators = true
+      isNewProject = !ProjectUtilCore.isValidProjectPath(projectPath)
+      isRefreshVfsNeeded = !ApplicationManager.getApplication().isUnitTestMode
+
+      this.forceOpenInNewFrame = forceOpenInNewFrame
+      this.projectToClose = projectToClose
+
+      beforeOpen = {
+        it.initProperties(vFileToOpen)
+        calculateBeforeOpenCallback(originalVFile).invoke(it)
+        true
+      }
+    }
+
+  /**
+   * when a file/subdirectory is selected for opening a Bazel project,
+   * this method provides information about the real project directory to open
+   */
+  fun calculateProjectFolderToOpen(virtualFile: VirtualFile): VirtualFile =
     findProjectFolderFromVFile(virtualFile)
       ?: error("Cannot find the suitable Bazel project folder to open for the given file $virtualFile.")
 
@@ -39,7 +114,11 @@ internal class BazelProjectOpenProcessor : BaseProjectOpenProcessor() {
    */
   override fun canOpenProject(file: VirtualFile): Boolean = findProjectFolderFromVFile(file) != null
 
-  override fun calculateBeforeOpenCallback(originalVFile: VirtualFile): (Project) -> Unit =
+  /**
+   * this method can be used to set up additional project properties before opening the Bazel project
+   * @param originalVFile the virtual file passed to the project open processor
+   */
+  private fun calculateBeforeOpenCallback(originalVFile: VirtualFile): (Project) -> Unit =
     when {
       originalVFile.isProjectViewFile() -> projectViewFileBeforeOpenCallback(originalVFile)
       // BUILD file at the root can be treated as a workspace file in this context
@@ -110,4 +189,11 @@ fun getBuildFileForPackageDirectory(packageDirectory: VirtualFile): VirtualFile?
     log.warn("Cannot retrieve Bazel BUILD file from directory ${packageDirectory.toNioPath()}", e)
     return null
   }
+}
+
+fun Project.initProperties(projectRootDir: VirtualFile) {
+  thisLogger().debug("Initializing properties for project: $projectRootDir")
+
+  this.isBazelProject = true
+  this.rootDir = projectRootDir
 }
