85bed77148958e4956e178867c6fa74a5aa031db
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2025-08-06 10:26:57 +0000

    [bazel][feature] support macro `java_export` from `rules_jvm_external` | #BAZEL-2283 Done
    Merge-request: IJ-MR-171511
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: acd89e4d953c20b6e73f5b91baba1388d5c11670

---
+++ server/server/src/test/kotlin/org/jetbrains/bazel/server/sync/projectResolver/ProjectResolverTest.kt
@@ -0,0 +1,50 @@
+package org.jetbrains.bazel.server.sync.projectResolver
+
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import org.jetbrains.bazel.info.BspTargetInfo.Dependency
+import org.jetbrains.bazel.info.BspTargetInfo.TargetInfo
+import org.jetbrains.bazel.label.Label
+import org.jetbrains.bazel.server.bzlmod.RepoMappingDisabled
+import org.jetbrains.bazel.server.sync.ProjectResolver
+import org.junit.jupiter.api.Test
+
+class ProjectResolverTest {
+  @Test
+  fun `should replace dependency labels from maven_project_jar to their corresponding java library`() {
+    val aProject = targetInfo("//a-project", emptyList(), "maven_project_jar")
+    val aLib = targetInfo("//a-lib", emptyList(), "java_library")
+    val b = targetInfo("//b", listOf("//a-project"), "java_test")
+    val rawTargetInfoMap = toIdToTargetInfoMap(aProject, aLib, b)
+    val repoMapping = RepoMappingDisabled
+
+    val processedDependencies =
+      ProjectResolver.processDependenciesList(
+        b.toBuilder().dependenciesBuilderList,
+        rawTargetInfoMap,
+        repoMapping,
+      )
+    processedDependencies shouldContainExactlyInAnyOrder listOf(dependency("@//a-lib"))
+  }
+
+  private fun targetInfo(
+    id: String,
+    dependenciesIds: List<String>,
+    kind: String,
+  ): TargetInfo =
+    TargetInfo
+      .newBuilder()
+      .setId(id)
+      .addAllDependencies(
+        dependenciesIds.map { dependency(it) },
+      ).setKind(kind)
+      .build()
+
+  private fun dependency(id: String): Dependency =
+    Dependency
+      .newBuilder()
+      .setId(id)
+      .build()
+
+  private fun toIdToTargetInfoMap(vararg targetIds: TargetInfo): Map<Label, TargetInfo> =
+    targetIds.associateBy { targetId -> Label.parse(targetId.id) }
+}
