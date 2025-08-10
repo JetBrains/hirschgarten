e9973c51b5c6f653cccbb59e14c9291964611dd1
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2025-08-10 17:51:18 +0000

    [bazel] refactor .teamcity configuration for bazel.teamcity.cloud;
    configuration now uses factory patterns in all the builds which are templated;
    switch from *.kts files to *.kt files to allow the configuration files to work with k2;
    removed configuration files and builds which weren't used (qodana android builds for example);
    more minor refactors;
    
    Created by Danil Tereshchenko (Danil.Tereshchenko) via Patronus
    
    Merge-request: IJ-MR-171925
    Merged-by: Danil Tereshchenko <Danil.Tereshchenko@jetbrains.com>
    
    GitOrigin-RevId: 07270e15639592f61aafbd3b569197b690f2b232

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,4 @@
 import configurations.*
-import jetbrains.buildServer.configs.kotlin.v10.toExtId
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
@@ -26,66 +25,33 @@ object ProjectTriggerRules {
 
 
 project {
-  vcsRoot(BaseConfiguration.GitHubVcs)
+  vcsRoot(VcsRootHirschgarten)
+  vcsRoot(VcsRootBazelQodana)
+  vcsRoot(VcsRootBuildBuddyQodana)
 
 // setup pipeline chain for bazel-bsp
   val allSteps =
     sequential {
       // 1. Run formatter first
-      // buildType(ProjectFormat.GitHub)
+       buildType(CheckFormating)
       // 2. Run everything else in parallel
       parallel(options = {
         onDependencyFailure = FailureAction.CANCEL
         onDependencyCancel = FailureAction.CANCEL
       }) {
-        buildType(ProjectBuild.GitHub)
-        buildType(ProjectUnitTests.GitHub)
-        buildType(PluginBenchmark.BenchmarkDefaultGitHub)
-        buildType(PluginBenchmark.BenchmarkWithVersionGitHub)
-        buildType(IdeStarterTests.HotswapTestGitHub)
-        buildType(IdeStarterTests.GoLandSyncTestGitHub)
-        buildType(IdeStarterTests.CoroutineDebugTestGitHub)
-        buildType(IdeStarterTests.ReopenWithoutResyncTestGitHub)
-        buildType(IdeStarterTests.RecoverDotBazelBspTestGitHub)
-        buildType(IdeStarterTests.RunLineMarkerTestGitHub)
-        buildType(IdeStarterTests.ExternalRepoResolveTestGitHub)
-        buildType(IdeStarterTests.JarSourceExcludeTestGitHub)
-        buildType(IdeStarterTests.BazelProjectModelModifierTestGitHub)
-        buildType(IdeStarterTests.BazelCoverageTestGitHub)
-        buildType(IdeStarterTests.TestResultsTreeTestGitHub)
-        buildType(IdeStarterTests.ImportRunConfigurationsTestGitHub)
-        buildType(IdeStarterTests.NonModuleTargetsTestGitHub)
-        buildType(IdeStarterTests.RunAllTestsActionTestGitHub)
-        buildType(IdeStarterTests.DisabledKotlinPluginTestGitHub)
-        buildType(IdeStarterTests.PyCharmTestGitHub)
-        buildType(IdeStarterTests.FastBuildTestGitHub)
-        buildType(IdeStarterTests.ProjectViewOpenTestGitHub)
-        buildType(IdeStarterTests.MoveKotlinFileTestGitHub)
-        buildType(ServerE2eTests.SampleRepoGitHub)
-        buildType(ServerE2eTests.LocalJdkGitHub)
-        buildType(ServerE2eTests.RemoteJdkGitHub)
-//            buildType(ServerE2eTests.ServerDownloadsBazeliskGitHub)
-//        buildType(ServerE2eTests.AndroidProjectGitHub)
-//        buildType(ServerE2eTests.AndroidKotlinProjectGitHub)
-        buildType(ServerE2eTests.ScalaProjectGitHub)
-        buildType(ServerE2eTests.KotlinProjectGitHub)
-        buildType(ServerE2eTests.GoProjectGitHub)
-        buildType(ServerE2eTests.PythonProjectGitHub)
-        buildType(ServerE2eTests.JavaDiagnosticsGitHub)
-        buildType(ServerE2eTests.ManualTargetsGitHub)
-        buildType(ServerE2eTests.BuildSyncGitHub)
-        buildType(ServerE2eTests.FirstPhaseSyncGitHub)
-        buildType(ServerE2eTests.PartialSyncGitHub)
-        buildType(ServerE2eTests.ExternalAutoloadsGitHub)
-        buildType(ServerE2eTests.NestedModulesGitHub)
-        buildType(StaticAnalysis.HirschgartenGitHub)
-//        buildType(StaticAnalysis.AndroidBazelRulesGitHub)
-//        buildType(StaticAnalysis.AndroidTestdpcGitHub)
-        buildType(StaticAnalysis.BazelGitHub)
-//        buildType(StaticAnalysis.JetpackComposeGitHub)
+        // Add all platform builds from factory
+        PluginBuildFactory.ForAllPlatforms.forEach { buildType(it) }
+        buildType(ProjectUnitTests)
+        // Add all benchmark tests from factory
+        PluginBenchmarkFactory.AllBenchmarkTests.forEach { buildType(it) }
+        // Add all IDE starter tests from factory
+        IdeStarterTestFactory.AllIdeStarterTests.forEach { buildType(it) }
+        // Add only enabled static analysis tests from factory
+        StaticAnalysisFactory.EnabledAnalysisTests.forEach { buildType(it) }
       }
 
-      buildType(ResultsAggregator.GitHub, options = {
+      buildType(
+        Aggregator, options = {
         onDependencyFailure = FailureAction.ADD_PROBLEM
         onDependencyCancel = FailureAction.ADD_PROBLEM
       })
@@ -94,23 +60,8 @@ project {
   // initialize all build steps for bazel-bsp
   allSteps.forEach { buildType(it) }
 
-  // setup trigger for bazel-bsp pipeline
-//  ProjectFormat.GitHub.triggers {
-//    vcs {
-//      branchFilter = ProjectBranchFilters.githubBranchFilter
-//      triggerRules = ProjectTriggerRules.triggerRules
-//    }
-//  }
 
-//  ResultsAggregator.GitHub.triggers {
-//    finishBuildTrigger {
-//      buildType = "${ProjectFormat.GitHub.id}"
-//      successfulOnly = true
-//      branchFilter = ProjectBranchFilters.githubBranchFilter
-//    }
-//  }
-
-  ResultsAggregator.GitHub.triggers {
+  Aggregator.triggers {
     vcs {
       branchFilter = ProjectBranchFilters.githubBranchFilter
       triggerRules = ProjectTriggerRules.triggerRules
@@ -120,53 +71,13 @@ project {
   // setup display order for bazel-bsp pipeline
   buildTypesOrderIds =
     arrayListOf(
-      //ProjectFormat.GitHub,
-      ProjectBuild.GitHub,
-      ProjectUnitTests.GitHub,
-      PluginBenchmark.BenchmarkDefaultGitHub,
-      PluginBenchmark.BenchmarkWithVersionGitHub,
-      IdeStarterTests.HotswapTestGitHub,
-      IdeStarterTests.GoLandSyncTestGitHub,
-      IdeStarterTests.CoroutineDebugTestGitHub,
-      IdeStarterTests.ReopenWithoutResyncTestGitHub,
-      IdeStarterTests.RecoverDotBazelBspTestGitHub,
-      IdeStarterTests.RunLineMarkerTestGitHub,
-      IdeStarterTests.ExternalRepoResolveTestGitHub,
-      IdeStarterTests.JarSourceExcludeTestGitHub,
-      IdeStarterTests.BazelProjectModelModifierTestGitHub,
-      IdeStarterTests.BazelCoverageTestGitHub,
-      IdeStarterTests.TestResultsTreeTestGitHub,
-      IdeStarterTests.ImportRunConfigurationsTestGitHub,
-      IdeStarterTests.NonModuleTargetsTestGitHub,
-      IdeStarterTests.RunAllTestsActionTestGitHub,
-      IdeStarterTests.DisabledKotlinPluginTestGitHub,
-      IdeStarterTests.PyCharmTestGitHub,
-      IdeStarterTests.FastBuildTestGitHub,
-      IdeStarterTests.ProjectViewOpenTestGitHub,
-      IdeStarterTests.MoveKotlinFileTestGitHub,
-      ServerE2eTests.SampleRepoGitHub,
-      ServerE2eTests.LocalJdkGitHub,
-      ServerE2eTests.RemoteJdkGitHub,
-      // ServerE2eTests.ServerDownloadsBazeliskGitHub,
-//      ServerE2eTests.AndroidProjectGitHub,
-//      ServerE2eTests.AndroidKotlinProjectGitHub,
-      ServerE2eTests.ScalaProjectGitHub,
-      ServerE2eTests.KotlinProjectGitHub,
-      ServerE2eTests.GoProjectGitHub,
-      ServerE2eTests.PythonProjectGitHub,
-      ServerE2eTests.JavaDiagnosticsGitHub,
-      ServerE2eTests.ManualTargetsGitHub,
-      ServerE2eTests.BuildSyncGitHub,
-      ServerE2eTests.FirstPhaseSyncGitHub,
-      ServerE2eTests.PartialSyncGitHub,
-      ServerE2eTests.NestedModulesGitHub,
-      ServerE2eTests.ExternalAutoloadsGitHub,
-      StaticAnalysis.HirschgartenGitHub,
-//      StaticAnalysis.AndroidBazelRulesGitHub,
-//      StaticAnalysis.AndroidTestdpcGitHub,
-      StaticAnalysis.BazelGitHub,
-//      StaticAnalysis.JetpackComposeGitHub,
-      ResultsAggregator.GitHub
+      CheckFormating,
+      *PluginBuildFactory.ForAllPlatforms.toTypedArray(),
+      ProjectUnitTests,
+      *PluginBenchmarkFactory.AllBenchmarkTests.toTypedArray(),
+      *IdeStarterTestFactory.AllIdeStarterTests.toTypedArray(),
+      *StaticAnalysisFactory.EnabledAnalysisTests.toTypedArray(),
+      Aggregator
     )
 }
 
