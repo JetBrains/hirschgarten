710f79bcf2d42f4c4b75d66efe203a27a27aa626
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2025-08-11 13:50:22 +0200

    [bazel][feature] make sure server takes into consideration Bazel version update | #BAZEL-1816 Done
    GitOrigin-RevId: 4be345749f56d7f5596889979a30e35013a29b65

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelversion/service/BazelVersionCheckerService.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/bazelversion/service/BazelVersionCheckerService.kt
@@ -6,33 +6,39 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.openapi.project.Project
+import org.jetbrains.bazel.config.rootDir
 import org.jetbrains.bazel.languages.bazelversion.psi.BazelVersionLiteral
 import org.jetbrains.bazel.languages.bazelversion.psi.toBazelVersionLiteral
 import org.jetbrains.bazel.languages.bazelversion.psi.toBazelVersionStringLiteral
 
 @Service(Service.Level.PROJECT)
 @State(name = "BazelVersionCache", storages = [Storage("bazelVersionCache.xml")])
-class BazelVersionCheckerService : PersistentStateComponent<BazelVersionCheckerService.State> {
+class BazelVersionCheckerService(private val project: Project) : PersistentStateComponent<BazelVersionCheckerService.State> {
   class State : BaseState() {
     var resolverId by string(null)
-    var bazelVersion by string(null)
+    var currentBazelVersion by string(null)
+    var latestBazelVersion by string(null)
   }
 
   private var resolverId: String? = null
-  private var bazelVersion: BazelVersionLiteral? = null
 
-  val latestBazelVersion: BazelVersionLiteral?
-    get() = bazelVersion
+  var latestBazelVersion: BazelVersionLiteral? = null
+    private set
+
+  var currentBazelVersion: BazelVersionLiteral? = null
+    private set
 
   override fun getState(): BazelVersionCheckerService.State =
     State().also {
       it.resolverId = resolverId
-      it.bazelVersion = bazelVersion?.toBazelVersionStringLiteral()
+      it.currentBazelVersion = currentBazelVersion?.toBazelVersionStringLiteral()
+      it.latestBazelVersion = latestBazelVersion?.toBazelVersionStringLiteral()
     }
 
   override fun loadState(state: BazelVersionCheckerService.State) {
     resolverId = state.resolverId
-    bazelVersion = state.bazelVersion?.toBazelVersionLiteral()
+    currentBazelVersion = state.currentBazelVersion?.toBazelVersionLiteral()
+    latestBazelVersion = state.latestBazelVersion?.toBazelVersionLiteral()
   }
 
   suspend fun refreshLatestBazelVersion(project: Project, currentVersion: BazelVersionLiteral?) {
@@ -44,6 +50,22 @@ class BazelVersionCheckerService : PersistentStateComponent<BazelVersionCheckerS
     val resolver =
       BazelVersionResolver.ep.extensionList
         .firstOrNull { it.id == state.resolverId } ?: return
-    bazelVersion = resolver.resolveLatestBazelVersion(project, currentVersion) ?: return
+    currentBazelVersion = currentVersion
+    latestBazelVersion = resolver.resolveLatestBazelVersion(project, currentVersion) ?: return
+  }
+
+  /**
+   * update the current version if it is different from the resolved version.
+   * @return true if the version was updated, false otherwise
+   */
+  fun updateCurrentVersion(): Boolean {
+    val projectPath = project.rootDir.toNioPath()
+    val resolvedVersion = BazelVersionWorkspaceResolver.resolveBazelVersionFromWorkspace(projectPath)
+    return if (currentBazelVersion != resolvedVersion) {
+      currentBazelVersion = resolvedVersion
+      true
+    } else {
+      false
+    }
   }
 }
