f6f543392cd1aa8f422f17d263dbb609b2a3b089
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2025-08-12 16:37:39 +0000

    BAZEL-2332 [feature] enable hotswap in Bazel test targets thanks to `script_path`
    Merge-request: IJ-MR-172195
    Merged-by: Xuan Son Trinh <xuanson.trinh@jetbrains.com>
    
    GitOrigin-RevId: 29b638126021b17d62da2c4626e543285092cac9

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/jvm/run/JvmDebuggableCommandLineState.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/jvm/run/JvmDebuggableCommandLineState.kt
@@ -1,9 +1,16 @@
 package org.jetbrains.bazel.jvm.run
 
 import com.intellij.debugger.DefaultDebugEnvironment
+import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.configurations.RemoteConnection
+import com.intellij.execution.process.OSProcessHandler
+import com.intellij.execution.process.ProcessEvent
+import com.intellij.execution.process.ProcessListener
+import com.intellij.execution.process.ProcessOutputType
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.util.Key
 import org.jetbrains.bazel.run.BazelCommandLineStateBase
+import org.jetbrains.bazel.run.BazelProcessHandler
 import org.jetbrains.bsp.protocol.DebugType
 
 // Longer timeout to account for Bazel build
@@ -31,4 +38,24 @@ abstract class JvmDebuggableCommandLineState(environment: ExecutionEnvironment, 
 
   val debugType: DebugType
     get() = DebugType.JDWP(port)
+
+  fun debugWithScriptPath(
+    workingDirectory: String?,
+    scriptPath: String,
+    handler: BazelProcessHandler,
+  ) {
+    val commandLine = GeneralCommandLine().withWorkDirectory(workingDirectory).withExePath(scriptPath)
+    val scriptHandler = OSProcessHandler(commandLine)
+    scriptHandler.addProcessListener(
+      object : ProcessListener {
+        override fun onTextAvailable(e: ProcessEvent, outputType: Key<*>) {
+          val type = outputType as? ProcessOutputType ?: ProcessOutputType.STDOUT
+          handler.notifyTextAvailable(e.text, type)
+        }
+      },
+    )
+
+    scriptHandler.startNotify()
+    scriptHandler.waitFor()
+  }
 }
--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/jvm/run/JvmTestHandler.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/jvm/run/JvmTestHandler.kt
@@ -25,9 +25,11 @@ import java.util.concurrent.atomic.AtomicReference
 
 class JvmTestHandler(configuration: BazelRunConfiguration) : BazelRunHandler {
   init {
+    // KotlinCoroutineLibraryFinderBeforeRunTaskProvider must be run before HotSwapTestBeforeRunTaskProvider
     configuration.beforeRunTasks =
       listOfNotNull(
         KotlinCoroutineLibraryFinderBeforeRunTaskProvider().createTask(configuration),
+        HotSwapTestBeforeRunTaskProvider().createTask(configuration),
       )
   }
 
@@ -39,6 +41,7 @@ class JvmTestHandler(configuration: BazelRunConfiguration) : BazelRunHandler {
   override fun getRunProfileState(executor: Executor, environment: ExecutionEnvironment): RunProfileState =
     when {
       executor is DefaultDebugExecutor -> {
+        environment.putCopyableUserData(SCRIPT_PATH_KEY, AtomicReference())
         environment.putCopyableUserData(KOTLIN_COROUTINE_LIB_KEY, AtomicReference())
         JvmTestWithDebugCommandLineState(environment, state)
       }
@@ -72,24 +75,33 @@ class JvmTestWithDebugCommandLineState(environment: ExecutionEnvironment, val se
 
   override fun execute(executor: Executor, runner: ProgramRunner<*>): ExecutionResult = executeWithTestConsole(executor)
 
-  override suspend fun startBsp(server: JoinedBuildServer, pidDeferred: CompletableDeferred<Long?>) {
-    val configuration = environment.runProfile as BazelRunConfiguration
-    val kotlinCoroutineLibParam = calculateKotlinCoroutineParams(environment, configuration.project).joinToString(" ")
-    val additionalBazelParams = settings.additionalBazelParams ?: ""
-    val testParams =
-      TestParams(
-        targets = configuration.targets,
-        originId = originId.toString(),
-        workingDirectory = settings.workingDirectory,
-        arguments = transformProgramArguments(settings.programArguments),
-        environmentVariables = settings.env.envs,
-        debug = debugType,
-        testFilter = settings.testFilter,
-        additionalBazelParams = (additionalBazelParams + kotlinCoroutineLibParam).trim().ifEmpty { null },
-      )
+  override suspend fun startBsp(
+    server: JoinedBuildServer,
+    pidDeferred: CompletableDeferred<Long?>,
+    handler: BazelProcessHandler,
+  ) {
+    val scriptPath = environment.getCopyableUserData(SCRIPT_PATH_KEY)?.get()
+    if (scriptPath != null) {
+      debugWithScriptPath(settings.workingDirectory, scriptPath.toString(), handler)
+    } else {
+      val configuration = environment.runProfile as BazelRunConfiguration
+      val kotlinCoroutineLibParam = calculateKotlinCoroutineParams(environment, configuration.project).joinToString(" ")
+      val additionalBazelParams = settings.additionalBazelParams ?: ""
+      val testParams =
+        TestParams(
+          targets = configuration.targets,
+          originId = originId.toString(),
+          workingDirectory = settings.workingDirectory,
+          arguments = transformProgramArguments(settings.programArguments),
+          environmentVariables = settings.env.envs,
+          debug = debugType,
+          testFilter = settings.testFilter,
+          additionalBazelParams = (additionalBazelParams + kotlinCoroutineLibParam).trim().ifEmpty { null },
+        )
 
-    BazelWorkspaceResolveService
-      .getInstance(environment.project)
-      .withEndpointProxy { it.buildTargetTest(testParams) }
+      BazelWorkspaceResolveService
+        .getInstance(environment.project)
+        .withEndpointProxy { it.buildTargetTest(testParams) }
+    }
   }
 }
