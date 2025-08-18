d6816df6dbc6a8e584e82f5688e3eac289c2dd73
Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
2025-08-18 16:38:15 +0200

    [bazel] fix priorities, invoke completions after fetching data, format
    GitOrigin-RevId: 4705a784454488a3ec226bcf4a9b62a9a38f60ed

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/completion/BazelDepCompletionContributor.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/completion/BazelDepCompletionContributor.kt
@@ -1,15 +1,20 @@
 package org.jetbrains.bazel.languages.starlark.completion
 
+import com.intellij.codeInsight.completion.CodeCompletionHandlerBase
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.CompletionType
-import com.intellij.openapi.components.service
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
 import com.intellij.patterns.PsiJavaPatterns.psiElement
 import com.intellij.psi.util.parentOfType
 import com.intellij.util.PlatformIcons
 import com.intellij.util.ProcessingContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jetbrains.bazel.coroutines.BazelCoroutineService
 import org.jetbrains.bazel.languages.starlark.StarlarkLanguage
 import org.jetbrains.bazel.languages.starlark.bazel.modules.BazelModuleRegistryService
@@ -36,15 +41,14 @@ class BazelDepCompletionProvider : CompletionProvider<CompletionParameters>() {
     context: ProcessingContext,
     result: CompletionResultSet,
   ) {
-    val project = parameters.editor.project ?: return
     val stringLiteral = parameters.position.parentOfType<StarlarkStringLiteralExpression>() ?: return
     val callExpression = stringLiteral.parentOfType<StarlarkCallExpression>() ?: return
-
     if (getFunctionName(callExpression) != "bazel_dep") return
 
-    val argumentName = getArgumentName(stringLiteral, callExpression) ?: return
+    val project = parameters.editor.project ?: return
     val registryService = BazelModuleRegistryService.getInstance(project)
 
+    val argumentName = getArgumentName(stringLiteral, callExpression) ?: return
     when (argumentName) {
       "name" -> {
         val cachedNames = registryService.getCachedModuleNames()
@@ -52,32 +56,39 @@ class BazelDepCompletionProvider : CompletionProvider<CompletionParameters>() {
           result.addAllElements(
             cachedNames.map { name: String ->
               getCompletionLookupElemenent(name, PlatformIcons.PACKAGE_ICON, 1.0)
-            }
+            },
           )
-        }
-        project.service<BazelCoroutineService>().start {
-          registryService.getModuleNames()
+        } else {
+          BazelCoroutineService.getInstance(project).start {
+            registryService.getModuleNames()
+            invokeCompletion(project, parameters.editor)
+          }
         }
       }
       "version" -> {
-        //result.addElement(
-        //  getCompletionLookupElemenent("latest", PlatformIcons.PROPERTY_ICON, 1.0),
-        //)
-        val moduleName = getModuleName(callExpression)
-        if (moduleName != null) {
-          val cachedVersions = registryService.getCachedModuleVersions(moduleName)
-          if (cachedVersions.isNotEmpty()) {
-            result.addAllElements(
-              cachedVersions.map { version: String ->
-                getCompletionLookupElemenent(version, PlatformIcons.PROPERTY_ICON, getVersionPriority(version))
-              }
-            )
-          }
-          project.service<BazelCoroutineService>().start {
+        val moduleName = getModuleName(callExpression) ?: return
+        val cachedVersions = registryService.getCachedModuleVersions(moduleName)
+        if (cachedVersions.isNotEmpty()) {
+          result.addAllElements(
+            cachedVersions.mapIndexed { index, version ->
+              getCompletionLookupElemenent(version, PlatformIcons.PROPERTY_ICON, (-index).toDouble())
+            },
+          )
+        } else {
+          BazelCoroutineService.getInstance(project).start {
             registryService.getModuleVersions(moduleName)
+            invokeCompletion(project, parameters.editor)
           }
         }
       }
+    }
+  }
+
+  private suspend fun invokeCompletion(project: Project, editor: Editor) {
+    withContext(Dispatchers.EDT) {
+      if (project.isDisposed || editor.isDisposed) return@withContext
+      CodeCompletionHandlerBase(CompletionType.BASIC, true, false, true)
+        .invokeCompletion(project, editor)
     }
   }
 
@@ -97,11 +108,5 @@ class BazelDepCompletionProvider : CompletionProvider<CompletionParameters>() {
     val nameArg = callExpression.getArgumentList()?.getKeywordArgument("name") ?: return null
     val stringLiteral = nameArg.getValue() as? StarlarkStringLiteralExpression ?: return null
     return stringLiteral.text.removeSurrounding("\"")
-  }
-
-  private fun getVersionPriority(version: String): Double {
-    return runCatching {
-      version.split(".").joinToString("").toDouble() / 1000
-    }.getOrDefault(0.0)
   }
 }
