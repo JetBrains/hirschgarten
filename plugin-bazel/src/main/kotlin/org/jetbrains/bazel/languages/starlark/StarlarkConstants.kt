512c14716ea58acdaaa21a71a3cc4240f567a19f
Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
2025-08-22 11:35:37 +0200

    [bazel] refactor creating new allow_empty argument and remove `TRUE_VALUE` constant
    GitOrigin-RevId: 8ef404c009fb3ac5a37e0e187719fe47a49ad430

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/StarlarkConstants.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/StarlarkConstants.kt
@@ -2,5 +2,4 @@ package org.jetbrains.bazel.languages.starlark
 
 object StarlarkConstants {
   const val ALLOW_EMPTY_KEYWORD = "allow_empty"
-  const val TRUE_VALUE = "True"
 }
--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/quickFixes/StarlarkGlobAllowEmptyQuickFix.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/quickFixes/StarlarkGlobAllowEmptyQuickFix.kt
@@ -9,10 +9,9 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileFactory
 import org.jetbrains.bazel.languages.starlark.StarlarkBundle
 import org.jetbrains.bazel.languages.starlark.StarlarkConstants.ALLOW_EMPTY_KEYWORD
-import org.jetbrains.bazel.languages.starlark.StarlarkConstants.TRUE_VALUE
 import org.jetbrains.bazel.languages.starlark.StarlarkLanguage
 import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkGlobExpression
-import org.jetbrains.bazel.languages.starlark.psi.expressions.arguments.StarlarkArgumentElement
+import java.util.Locale.getDefault
 
 class StarlarkGlobAllowEmptyQuickFix(callExpression: StarlarkGlobExpression) :
   PsiUpdateModCommandAction<StarlarkGlobExpression>(callExpression) {
@@ -24,53 +23,57 @@ class StarlarkGlobAllowEmptyQuickFix(callExpression: StarlarkGlobExpression) :
     updater: ModPsiUpdater,
   ) {
     val writableElement = updater.getWritable(element)
-    val argumentList = writableElement.getArguments()
     val allowEmptyArgument = writableElement.getKeywordArgument(ALLOW_EMPTY_KEYWORD)
+    val newArgument =
+      createFromText(
+        writableElement.project,
+        "$ALLOW_EMPTY_KEYWORD = ${
+          true.toString().replaceFirstChar { if (it.isLowerCase()) it.titlecase(getDefault()) else it.toString() }
+        }",
+      )
 
     if (allowEmptyArgument != null) {
-      val newArgument = createFromText(allowEmptyArgument.project, "$ALLOW_EMPTY_KEYWORD = $TRUE_VALUE")
       updater.getWritable(allowEmptyArgument).replace(newArgument)
     } else {
-      addAllowEmptyArgument(updater, writableElement, argumentList)
+      addAllowEmptyArgument(updater, writableElement, newArgument)
     }
   }
 
   private fun addAllowEmptyArgument(
     updater: ModPsiUpdater,
     element: StarlarkGlobExpression,
-    argumentList: Array<StarlarkArgumentElement>,
+    newArgument: PsiElement,
   ) {
     val project = element.project
+    val argumentList = element.getArguments()
     val lastArgument = argumentList.lastOrNull()
 
     if (lastArgument != null) {
-      val newArgumentElement = createFromText(project, "$ALLOW_EMPTY_KEYWORD = $TRUE_VALUE")
       val writableLastArgument = updater.getWritable(lastArgument)
       val parent = writableLastArgument.parent
       var anchor: PsiElement = writableLastArgument
 
       // add comma if needed
       if (!writableLastArgument.text.trimEnd().endsWith(",")) {
-        val comma = createFromText(project, ",")
-        anchor = parent.addAfter(comma, anchor)
+        anchor = parent.addAfter(createFromText(project, ","), anchor)
       }
 
-      // add allow_empty argument
-      val addedArgument = parent.addAfter(newArgumentElement, anchor)
-
-      // add space before allow_empty argument
-      val space = createFromText(project, " ")
-      parent.addBefore(space, addedArgument)
-    } else {
-      val newArgumentList = createFromText(project, "($ALLOW_EMPTY_KEYWORD = $TRUE_VALUE)")
+      // add allow_empty argument separated by space
+      val addedArgument = parent.addAfter(newArgument, anchor)
+      parent.addBefore(createFromText(project, " "), addedArgument)
+    } else { // empty argument list
       val writableElement = updater.getWritable(element)
 
+      // delete empty parentheses if needed
       val next = writableElement.nextSibling
       if (next != null && next.text.equalsIgnoreWhitespaces("()")) {
-        next.replace(newArgumentList)
-      } else {
-        writableElement.addAfter(newArgumentList, writableElement.firstChild)
+        next.delete()
       }
+
+      // add allow_empty argument enclosed in parentheses
+      val addedArgument = writableElement.addAfter(newArgument, writableElement.firstChild)
+      writableElement.addBefore(createFromText(project, "("), addedArgument)
+      writableElement.addAfter(createFromText(project, ")"), addedArgument)
     }
   }
 
