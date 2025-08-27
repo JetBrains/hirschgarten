f9d7bacd24fd1c02d5985894b11b68955eeb70ae
Ilia Malakhov <ilia.malakhov@jetbrains.com>
2025-08-27 10:29:50 +0200

    [bazel] IJPL-188230 don't create module on project opening
    GitOrigin-RevId: 92a9c488132b77e4bd87d3dcf0cf9263b4d91e78

---
+++ sdkcompat/v252/org/jetbrains/bazel/sdkcompat/OpenProjectTaskBuilderUtils.kt
@@ -0,0 +1,6 @@
+package org.jetbrains.bazel.sdkcompat
+
+import com.intellij.ide.impl.OpenProjectTaskBuilder
+
+@Suppress("UnusedReceiverParameter", "unused")
+fun OpenProjectTaskBuilder.createModule(create: Boolean): Unit = Unit
