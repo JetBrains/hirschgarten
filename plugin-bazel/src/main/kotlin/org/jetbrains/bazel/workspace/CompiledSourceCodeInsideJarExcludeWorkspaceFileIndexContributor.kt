13a7a6a2c75e20aa9723cf6685edd4a4b2a3110b
Lev Leontev <lev.leontev@jetbrains.com>
2025-08-27 14:16:13 +0200

    BAZEL-2320: fix exclude in CompiledSourceCodeInsideJarExcludeWorkspaceFileIndexContributor
    GitOrigin-RevId: cb4e8c8bee33c7e49443a9c861fbb0836760a411
    (cherry picked from commit 8b8cf542a4f1b9baef1d53d43d8a830a1e36ae76)

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/workspace/CompiledSourceCodeInsideJarExcludeWorkspaceFileIndexContributor.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/workspace/CompiledSourceCodeInsideJarExcludeWorkspaceFileIndexContributor.kt
@@ -3,11 +3,13 @@ package org.jetbrains.bazel.workspace
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.isFile
 import com.intellij.platform.workspace.jps.entities.LibraryRootTypeId
 import com.intellij.platform.workspace.storage.EntityStorage
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndexContributor
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileSetRegistrar
 import org.jetbrains.bazel.sdkcompat.workspacemodel.entities.LibraryCompiledSourceCodeInsideJarExcludeEntity
+import kotlin.collections.contains
 
 /**
  * Don't index irrelevant files inside jars that are built from internal targets.
@@ -70,7 +72,7 @@ class CompiledSourceCodeInsideJarExcludeWorkspaceFileIndexContributor :
           val relativePathWithoutNestedClass = removeNestedClass(relativePath)
           if (relativePathWithoutNestedClass in relativePathsToExclude) return@registerExclusionCondition true
           if (rootFile.url in librariesFromInternalTargetsUrls) {
-            return@registerExclusionCondition virtualFile.extension !in JVM_EXTENSIONS
+            return@registerExclusionCondition virtualFile.hasNonJvmExtension()
           }
           false
         },
@@ -81,7 +83,7 @@ class CompiledSourceCodeInsideJarExcludeWorkspaceFileIndexContributor :
         registrar.registerExclusionCondition(
           root = contentRootUrl,
           condition = { virtualFile ->
-            virtualFile.extension !in JVM_EXTENSIONS
+            virtualFile.hasNonJvmExtension()
           },
           entity = entity,
         )
@@ -108,3 +110,5 @@ private fun removeNestedClass(relativePath: String): String {
   if (dotIndex == -1) return relativePath
   return relativePath.substring(0, dollarIndex) + relativePath.substring(dotIndex)
 }
+
+private fun VirtualFile.hasNonJvmExtension() = isFile && extension !in JVM_EXTENSIONS
