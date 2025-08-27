0ae45c47366cdfcde38bf6e9a989c20a5f3a24c7
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2025-08-27 11:01:41 +0000

    [bazel] [maintenence] Add a mapper sanity test
    Merge-request: IJ-MR-172524
    Merged-by: Andrzej Gluszak <Andrzej.Gluszak@jetbrains.com>
    
    GitOrigin-RevId: 521707edf2eb951443d6b7646c679a73ce122ffb

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/BazelResolvedWorkspace.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/BazelResolvedWorkspace.kt
@@ -11,7 +11,7 @@ data class BazelResolvedWorkspace(
   val hasError: Boolean = false,
 )
 
-class BuildTargetCollection(
+data class BuildTargetCollection(
   internal val buildTargets: MutableList<RawBuildTarget> = mutableListOf(),
   internal val nonModuleTargets: MutableList<RawBuildTarget> = mutableListOf(),
 ) {
