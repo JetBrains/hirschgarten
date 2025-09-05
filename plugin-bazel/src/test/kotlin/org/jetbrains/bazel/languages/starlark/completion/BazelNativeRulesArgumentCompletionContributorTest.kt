baea50e9bc50e76fbdd21dac78434c6a87ba5a02
Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
2025-09-05 08:39:10 +0000

    [bazel] Add class name completion in BUILD files | #BAZEL-2105 Done
    [bazel] improve isCompletableClass() and add tests
    
    [bazel] remove unnecessary comments from all Starlark completion tests
    
    [bazel] handle nested classes completion
    
    [bazel] add completing classnames by segments in Starlark
    
    [bazel] add kotlin examples and insertion handling tests for Starlark class completion
    
    [bazel] register StarlarkClassnameCompletionContributor and format
    
    [bazel] replace getVariants() method with StarlarkClassnameCompletionContributor
    
    [bazel] replace the property name to avoid negation, change getVariants() return type to Array<LookupElement>
    
    [bazel] Test class name completion in BUILD files
    
    [bazel] Add class name completion in BUILD files
    
    
    Merge-request: IJ-MR-170842
    Merged-by: Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
    
    GitOrigin-RevId: b010375e50224cb74ad216f6623b7c603630873d

--- plugin-bazel/src/test/kotlin/org/jetbrains/bazel/languages/starlark/completion/BazelNativeRulesArgumentCompletionContributorTest.kt
+++ plugin-bazel/src/test/kotlin/org/jetbrains/bazel/languages/starlark/completion/BazelNativeRulesArgumentCompletionContributorTest.kt
@@ -11,14 +11,11 @@ import org.junit.runners.JUnit4
 class BazelNativeRulesArgumentCompletionContributorTest : BasePlatformTestCase() {
   @Test
   fun `should complete native rule arguments`() {
-    // given
     myFixture.configureByText("BUILD", "java_binary(<caret>)")
     myFixture.type("e")
 
-    // when
     val lookups = myFixture.completeBasic().flatMap { it.allLookupStrings }
 
-    // then
     lookups shouldContainAll
       listOf(
         "name",
@@ -33,40 +30,31 @@ class BazelNativeRulesArgumentCompletionContributorTest : BasePlatformTestCase()
 
   @Test
   fun `should not double complete native rule arguments`() {
-    // given
     myFixture.configureByText("BUILD", "java_binary(name = \"\", <caret>)")
     myFixture.type("e")
 
-    // when
     val lookups = myFixture.completeBasic().flatMap { it.allLookupStrings }
 
-    // then
     lookups shouldNotContain listOf("name")
   }
 
   @Test
   fun `should complete empty list`() {
-    // given
     myFixture.configureByText("BUILD", "java_binary(<caret>)")
     myFixture.type("src")
 
-    // when
     myFixture.completeBasic()
 
-    // then
     myFixture.checkResult("java_binary(srcs = [<caret>],)")
   }
 
   @Test
   fun `should complete empty string`() {
-    // given
     myFixture.configureByText("BUILD", "java_binary(<caret>)")
     myFixture.type("resource_strip_prefi")
 
-    // when
     myFixture.completeBasic()
 
-    // then
     myFixture.checkResult("java_binary(resource_strip_prefix = \"<caret>\",)")
   }
 }
