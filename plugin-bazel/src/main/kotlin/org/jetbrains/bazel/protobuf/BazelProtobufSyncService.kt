47d10d1236fdf051134664b546bcfd43e4cd54cb
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-09-10 10:59:47 +0300

    fix imports
    GitOrigin-RevId: a500e163e92691a64eb81e09b55eb8ab2a8cff1d

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/protobuf/BazelProtobufSyncService.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/protobuf/BazelProtobufSyncService.kt
@@ -2,22 +2,13 @@ package org.jetbrains.bazel.protobuf
 
 import com.intellij.configurationStore.SettingsSavingComponent
 import com.intellij.openapi.components.Service
-import com.intellij.openapi.components.State
-import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.backend.workspace.WorkspaceModel
 import com.intellij.platform.backend.workspace.virtualFile
 import com.intellij.platform.workspace.storage.impl.url.toVirtualFileUrl
-import kotlinx.io.IOException
-import org.jetbrains.bazel.commons.LanguageClass
-import org.jetbrains.bazel.target.targetUtils
-import org.jetbrains.bsp.protocol.ProtobufBuildTarget
-import java.io.DataInputStream
-import java.io.DataOutputStream
-import java.nio.file.Path
-import kotlin.io.path.absolutePathString
+import java.io.IOException
 
 @Service(Service.Level.PROJECT)
 class BazelProtobufSyncService(val project: Project) : SettingsSavingComponent {
