5592f7af49e06f53d401313d34bd4fa10b206d80
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-09-11 19:37:03 +0300

    BAZEL-2160 cleanup
    GitOrigin-RevId: 7d96e5f37c28adf8651a57ebc23b295da9b4f24d

--- sdkcompat/master/org/jetbrains/bazel/sdkcompat/BazelProjectStorePathCustomizer.kt
+++ sdkcompat/master/org/jetbrains/bazel/sdkcompat/BazelProjectStorePathCustomizer.kt
@@ -4,10 +4,55 @@ package org.jetbrains.bazel.sdkcompat
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectStorePathCustomizer
+import com.intellij.openapi.project.getProjectCacheFileName
+import com.intellij.openapi.project.projectsDataDir
 import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.io.path.div
 import kotlin.io.path.isDirectory
+
+private val shareableConfigFiles =
+  listOf(
+    "AIAssistantCustomInstructionsStorage.xml",
+    "anchors.xml",
+    "ant.xml",
+    "checker.xml",
+    "codeInsightSettings.xml",
+    "codeStyleSettings.xml",
+    "compiler.xml",
+    "csv-editor.xml",
+    "dependencyResolver.xml",
+    "deployment.xml",
+    "encodings.xml",
+    "excludeFromValidation.xml",
+    "externalDependencies.xml",
+    "fileColors.xml",
+    "gradle.xml",
+    "IntelliLang.xml",
+    "jarRepositories.xml",
+    "jsLibraryMappings.xml",
+    "jsonSchemas.xml",
+    "kotlinc.xml",
+    "kotlinTestDataPluginTestDataPaths.xml",
+    "ktfmt.xml",
+    "ktor.xml",
+    "php.xml",
+    "projectDictionary.xml",
+    "remote-targets.xml",
+    "scala_settings.xml",
+    "sqldialects.xml",
+    "terraform.xml",
+    "uiDesigner.xml",
+    "vcs.xml",
+    "misc.xml",
+    // scheme manager
+    "codeStyles",
+    "copyright",
+    "dictionaries",
+    "fileTemplates",
+    "inspectionProfiles",
+    "scopes",
+  )
 
 internal class BazelProjectStorePathCustomizer : ProjectStorePathCustomizer {
   override fun getStoreDirectoryPath(projectRoot: Path): ProjectStorePathCustomizer.StoreDescriptor? {
@@ -15,11 +60,10 @@ internal class BazelProjectStorePathCustomizer : ProjectStorePathCustomizer {
       return null
     } else {
       val historicalProjectBasePath = projectRoot.parent
-      val dotIdea =
-        historicalProjectBasePath /
-          Project.DIRECTORY_STORE_FOLDER /
-          "${Project.DIRECTORY_STORE_FOLDER}.${projectRoot.fileName}" /
-          Project.DIRECTORY_STORE_FOLDER
+      // we should use `getProjectCacheFileName` API,
+      // as in this dir is located a lot of other project-related data, so, location of dir should be in an expected location
+      val cacheDirectoryName = getProjectCacheFileName(projectRoot)
+      val dotIdea = projectsDataDir.resolve(cacheDirectoryName).resolve("bazel.idea")
       return object : ProjectStorePathCustomizer.StoreDescriptor(
         projectIdentityDir = projectRoot,
         dotIdea = dotIdea,
@@ -31,47 +75,6 @@ internal class BazelProjectStorePathCustomizer : ProjectStorePathCustomizer {
             return emptyMap()
           }
 
-          val shareableConfigFiles =
-            listOf(
-              "AIAssistantCustomInstructionsStorage.xml",
-              "anchors.xml",
-              "ant.xml",
-              "checker.xml",
-              "codeInsightSettings.xml",
-              "codeStyleSettings.xml",
-              "compiler.xml",
-              "csv-editor.xml",
-              "dependencyResolver.xml",
-              "deployment.xml",
-              "encodings.xml",
-              "excludeFromValidation.xml",
-              "externalDependencies.xml",
-              "fileColors.xml",
-              "gradle.xml",
-              "IntelliLang.xml",
-              "jarRepositories.xml",
-              "jsLibraryMappings.xml",
-              "jsonSchemas.xml",
-              "kotlinc.xml",
-              "kotlinTestDataPluginTestDataPaths.xml",
-              "ktfmt.xml",
-              "ktor.xml",
-              "php.xml",
-              "projectDictionary.xml",
-              "remote-targets.xml",
-              "scala_settings.xml",
-              "sqldialects.xml",
-              "terraform.xml",
-              "uiDesigner.xml",
-              "vcs.xml",
-              // scheme manager
-              "codeStyles",
-              "copyright",
-              "dictionaries",
-              "fileTemplates",
-              "inspectionProfiles",
-              "scopes",
-            )
           val result = HashMap<String, Path>(shareableConfigFiles.size)
           for (filePath in shareableConfigFiles) {
             result.put(filePath, rootDotIdea.resolve(filePath))
