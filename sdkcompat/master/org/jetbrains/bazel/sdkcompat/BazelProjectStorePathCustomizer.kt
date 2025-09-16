41b16b9dd30c89fbfac7bb4bfb51e5fa60e67275
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-09-16 14:23:54 +0200

    BAZEL-2160 BazelProjectStorePathCustomizer - do not use isDirectory, as we also want to check that file exists (otherwise, processor is applied for unrelated files)
    GitOrigin-RevId: baf1b4bb5588c7f510cafbc83dda373e9232b85e

--- sdkcompat/master/org/jetbrains/bazel/sdkcompat/BazelProjectStorePathCustomizer.kt
+++ sdkcompat/master/org/jetbrains/bazel/sdkcompat/BazelProjectStorePathCustomizer.kt
@@ -23,26 +23,28 @@ import java.nio.file.Path
 
 private class BazelProjectStorePathCustomizer : ProjectStorePathCustomizer {
   override fun getStoreDirectoryPath(projectRoot: Path): ProjectStoreDescriptor? {
-    if (Files.isDirectory(projectRoot)) {
+    // do not use isDirectory, as we also want to check that file exists
+    val fileName = projectRoot.fileName?.toString() ?: return null
+    if (!fileName.contains('.') || !Files.isRegularFile(projectRoot)) {
       return null
-    } else {
-      // we should use `getProjectCacheFileName` API,
-      // as in this dir is located a lot of other project-related data, so, location of dir should be in an expected location
-      val cacheDirectoryName = getProjectCacheFileName(projectRoot)
-      val dotIdea = projectsDataDir.resolve(cacheDirectoryName).resolve("bazel.idea")
-      val workspaceXml =
-        PathManager
-          .getConfigDir()
-          .resolve("workspace")
-          .resolve("bazel")
-          .resolve("$cacheDirectoryName.xml")
-      return BazelProjectStoreDescriptor(
-        projectIdentityFile = projectRoot,
-        dotIdea = dotIdea,
-        historicalProjectBasePath = projectRoot.parent,
-        workspaceXml = workspaceXml,
-      )
     }
+
+    // we should use `getProjectCacheFileName` API,
+    // as in this dir is located a lot of other project-related data, so, location of dir should be in an expected location
+    val cacheDirectoryName = getProjectCacheFileName(projectRoot)
+    val dotIdea = projectsDataDir.resolve(cacheDirectoryName).resolve("bazel.idea")
+    val workspaceXml =
+      PathManager
+        .getConfigDir()
+        .resolve("workspace")
+        .resolve("bazel")
+        .resolve("$cacheDirectoryName.xml")
+    return BazelProjectStoreDescriptor(
+      projectIdentityFile = projectRoot,
+      dotIdea = dotIdea,
+      historicalProjectBasePath = projectRoot.parent,
+      workspaceXml = workspaceXml,
+    )
   }
 }
 
