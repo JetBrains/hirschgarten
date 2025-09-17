ac412eb028dcdf24e3fee809ccaa3c5cff8f4688
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-09-17 10:35:05 +0200

    wip
    GitOrigin-RevId: 54f55c6cdf5ff0ab3d5ea3f335bf0fb53c86df06

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/languages/LanguagePluginContext.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/languages/LanguagePluginContext.kt
@@ -3,9 +3,11 @@ package org.jetbrains.bazel.sync.workspace.languages
 import org.jetbrains.bazel.commons.RepoMapping
 import org.jetbrains.bazel.info.BspTargetInfo
 import org.jetbrains.bazel.sync.workspace.graph.DependencyGraph
+import org.jetbrains.bsp.protocol.SourceItem
 
 data class LanguagePluginContext(
   val target: BspTargetInfo.TargetInfo,
   val graph: DependencyGraph,
   val repoMapping: RepoMapping,
+  val sources: List<SourceItem>
 )
---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/languages/java/JavaSourceRootPackageInference.kt
@@ -0,0 +1,192 @@
+package org.jetbrains.bazel.sync.workspace.languages.java
+
+import org.jetbrains.bazel.sync.workspace.languages.JvmPackageResolver
+import org.jetbrains.bsp.protocol.SourceItem
+import java.nio.file.Path
+import kotlin.io.path.exists
+import kotlin.io.path.extension
+
+private val SUPPORTED_EXTENSIONS = listOf("java")
+
+// this package resolves based inference mechanism is trying
+// to optimize amount of calls package resolver
+//
+// for large monorepos randomly accessing over 100_000 files can be inefficient
+// and profiler snapshot prove that(on some systems opening file handles can take over 100 seconds in total)
+//
+// this implementation tries to optimize access to files by grouping them by their package
+// and then resolving package for each group by resolving relative path
+//
+// it is not perfect but still better than randomly accessing thousands of files
+//
+class JavaSourceRootPackageInference(val packageResolver: JvmPackageResolver) {
+
+  fun inferPackages(sources: List<SourceItem>) {
+    if (sources.isEmpty()) {
+      return
+    }
+
+    val supportedSources = sources.filter { it.path.exists() }
+      .filter { SUPPORTED_EXTENSIONS.contains(it.path.extension) }
+
+    if (supportedSources.isEmpty()) {
+      return
+    }
+
+    if (supportedSources.size == 1) {
+      val firstSource = supportedSources.first()
+      firstSource.jvmPackagePrefix = packageResolver.calculateJvmPackagePrefix(firstSource.path)
+      return
+    }
+
+    val tree = SourceItemPropagationPrefixTree()
+    for (item in supportedSources) {
+      val segments = item.path.asSequence()
+        .map { it.toString() }
+      tree.push(segments, item)
+    }
+
+    tree.propagateJavaPackages { packageResolver.calculateJvmPackagePrefix(it) }
+  }
+}
+
+class SourceItemPropagationPrefixTree {
+  private data class Node(
+    val segment: String,
+    var item: SourceItem?,
+    val children: MutableList<Node> = mutableListOf(),
+  )
+
+  private var root = Node("", null)
+
+  fun push(path: Sequence<String>, data: SourceItem) {
+
+    var current = root
+    for (segment in path) {
+      var node: Node? = null
+      for (child in current.children) {
+        if (child.segment == segment) {
+          node = child
+          break
+        }
+      }
+      if (node == null) {
+        node = Node(segment, null)
+        current.children.add(node)
+      }
+      current = node
+    }
+    current.item = data
+  }
+
+  fun propagateJavaPackages(slowPackageResolver: (item: Path) -> String?) {
+    val queue = ArrayDeque<Node>()
+    queue.add(root)
+    while (true) {
+      val node = queue.removeFirstOrNull()
+      if (node == null) {
+        break
+      }
+      // we want to start propagation from roots
+      // so if we find subtree whose child has existing file
+      // we start propagation from it
+      if (node.children.any { it.item != null }) {
+        this.propagateInSubtree(node, slowPackageResolver)
+      }
+      for (child in node.children) {
+        queue.add(child)
+      }
+    }
+  }
+
+  private fun propagateInSubtree(subtreeRoot: Node, slowPackageResolver: (item: Path) -> String?) {
+    // find subtree package reference
+    // it can be any file in specific directory
+    val rootSourceFile = findFirstSourceFileInSubtree(subtreeRoot) ?: return
+    val resolvedPackage = slowPackageResolver(rootSourceFile.path) ?: return
+    // propagate all files in subtree in relation to root source file
+    propagateFromRootSourceFileInSubtree(subtreeRoot, rootSourceFile, resolvedPackage)
+  }
+
+  private fun findFirstSourceFileInSubtree(subtreeRoot: Node): SourceItem? {
+    val queue = ArrayDeque<Node>()
+    queue.add(subtreeRoot)
+
+    while (true) {
+      val node = queue.removeFirstOrNull()
+      if (node == null) {
+        break
+      }
+      if (node.item != null) {
+        return node.item
+      }
+      queue.addAll(node.children)
+    }
+    return null
+  }
+
+  private fun propagateFromRootSourceFileInSubtree(subtreeRoot: Node, rootSourceFile: SourceItem, resolvedPackage: String) {
+    val rootPath = rootSourceFile.path.parent
+
+    val queue = ArrayDeque<Node>()
+    queue.add(subtreeRoot)
+
+    while (true) {
+      val node = queue.removeFirstOrNull()
+      if (node == null) {
+        break
+      }
+      val item = node.item
+      if (item != null) {
+        val currentParentPath = item.path.parent
+        item.jvmPackagePrefix = calculatePackageFromPaths(rootPath, currentParentPath, resolvedPackage)
+      }
+
+      queue.addAll(node.children)
+    }
+  }
+
+  private fun calculatePackageFromPaths(rootPath: Path?, currentPath: Path?, rootPackage: String): String {
+    if (rootPath == currentPath) {
+      return rootPackage
+    }
+
+    if (rootPath == null || currentPath == null) {
+      return rootPackage
+    }
+
+    val repSegments = rootPath.map { it.toString() }
+    val currSegments = currentPath.map { it.toString() }
+
+    var commonLength = 0
+    val minLength = minOf(repSegments.size, currSegments.size)
+
+    for (i in 0 until minLength) {
+      if (repSegments[i] == currSegments[i]) {
+        commonLength++
+      } else {
+        break
+      }
+    }
+
+    val rootPackageParts = if (rootPackage.isEmpty()) {
+      emptyList()
+    } else {
+      rootPackage.split(".")
+    }
+
+    val basePackageParts = if (repSegments.size > commonLength) {
+      val segmentsToRemove = repSegments.size - commonLength
+      rootPackageParts.dropLast(segmentsToRemove)
+    } else {
+      rootPackageParts
+    }
+
+    // remove a common path
+    val additionalSegments = currSegments.drop(commonLength)
+    val finalPackageParts = basePackageParts + additionalSegments
+
+    return finalPackageParts.joinToString(".")
+  }
+
+}
