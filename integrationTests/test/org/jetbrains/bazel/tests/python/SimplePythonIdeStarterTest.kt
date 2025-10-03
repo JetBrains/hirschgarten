6d1c312b384d77ae1e1b65f5de0247af667a7c70
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-10-04 00:49:04 +0400

    BAZEL-2524 | Migrate bazel plugin tests to JPS test runner
    Co-authored-by: Andrzej Gluszak <Andrzej.Gluszak@jetbrains.com>
    
    Merge-request: IJ-MR-176313
    Merged-by: Andrzej Gluszak <Andrzej.Gluszak@jetbrains.com>
    
    (cherry picked from commit 175606937f5e3f91ea6cc5db172a852046c0e235)
    
    GitOrigin-RevId: 273a05c04a8739d8ec87540afe0ccbbd39a72930

---
+++ integrationTests/test/org/jetbrains/bazel/tests/python/SimplePythonIdeStarterTest.kt
@@ -0,0 +1,46 @@
+package org.jetbrains.bazel.tests.python
+
+import com.intellij.driver.sdk.step
+import com.intellij.driver.sdk.ui.components.common.ideFrame
+import com.intellij.driver.sdk.waitForIndicators
+import com.intellij.ide.starter.driver.engine.runIdeWithDriver
+import com.intellij.tools.ide.performanceTesting.commands.checkOnRedCode
+import com.intellij.tools.ide.performanceTesting.commands.openFile
+import org.jetbrains.bazel.data.IdeaBazelCases
+import org.jetbrains.bazel.data.PyCharmBazelCases
+import org.jetbrains.bazel.ideStarter.IdeStarterBaseProjectTest
+import org.jetbrains.bazel.ideStarter.execute
+import org.jetbrains.bazel.ideStarter.syncBazelProject
+import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.minutes
+
+/**
+ * ```sh
+ * bazel test //plugin-bazel/src/test/kotlin/org/jetbrains/bazel/compatibility:SimplePythonTest --jvmopt="-Dbazel.ide.starter.test.cache.directory=$HOME/IdeaProjects/hirschgarten" --sandbox_writable_path=/ --action_env=PATH --java_debug --test_arg=--wrapper_script_flag=--debug=8000
+ * ```
+ */
+class SimplePythonIdeStarterTest : IdeStarterBaseProjectTest() {
+
+  @Test
+  fun checkImportStatements() {
+    createContext("simplePython", PyCharmBazelCases.SimplePython)
+      .runIdeWithDriver(runTimeout = timeout)
+      .useDriverAndCloseIde {
+        ideFrame {
+          syncBazelProject()
+          waitForIndicators(10.minutes)
+
+          step("check no red node in main/main.py") {
+            execute { openFile("main/main.py") }
+            execute { checkOnRedCode() }
+            takeScreenshot("main.py")
+          }
+          step("check no red node in lib/libA/src/bkng/lib/aaa/hello.py") {
+            execute { openFile("lib/libA/src/bkng/lib/aaa/hello.py") }
+            execute { checkOnRedCode() }
+            takeScreenshot("main.py")
+          }
+        }
+      }
+  }
+}
