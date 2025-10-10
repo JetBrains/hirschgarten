d7a803ee2996a3016f492fae42e05a99056757d8
Marcin Kocot <marcin.kocot@jetbrains.com>
2025-10-10 12:43:24 +0000

    [bazel] [fix] Fix Python interpreter obtaining from aspects | #BAZEL-1659 Done
    Co-authored-by: Andrzej Gluszak <Andrzej.Gluszak@jetbrains.com>
    
    Merge-request: IJ-MR-175407
    Merged-by: Marcin Kocot <Marcin.Kocot@jetbrains.com>
    
    GitOrigin-RevId: 15dd795037fb5275adbbc7f736a94570ab6b32cc

--- server/server/src/main/kotlin/org/jetbrains/bazel/server/bsp/managers/BazelToolchainManager.kt
+++ server/server/src/main/kotlin/org/jetbrains/bazel/server/bsp/managers/BazelToolchainManager.kt
@@ -3,10 +3,11 @@ package org.jetbrains.bazel.server.bsp.managers
 class BazelToolchainManager {
   fun getToolchain(rulesetLanguage: RulesetLanguage): String? =
     when (rulesetLanguage.language) {
-      Language.Scala -> """"@${rulesetLanguage.rulesetName}//scala:toolchain_type""""
+      Language.Scala -> rulesetLanguage.rulesetName?.let { """"@$it//scala:toolchain_type"""" }
       Language.Java -> """config_common.toolchain_type("@bazel_tools//tools/jdk:runtime_toolchain_type", mandatory = False)"""
-      Language.Kotlin -> """"@${rulesetLanguage.rulesetName}//kotlin/internal:kt_toolchain_type""""
-      Language.Go -> """"@${rulesetLanguage.rulesetName}//go:toolchain""""
+      Language.Kotlin -> rulesetLanguage.rulesetName?.let { """"@$it//kotlin/internal:kt_toolchain_type"""" }
+      Language.Go -> rulesetLanguage.rulesetName?.let { """"@$it//go:toolchain"""" }
+      Language.Python -> rulesetLanguage.rulesetName?.let { """"@$it//python:toolchain_type"""" }
       else -> null
     }
 }
