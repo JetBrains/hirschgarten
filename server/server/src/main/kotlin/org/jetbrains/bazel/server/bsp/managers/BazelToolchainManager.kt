09a940fd5df9f1f1bf3cf6ef2c169a5287835590
Lev Leontev <lev.leontev@jetbrains.com>
2025-10-14 15:33:30 +0200

    BAZEL-2564: make Python toolchain optional
    (cherry picked from commit cc608570cd7f091f18e813e0317eebc4c1fede8a)
    
    IJ-CR-178467
    
    GitOrigin-RevId: 1ab4f39ab33e4a7f1359e84d6f657b83b08400a5

--- server/server/src/main/kotlin/org/jetbrains/bazel/server/bsp/managers/BazelToolchainManager.kt
+++ server/server/src/main/kotlin/org/jetbrains/bazel/server/bsp/managers/BazelToolchainManager.kt
@@ -7,7 +7,7 @@ class BazelToolchainManager {
       Language.Java -> """config_common.toolchain_type("@bazel_tools//tools/jdk:runtime_toolchain_type", mandatory = False)"""
       Language.Kotlin -> rulesetLanguage.rulesetName?.let { """"@$it//kotlin/internal:kt_toolchain_type"""" }
       Language.Go -> rulesetLanguage.rulesetName?.let { """"@$it//go:toolchain"""" }
-      Language.Python -> rulesetLanguage.rulesetName?.let { """"@$it//python:toolchain_type"""" }
+      Language.Python -> rulesetLanguage.rulesetName?.let { """config_common.toolchain_type("@$it//python:toolchain_type", mandatory = False)""" }
       else -> null
     }
 }
