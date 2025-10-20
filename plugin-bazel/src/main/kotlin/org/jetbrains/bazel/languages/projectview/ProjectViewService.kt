168a9ea8307fc6557ca78c81b6a2de2b4239bf6e
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-10-20 12:37:55 +0200

    always reparse project view file
    GitOrigin-RevId: 98772bddf5b9a91a70199f1593b8ce4cfa393879

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/ProjectViewService.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/ProjectViewService.kt
@@ -1,14 +1,17 @@
 package org.jetbrains.bazel.languages.projectview
 
 import com.google.common.hash.HashCode
-import com.intellij.openapi.application.ReadAction
-import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.application.writeAction
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.ex.dummy.DummyFileSystem
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiManager
+import kotlinx.coroutines.runBlocking
 import org.jetbrains.bazel.config.rootDir
 import org.jetbrains.bazel.flow.open.ProjectViewFileUtils
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewPsiFile
@@ -25,17 +28,20 @@ import kotlin.io.path.notExists
  */
 @Service(Service.Level.PROJECT)
 class ProjectViewService(private val project: Project) {
-  val defaultProjectViewVirtualFile: VirtualFile =
-    WriteAction.computeAndWait<VirtualFile, RuntimeException> {
-      val vfs = DummyFileSystem.getInstance()
-        .createRoot("tmp")
-        .createChildData(null, ".bazelproject")
+  val defaultProjectViewVirtualFile: VirtualFile by lazy {
+    runBlockingCancellable {
+      writeAction {
+        val vfs = DummyFileSystem.getInstance()
+          .createRoot("tmp")
+          .createChildData(null, ".bazelproject")
 
-      val content = ProjectViewFileUtils.projectViewTemplate(project.rootDir)
-      vfs.setBinaryContent(content.toByteArray())
+        val content = ProjectViewFileUtils.projectViewTemplate(project.rootDir).format(".")
+        vfs.setBinaryContent(content.toByteArray())
 
-      return@computeAndWait vfs
+        return@writeAction vfs
+      }
     }
+  }
 
   /**
    * Gets the current ProjectView, parsing it if necessary.
@@ -78,18 +84,26 @@ class ProjectViewService(private val project: Project) {
     }
   }
 
-  private fun parseProjectView(projectViewPath: Path): ProjectView =
-    ReadAction.compute<ProjectView, RuntimeException> {
-      val virtualFile =
+  private fun parseProjectView(projectViewPath: Path): ProjectView {
+    val defaultProjectViewVirtualFile = defaultProjectViewVirtualFile
+    return runBlockingCancellable {
+      val virtualFile = readAction {
         VirtualFileManager.getInstance().findFileByNioPath(projectViewPath)
           ?: defaultProjectViewVirtualFile
+      }
 
-      val psiFile =
-        PsiManager.getInstance(project).findFile(virtualFile) as? ProjectViewPsiFile
+      writeAction {
+        PsiDocumentManager.getInstance(project)
+          .reparseFiles(listOf(virtualFile), false)
+      }
+
+      readAction {
+        val psi = PsiManager.getInstance(project).findFile(virtualFile) as? ProjectViewPsiFile
           ?: error("Could not parse project view file at $projectViewPath")
-
-      ProjectView.fromProjectViewPsiFile(psiFile)
+        return@readAction ProjectView.fromProjectViewPsiFile(psi)
+      }
     }
+  }
 
   fun getProjectViewConfigurationHash(): HashCode = ProjectViewHasher.hash(getProjectView())
 
