45ce11bfc996155749e62d6861f6f2134a1ee977
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-10-22 08:19:16 +0000

    BAZEL-2566 | Always fallback to default project view
    fix project open ise
    
    fix project open ise
    
    
    Merge-request: BAZEL-MR-2482
    Merged-by: Elena Semenova <Elena.Semenova@jetbrains.com>

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/ProjectViewService.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/ProjectViewService.kt
@@ -1,20 +1,21 @@
 package org.jetbrains.bazel.languages.projectview
 
 import com.google.common.hash.HashCode
-import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiManager
 import org.jetbrains.bazel.config.rootDir
 import org.jetbrains.bazel.flow.open.ProjectViewFileUtils
+import org.jetbrains.bazel.languages.projectview.base.ProjectViewLanguage
 import org.jetbrains.bazel.languages.projectview.psi.ProjectViewPsiFile
 import org.jetbrains.bazel.settings.bazel.bazelProjectSettings
 import org.jetbrains.bazel.settings.bazel.setProjectViewPath
 import java.nio.file.Path
-import java.nio.file.attribute.FileTime
 import kotlin.io.path.exists
-import kotlin.io.path.getLastModifiedTime
 import kotlin.io.path.notExists
 
 /**
@@ -23,6 +24,13 @@ import kotlin.io.path.notExists
  */
 @Service(Service.Level.PROJECT)
 class ProjectViewService(private val project: Project) {
+  fun getDefaultProjectViewFileContent(): ProjectView {
+    val content = ProjectViewFileUtils.projectViewTemplate(project).format(".")
+    val psiFile = PsiFileFactory.getInstance(project)
+      .createFileFromText(ProjectViewLanguage, content) as ProjectViewPsiFile
+    return ProjectView.fromProjectViewPsiFile(psiFile)
+  }
+
   /**
    * Gets the current ProjectView, parsing it if necessary.
    * Uses caching with file modification time checking for efficiency.
@@ -59,18 +67,21 @@ class ProjectViewService(private val project: Project) {
     }
   }
 
-  private fun parseProjectView(projectViewPath: Path): ProjectView =
-    ReadAction.compute<ProjectView, RuntimeException> {
-      val virtualFile =
-        VirtualFileManager.getInstance().findFileByNioPath(projectViewPath)
-          ?: error("Could not find project view file at $projectViewPath")
+  private fun parseProjectView(projectViewPath: Path): ProjectView {
+    return runBlockingCancellable {
+      val projectView = readAction {
+        val virtualFile = VirtualFileManager.getInstance().findFileByNioPath(projectViewPath)
+          ?: return@readAction null
 
-      val psiFile =
-        PsiManager.getInstance(project).findFile(virtualFile) as? ProjectViewPsiFile
-          ?: error("Could not parse project view file at $projectViewPath")
+        val psi = PsiManager.getInstance(project).findFile(virtualFile) as? ProjectViewPsiFile
+          ?: return@readAction null
 
-      ProjectView.fromProjectViewPsiFile(psiFile)
+        return@readAction ProjectView.fromProjectViewPsiFile(psi)
+      }
+      return@runBlockingCancellable projectView
+        ?: readAction { getDefaultProjectViewFileContent() }
     }
+  }
 
   fun getProjectViewConfigurationHash(): HashCode = ProjectViewHasher.hash(getProjectView())
 
