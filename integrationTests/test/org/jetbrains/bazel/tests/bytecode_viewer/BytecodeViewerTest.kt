cf8d20a23b9c87e0c6c7b737845ae6ee7b4c07cd
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-10-24 09:49:22 +0000

    BAZEL-2089 [fix] view bytecode feature not working for classes compiled using bazel
    Merge-request: IJ-MR-168190
    Merged-by: Krystian Gajewski <krystian.gajewski@jetbrains.com>
    
    GitOrigin-RevId: 9f2a2ddad591783ad27b5b57f2ed2eca937f6854

---
+++ integrationTests/test/org/jetbrains/bazel/tests/bytecode_viewer/BytecodeViewerTest.kt
@@ -0,0 +1,67 @@
+package org.jetbrains.bazel.tests.bytecode_viewer
+
+import com.intellij.driver.sdk.invokeAction
+import com.intellij.driver.sdk.step
+import com.intellij.driver.sdk.ui.components.common.ideFrame
+import com.intellij.driver.sdk.wait
+import com.intellij.driver.sdk.waitForIndicators
+import com.intellij.ide.starter.driver.engine.runIdeWithDriver
+import com.intellij.tools.ide.performanceTesting.commands.build
+import com.intellij.tools.ide.performanceTesting.commands.goto
+import com.intellij.tools.ide.performanceTesting.commands.openFile
+import com.intellij.tools.ide.performanceTesting.commands.reloadFiles
+import com.intellij.tools.ide.performanceTesting.commands.sleep
+import org.jetbrains.bazel.data.IdeaBazelCases
+import org.jetbrains.bazel.ideStarter.IdeStarterBaseProjectTest
+import org.jetbrains.bazel.ideStarter.buildAndSync
+import org.jetbrains.bazel.ideStarter.execute
+import org.jetbrains.bazel.ideStarter.syncBazelProject
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+
+/**
+ * ```sh
+ * bazel test //plugin-bazel/src/test/kotlin/org/jetbrains/bazel/bytecode_viewer --jvmopt="-Dbazel.ide.starter.test.cache.directory=$HOME/IdeaProjects/hirschgarten" --sandbox_writable_path=/ --action_env=PATH --java_debug --test_arg=--wrapper_script_flag=--debug=8000
+ * ```
+ */
+
+class BytecodeViewerTest : IdeStarterBaseProjectTest() {
+  @Test
+  fun testBytecodeViewer() {
+    createContext("bytecode_viewer", IdeaBazelCases.BytecodeViewer)
+      .applyVMOptionsPatch {
+        this.addSystemProperty("expose.ui.hierarchy.url", "true")
+      }
+      .runIdeWithDriver(runTimeout = timeout)
+      .useDriverAndCloseIde {
+        ideFrame {
+          syncBazelProject()
+          execute { buildAndSync() }
+          waitForIndicators(10.minutes)
+
+          step("Resync project and check if the sync is successful") {
+            execute { reloadFiles() }
+            execute { build() }
+            execute { waitForIndicators(2.minutes) }
+            execute { openFile("SimpleTest.java") }
+            execute { sleep(5_000) }
+            execute { build(listOf("SimpleJavaTest")) }
+            execute { waitForIndicators(2.minutes) }
+            execute { goto(5, 17) }
+            execute { sleep(5_000) }
+            invokeAction("BytecodeViewer")
+            wait(5.seconds)
+            val buildView = x("//div[@class='BytecodeToolWindowPanel']")
+            val text = buildView.getAllTexts().joinToString { it.text }
+
+            // we just want any text resembling bytecode to show
+            val bytecodeKeywords = setOf("ICONST_3", "ICONST_2", "INVOKESTATIC")
+            assert(bytecodeKeywords.all { text.contains(it) })
+          }
+        }
+      }
+  }
+}
