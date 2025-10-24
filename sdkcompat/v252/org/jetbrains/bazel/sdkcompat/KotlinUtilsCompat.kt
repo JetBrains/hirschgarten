33966cf4ca4f40c7beab355097a3a7bd9fefa919
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2025-10-24 15:40:49 +0200

    [bazel] BAZEL-2600 old sdkcompat removed
    GitOrigin-RevId: 953520c072e34870e9c49f69c10745833532a1ea

--- sdkcompat/v252/org/jetbrains/bazel/sdkcompat/KotlinUtilsCompat.kt
+++
@@ -1,6 +0,0 @@
-package org.jetbrains.bazel.sdkcompat
-
-import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.analysis.api.platform.modification.publishGlobalModuleStateModificationEvent
-
-fun Project.publishGlobalModuleStateModificationEventCompat() = publishGlobalModuleStateModificationEvent()
