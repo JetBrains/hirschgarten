3fae4747058f620e2474fec4d4abcfa20e729a08
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-10-24 16:28:42 +0200

    fix failing test
    GitOrigin-RevId: f24eb1deda92b59fe8506b433f517daf812c2d7a

--- pluginTests/test/org/jetbrains/bazel/languages/projectview/action/ExcludeFromProjectViewDirectoriesActionTest.kt
+++ pluginTests/test/org/jetbrains/bazel/languages/projectview/action/ExcludeFromProjectViewDirectoriesActionTest.kt
@@ -95,6 +95,7 @@ class ExcludeFromProjectViewDirectoriesActionTest : ProjectViewDirectoriesAction
     useAndOpenProjectView(
       """
       directories:
+      
     """.trimIndent(),
     )
     performActionOnProjectDir("foo")
