0b8e8121979020f68a2b6d1d5d20f586e7fd4a19
Damian Baczyński <damian.baczynski@jetbrains.com>
2025-10-27 13:44:22 +0100

    fix tests
    GitOrigin-RevId: 71a95d02256e86b349f33172c9ffbd1d78bdab1c

--- pluginTests/test/org/jetbrains/bazel/languages/projectview/action/ExcludeFromProjectViewDirectoriesActionTest.kt
+++ pluginTests/test/org/jetbrains/bazel/languages/projectview/action/ExcludeFromProjectViewDirectoriesActionTest.kt
@@ -20,7 +20,7 @@ class ExcludeFromProjectViewDirectoriesActionTest : ProjectViewDirectoriesAction
   @Test
   fun `test presentation template is correct`() {
     val presentation = action.templatePresentation
-    presentation.text shouldBe "Exclude from the {0} Directories"
+    presentation.text shouldBe "Exclude from {0} directories section"
     presentation.description shouldBe "Exclude this directory from the project view file."
     presentation.icon shouldBe null
   }
@@ -56,7 +56,7 @@ class ExcludeFromProjectViewDirectoriesActionTest : ProjectViewDirectoriesAction
     )
     val presentation = testPresentationOn(createActionContext(myFixture.tempDirFixture.findOrCreateDir("foo")))
     presentation.isEnabledAndVisible shouldBe true
-    presentation.text shouldBe "Exclude from the .user.bazelproject Directories"
+    presentation.text shouldBe "Exclude from .user.bazelproject directories section"
     presentation.description shouldBe null
     presentation.icon shouldBe null
   }
