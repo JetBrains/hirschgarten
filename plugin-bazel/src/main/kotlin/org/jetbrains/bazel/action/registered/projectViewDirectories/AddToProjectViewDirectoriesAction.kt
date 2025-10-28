07818109ef1a3983126f69184a1e60c54ff182be
Damian Baczyński <damian.baczynski@jetbrains.com>
2025-10-28 11:22:40 +0000

    [bazel] BAZEL-2592 Change project view add/exclude action labels to contain project view filename.
    Co-authored-by: Justin Kaeser <justin.kaeser@jetbrains.com>
    
    Merge-request: IJ-MR-179937
    Merged-by: Damian Baczyński <damian.baczynski@jetbrains.com>
    
    GitOrigin-RevId: 8269ade6554e198b3f1f15c224d15593fb521a26

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/action/registered/projectViewDirectories/AddToProjectViewDirectoriesAction.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/action/registered/projectViewDirectories/AddToProjectViewDirectoriesAction.kt
@@ -8,12 +8,13 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 class AddToProjectViewDirectoriesAction : AnAction() {
   val action: ProjectViewDirectoriesAction =
     ProjectViewDirectoriesAction(
-      ProjectViewDirectoriesAction.NotificationFactory(
+      titleKey = "action.Bazel.AddToProjectViewDirectoriesAction.text",
+      itemExists = ProjectViewDirectoriesAction.NotificationFactory(
         "Directory already included",
         { "Directory \'$it\' is already included in the project view file directories section." },
         NotificationType.INFORMATION,
       ),
-      ProjectViewDirectoriesAction.NotificationFactory(
+      oppositeStateItemExists = ProjectViewDirectoriesAction.NotificationFactory(
         "Including excluded directory",
         { "Attempted to include directory \'$it\' which has been already excluded from the project view file directories section." },
         NotificationType.WARNING,
