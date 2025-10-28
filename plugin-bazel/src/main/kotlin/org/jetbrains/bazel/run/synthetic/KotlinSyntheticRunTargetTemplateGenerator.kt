072967b8d43529ac244bba66e52de893815bc728
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-10-28 10:23:32 +0100

    move kotlin template generator to separate class
    GitOrigin-RevId: c458fe5d2d5f3cc1b901ba3ee947b70ca99d2e76

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/run/synthetic/KotlinSyntheticRunTargetTemplateGenerator.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/run/synthetic/KotlinSyntheticRunTargetTemplateGenerator.kt
@@ -1,11 +1,10 @@
 package org.jetbrains.bazel.run.synthetic
 
-import com.intellij.lang.jvm.util.JvmClassUtil
-import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNameIdentifierOwner
 import org.intellij.lang.annotations.Language
 import org.jetbrains.bsp.protocol.BuildTarget
+import org.jetbrains.bsp.protocol.KotlinBuildTarget
 import org.jetbrains.kotlin.fileClasses.javaFileFacadeFqName
 import org.jetbrains.kotlin.idea.base.psi.KotlinPsiHeuristics
 import org.jetbrains.kotlin.psi.KtClassOrObject
@@ -13,21 +12,21 @@ import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.psiUtil.getNonStrictParentOfType
 import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
 
-class JvmSyntheticRunTargetTemplateGenerator : MainClassSyntheticRunTargetTemplateGenerator() {
-  override val id: String = "jvm"
-
-  // TODO: move kotlin resolve to different template generator
+class KotlinSyntheticRunTargetTemplateGenerator : MainClassSyntheticRunTargetTemplateGenerator() {
   override fun getMainClass(element: PsiElement): String? {
-    return tryGetJavaMainClass(element)
-      ?: tryGetKotlinMainClassInClass(element)
+    return tryGetKotlinMainClassInClass(element)
       ?: tryGetKotlinMainClassInFile(element)
   }
 
-  override fun getBuildContent(target: BuildTarget, syntheticTarget: String, mainClass: String): String {
+  override fun getBuildContent(
+    target: BuildTarget,
+    syntheticTarget: String,
+    mainClass: String,
+  ): String {
     @Language("starlark") val build = """
-      load("@rules_java//java:defs.bzl", "java_binary")
+      load("@rules_kotlin//kotlin:jvm.bzl", "kt_jvm_binary")
 
-      java_binary(
+      kt_jvm_binary(
         name = "$syntheticTarget",
         main_class = "${mainClass}",
         runtime_deps = ["${target.id}"],
@@ -36,10 +35,10 @@ class JvmSyntheticRunTargetTemplateGenerator : MainClassSyntheticRunTargetTempla
     return build
   }
 
-  fun tryGetJavaMainClass(element: PsiElement): String? {
-    val psiClass = element.getNonStrictParentOfType<PsiClass>() ?: return null
-    return JvmClassUtil.getJvmClassName(psiClass)
-  }
+  override val id: String
+    get() = "kotlin"
+
+  override fun isSupported(target: BuildTarget): Boolean = target.data is KotlinBuildTarget
 
   fun tryGetKotlinMainClassInClass(element: PsiElement): String? {
     val parent = element.getStrictParentOfType<PsiNameIdentifierOwner>() ?: return null
@@ -52,5 +51,4 @@ class JvmSyntheticRunTargetTemplateGenerator : MainClassSyntheticRunTargetTempla
     val ktFile = parent.getNonStrictParentOfType<KtFile>() ?: return null
     return ktFile.javaFileFacadeFqName.asString()
   }
-
 }
