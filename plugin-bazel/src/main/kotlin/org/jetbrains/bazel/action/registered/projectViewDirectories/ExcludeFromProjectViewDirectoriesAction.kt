24598a001e38d090bac4411726daa9b177ab3e90
Damian Baczyński <damian.baczynski@jetbrains.com>
2025-10-29 11:01:37 +0100

    [bazel] BAZEL-2593 Show add/exclude project view actions only when relevant
    GitOrigin-RevId: 0135e7b197e053d74ea88e726f298e057b6d7447

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/action/registered/projectViewDirectories/ExcludeFromProjectViewDirectoriesAction.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/action/registered/projectViewDirectories/ExcludeFromProjectViewDirectoriesAction.kt
@@ -1,34 +1,79 @@
 package org.jetbrains.bazel.action.registered.projectViewDirectories
 
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.command.writeCommandAction
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.ide.progress.runWithModalProgressBlocking
+import org.jetbrains.bazel.config.BazelPluginBundle
+import org.jetbrains.bazel.config.bazelProjectProperties
+import org.jetbrains.bazel.languages.projectview.psi.addDirectoriesExclude
+import org.jetbrains.bazel.languages.projectview.psi.getProjectViewPsiFileOrNull
+import org.jetbrains.bazel.languages.projectview.psi.removeDirectoriesInclude
+import org.jetbrains.bazel.settings.bazel.bazelProjectSettings
+import org.jetbrains.bazel.utils.findNearestParent
+import org.jetbrains.bazel.utils.selectedDirectory
+import org.jetbrains.bazel.workspace.excludedRoots
+import org.jetbrains.bazel.workspace.includedRoots
 
 class ExcludeFromProjectViewDirectoriesAction : AnAction() {
-  val action: ProjectViewDirectoriesAction =
-    ProjectViewDirectoriesAction(
-      titleKey = "action.Bazel.ExcludeFromProjectViewDirectoriesAction.text",
-      itemExists = ProjectViewDirectoriesAction.NotificationFactory(
-        "Directory already excluded",
-        { "Directory \'$it\' is already excluded from the project view file directories section." },
-        NotificationType.INFORMATION,
-      ),
-      oppositeStateItemExists = ProjectViewDirectoriesAction.NotificationFactory(
-        "Excluding included directory",
-        { "Attempted to exclude directory \'$it\' which has been already included in the project view file directories section." },
-        NotificationType.WARNING,
-      ),
-    )
 
+  @Suppress("HardCodedStringLiteral")
   override fun actionPerformed(e: AnActionEvent) {
+    val directory = e.selectedDirectory ?: return
     val project = e.project ?: return
-    val selectedDirectoryRelativePath = action.getSelectedDirectoryRelativePath(project, e) ?: return
-    val item = "-$selectedDirectoryRelativePath"
-    action.addItemToProjectView(item, e)
+    val projectViewPsi = project.getProjectViewPsiFileOrNull() ?: return
+    val includes = project.includedRoots().orEmpty()
+    val excludes = project.excludedRoots().orEmpty()
+    val nearestParent = directory.findNearestParent(includes + excludes - directory)
+    if (nearestParent in excludes) return
+    val isExplicitlyIncluded = directory in includes
+    val isIncluded = nearestParent in includes
+    if (!isExplicitlyIncluded && !isIncluded) return
+    runWithModalProgressBlocking(project, "Excluding the Directory from Project View...") {
+      writeCommandAction(project, "EditProjectViewDirectories") {
+        if (isExplicitlyIncluded) projectViewPsi.removeDirectoriesInclude(directory)
+        if (isIncluded) projectViewPsi.addDirectoriesExclude(directory)
+      }
+    }
   }
 
-  override fun update(e: AnActionEvent) = action.update(e)
+  override fun update(e: AnActionEvent) {
+    val project = e.project
+    val projectViewFile = project?.getProjectViewIfApplicable(e)
+    val rootDir = project?.bazelProjectProperties?.rootDir
+    if (projectViewFile == null || rootDir == null) {
+      e.presentation.isEnabledAndVisible = false
+      return
+    }
+    val relativePath = VfsUtil.getRelativePath(projectViewFile, rootDir)
+    if (relativePath == null) {
+      e.presentation.isEnabledAndVisible = false
+      return
+    }
+    e.presentation.isEnabledAndVisible = true
+    e.presentation.text = BazelPluginBundle.message("action.Bazel.ExcludeFromProjectViewDirectoriesAction.text", projectViewFile.name)
+  }
 
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+  private fun Project.getProjectViewIfApplicable(e: AnActionEvent): VirtualFile? {
+    if (DumbService.isDumb(this)) return null
+    val directory = e.selectedDirectory ?: return null
+    val projectViewFile = bazelProjectSettings.projectViewPath ?: return null
+    val includes = includedRoots().orEmpty()
+    val excludes = excludedRoots().orEmpty()
+    return when (directory) {
+      in includes -> projectViewFile
+      in excludes -> null
+      else -> when (directory.findNearestParent(includes + excludes)) {
+        in includes -> projectViewFile
+        else -> null
+      }
+    }
+  }
 }
