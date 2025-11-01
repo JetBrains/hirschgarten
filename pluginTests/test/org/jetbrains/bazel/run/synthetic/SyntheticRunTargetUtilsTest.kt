bff4c60590445c9ead6bdbc060e97fd89a15e573
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-11-01 22:58:27 +0100

    add unit tests
    GitOrigin-RevId: 41865bbc941926fc182b1e839e3789ac1b0a7eac

---
+++ pluginTests/test/org/jetbrains/bazel/run/synthetic/SyntheticRunTargetUtilsTest.kt
@@ -0,0 +1,88 @@
+package org.jetbrains.bazel.run.synthetic
+
+import com.intellij.lang.java.JavaLanguage
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import org.jetbrains.bazel.label.Label
+import org.jetbrains.bazel.test.framework.target.TestBuildTargetFactory
+import org.jetbrains.kotlin.idea.KotlinLanguage
+
+class SyntheticRunTargetUtilsTest : BasePlatformTestCase() {
+
+  fun `test getSyntheticTargetLabel with single package part`() {
+    val label = SyntheticRunTargetUtils.getSyntheticTargetLabel(
+      packageParts = arrayOf("simple"),
+      targetName = "test_binary"
+    )
+
+    label.toString() shouldContain ".bazelbsp/synthetic_targets/simple"
+    label.toString() shouldContain "test_binary"
+  }
+
+  fun `test getSyntheticTargetLabel with multiple package parts`() {
+    val label = SyntheticRunTargetUtils.getSyntheticTargetLabel(
+      packageParts = arrayOf("part1", "part2", "part3"),
+      targetName = "my_target"
+    )
+
+    val labelStr = label.toString()
+    labelStr shouldContain ".bazelbsp/synthetic_targets"
+    labelStr shouldContain "part1"
+    labelStr shouldContain "part2"
+    labelStr shouldContain "part3"
+    labelStr shouldContain "my_target"
+  }
+
+  fun `test getTemplateGenerators for java target`() {
+    val target = TestBuildTargetFactory.createSimpleJavaLibraryTarget(id = Label.parse("//test:java_lib"))
+
+    val generators = SyntheticRunTargetUtils.getTemplateGenerators(target, JavaLanguage.INSTANCE)
+
+    generators.shouldNotBeEmpty()
+    generators.all { it.isSupported(target) } shouldBe true
+  }
+
+  fun `test getTemplateGenerators for kotlin target`() {
+    val target = TestBuildTargetFactory.createSimpleKotlinLibraryTarget(id = Label.parse("//test:kotlin_lib"))
+
+    val generators = SyntheticRunTargetUtils.getTemplateGenerators(target, KotlinLanguage.INSTANCE)
+
+    generators.shouldNotBeEmpty()
+    generators.all { it.isSupported(target) } shouldBe true
+  }
+
+  fun `test escapeTargetLabel with special characters`() {
+    val testCases = mapOf(
+      "//foo:bar" to "__foo_bar",
+      "@repo//pkg:target" to "_repo__pkg_target",
+      "my-target-123" to "my_target_123",
+      "Target.With.Dots" to "Target_With_Dots",
+      "normal_name_123" to "normal_name_123"
+    )
+
+    testCases.forEach { (input, expected) ->
+      SyntheticRunTargetUtils.escapeTargetLabel(input) shouldBe expected
+    }
+  }
+
+  fun `test getTemplateGenerators filters unsupported generators`() {
+    val javaTarget = TestBuildTargetFactory.createSimpleJavaLibraryTarget(id = Label.parse("//test:java"))
+    val generators = SyntheticRunTargetUtils.getTemplateGenerators(javaTarget, KotlinLanguage.INSTANCE)
+
+    generators.shouldBeEmpty()
+  }
+
+  fun `test synthetic target label structure`() {
+    val label = SyntheticRunTargetUtils.getSyntheticTargetLabel(
+      packageParts = arrayOf("escaped_target", "escaped_main"),
+      targetName = "synthetic_binary"
+    )
+
+    val labelStr = label.toString()
+    labelStr shouldContain "//.bazelbsp/synthetic_targets"
+    labelStr shouldContain ":synthetic_binary"
+  }
+}
