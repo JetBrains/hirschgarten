954a55e5e07d89c6a6f048af00ca1d9eae4329a0
Klaus T. Aehlig <klaus.aehlig@jetbrains.com>
2025-11-04 18:39:57 +0100

    BAZEL-2627 bazel new project: specify java language and toolchain
    Generate a .bazelrc file setting native flags to specify the java
    language standard and the runtime. In order to be independent of
    the host toolchain use the remotejdk, i.e., the jdk with definition
    bundled in bazel.
    
    GitOrigin-RevId: 7a66870e6e209f2fa84a535a71394fa8e6fd7a90

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/starters/NewProjectWizardConstants.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/ui/starters/NewProjectWizardConstants.kt
@@ -4,6 +4,8 @@ object NewProjectWizardConstants {
   const val BAZEL_VERSION = "8.1.1"
   const val RULES_JAVA_VERSION = "8.10.0"
   const val RULES_JVM_EXTERNAL_VERSION = "6.7"
+  const val JAVA_LANG_VERSION = "21"
+  const val JAVA_RUNTIME_VERSION = "remotejdk_21"
   const val JUNIT_VERSION = "4.13.2"
   const val RULES_KOTLIN_VERSION = "2.1.0"
 }
