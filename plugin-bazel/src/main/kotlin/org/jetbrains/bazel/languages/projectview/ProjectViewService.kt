84411e6c0d1f41dc0e1d603cb32798a233235ddc
Lev Leontev <lev.leontev@jetbrains.com>
2025-11-05 14:36:46 +0100

    BAZEL-2634: add missing isBazelProject check
    GitOrigin-RevId: 4fa690873eebf97fdf596ad5a78cdfbab7c07392

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/ProjectViewService.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/ProjectViewService.kt
@@ -54,6 +54,9 @@ class ProjectViewService(private val project: Project) {
     getProjectView()
   }
 
+  /**
+   * Get the cached project view which is reset on every project resync.
+   */
   fun getCachedProjectView(): ProjectView = SyncCache.getInstance(project).get(cachedProjectViewComputable)
 
   private fun findProjectViewPath(): VirtualFile? {
--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/run/test/JetBrainsTestRunner.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/run/test/JetBrainsTestRunner.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.bazel.run.test
 
 import com.intellij.openapi.project.Project
+import org.jetbrains.annotations.VisibleForTesting
+import org.jetbrains.bazel.config.isBazelProject
 import org.jetbrains.bazel.languages.projectview.ProjectViewService
 import org.jetbrains.bazel.languages.projectview.useJetBrainsTestRunner
 import org.jetbrains.bazel.run.BazelRunConfigurationState
@@ -12,12 +14,14 @@ private const val JB_TEST_UNIQUE_IDS = "JB_TEST_UNIQUE_IDS"
 private const val JB_TEST_FILTER = "JB_TEST_FILTER"
 private const val JB_IDE_SM_RUN = "JB_IDE_SM_RUN"
 
+@VisibleForTesting
 public var forceDisableJetBrainsTestRunner = false
 
 fun Project.useJetBrainsTestRunner(): Boolean {
   if (forceDisableJetBrainsTestRunner) {
     return false
   }
+  if (!isBazelProject) return false
   return ProjectViewService.getInstance(this).getCachedProjectView().useJetBrainsTestRunner
 }
 
