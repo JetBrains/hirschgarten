2b7ff614f338282af96ff138b72b6bc91e5f0f9d
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2025-11-05 15:48:07 +0100

    [bazel] BAZEL-2589, BAZEL-2528 BazelProjectStorePathCustomizer should consider BUILD files
    this can happen during the first checkout
    
    
    (cherry picked from commit 26c8894a139461f24c8179b1b505a6665e9b0153)
    
    IJ-CR-181674
    
    
    (cherry picked from commit 73bd4cc1e6556ef5e42fd36e953b73dae295b6b3)
    
    IJ-CR-182205
    
    GitOrigin-RevId: d941675ee33112e5ec3114b0794464f7164049e7

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/BazelProjectStorePathCustomizer.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/BazelProjectStorePathCustomizer.kt
@@ -5,16 +5,20 @@ import com.intellij.configurationStore.ProjectStorePathCustomizer
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.project.getProjectCacheFileName
 import com.intellij.openapi.project.projectsDataDir
+import org.jetbrains.bazel.commons.constants.Constants
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.io.path.extension
+import kotlin.io.path.name
 
 private class BazelProjectStorePathCustomizer : ProjectStorePathCustomizer {
   override fun getStoreDirectoryPath(projectRoot: Path): ProjectStoreDescriptor? {
-    // do not use isDirectory, as we also want to check that file exists
-    val fileName = projectRoot.fileName?.toString() ?: return null
-    if (!fileName.contains('.') || !Files.isRegularFile(projectRoot)) {
-      return null
-    }
+    // do not use isDirectory, as we also want to check that the file exists
+    if (!Files.isRegularFile(projectRoot)) return null
+
+    if (projectRoot.name !in Constants.SUPPORTED_CONFIG_FILE_NAMES &&
+      !Constants.SUPPORTED_EXTENSIONS.contains(projectRoot.extension)
+    ) return null
 
     // we should use `getProjectCacheFileName` API,
     // as in this dir is located a lot of other project-related data, so, location of dir should be in an expected location
