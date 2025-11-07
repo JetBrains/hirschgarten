2e3d992ecfe4853bbe4741155e3147c9e7d3015e
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-11-07 14:18:03 +0100

    add tests for pattern eval
    GitOrigin-RevId: 6ad661d8cf18edb27ae3e01287cf86523e03f22d

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/languages/java/JavaLanguagePlugin.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/languages/java/JavaLanguagePlugin.kt
@@ -11,16 +11,15 @@ import org.jetbrains.bazel.sync.workspace.languages.JvmPackageResolver
 import org.jetbrains.bazel.sync.workspace.languages.LanguagePlugin
 import org.jetbrains.bazel.sync.workspace.languages.LanguagePluginContext
 import org.jetbrains.bazel.sync.workspace.languages.java.source_root.JavaSourceRootPackageInference
+import org.jetbrains.bazel.sync.workspace.languages.java.source_root.prefix.SourcePatternEval
 import org.jetbrains.bazel.sync.workspace.languages.java.source_root.prefix.JavaSourceRootPatternContributor
 import org.jetbrains.bazel.sync.workspace.languages.java.source_root.prefix.SourceRootPattern
 import org.jetbrains.bazel.sync.workspace.languages.java.source_root.projectview.javaSROEnable
-import org.jetbrains.bazel.sync.workspace.languages.java.source_root.projectview.javaSROExcludePatterns
 import org.jetbrains.bazel.sync.workspace.languages.jvm.JVMPackagePrefixResolver
 import org.jetbrains.bazel.workspacecontext.WorkspaceContext
 import org.jetbrains.bsp.protocol.JvmBuildTarget
 import org.jetbrains.bsp.protocol.SourceItem
 import java.nio.file.Path
-import kotlin.io.path.relativeTo
 
 class JavaLanguagePlugin(
   private val bazelPathsResolver: BazelPathsResolver,
@@ -80,30 +79,16 @@ class JavaLanguagePlugin(
   override fun transformSources(sources: List<SourceItem>): List<SourceItem> {
     if (cachedJavaSROEnable) {
       val root = bazelPathsResolver.workspaceRoot()
-      val matchedSources = mutableListOf<SourceItem>()
-      val unmatchedSources = mutableListOf<SourceItem>()
-      for (item in sources) {
-        val rel = item.path.relativeTo(root)
-        val isExcluded = if (cachedSROExcludeMatchers.isEmpty()) {
-          false
-        } else {
-          cachedSROExcludeMatchers.any { it(rel.toString()) }
-        }
-        if (isExcluded) {
-          unmatchedSources.add(item)
-        } else {
-          if (cachedSROIncludeMatchers.any { it(rel.toString()) }) {
-            matchedSources.add(item)
-          } else {
-            unmatchedSources.add(item)
-          }
-        }
+      val (matched, unmatched) = SourcePatternEval.evalSources(
+        workspaceRoot = root,
+        sources = sources,
+        includes = cachedSROIncludeMatchers,
+        excludes = cachedSROExcludeMatchers,
+      )
+      if (matched.isNotEmpty()) {
+        packageInference.inferPackages(matched)
       }
-
-      if (matchedSources.isNotEmpty()) {
-        packageInference.inferPackages(matchedSources)
-      }
-      for (item in unmatchedSources) {
+      for (item in unmatched) {
         item.jvmPackagePrefix = packageResolver.calculateJvmPackagePrefix(item.path)
       }
     } else {
