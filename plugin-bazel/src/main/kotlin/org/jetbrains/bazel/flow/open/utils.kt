55dae11de585c6d682a244a3be5de6cc7d1ea675
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2025-11-10 17:00:48 +0100

    [bazel] IJPL-216121 replacing VirtualFiles operations with pure NIO operations
    GitOrigin-RevId: 15522730b9884b0efc5ad516200b0627cbfcd771

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/utils.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/utils.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.isFile
 import com.intellij.openapi.vfs.refreshAndFindVirtualFile
+import com.intellij.openapi.vfs.toNioPathOrNull
 import com.intellij.util.concurrency.annotations.RequiresBackgroundThread
 import org.jetbrains.bazel.commons.constants.Constants
 import org.jetbrains.bazel.config.isBazelProject
@@ -14,61 +15,75 @@ import java.nio.file.Files.isDirectory
 import java.nio.file.Files.isRegularFile
 import java.nio.file.Files.newDirectoryStream
 import java.nio.file.Path
+import kotlin.io.path.extension
+import kotlin.io.path.name
 
 /**
  * this method can be used to set up additional project properties before opening the Bazel project
  * @param virtualFile the virtual file passed to the project open processor
  */
 @RequiresBackgroundThread
-fun getProjectViewPath(projectRootDir: VirtualFile, virtualFile: VirtualFile): Path? =
-  when {
-    virtualFile.isProjectViewFile() -> virtualFile.toNioPath()
+fun getProjectViewPath(
+  projectRootDir: VirtualFile,
+  virtualFile: VirtualFile,
+): Path? {
+  val path = virtualFile.toNioPathOrNull()
+    ?: return null
+
+  return when {
+    path.isProjectViewFile() -> path
     // BUILD file at the root can be treated as a workspace file in this context
-    virtualFile.isBuildFile() -> {
-      virtualFile.parent
+    path.isBuildFile() -> {
+      path.parent
         // ?.takeUnless { it.isWorkspaceRoot() }
         ?.let { calculateProjectViewFilePath(projectRootDir, it) }
     }
 
-    virtualFile.isWorkspaceFile() -> null
-    virtualFile.isWorkspaceRoot() -> null
+    path.isWorkspaceFile() -> null
+    path.isWorkspaceRoot() -> null
     else -> {
-      virtualFile.getBuildFileForPackageDirectory()
+      path.getBuildFileForPackageDirectory()
         ?.parent
         ?.let { calculateProjectViewFilePath(projectRootDir, it) }
     }
   }
+}
 
-private fun calculateProjectViewFilePath(projectRootDir: VirtualFile, bazelPackageDir: VirtualFile): Path =
-  ProjectViewFileUtils.calculateProjectViewFilePath(
-    projectRootDir = projectRootDir,
-    projectViewPath = null,
-    overwrite = true,
-    bazelPackageDir = bazelPackageDir,
-  )
+fun calculateProjectViewFilePath(
+  projectRootDir: VirtualFile,
+  bazelPackageDir: Path,
+): Path = ProjectViewFileUtils.calculateProjectViewFilePath(
+  projectRootDir = projectRootDir,
+  projectViewPath = null,
+  overwrite = true,
+  bazelPackageDir = bazelPackageDir.refreshAndFindVirtualFile(),
+)
 
 /**
  * when a file/subdirectory is selected for opening a Bazel project,
  * this method provides information about the real project directory to open
  */
-tailrec fun findProjectFolderFromVFile(file: VirtualFile?): VirtualFile? =
-  when {
-    file == null -> null
-    file.isWorkspaceRoot() -> file
+@RequiresBackgroundThread
+tailrec fun findProjectFolderFromVFile(file: VirtualFile?): VirtualFile? {
+  val path = file?.toNioPathOrNull()
+    ?: return null
+
+  return when {
+    path.isWorkspaceRoot() -> file
     // this is to prevent opening a file that is not an acceptable Bazel config file, #BAZEL-1940
     // TODO(Son): figure out how to write a test for it to avoid regression later
-    file.isFile && !file.isEligibleFile() -> null
+    isRegularFile(path) && !path.isEligibleFile() -> null
 
     else -> findProjectFolderFromVFile(file.parent)
   }
+}
 
-fun VirtualFile.isEligibleFile(): Boolean =
+private fun Path.isEligibleFile(): Boolean =
   isWorkspaceFile() || isBuildFile() || isProjectViewFile()
 
-fun VirtualFile.isProjectViewFile(): Boolean =
-  isFile && extension == Constants.PROJECT_VIEW_FILE_EXTENSION
-
-fun VirtualFile.isWorkspaceRoot(): Boolean = toNioPath().isWorkspaceRoot()
+private fun Path.isProjectViewFile(): Boolean =
+  isRegularFile(this) &&
+    extension == Constants.PROJECT_VIEW_FILE_EXTENSION
 
 private fun Path.isWorkspaceRoot(): Boolean =
   isDirectory(this) &&
@@ -77,22 +92,26 @@ private fun Path.isWorkspaceRoot(): Boolean =
       .map { resolve(it) }
       .any { isRegularFile(it) }
 
-private fun VirtualFile.isWorkspaceFile() = isFile && name in Constants.WORKSPACE_FILE_NAMES
+private fun Path.isWorkspaceFile() =
+  isRegularFile(this) &&
+    name in Constants.WORKSPACE_FILE_NAMES
+
+private fun Path.isBuildFile(): Boolean =
+  isRegularFile(this) &&
+    name in Constants.BUILD_FILE_NAMES
 
 fun VirtualFile.isBuildFile(): Boolean =
   isFile && name in Constants.BUILD_FILE_NAMES
 
-fun VirtualFile.getBuildFileForPackageDirectory(): VirtualFile? {
-  try {
-    if (!isDirectory) return null
-    val path = toNioPath()
+private fun Path.getBuildFileForPackageDirectory(): Path? {
+  if (!isDirectory(this)) return null
 
-    return newDirectoryStream(path, BUILD_FILE_GLOB)
+  return try {
+    newDirectoryStream(this, BUILD_FILE_GLOB)
       .firstOrNull { isRegularFile(it) }
-      ?.refreshAndFindVirtualFile()
   } catch (e: IOException) {
-    thisLogger().warn("Cannot retrieve Bazel BUILD file from directory $path", e)
-    return null
+    thisLogger().warn("Cannot retrieve Bazel BUILD file from directory: $this", e)
+    null
   }
 }
 
