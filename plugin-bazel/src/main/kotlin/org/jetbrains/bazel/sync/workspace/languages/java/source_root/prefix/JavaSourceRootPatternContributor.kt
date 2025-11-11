d0a74b51385298896124e960de3d1d314c4220a4
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2025-11-11 20:38:44 +0100

    use fun interface
    GitOrigin-RevId: 23597d0f4246ee8df7c1a31aa5d764b7c292ec33

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/languages/java/source_root/prefix/JavaSourceRootPatternContributor.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/languages/java/source_root/prefix/JavaSourceRootPatternContributor.kt
@@ -3,7 +3,9 @@ package org.jetbrains.bazel.sync.workspace.languages.java.source_root.prefix
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 
-typealias SourceRootPattern = (root: String) -> Boolean
+fun interface SourceRootPattern {
+  fun matches(root: String): Boolean
+}
 
 interface JavaSourceRootPatternContributor {
   fun getPatterns(project: Project): JavaSourceRootPatterns
