4fc9206cb7919519e84b3a8b91b59f87f1ad1114
Lev Leontev <lev.leontev@jetbrains.com>
2025-11-11 17:29:34 -0500

    BAZEL-2655: fix import_depth for Go/Python, document in default .bazelproject
    GitOrigin-RevId: 2cf922819b3199015aafbe8f1726ca17f60b147c
    (cherry picked from commit 2a2466bc0a322456bdaacd3540a0c1d909f65f11)

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/sections/ImportDepthSection.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/projectview/sections/ImportDepthSection.kt
@@ -6,7 +6,10 @@ import org.jetbrains.bazel.languages.projectview.sections.presets.IntScalarSecti
 class ImportDepthSection : IntScalarSection() {
   override val name = NAME
   override val sectionKey = KEY
-  override val doc = "Specifies the maximum depth of subdirectories to be imported."
+  override val doc =
+    "Specifies how many levels of Bazel targets' dependencies should be imported as modules. " +
+      "Only the targets that are present in the workspace are imported. You can use a negative value to import all transitive dependencies. " +
+      "The default value is -1, meaning that all transitive context is included."
 
   companion object {
     const val NAME = "import_depth"
