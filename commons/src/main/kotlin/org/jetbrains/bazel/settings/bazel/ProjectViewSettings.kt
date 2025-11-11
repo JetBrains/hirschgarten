0a6ef07a4cac310cf66a422f9e939bc0377d8ea8
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2025-11-11 15:25:14 +0100

    [bazel] IJPL-216121 setProjectViewPath removed, no explicit conversions
    GitOrigin-RevId: f613432d322c337bbe96f2fbfe0cdc7e6fad77ed

--- commons/src/main/kotlin/org/jetbrains/bazel/settings/bazel/ProjectViewSettings.kt
+++ commons/src/main/kotlin/org/jetbrains/bazel/settings/bazel/ProjectViewSettings.kt
@@ -1,37 +1,22 @@
 package org.jetbrains.bazel.settings.bazel
 
 import com.intellij.openapi.application.EDT
-import com.intellij.openapi.components.service
 import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
 import com.intellij.openapi.fileEditor.impl.FileEditorOpenOptions
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.platform.backend.workspace.WorkspaceModel
-import com.intellij.platform.backend.workspace.virtualFile
-import com.intellij.platform.workspace.storage.impl.url.toVirtualFileUrl
+import com.intellij.openapi.vfs.VirtualFile
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.nio.file.Path
 
-fun Project.setProjectViewPath(newProjectViewFilePath: Path) {
-  val vfsUrlManager = WorkspaceModel.getInstance(this)
-    .getVirtualFileUrlManager()
-  newProjectViewFilePath.toVirtualFileUrl(vfsUrlManager).virtualFile?.let {
-    bazelProjectSettings = bazelProjectSettings.withNewProjectViewPath(it)
-  }
-}
-
-suspend fun openProjectViewInEditor(project: Project, projectViewPath: Path) {
-  val virtualFile =
-    withContext(Dispatchers.Default) {
-      VirtualFileManager.getInstance().refreshAndFindFileByNioPath(projectViewPath.toAbsolutePath())
-    } ?: return
-
+suspend fun openProjectViewInEditor(
+  project: Project,
+  file: VirtualFile,
+) {
   // README.md should still be the selected tab afterward
   val fileEditorManager = FileEditorManagerEx.getInstanceEx(project)
   withContext(Dispatchers.EDT) {
     fileEditorManager.openFile(
-      virtualFile,
+      file,
       FileEditorOpenOptions(
         selectAsCurrent = false,
         requestFocus = false,
--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/RegenerateProjectViewFileContentPreSyncHook.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/RegenerateProjectViewFileContentPreSyncHook.kt
@@ -1,22 +1,26 @@
 package org.jetbrains.bazel.flow.open
 
 import com.intellij.openapi.vfs.isFile
+import com.intellij.openapi.vfs.refreshAndFindVirtualFile
 import com.intellij.openapi.vfs.toNioPathOrNull
 import org.jetbrains.bazel.config.rootDir
 import org.jetbrains.bazel.settings.bazel.bazelProjectSettings
-import org.jetbrains.bazel.settings.bazel.openProjectViewInEditor
-import org.jetbrains.bazel.settings.bazel.setProjectViewPath
 import org.jetbrains.bazel.sync.ProjectPreSyncHook
 
 class RegenerateProjectViewFileContentPreSyncHook : ProjectPreSyncHook {
   override suspend fun onPreSync(environment: ProjectPreSyncHook.ProjectPreSyncHookEnvironment) {
     val project = environment.project
 
-    val projectViewPath = project.bazelProjectSettings.projectViewPath
-    if (projectViewPath?.isFile != true) {
-      val projectViewPath = ProjectViewFileUtils.calculateProjectViewFilePath(project.rootDir, projectViewPath?.toNioPathOrNull())
-      project.setProjectViewPath(projectViewPath)
-      openProjectViewInEditor(project, projectViewPath)
-    }
+    var projectViewPath = project.bazelProjectSettings.projectViewPath
+    if (projectViewPath?.isFile == true) return
+
+    projectViewPath = ProjectViewFileUtils.calculateProjectViewFilePath(
+      projectRootDir = project.rootDir,
+      projectViewPath = projectViewPath?.toNioPathOrNull(),
+    ).refreshAndFindVirtualFile()!!
+    project.bazelProjectSettings = project.bazelProjectSettings
+      .withNewProjectViewPath(projectViewPath)
+
+    openProjectViewInEditor(project, projectViewPath)
   }
 }
