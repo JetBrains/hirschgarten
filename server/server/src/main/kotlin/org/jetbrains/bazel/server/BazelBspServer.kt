2b20762f3cea7c6920a6eb846eb19d85e270bc5f
Klaus T. Aehlig <klaus.aehlig@jetbrains.com>
2025-11-18 14:43:48 +0100

    [BAZEL-2677] bazel aspect: do not use --inject_repository
    In order to obtain information about the bazel project structure, the
    bazel plugin uses a custom aspect. This aspect is made available in
    a directory .bazelbsp created by Intellij; it is added to the build
    as injected repository with the --inejct_repository option. This
    option has the disadvantage that the repository structure differs
    from a typical command-line build and hence a switch between a build
    in Intellij and one on the command-line invalidates analysis. Avoid
    this repository injection (and hence the invalidation of analysis) by
    using the fact that the directory lives in the workspace anyway; so
    we address the aspect repository as //.bazelbsp/aspects:... as part
    of the main repsoitory instead of as @bazelbsp_aspect//aspects:...
    with bazel_bsp an injected repository.
    
    GitOrigin-RevId: bf372f8ca87e9bccadfeaf538d8e320e0d49c8ef

--- server/server/src/main/kotlin/org/jetbrains/bazel/server/BazelBspServer.kt
+++ server/server/src/main/kotlin/org/jetbrains/bazel/server/BazelBspServer.kt
@@ -91,7 +91,6 @@ class BazelBspServer(
       InternalAspectsResolver(
         bspInfo = bspInfo,
         bazelRelease = bazelInfo.release,
-        shouldUseInjectRepository = bazelInfo.shouldUseInjectRepository(),
       )
 
     val bazelBspAspectsManager =
