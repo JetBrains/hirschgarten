fb714a1afd794dc2495fe26b8b4dc485460d7b4f
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2025-12-12 02:26:18 +0000

    BAZEL-2734 [bazel]: implement proper solution for linking Bazel project in headless
    Consolidate headless project linking into OpenBazelProjectAndSyncStartupActivity
    
    - Move flag-based linking logic from OpenBazelProjectReplacingOtherProjectModels
      into OpenBazelProjectAndSyncStartupActivity
    - Delete OpenBazelProjectReplacingOtherProjectModels class
    
    This moves forced porject linkinking with a flag from ProjectActivity (postStartupActivity) to InitProjectActivity (projectPreInit).
    
    It ensures the project is linked as Bazel BEFORE BazelStartupActivity runs, because:
    
    0. When -Dbazel.project.auto.open.if.present=true passed it marks project as Bazel project
    1. OpenBazelProjectAndSyncStartupActivity (projectPreInit) links project
    2. BazelStartupActivity (postStartupActivity) sees isBazelProject=true
    3. BazelStartupActivity triggers sync
    
    The previous hack (initializeBazelSyncEnvironment + manual sync call) is removed alongside OpenBazelProjectReplacingOtherProjectModels class.
    
    
    (cherry picked from commit 5f6774995648d8f99c7a948a6b20c5d31f0c02e7)
    
    IJ-MR-185333
    
    GitOrigin-RevId: ec5c83b959791aa3ce97e8a6d3eaa7dd22b7dc1a

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/OpenBazelProjectAndSyncStartupActivity.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/flow/open/OpenBazelProjectAndSyncStartupActivity.kt
@@ -1,15 +1,28 @@
 package org.jetbrains.bazel.flow.open
 
+import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.components.serviceAsync
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.startup.InitProjectActivity
 import com.intellij.project.ProjectStoreOwner
+import org.jetbrains.bazel.config.BazelFeatureFlags
 import org.jetbrains.bazel.config.BazelProjectProperties
 import org.jetbrains.bazel.settings.bazel.setProjectViewPath
 
 // todo rename to ImportBazelProjectAndSyncStartupActivity
 internal class OpenBazelProjectAndSyncStartupActivity : InitProjectActivity {
   override suspend fun run(project: Project) {
+    // If flag is set and project is not yet linked as Bazel, link it first
+    // This handles headless mode with -Dbazel.project.auto.open.if.present=true
+    if (shouldLinkAsBazelProject(project)) {
+      val projectFolder = findProjectFolderFromVFile(project.guessProjectDir()) ?: return
+      BazelOpenProjectProvider().linkToExistingProjectAsync(projectFolder, project)
+      // BazelStartupActivity will handle sync since isBazelProject will be true
+      return
+    }
+
     if (project !is ProjectStoreOwner) return
 
     val storeDescriptor = project.componentStore
@@ -30,5 +43,19 @@ internal class OpenBazelProjectAndSyncStartupActivity : InitProjectActivity {
     }
 
     project.initProperties(projectRootDir)
+  }
+
+  private fun shouldLinkAsBazelProject(project: Project): Boolean {
+    val externalProjectPath = project.basePath.orEmpty()
+
+    // Already linked - nothing to do
+    if (BazelUnlinkedProjectAware().isLinkedProject(project, externalProjectPath)) return false
+
+    // Link if flag is set
+    if (BazelFeatureFlags.autoOpenProjectIfPresent) return true
+
+    // Link if it's a Google Bazel project (.ijwb) and Google plugin is not installed
+    val isGoogleBazelPluginEnabled = PluginManagerCore.getPluginSet().isPluginEnabled(PluginId.getId("com.google.idea.bazel.ijwb"))
+    return !isGoogleBazelPluginEnabled && externalProjectPath.endsWith(".ijwb")
   }
 }
