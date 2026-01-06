16f1f91545c2c596871d866f4503299babadf51a
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2026-01-06 19:13:23 +0100

    use MutableSet to avoid unnecessary set copies
    GitOrigin-RevId: 4d77cfa3c3ea715d1a37c2a93dc6c87152ea0df3

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync_new/index/impl/PersistentStoreOne2ManyIndex.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync_new/index/impl/PersistentStoreOne2ManyIndex.kt
@@ -10,7 +10,7 @@ import org.jetbrains.bazel.sync_new.storage.StorageContext
 private class PersistentStoreOne2ManyIndex<K, V>(
   val owner: SyncIndexContext,
   override val name: String,
-  val storage: KVStore<K, Set<V>>,
+  val storage: KVStore<K, MutableSet<V>>,
 ) : One2ManyIndex<K, V>, Disposable {
 
   init {
@@ -19,16 +19,14 @@ private class PersistentStoreOne2ManyIndex<K, V>(
 
   override fun add(key: K, value: Iterable<V>) {
     storage.compute(key) { _, set ->
-      if (set == null) {
-        value.toSet()
-      } else {
-        set + value
-      }
+      val set = set ?: mutableSetOf()
+      set.addAll(value)
+      set
     }
   }
 
   override fun set(key: K, value: Iterable<V>) {
-    storage.put(key, value.toSet())
+    storage.put(key, value.toMutableSet())
   }
 
   override fun get(key: K): Sequence<V> = storage.get(key)?.asSequence() ?: emptySequence()
@@ -43,12 +41,8 @@ private class PersistentStoreOne2ManyIndex<K, V>(
       if (set == null) {
         return@compute null
       }
-      val result = set - value
-      if (result.isEmpty()) {
-        null
-      } else {
-        result
-      }
+      set.remove(value)
+      set
     }
   }
 
@@ -63,7 +57,7 @@ private class PersistentStoreOne2ManyIndex<K, V>(
 
 fun <K, V> SyncIndexContext.createOne2ManyIndex(
   name: String,
-  store: (name: String, storage: StorageContext) -> KVStore<K, Set<V>>,
+  store: (name: String, storage: StorageContext) -> KVStore<K, MutableSet<V>>,
 ): One2ManyIndex<K, V> {
   val store = store(SyncIndexUtils.toStorageName(name), storageContext)
   return PersistentStoreOne2ManyIndex(this, name, store)
