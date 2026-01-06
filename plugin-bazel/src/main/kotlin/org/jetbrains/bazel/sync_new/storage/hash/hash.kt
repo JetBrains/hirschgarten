0473b678e2891c9dde2f1a71d4cc45bf008b63a0
Krystian Gajewski <krystian.gajewski@jetbrains.com>
2026-01-06 19:11:50 +0100

    use hashes for paths and labels in vfs change state
    Space-RevId: 5881f187b67584e03023e9f0686e0845132e5c28
    
    GitOrigin-RevId: 0aacad7e1c08fcd2cbd101cbd2bfe30f952b280c

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync_new/storage/hash/hash.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync_new/storage/hash/hash.kt
@@ -3,18 +3,13 @@ package org.jetbrains.bazel.sync_new.storage.hash
 import com.dynatrace.hash4j.hashing.HashStream128
 import com.dynatrace.hash4j.hashing.HashValue128
 import com.dynatrace.hash4j.hashing.Hashing
-import org.jetbrains.bazel.sync_new.codec.Codec
+import org.jetbrains.bazel.label.Label
+import java.nio.file.Path
+import kotlin.io.path.absolutePathString
 
 inline fun hash(crossinline op: HashStream128.() -> Unit): HashValue128 {
   return Hashing.xxh3_128().hashStream().apply(op).get()
 }
 
-fun hash128Comparator(): Comparator<HashValue128> = Comparator { left, right ->
-  when {
-    left.mostSignificantBits > right.mostSignificantBits -> 1
-    left.mostSignificantBits < right.mostSignificantBits -> -1
-    left.leastSignificantBits > right.leastSignificantBits -> 1
-    left.leastSignificantBits < right.leastSignificantBits -> -1
-    else -> 0
-  }
-}
+fun hash(path: Path): HashValue128 = hash { putString(path.absolutePathString()) }
+fun hash(label: Label): HashValue128 = hash { putLabel(label) }
