53b0cee5638a402280da70768c231e363ad92a19
Damian Baczyński <damian.baczynski@jetbrains.com>
2026-01-27 16:29:49 +0100

    tests [bazel]: Migrate BazelSyncCodeInsightTestFixture usage to JUnit 5
    GitOrigin-RevId: fe83e8344f18772a5125fea505e6dacc283396a9

--- pluginTests/test/org/jetbrains/bazel/redcodes/KotlinModuleInternalManglingTest.kt
+++ pluginTests/test/org/jetbrains/bazel/redcodes/KotlinModuleInternalManglingTest.kt
@@ -1,19 +1,29 @@
 package org.jetbrains.bazel.redcodes
 
 import com.intellij.openapi.application.EDT
-import io.kotest.common.runBlocking
+import com.intellij.testFramework.junit5.TestApplication
+import com.intellij.testFramework.junit5.fixture.projectFixture
+import com.intellij.testFramework.junit5.fixture.tempPathFixture
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
-import org.jetbrains.bazel.test.framework.BazelSyncCodeInsightFixtureTestCase
+import org.jetbrains.bazel.test.framework.bazelSyncCodeInsightFixture
 import org.jetbrains.bazel.test.framework.checkHighlighting
+import org.junit.jupiter.api.Test
 
-class KotlinModuleInternalManglingTest : BazelSyncCodeInsightFixtureTestCase() {
+@TestApplication
+class KotlinModuleInternalManglingTest {
 
+  private val projectFixture = projectFixture(openAfterCreation = true)
+  private val tempDir = tempPathFixture()
+  private val fixture by bazelSyncCodeInsightFixture(projectFixture, tempDir)
+
+  @Test
   fun testHighlighting() = runBlocking {
-    myFixture.copyBazelTestProject("redcodes/kotlin_module_internal_mangling")
-    myFixture.performBazelSync()
+    fixture.copyBazelTestProject("redcodes/kotlin_module_internal_mangling")
+    fixture.performBazelSync()
     withContext(Dispatchers.EDT) {
-      myFixture.checkHighlighting("B.java")
+      fixture.checkHighlighting("B.java")
     }
   }
 }
