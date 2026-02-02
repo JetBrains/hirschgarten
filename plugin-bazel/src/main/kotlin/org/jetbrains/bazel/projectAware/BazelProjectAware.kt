e1d1554e3c0b74d8a6a7f7de769938bd89f7abe1
Marcin Kocot <marcin.kocot@jetbrains.com>
2026-02-02 13:10:04 +0000

    BAZEL-2680: "Sync Bazel changes" button improvements
    Merge-request: IJ-MR-188226
    Merged-by: Marcin Kocot <Marcin.Kocot@jetbrains.com>
    (cherry picked from commit b4a56eb3b7c3ea169079fba04f9e5245f71a997c)
    
    GitOrigin-RevId: b17215154e8f0afe1a4dfe2d8c16f66bef2e5e8f

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/projectAware/BazelProjectAware.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/projectAware/BazelProjectAware.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.bazel.projectAware
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.externalSystem.autoimport.ExternalSystemProjectAware
 import com.intellij.openapi.externalSystem.autoimport.ExternalSystemProjectId
 import com.intellij.openapi.externalSystem.autoimport.ExternalSystemProjectListener
@@ -8,15 +9,32 @@ import com.intellij.openapi.externalSystem.autoimport.ExternalSystemProjectReloa
 import com.intellij.openapi.externalSystem.autoimport.ExternalSystemProjectTracker
 import com.intellij.openapi.externalSystem.autoimport.ExternalSystemRefreshStatus
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.search.FilenameIndex
+import com.intellij.psi.search.GlobalSearchScope
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.bazel.commons.constants.Constants
+import org.jetbrains.bazel.config.BazelPluginConstants
+import org.jetbrains.bazel.config.bazelProjectProperties
 import org.jetbrains.bazel.config.rootDir
 import org.jetbrains.bazel.coroutines.BazelCoroutineService
+import org.jetbrains.bazel.settings.bazel.bazelProjectSettings
+import org.jetbrains.bazel.sync.SyncCache
 import org.jetbrains.bazel.sync.scope.SecondPhaseSync
 import org.jetbrains.bazel.sync.status.SyncStatusListener
 import org.jetbrains.bazel.sync.task.ProjectSyncTask
+import kotlin.io.path.pathString
 
-abstract class BazelProjectAware(private val workspace: BazelWorkspace) : ExternalSystemProjectAware {
+class BazelProjectAware(private val workspace: BazelWorkspace) : ExternalSystemProjectAware {
+  override val projectId: ExternalSystemProjectId = getBazelProjectId(workspace.project.rootDir)
+
+  private val cachedBazelFiles =
+    SyncCache.SyncCacheComputable {
+      runBlocking { calculateBazelConfigFiles(workspace.project) }
+    }
+
   override val settingsFiles: Set<String>
-    get() = emptySet()
+    get() = SyncCache.getInstance(workspace.project).get(cachedBazelFiles)
 
   override fun reloadProject(context: ExternalSystemProjectReloadContext) {
     if (context.isExplicitReload) {
@@ -51,28 +69,48 @@ abstract class BazelProjectAware(private val workspace: BazelWorkspace) : Extern
   companion object {
     @JvmStatic
     fun initialize(workspace: BazelWorkspace) {
-      val projectAwareExtension = ProjectAwareExtension.ep.extensionList.firstOrNull()
-      projectAwareExtension?.also {
-        val project = workspace.project
-        val projectAware =
-          object : BazelProjectAware(workspace) {
-            override val projectId: ExternalSystemProjectId
-              get() = it.getProjectId(project.rootDir)
-          }
-        val projectTracker = ExternalSystemProjectTracker.getInstance(project)
-        projectTracker.register(projectAware)
-        projectTracker.activate(projectAware.projectId)
-      }
+      val project = workspace.project
+      val projectAware = BazelProjectAware(workspace)
+      val projectTracker = ExternalSystemProjectTracker.getInstance(project)
+      projectTracker.register(projectAware)
+      projectTracker.activate(projectAware.projectId)
     }
 
+    /**
+     * Shows the "Sync Bazel changes" button.
+     * After using this function, the button will not disappear automatically until the project is reloaded.
+     */
     @JvmStatic
     fun notify(project: Project) {
-      val projectAwareExtension = ProjectAwareExtension.ep.extensionList.firstOrNull()
-      projectAwareExtension?.also {
-        val projectTracker = ExternalSystemProjectTracker.getInstance(project)
-        projectTracker.markDirty(it.getProjectId(project.rootDir))
-        projectTracker.scheduleChangeProcessing()
-      }
+      val projectTracker = ExternalSystemProjectTracker.getInstance(project)
+      projectTracker.markDirty(getBazelProjectId(project.rootDir))
+      projectTracker.scheduleChangeProcessing()
     }
   }
 }
+
+private suspend fun calculateBazelConfigFiles(project: Project): Set<String> {
+  val rootDir = project.bazelProjectProperties.rootDir
+  val searchScope = GlobalSearchScope.projectScope(project)
+  val projectView = project.bazelProjectSettings.projectViewPath
+  val globalFiles = readAction { GLOBAL_CONFIG_FILES.map { rootDir?.findChild(it) } }
+
+  // getVirtualFilesByNames() function is private in FilenameIndex, so a mutable list is used as a workaround
+  val localFiles = mutableListOf<VirtualFile>()
+  readAction {
+    // the search must be case-sensitive, otherwise it will be very expensive
+    FilenameIndex.processFilesByNames(LOCAL_CONFIG_FILES, true, searchScope, null) { localFiles.add(it); true }
+  }
+
+  val configFilePaths = (globalFiles + localFiles).mapNotNull { it?.path }.toSet()
+  return configFilePaths + setOfNotNull(projectView?.pathString)
+}
+
+private fun getBazelProjectId(projectPath: VirtualFile): ExternalSystemProjectId =
+  ExternalSystemProjectId(BazelPluginConstants.SYSTEM_ID, projectPath.path)
+
+// Bazel config files to look for only in the project root
+private val GLOBAL_CONFIG_FILES: Array<String> = Constants.WORKSPACE_FILE_NAMES + Constants.BAZELISK_FILE_NAMES + Constants.BAZEL_RC_FILE_NAME
+
+// Bazel config files to look for in all project directories
+private val LOCAL_CONFIG_FILES: Set<String> = Constants.BUILD_FILE_NAMES.toSet()
