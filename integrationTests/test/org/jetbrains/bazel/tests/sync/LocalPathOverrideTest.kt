0a6473bc6f2015e88bb55e9f1620aa7d34426b81
Klaus T. Aehlig <klaus.aehlig@jetbrains.com>
2026-02-03 16:19:10 +0100

    BAZEL-2885 Add test for local path overrides not in the project view
    GitOrigin-RevId: c85a2ee4bef04b81c79d04fdf32fdb1b8f7c7321

---
+++ integrationTests/test/org/jetbrains/bazel/tests/sync/LocalPathOverrideTest.kt
@@ -0,0 +1,54 @@
+package org.jetbrains.bazel.tests.sync
+
+import com.intellij.driver.sdk.step
+import com.intellij.driver.sdk.ui.UiText.Companion.asString
+import com.intellij.driver.sdk.ui.components.common.editorTabs
+import com.intellij.driver.sdk.ui.components.common.gutter
+import com.intellij.driver.sdk.ui.components.common.ideFrame
+import com.intellij.ide.starter.driver.engine.runIdeWithDriver
+import com.intellij.tools.ide.performanceTesting.commands.goToDeclaration
+import com.intellij.tools.ide.performanceTesting.commands.goto
+import org.jetbrains.bazel.data.IdeaBazelCases
+import org.jetbrains.bazel.ideStarter.IdeStarterBaseProjectTest
+import org.jetbrains.bazel.ideStarter.execute
+import org.jetbrains.bazel.ideStarter.openFile
+import org.jetbrains.bazel.ideStarter.syncBazelProject
+import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+
+class LocalPathOverrideTest : IdeStarterBaseProjectTest() {
+
+  @Test
+  fun `local modules outside projectview detected`() {
+    createContext("localPathOverride", IdeaBazelCases.LocalPathOverride)
+      .runIdeWithDriver(runTimeout = timeout)
+      .useDriverAndCloseIde {
+        ideFrame {
+          step("Initial sync") {
+            syncBazelProject()
+            waitForIndicators(5.minutes)
+            takeScreenshot("afterSync")
+          }
+
+          step("Navigate from main BUILD file") {
+            execute { openFile("BUILD")}
+            execute { goto(7,17)}
+            takeScreenshot("beforeNavigation")
+            execute { goToDeclaration() }
+            takeScreenshot("afterNavigation")
+          }
+
+          step("BUILD file should contain build target") {
+            editorTabs().gutter().getGutterIcons().first { it.getIconPath().contains("build") }.click()
+            takeScreenshot("After library build started")
+            val buildView = x { byType("com.intellij.build.BuildView") }
+            buildView.waitContainsText("Build completed successfully", timeout = 60.seconds)
+            takeScreenshot("afterBuildCompleted")
+            // double check we actually built the correct single target
+            assert(buildView.getAllTexts().asString().contains("utils.jdeps"))
+          }
+        }
+      }
+  }
+}
