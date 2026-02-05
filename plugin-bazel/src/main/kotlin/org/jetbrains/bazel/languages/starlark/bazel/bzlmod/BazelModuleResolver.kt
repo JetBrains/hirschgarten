cfb96a7bea2a81a7ef3c3498045dbf748b18e9c3
Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
2026-02-05 15:59:19 +0000

    [bazel] modules and versions completion based on bcr |  #BAZEL-1028 Done
    [bazel] remove action text from plugin.xml
    
    Space-RevId: f4a218562d4e09ec3509a0246f2cac78cb1858fd
    [bazel] update branch
    
    Space-RevId: 97aa94207293b863f3645087a6616dd16877fd22
    [bazel] add comment to custom expiry implementation
    
    Space-RevId: e718ba0b71df81ecc52510a928c69112ba63b1e5
    [bazel] replace refreshing modules on sync with action
    
    Space-RevId: 42de3bd67b9d4d3e7b60f885ce56e26e5282869e
    [bazel] update branch
    
    Space-RevId: 883a82f35b99a2dbedd28f748f2ccf4dc036e15c
    [bazel] apply suggestions
    
    Space-RevId: ed3697a998a7b8b5f761ada407c407a8d835250d
    [bazel] update branch
    
    Space-RevId: d624e4ac7bec60d05334faff31c218e47d3acc2f
    [bazel] make http request async, log exceptions, update cache expiry calculation
    
    Space-RevId: 4802eb283c9f47bdad24d308b319240d3497a4d1
    [bazel] update branch
    
    Space-RevId: 5d23269fe9eb08042145ae309272f3ca94da53f8
    [bazel] update branch
    
    Space-RevId: 5e9187e304cd26735d6b0261f2e2385efb23c286
    [bazel] invoke completions using runBlockingCancellable
    
    Space-RevId: 156b081fa4c2208c6840ed8a08a0d2fdc59fd670
    [bazel] fix problems with non-semver version names by fetching from metadata.json
    
    Space-RevId: 8df2020492034f41ce7d9be787ae25e489c2ef82
    [bazel] test bazel_dep completion
    
    Space-RevId: 381206716c72080b7da4ed69d3c6b83126e42577
    update branch
    
    Space-RevId: 8d876f30d523a9a0d66d102cfc272fe2c0fcb891
    [bazel] add loading progress for bcr suggestions
    
    Space-RevId: 21eb8f64c0769305fd45e1d4a92d4047b1fa7a37
    [bazel] fix priorities, invoke completions after fetching data, format
    
    Space-RevId: 4705a784454488a3ec226bcf4a9b62a9a38f60ed
    [bazel] add service for fetching modules from BCR and add completions for bazel_dep
    
    Space-RevId: 1ec84e2b5bb5240c286abc7d7a08017f9d1b2f8e
    
    Merge-request: IJ-MR-174835
    Merged-by: Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
    
    
    Merge-request: IJ-MR-190528
    Merged-by: Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
    
    GitOrigin-RevId: 0854d73678ff1f7bb28b597b065f253325b41e47

---
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/languages/starlark/bazel/bzlmod/BazelModuleResolver.kt
@@ -0,0 +1,21 @@
+package org.jetbrains.bazel.languages.starlark.bazel.bzlmod
+
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.project.Project
+import org.jetbrains.bazel.annotations.InternalApi
+
+@InternalApi
+interface BazelModuleResolver {
+  val id: String
+  val name: String
+
+  suspend fun getModuleNames(project: Project): List<String>?
+
+  suspend fun getModuleVersions(project: Project, moduleName: String): List<String>?
+
+  suspend fun refreshModuleNames(project: Project)
+
+  companion object {
+    val EP_NAME: ExtensionPointName<BazelModuleResolver> = ExtensionPointName.create("org.jetbrains.bazel.bzlmodResolver")
+  }
+}
