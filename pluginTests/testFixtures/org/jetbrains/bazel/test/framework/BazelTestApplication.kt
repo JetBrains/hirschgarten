5b1b639d462f2dd1333980259ac8e28307a96489
Lev Leontev <lev.leontev@jetbrains.com>
2026-02-10 17:26:22 +0100

    BAZEL-2830: move NO_FS_ROOTS_ACCESS_CHECK from static variable
    GitOrigin-RevId: d1999db76d662dc846b8cea04045381e697fdc25

--- pluginTests/testFixtures/org/jetbrains/bazel/test/framework/BazelTestApplication.kt
+++ pluginTests/testFixtures/org/jetbrains/bazel/test/framework/BazelTestApplication.kt
@@ -2,6 +2,7 @@ package org.jetbrains.bazel.test.framework
 
 import com.intellij.testFramework.junit5.TestApplication
 import org.jetbrains.annotations.TestOnly
+import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.ExtensionContext
@@ -12,8 +13,20 @@ import org.junit.jupiter.api.extension.ExtensionContext
 @TestApplication
 annotation class BazelTestApplication
 
-private class DisableVfsAccessChecksExtension : BeforeAllCallback {
+private class DisableVfsAccessChecksExtension : BeforeAllCallback, AfterAllCallback {
+  private var oldPropertyValue: String? = null
+
   override fun beforeAll(context: ExtensionContext) {
+    oldPropertyValue = System.getProperty("NO_FS_ROOTS_ACCESS_CHECK")
     System.setProperty("NO_FS_ROOTS_ACCESS_CHECK", "true")
+  }
+
+  override fun afterAll(context: ExtensionContext) {
+    if (oldPropertyValue != null) {
+      System.setProperty("NO_FS_ROOTS_ACCESS_CHECK", oldPropertyValue)
+    }
+    else {
+      System.clearProperty("NO_FS_ROOTS_ACCESS_CHECK")
+    }
   }
 }
