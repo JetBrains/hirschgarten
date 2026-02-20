fbd89b9d37c1e57f432585540d392028b5d1a7be
Evgenii Pasynkov <evgeny.pasynkov@jetbrains.com>
2026-02-20 11:00:17 +0100

    BAZEL-2825 [bazel]: Bazel graceful termination tweak
    Split registry keys to kill server and client tree respectively. By default the client tree should be terminated
    
    GitOrigin-RevId: 91c06dc4bb2092da319e2a95a8b6c855ab58e227

--- server/bazelrunner/src/main/kotlin/org/jetbrains/bazel/bazelrunner/outputs/OutputProcessor.kt
+++ server/bazelrunner/src/main/kotlin/org/jetbrains/bazel/bazelrunner/outputs/OutputProcessor.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.bazel.bazelrunner.outputs
 
 import kotlinx.coroutines.coroutineScope
+import org.jetbrains.bazel.config.BazelFeatureFlags
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
@@ -89,21 +90,20 @@ abstract class OutputProcessor(private val process: SpawnedProcess, vararg logge
     executorService.submit(runnable).also { runningProcessors.add(it) }
   }
 
-  suspend fun waitForExit(serverPidFuture: CompletableFuture<Long>?, killServerOnCancel: Boolean): Int =
+  suspend fun waitForExit(serverPidFuture: CompletableFuture<Long>?): Int =
     coroutineScope {
       try {
         return@coroutineScope process.awaitExit()
       } catch (e: CancellationException) {
         val processSpawner = ProcessSpawner.getInstance()
-        if (killServerOnCancel) {
+        if (BazelFeatureFlags.killClientTreeOnCancel) {
           processSpawner.killProcessTree(process)
-          processSpawner.killProcess(process)
-          if (serverPidFuture?.isDone == true) {
-            val pid = serverPidFuture.get()
-            processSpawner.killProcess(pid.toInt())
-          }
-        } else {
-          processSpawner.killProcess(process)
+        }
+        processSpawner.killProcess(process)
+
+        if (BazelFeatureFlags.killServerOnCancel && serverPidFuture?.isDone == true) {
+          val pid = serverPidFuture.get()
+          processSpawner.killProcess(pid.toInt())
         }
         throw e
       } finally {
