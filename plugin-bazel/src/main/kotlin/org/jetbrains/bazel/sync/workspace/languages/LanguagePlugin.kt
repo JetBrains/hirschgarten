d87901ac32e9f4080876374dd0771ce026c9dfa4
Klaus T. Aehlig <klaus.aehlig@jetbrains.com>
2026-02-20 12:21:14 +0100

    BAZEL-2920: Give LanguagePlugin.prepareSync the full view of the targets
    ... instead of only the the individual targets as sequence. In this
    way, the is the possibility to obtain information from referred-to
    targets (like implicit dependencies or toolchains).
    
    GitOrigin-RevId: 81f99154ae487903b49bbe86fa53b3dc3e25c87c

--- plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/languages/LanguagePlugin.kt
+++ plugin-bazel/src/main/kotlin/org/jetbrains/bazel/sync/workspace/languages/LanguagePlugin.kt
@@ -3,6 +3,7 @@ package org.jetbrains.bazel.sync.workspace.languages
 import com.intellij.openapi.project.Project
 import org.jetbrains.bazel.commons.LanguageClass
 import org.jetbrains.bazel.info.BspTargetInfo
+import org.jetbrains.bazel.label.Label
 import org.jetbrains.bazel.workspacecontext.WorkspaceContext
 import org.jetbrains.bsp.protocol.BuildTargetData
 import org.jetbrains.bsp.protocol.SourceItem
@@ -15,7 +16,7 @@ interface LanguagePlugin<BuildTarget : BuildTargetData> {
 
   fun resolveAdditionalResources(targetInfo: BspTargetInfo.TargetInfo): Sequence<Path> = emptySequence()
 
-  fun prepareSync(project: Project, targets: Sequence<BspTargetInfo.TargetInfo>, workspaceContext: WorkspaceContext) {}
+  fun prepareSync(project: Project, targets: Map<Label, BspTargetInfo.TargetInfo>, workspaceContext: WorkspaceContext) {}
 
   fun transformSources(sources: List<SourceItem>): List<SourceItem> = sources
 
