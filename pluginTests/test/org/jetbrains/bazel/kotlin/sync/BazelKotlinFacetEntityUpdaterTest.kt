8acb21be8850694713af0abc56796319124e2f2a
Lev Leontev <lev.leontev@jetbrains.com>
2026-03-04 20:43:09 +0100

    BAZEL-2350: fallback to Kotlin version from kotlinc options for rules_jvm
    GitOrigin-RevId: 6b0237c751c01939b516c0146e3a0632ae07a232

--- pluginTests/test/org/jetbrains/bazel/kotlin/sync/BazelKotlinFacetEntityUpdaterTest.kt
+++ pluginTests/test/org/jetbrains/bazel/kotlin/sync/BazelKotlinFacetEntityUpdaterTest.kt
@@ -24,6 +24,7 @@ import org.jetbrains.bazel.workspacemodel.entities.KotlinAddendum
 import org.jetbrains.bsp.protocol.JvmBuildTarget
 import org.jetbrains.bsp.protocol.KotlinBuildTarget
 import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments
+import org.jetbrains.kotlin.config.IKotlinFacetSettings
 import org.jetbrains.kotlin.idea.facet.KotlinFacetType
 import org.junit.jupiter.api.Test
 import kotlin.io.path.Path
@@ -38,14 +39,12 @@ class BazelKotlinFacetEntityUpdaterTest : WorkspaceModelBaseTest() {
       val javaHome = Path("/fake/path/to/local_jdk")
       val javaVersion = "11"
 
-      val associates = listOf("//target4", "target5")
-
       val kotlinBuildTarget =
         KotlinBuildTarget(
           languageVersion = "1.8",
           apiVersion = "1.8",
           kotlincOptions = listOf("-opt-in=com.example.ExperimentalApi"),
-          associates = associates.map { Label.parse(it) },
+          associates = listOf("//target4", "//target5").map { Label.parse(it) },
           moduleName = "kotlin-module",
           jvmBuildTarget =
             JvmBuildTarget(
@@ -54,63 +53,11 @@ class BazelKotlinFacetEntityUpdaterTest : WorkspaceModelBaseTest() {
             ),
         )
 
-      val module =
-        GenericModuleInfo(
-          name = "module1",
-          type = ModuleTypeId("JAVA_MODULE"),
-          dependencies =
-            listOf(
-              Dependency("module2"),
-              Dependency("module3"),
-            ),
-          kind =
-            TargetKind(
-              kindString = "java_library",
-              ruleType = RuleType.LIBRARY,
-              languageClasses = setOf(LanguageClass.JAVA),
-            ),
-          associates = associates,
-        )
-
-      val baseDirContentRoot =
-        ContentRoot(
-          path = projectBasePath.toAbsolutePath(),
-        )
-      val javaModule =
-        JavaModule(
-          genericModuleInfo = module,
-          baseDirContentRoot = baseDirContentRoot,
-          sourceRoots = listOf(),
-          resourceRoots = listOf(),
-          jvmJdkName = "${projectBasePath.name}-$javaVersion",
-          kotlinAddendum =
-            KotlinAddendum(
-              languageVersion = kotlinBuildTarget.languageVersion,
-              apiVersion = kotlinBuildTarget.apiVersion,
-              moduleName = kotlinBuildTarget.moduleName,
-              kotlincOptions = kotlinBuildTarget.kotlincOptions,
-            ),
-        )
-
       // when
-      updateWorkspaceModel {
-        val returnedModuleEntity =
-          addEmptyJavaModuleEntity(
-            module.name,
-            it,
-          )
-        addKotlinFacetEntity(javaModule, returnedModuleEntity, it)
-      }
+      val retrievedFacetSettings = addToWorkspaceModelAndGetFacet(kotlinBuildTarget)
 
       // then
-      val moduleManager = ModuleManager.getInstance(project)
-      val retrievedModule = moduleManager.findModuleByName(module.name)
-      retrievedModule.shouldNotBeNull()
-      val facetManager = FacetManager.getInstance(retrievedModule)
-      val facet = facetManager.getFacetByType(KotlinFacetType.TYPE_ID)
-      facet.shouldNotBeNull()
-      val retrievedFacetSettings = facet.configuration.settings
-      retrievedFacetSettings.additionalVisibleModuleNames shouldBe associates
+      retrievedFacetSettings.additionalVisibleModuleNames shouldBe kotlinBuildTarget.associates.map { it.toString() }
       retrievedFacetSettings
         .compilerArguments
         .shouldBeInstanceOf<K2JVMCompilerArguments>()
@@ -123,6 +70,103 @@ class BazelKotlinFacetEntityUpdaterTest : WorkspaceModelBaseTest() {
           optIn.shouldNotBeNull() shouldHaveSingleElement "com.example.ExperimentalApi"
         }
     }
+  }
+
+  // https://youtrack.jetbrains.com/issue/BAZEL-2350
+  @Test
+  fun `should parse Kotlin version from kotlincOptions if rules_jvm is used and not rules_kotlin`() {
+    runInEdtAndWait {
+      // given
+      val javaHome = Path("/fake/path/to/local_jdk")
+      val javaVersion = "11"
+
+      val kotlinBuildTarget =
+        KotlinBuildTarget(
+          languageVersion = null,
+          apiVersion = null,
+          kotlincOptions = listOf("-language-version=1.8", "-api-version=1.9"),
+          associates = emptyList(),
+          moduleName = "kotlin-module",
+          jvmBuildTarget =
+            JvmBuildTarget(
+              javaHome = javaHome,
+              javaVersion = javaVersion,
+            ),
+        )
+
+      // when
+      val retrievedFacetSettings = addToWorkspaceModelAndGetFacet(kotlinBuildTarget)
+
+      // then
+      retrievedFacetSettings
+        .compilerArguments
+        .shouldBeInstanceOf<K2JVMCompilerArguments>()
+        .apply {
+          moduleName shouldBe "kotlin-module"
+          apiVersion shouldBe "1.9"
+          languageVersion shouldBe "1.8"
+          autoAdvanceLanguageVersion shouldBe false
+          autoAdvanceApiVersion shouldBe false
+        }
+    }
+  }
+
+  private fun addToWorkspaceModelAndGetFacet(kotlinBuildTarget: KotlinBuildTarget): IKotlinFacetSettings {
+    val module =
+      GenericModuleInfo(
+        name = "module1",
+        type = ModuleTypeId("JAVA_MODULE"),
+        dependencies =
+          listOf(
+            Dependency("module2"),
+            Dependency("module3"),
+          ),
+        kind =
+          TargetKind(
+            kindString = "java_library",
+            ruleType = RuleType.LIBRARY,
+            languageClasses = setOf(LanguageClass.JAVA),
+          ),
+        associates = kotlinBuildTarget.associates.map { it.toString() },
+      )
+
+    val baseDirContentRoot =
+      ContentRoot(
+        path = projectBasePath.toAbsolutePath(),
+      )
+    val javaModule =
+      JavaModule(
+        genericModuleInfo = module,
+        baseDirContentRoot = baseDirContentRoot,
+        sourceRoots = listOf(),
+        resourceRoots = listOf(),
+        jvmJdkName = "${projectBasePath.name}-${kotlinBuildTarget.jvmBuildTarget!!.javaVersion}",
+        kotlinAddendum =
+          KotlinAddendum(
+            languageVersion = kotlinBuildTarget.languageVersion,
+            apiVersion = kotlinBuildTarget.apiVersion,
+            moduleName = kotlinBuildTarget.moduleName,
+            kotlincOptions = kotlinBuildTarget.kotlincOptions,
+          ),
+      )
+
+    updateWorkspaceModel {
+      val returnedModuleEntity =
+        addEmptyJavaModuleEntity(
+          module.name,
+          it,
+        )
+      addKotlinFacetEntity(javaModule, returnedModuleEntity, it)
+    }
+
+    val moduleManager = ModuleManager.getInstance(project)
+    val retrievedModule = moduleManager.findModuleByName(module.name)
+    retrievedModule.shouldNotBeNull()
+    val facetManager = FacetManager.getInstance(retrievedModule)
+    val facet = facetManager.getFacetByType(KotlinFacetType.TYPE_ID)
+    facet.shouldNotBeNull()
+    val retrievedFacetSettings = facet.configuration.settings
+    return retrievedFacetSettings
   }
 
   private fun addKotlinFacetEntity(
