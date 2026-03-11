99b1592cf35498e6a6d895baa639c783d4ae9145
Damian Baczyński <damian.baczynski@jetbrains.com>
2026-03-11 15:38:43 +0100

    BAZEL-3012 [bazel]: stop adding kotlin compiler plugin dependencies as libraries
    Kotlin compiler plugin JARs (and their transitive deps like kotlin-compiler-embeddable) were incorrectly added as compile scope dependencies of workspace targets, making compiler plugins code resolvable from sources. This caused false-green highlighting for references that would fail actual Bazel compilation.
    
    In the original issue, it causes a red code because kotlin-compiler-embeddable has `CancellationPromise` embedded without any methods, which is resolved instead of the type from intellij.core-api.
    
    GitOrigin-RevId: 5d68ef24b8f07aea3ee515d4fe54c5af1f61a374

---
+++ pluginTests/test/org/jetbrains/bazel/redcodes/KotlinCompilerPluginTest.kt
@@ -0,0 +1,29 @@
+package org.jetbrains.bazel.redcodes
+
+import com.intellij.openapi.application.EDT
+import com.intellij.testFramework.junit5.fixture.projectFixture
+import com.intellij.testFramework.junit5.fixture.tempPathFixture
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.jetbrains.bazel.test.framework.BazelTestApplication
+import org.jetbrains.bazel.test.framework.bazelSyncCodeInsightFixture
+import org.jetbrains.bazel.test.framework.checkHighlighting
+import org.junit.jupiter.api.Test
+
+@BazelTestApplication
+class KotlinCompilerPluginTest {
+
+  private val projectFixture = projectFixture(openAfterCreation = true)
+  private val tempDir = tempPathFixture()
+  private val fixture by bazelSyncCodeInsightFixture(projectFixture, tempDir)
+
+  @Test
+  fun testHighlighting() = runBlocking(Dispatchers.Default) {
+    fixture.copyBazelTestProject("redcodes/kotlin_compiler_plugin")
+    fixture.performBazelSync()
+    withContext(Dispatchers.EDT) {
+      fixture.checkHighlighting("Lib.kt")
+    }
+  }
+}
