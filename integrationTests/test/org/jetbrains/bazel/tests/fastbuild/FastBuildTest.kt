d21db7e8ae83024c30223698f673b033c63d64d7
Lev Leontev <lev.leontev@jetbrains.com>
2026-03-11 03:58:53 +0100

    BAZEL-3014: update hotswap tests
    IJ-MR-195581
    (cherry picked from commit 2997257c06699dc3d7d71b6be2ca841d1d352f14)
    
    GitOrigin-RevId: 0b55889bb4971ae60cd04bfe73c011afa4eafd89

--- integrationTests/test/org/jetbrains/bazel/tests/fastbuild/FastBuildTest.kt
+++ integrationTests/test/org/jetbrains/bazel/tests/fastbuild/FastBuildTest.kt
@@ -4,6 +4,7 @@ import com.intellij.driver.client.Remote
 import com.intellij.driver.client.service
 import com.intellij.driver.sdk.VirtualFile
 import com.intellij.driver.sdk.invokeAction
+import com.intellij.driver.sdk.openToolWindow
 import com.intellij.driver.sdk.singleProject
 import com.intellij.driver.sdk.step
 import com.intellij.driver.sdk.ui.components.UiComponent.Companion.waitFound
@@ -11,6 +12,7 @@ import com.intellij.driver.sdk.ui.components.common.codeEditor
 import com.intellij.driver.sdk.ui.components.common.editorTabs
 import com.intellij.driver.sdk.ui.components.common.gutter
 import com.intellij.driver.sdk.ui.components.common.ideFrame
+import com.intellij.driver.sdk.ui.components.common.toolwindows.debugToolWindow
 import com.intellij.driver.sdk.ui.components.elements.popup
 import com.intellij.driver.sdk.ui.pasteText
 import com.intellij.driver.sdk.ui.ui
@@ -24,7 +26,8 @@ import org.jetbrains.bazel.ideStarter.findFile
 import org.jetbrains.bazel.ideStarter.openFile
 import org.jetbrains.bazel.ideStarter.syncBazelProject
 import org.jetbrains.bazel.ideStarter.withBazelFeatureFlag
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
@@ -33,12 +36,16 @@ import kotlin.time.Duration.Companion.seconds
  * bazel test //plugin-bazel/src/test/kotlin/org/jetbrains/bazel/fastbuild --jvmopt="-Dbazel.ide.starter.test.cache.directory=$HOME/IdeaProjects/hirschgarten" --sandbox_writable_path=/ --action_env=PATH --java_debug --test_arg=--wrapper_script_flag=--debug=8000
  * ```
  */
+// TODO(Dan): rename to HotSwapTest
 class FastBuildTest : IdeStarterBaseProjectTest() {
-  @Test
-  fun `fast build hotswap should apply code changes during debug`() {
+  @ParameterizedTest
+  @ValueSource(booleans = [false, true])
+  fun `hotswap should apply code changes during debug`(useFastBuildInsteadOfHotswap: Boolean) {
     val context = createContext("fastbuild", IdeaBazelCases.FastBuild)
       .withBazelFeatureFlag(BazelFeatureFlags.BUILD_PROJECT_ON_SYNC)
-      .withBazelFeatureFlag(BazelFeatureFlags.FAST_BUILD_ENABLED)
+      .applyVMOptionsPatch {
+        addSystemProperty(BazelFeatureFlags.FAST_BUILD_ENABLED, useFastBuildInsteadOfHotswap.toString())
+      }
     context
       .runIdeWithDriver(runTimeout = timeout)
       .useDriverAndCloseIde {
@@ -61,8 +68,8 @@ class FastBuildTest : IdeStarterBaseProjectTest() {
             popup().waitOneText("Debug run").click()
           }
 
-          val resumeProgram = x { byAccessibleName("Resume Program") }
           step("Wait for breakpoint to be hit") {
+            val resumeProgram = debugToolWindow().resumeButton
             resumeProgram.waitFound(timeout = 3.minutes)
             waitFor(
               message = "Resume Program should be enabled",
@@ -85,15 +92,21 @@ class FastBuildTest : IdeStarterBaseProjectTest() {
             projectTaskManager.compile(arrayOf(checkNotNull(driver.findFile("Calculator.java"))))
           }
 
-          step("Agree to the 'Reload Changed Classes' dialog") {
-            val reloadButton = ui.x { byAccessibleName("Reload") }
-            reloadButton.waitFound(timeout = 30.seconds)
-            reloadButton.click()
-            Thread.sleep(5000)
+          if (useFastBuildInsteadOfHotswap) {
+            step("Agree to the 'Reload Changed Classes' dialog") {
+              val reloadButton = ui.x { byAccessibleName("Reload") }
+              reloadButton.waitFound(timeout = 30.seconds)
+              reloadButton.click()
+            }
+          }
+          waitForIndicators(30.seconds)
+
+          step("Switch back to the Debug tool window") {
+            openToolWindow("Debug")
           }
 
           step("Resume program") {
-            resumeProgram.click()
+            debugToolWindow().resumeButton.click()
           }
 
           val consoleView = x { byClass("ConsoleViewImpl") }
