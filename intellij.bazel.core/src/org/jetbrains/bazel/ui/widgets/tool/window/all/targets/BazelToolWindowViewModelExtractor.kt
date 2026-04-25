2ad19c942030dba521b804612aa02f5f04a54e18
Krystian Gajewski <krystian.gajewski.external@jetbrains.com>
2026-04-25 13:49:55 +0200

    BAZEL-2896 bazel: fix UI freeze on target tree updates in remdev
    Pass target tree tool window as LUX component instead of BeControls one. Target tree is being recreated from scratch on each sync/target list change. To avoid sending entire tree component state change over RD, we can simply pass partial draw data + user interactions to thin client using LUX component.
    
    GitOrigin-RevId: 89ef0c0219b1592b56db49002ec1cbe2be60af60

--- intellij.bazel.core/src/org/jetbrains/bazel/ui/widgets/tool/window/all/targets/BazelToolWindowViewModelExtractor.kt
+++ intellij.bazel.core/src/org/jetbrains/bazel/ui/widgets/tool/window/all/targets/BazelToolWindowViewModelExtractor.kt
@@ -6,9 +6,12 @@ import com.intellij.ui.viewModel.extraction.ToolWindowViewModelExtractor
 import org.jetbrains.bazel.config.BazelPluginConstants
 import org.jetbrains.bazel.config.bazelProjectProperties
 
-private class BazelToolWindowViewModelExtractor : ToolWindowViewModelExtractor {
+internal class BazelToolWindowViewModelExtractor : ToolWindowViewModelExtractor {
   override fun isApplicable(toolWindowId: String, session: ClientProjectSession): Boolean =
     toolWindowId == BazelPluginConstants.BAZEL_TOOLWINDOW_ID && session.project.bazelProjectProperties.isBazelProject
 
-  override fun getMode(): ToolWindowExtractorMode = ToolWindowExtractorMode.MIRROR
+  // force targets window to be shared as a LUX component,
+  // for large number of targets sending every node update over RD is wasteful and
+  // cause massive freezes(with monolithic updates)
+  override fun getMode(): ToolWindowExtractorMode = ToolWindowExtractorMode.PROJECTOR_INSTANCING
 }
