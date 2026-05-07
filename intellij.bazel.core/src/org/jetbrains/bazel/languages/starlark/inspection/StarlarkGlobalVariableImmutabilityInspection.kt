ae65d3d1364c9867be96966a68a2b01c53c4238a
Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
2026-05-07 09:54:55 +0000

    [bazel] BAZEL-3098 Starlark global variable immutability inspection
    [bazel] disable inspection for WORKSPACE files, add html description
    
    [bazel] remove unnecessary check for top-level assignments
    
    [bazel] apply suggestions
    
    [bazel] global variable immutability inspection and tests
    
    
    Merge-request: IJ-MR-200983
    Merged-by: Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
    
    (cherry picked from commit b7eb411234130dccd94cc5860701f02d833c5ff5)
    
    IJ-MR-200983
    
    GitOrigin-RevId: 72c981d5dcef902c6ab47454f7e91336e1952b96

---
+++ intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/inspection/StarlarkGlobalVariableImmutabilityInspection.kt
@@ -0,0 +1,47 @@
+package org.jetbrains.bazel.languages.starlark.inspection
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.childrenOfType
+import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.bazel.commons.constants.Constants
+import org.jetbrains.bazel.languages.starlark.StarlarkBundle
+import org.jetbrains.bazel.languages.starlark.StarlarkFileType
+import org.jetbrains.bazel.languages.starlark.psi.StarlarkFile
+import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkAssignmentStatement
+
+@ApiStatus.Internal
+class StarlarkGlobalVariableImmutabilityInspection : LocalInspectionTool() {
+  override fun isAvailableForFile(file: PsiFile): Boolean = file.fileType is StarlarkFileType
+
+  override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = GlobalVarVisitor(holder)
+
+  class GlobalVarVisitor(private val holder: ProblemsHolder) : PsiElementVisitor() {
+    val excludedFileNames = Constants.BUILD_FILE_NAMES +
+                            arrayOf(Constants.WORKSPACE_FILE_NAME, "WORKSPACE.bazel", "WORKSPACE.bzlmod")
+
+    override fun visitFile(psiFile: PsiFile) {
+      val file = psiFile as? StarlarkFile ?: return
+      if (file.name in excludedFileNames) return
+      val assignmentsByName = mutableMapOf<String, MutableList<StarlarkAssignmentStatement>>()
+
+      file.childrenOfType<StarlarkAssignmentStatement>().forEach { assignment ->
+        val name = assignment.name ?: return@forEach
+        assignmentsByName.getOrPut(name) { mutableListOf() }.add(assignment)
+      }
+
+      assignmentsByName.values
+        .filter { it.size > 1 }
+        .forEach { reassignments ->
+          reassignments.drop(1).forEach { stmt ->
+            holder.registerProblem(
+              stmt,
+              StarlarkBundle.message("inspection.description.global.variable.immutable"),
+            )
+          }
+        }
+    }
+  }
+}
