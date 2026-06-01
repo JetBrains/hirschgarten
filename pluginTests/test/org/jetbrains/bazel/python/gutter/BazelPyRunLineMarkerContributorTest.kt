1f4220d9bcba9fba27ed514d743580b798df36f5
Marcin Kocot <marcin.kocot.external@jetbrains.com>
2026-06-01 13:05:45 +0000

    BAZEL-2285 BAZEL-3189 Running single test functions in Python
    Merge-request: IJ-MR-206431
    Merged-by: Marcin Kocot <Marcin.Kocot.external@jetbrains.com>
    
    GitOrigin-RevId: b1a9a7129471c43683e80b7ebb5f6549584fa3eb

--- pluginTests/test/org/jetbrains/bazel/python/gutter/BazelPyRunLineMarkerContributorTest.kt
+++ pluginTests/test/org/jetbrains/bazel/python/gutter/BazelPyRunLineMarkerContributorTest.kt
@@ -1,13 +1,16 @@
 package org.jetbrains.bazel.python.gutter
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiUtilCore
 import com.intellij.testFramework.builders.ModuleFixtureBuilder
 import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 import com.intellij.testFramework.fixtures.ModuleFixture
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
 import org.jetbrains.bazel.commons.LanguageClass
 import org.jetbrains.bazel.commons.RuleType
 import org.jetbrains.bazel.commons.TargetKind
@@ -40,10 +43,11 @@ internal class BazelPyRunLineMarkerContributorTest : CodeInsightFixtureTestCase<
     project.addFileToTarget(mainFile, LABEL)
 
     // WHEN
-    val elementAtCaret = mainFile.findElementAt(myFixture.caretOffset)!!
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(mainFile, myFixture.caretOffset)!!
     val runLineMarkerInfo = bazelPyRunLineMarkerContributor.getInfo(elementAtCaret)!!
 
     // THEN
+    runLineMarkerInfo.icon.shouldBeNull()
     runLineMarkerInfo.actions.shouldHaveSize(2)
   }
 
@@ -56,7 +60,7 @@ internal class BazelPyRunLineMarkerContributorTest : CodeInsightFixtureTestCase<
     project.addFileToTarget(mainFile, LABEL)
 
     // WHEN
-    val elementAtCaret = mainFile.findElementAt(myFixture.caretOffset)!!
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(mainFile, myFixture.caretOffset)!!
     val runLineMarkerInfo = bazelPyRunLineMarkerContributor.getInfo(elementAtCaret)!!
 
     // THEN
@@ -74,7 +78,7 @@ internal class BazelPyRunLineMarkerContributorTest : CodeInsightFixtureTestCase<
     project.addFileToTarget(otherFile, LABEL)
 
     // WHEN
-    val elementAtCaret = otherFile.findElementAt(myFixture.caretOffset)!!
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(otherFile, myFixture.caretOffset)!!
     val runLineMarkerInfo = bazelPyRunLineMarkerContributor.getInfo(elementAtCaret)
 
     // THEN
@@ -90,7 +94,7 @@ internal class BazelPyRunLineMarkerContributorTest : CodeInsightFixtureTestCase<
     project.removeFileFromAllTargets(notInTargetFile)
 
     // WHEN
-    val elementAtCaret = notInTargetFile.findElementAt(myFixture.caretOffset)!!
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(notInTargetFile, myFixture.caretOffset)!!
     val runLineMarkerInfo = bazelPyRunLineMarkerContributor.getInfo(elementAtCaret)
 
     // THEN
@@ -106,7 +110,201 @@ internal class BazelPyRunLineMarkerContributorTest : CodeInsightFixtureTestCase<
     project.addFileToTarget(runnableFile, LABEL)
 
     // WHEN
-    val elementAtCaret = runnableFile.findElementAt(myFixture.caretOffset)!!
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(runnableFile, myFixture.caretOffset)!!
+    val runLineMarkerInfo = bazelPyRunLineMarkerContributor.getInfo(elementAtCaret)
+
+    // THEN
+    runLineMarkerInfo.shouldBeNull()
+  }
+
+  @Test
+  fun `should show bazel-related run actions for pytest function`() {
+    // GIVEN
+    val testFile =
+      myFixture.pythonFile(
+        "test_sample.py",
+        """
+        def <caret>test_passes():
+            assert True
+        """.trimIndent(),
+      )
+
+    project.addPyTestTarget(label = LABEL)
+    project.addFileToTarget(testFile, LABEL)
+
+    // WHEN
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(testFile, myFixture.caretOffset)!!
+    val runLineMarkerInfo = bazelPyRunLineMarkerContributor.getInfo(elementAtCaret)!!
+
+    // THEN
+    runLineMarkerInfo.icon shouldBe AllIcons.Actions.Execute
+    runLineMarkerInfo.actions.shouldHaveSize(3)
+  }
+
+  @Test
+  fun `should not calculate slow info for pytest function`() {
+    // GIVEN
+    val testFile =
+      myFixture.pythonFile(
+        "test_sample.py",
+        """
+        def <caret>test_passes():
+            assert True
+        """.trimIndent(),
+      )
+
+    project.addPyTestTarget(label = LABEL)
+    project.addFileToTarget(testFile, LABEL)
+
+    // WHEN
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(testFile, myFixture.caretOffset)!!
+    val slowRunLineMarkerInfo = bazelPyRunLineMarkerContributor.getSlowInfo(elementAtCaret)
+
+    // THEN
+    slowRunLineMarkerInfo.shouldBeNull()
+  }
+
+  @Test
+  fun `should pass pytest node id as test executable argument for pytest function`() {
+    // GIVEN
+    val testFile =
+      myFixture.pythonFile(
+        "test_sample.py",
+        """
+        def <caret>test_passes():
+            assert True
+        """.trimIndent(),
+      )
+
+    project.addPyTestTarget(label = LABEL)
+    project.addFileToTarget(testFile, LABEL)
+
+    // WHEN
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(testFile, myFixture.caretOffset)!!
+    val extraProgramArguments = bazelPyRunLineMarkerContributor.getExtraProgramArguments(elementAtCaret)
+
+    // THEN
+    extraProgramArguments shouldBe listOf("test_sample.py::test_passes")
+  }
+
+  @Test
+  fun `should pass pytest node id as test executable argument for pytest class method`() {
+    // GIVEN
+    val testFile =
+      myFixture.pythonFile(
+        "test_sample.py",
+        """
+        class TestSample:
+            def <caret>test_passes(self):
+                assert True
+        """.trimIndent(),
+      )
+
+    project.addPyTestTarget(label = LABEL)
+    project.addFileToTarget(testFile, LABEL)
+
+    // WHEN
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(testFile, myFixture.caretOffset)!!
+    val extraProgramArguments = bazelPyRunLineMarkerContributor.getExtraProgramArguments(elementAtCaret)
+
+    // THEN
+    extraProgramArguments shouldBe listOf("test_sample.py::TestSample::test_passes")
+  }
+
+  @Test
+  fun `should pass unittest method name as test executable argument for unittest method`() {
+    // GIVEN
+    val testFile =
+      myFixture.pythonFile(
+        "unittest_test.py",
+        """
+        import unittest
+
+        class SampleTest(unittest.TestCase):
+            def <caret>test_passes(self):
+                self.assertTrue(True)
+        """.trimIndent(),
+      )
+
+    project.addPyTestTarget(label = LABEL)
+    project.addFileToTarget(testFile, LABEL)
+
+    // WHEN
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(testFile, myFixture.caretOffset)!!
+    val extraProgramArguments = bazelPyRunLineMarkerContributor.getExtraProgramArguments(elementAtCaret)
+
+    // THEN
+    extraProgramArguments shouldBe listOf("SampleTest.test_passes")
+  }
+
+  @Test
+  fun `should show bazel-related run actions for unittest method`() {
+    // GIVEN
+    val testFile =
+      myFixture.pythonFile(
+        "unittest_test.py",
+        """
+        from unittest import TestCase
+
+        class SampleTest(TestCase):
+            def <caret>test_passes(self):
+                self.assertTrue(True)
+        """.trimIndent(),
+      )
+
+    project.addPyTestTarget(label = LABEL)
+    project.addFileToTarget(testFile, LABEL)
+
+    // WHEN
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(testFile, myFixture.caretOffset)!!
+    val runLineMarkerInfo = bazelPyRunLineMarkerContributor.getInfo(elementAtCaret)!!
+
+    // THEN
+    runLineMarkerInfo.icon shouldBe AllIcons.Actions.Execute
+    runLineMarkerInfo.actions.shouldHaveSize(3)
+  }
+
+  @Test
+  fun `should hide bazel-related run actions for pytest class name`() {
+    // GIVEN
+    val testFile =
+      myFixture.pythonFile(
+        "test_sample.py",
+        """
+        class <caret>TestSample:
+            def test_passes(self):
+                assert True
+        """.trimIndent(),
+      )
+
+    project.addPyTestTarget(label = LABEL)
+    project.addFileToTarget(testFile, LABEL)
+
+    // WHEN
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(testFile, myFixture.caretOffset)!!
+    val runLineMarkerInfo = bazelPyRunLineMarkerContributor.getInfo(elementAtCaret)
+
+    // THEN
+    runLineMarkerInfo.shouldBeNull()
+  }
+
+  @Test
+  fun `should hide bazel-related run actions for non-test function in py test target`() {
+    // GIVEN
+    val testFile =
+      myFixture.pythonFile(
+        "test_sample.py",
+        """
+        def <caret>helper():
+            return True
+        """.trimIndent(),
+      )
+
+    project.addPyTestTarget(label = LABEL)
+    project.addFileToTarget(testFile, LABEL)
+
+    // WHEN
+    val elementAtCaret = PsiUtilCore.getElementAtOffset(testFile, myFixture.caretOffset)!!
     val runLineMarkerInfo = bazelPyRunLineMarkerContributor.getInfo(elementAtCaret)
 
     // THEN
@@ -119,13 +317,42 @@ internal class BazelPyRunLineMarkerContributorTest : CodeInsightFixtureTestCase<
       """
       <caret>if __name__ == '__main__':
           print(f'Hello world')
-    """.trimIndent(),
+      """.trimIndent(),
     )
+
+  private fun CodeInsightTestFixture.pythonFile(fileName: String, content: String): PsiFile =
+    configureByText(fileName, content)
 
   private fun Project.addPyBinaryTarget(
     label: Label,
     mainFile: PsiFile? = null,
     mainModule: String? = null,
+  ) {
+    addPythonTarget(
+      label = label,
+      ruleKind = "py_binary",
+      ruleType = RuleType.BINARY,
+      mainFile = mainFile,
+      mainModule = mainModule,
+    )
+  }
+
+  private fun Project.addPyTestTarget(label: Label) {
+    addPythonTarget(
+      label = label,
+      ruleKind = "py_test",
+      ruleType = RuleType.TEST,
+      mainFile = null,
+      mainModule = null,
+    )
+  }
+
+  private fun Project.addPythonTarget(
+    label: Label,
+    ruleKind: String,
+    ruleType: RuleType,
+    mainFile: PsiFile?,
+    mainModule: String?,
   ) {
     targetUtils.setTargets(
       listOf(
@@ -134,9 +361,9 @@ internal class BazelPyRunLineMarkerContributorTest : CodeInsightFixtureTestCase<
           dependencies = emptyList(),
           kind =
             TargetKind(
-              kind = "py_binary",
+              kind = ruleKind,
               languageClasses = setOf(LanguageClass.PYTHON),
-              ruleType = RuleType.BINARY,
+              ruleType = ruleType,
             ),
           sources = emptyList(),
           resources = emptyList(),
