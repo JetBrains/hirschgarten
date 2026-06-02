f21ff192590f3f458c38372f6aa1ee029b48e606
Damian Baczyński <damian.baczynski@jetbrains.com>
2026-06-02 14:13:37 +0200

    BAZEL-3227: Optimize BazelTargetReferenceSearcher
    Optimization depends on the word index. First we search just in the file that declares element. Then we use a combination of Bazel package and target name to find external usages. Using target name against the word index might result in a lot of unnecessary candidates.
    
    GitOrigin-RevId: 76daf8d82311eb80a1aa9180c6b6eb8b134436a5

--- intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/StarlarkSearchScope.kt
+++ intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/StarlarkSearchScope.kt
@@ -1,7 +1,10 @@
 package org.jetbrains.bazel.languages.starlark
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.search.DelegatingGlobalSearchScope
 import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.bazel.languages.starlark.bazel.BazelFileType
 
 internal fun Project.starlarkProjectScope(): GlobalSearchScope = GlobalSearchScope
   .projectScope(this)
@@ -10,6 +13,18 @@ internal fun Project.starlarkProjectScope(): GlobalSearchScope = GlobalSearchSco
 internal fun GlobalSearchScope.restrictToStarlarkFiles(): GlobalSearchScope {
   return GlobalSearchScope.getScopeRestrictedByFileTypes(
     this,
-    StarlarkFileType
+    StarlarkFileType,
   )
+}
+
+internal fun GlobalSearchScope.restrictToStarlarkFiles(
+  vararg types: BazelFileType
+): GlobalSearchScope = StarlarkFilesByTypeScope(restrictToStarlarkFiles(), types.toSet())
+
+private class StarlarkFilesByTypeScope(
+  base: GlobalSearchScope,
+  private val types: Set<BazelFileType>,
+) : DelegatingGlobalSearchScope(base) {
+
+  override fun contains(file: VirtualFile): Boolean = super.contains(file) && BazelFileType.ofFileName(file.name) in types
 }
