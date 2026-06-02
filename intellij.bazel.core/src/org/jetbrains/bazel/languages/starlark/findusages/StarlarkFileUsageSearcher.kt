c2537a1f03bf278815ac0d2c641b7f94dfbfa9b0
Damian Baczyński <damian.baczynski@jetbrains.com>
2026-06-02 11:00:14 +0200

    BAZEL-3224: Optmize StarlarkFileUsageSearcher
    1. It uses PsiSearchHelper.processCandidateFilesForText to prefilter psi files when searching for explicit file candidates.
    
    2. Searches for given file only in globs from current package BUILD file. This optimization is possible due to glob restrictions.
    
    GitOrigin-RevId: cc2425a1091a9ad983203143b3c957a3ef141ead

--- intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/findusages/StarlarkFileUsageSearcher.kt
+++ intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/findusages/StarlarkFileUsageSearcher.kt
@@ -1,24 +1,26 @@
 package org.jetbrains.bazel.languages.starlark.findusages
 
 import com.intellij.openapi.application.QueryExecutorBase
+import com.intellij.openapi.vfs.findPsiFile
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiReference
-import com.intellij.psi.search.FileTypeIndex
+import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.GlobalSearchScopeUtil
+import com.intellij.psi.search.PsiSearchHelper
+import com.intellij.psi.search.UsageSearchContext
 import com.intellij.psi.search.searches.ReferencesSearch.SearchParameters
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.Processor
 import org.jetbrains.bazel.config.isBazelProject
 import org.jetbrains.bazel.label.Label
-import org.jetbrains.bazel.languages.starlark.StarlarkFileType
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkFile
 import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkGlobExpression
-import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkListLiteralExpression
 import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkStringLiteralExpression
-import org.jetbrains.bazel.languages.starlark.psi.expressions.arguments.StarlarkArgumentElement
 import org.jetbrains.bazel.languages.starlark.references.BazelLabelReference
+import org.jetbrains.bazel.languages.starlark.references.findBuildFile
 import org.jetbrains.bazel.languages.starlark.references.resolveLabel
+import org.jetbrains.bazel.languages.starlark.restrictToStarlarkFiles
 
 /**
  * Finds file usages in Starlark files. Requires [StarlarkFileUseScopeEnlarger] to enlarge the scope of the [PsiFile] with Starlark files.
@@ -26,43 +28,57 @@ import org.jetbrains.bazel.languages.starlark.references.resolveLabel
 internal class StarlarkFileUsageSearcher : QueryExecutorBase<PsiReference, SearchParameters>(true) {
   override fun processQuery(params: SearchParameters, processor: Processor<in PsiReference>) {
     if (!params.project.isBazelProject) return
-
-    val psiManager = PsiManager.getInstance(params.project)
     val file = params.elementToSearch as? PsiFile ?: return
-    val scope = GlobalSearchScopeUtil.toGlobalSearchScope(params.effectiveSearchScope, params.project)
-    val starlarkFiles = FileTypeIndex.getFiles(StarlarkFileType, scope)
-    if (starlarkFiles.isEmpty()) return
+    val scope = GlobalSearchScopeUtil
+      .toGlobalSearchScope(params.effectiveSearchScope, params.project)
+      .restrictToStarlarkFiles()
+    if (!processExplicitLabelReferences(file, scope, processor)) return
+    processGlobReferences(file, scope, processor)
+  }
 
-    starlarkFiles.forEach { virtualFile ->
-      val starlarkFile = psiManager.findFile(virtualFile) as? StarlarkFile ?: return@forEach
-      val baseDir = virtualFile.parent ?: return@forEach
-
-      val shouldContinue = PsiTreeUtil.processElements(starlarkFile) { element ->
-        when (element) {
-          is StarlarkStringLiteralExpression -> {
-            if (isReferringToFile(element, file, starlarkFile, psiManager)) {
-              val ref = BazelLabelReference(element, true)
-              if (!processor.process(ref)) {
-                return@processElements false
-              }
-            }
-          }
-
-          is StarlarkGlobExpression -> {
-            if (element.getGlob()?.match(file.virtualFile) == true) {
-              val ref = element.reference
-              if (!processor.process(ref)) {
-                return@processElements false
-              }
-            }
-          }
-        }
-        true
+  /**
+   * Uses the word index to find only Starlark files whose string literals might contain the target filename,
+   * then checks those candidates for explicit label references.
+   */
+  private fun processExplicitLabelReferences(
+    file: PsiFile,
+    scope: GlobalSearchScope,
+    processor: Processor<in PsiReference>,
+  ): Boolean = PsiSearchHelper
+    .getInstance(file.project)
+    .processCandidateFilesForText(
+      scope,
+      UsageSearchContext.IN_STRINGS,
+      true,
+      file.name,
+    ) { candidate ->
+      val starlarkFile = candidate.findPsiFile(file.project) as? StarlarkFile ?: return@processCandidateFilesForText true
+      PsiTreeUtil.processElements(starlarkFile, StarlarkStringLiteralExpression::class.java) { element ->
+        !isReferringToFile(element, file, starlarkFile) || processor.process(BazelLabelReference(element, true))
       }
+    }
 
-      if (!shouldContinue) {
-        return
+  /**
+   * Finds the BUILD file for the Bazel package to which [file] belongs to and checks if any glob in this BUILD file contains [file].
+   *
+   * It's enough to search through only single BUILD file due to the [glob restrictions](https://bazel.build/reference/be/functions#glob).
+   * Globs are matching only files in current Bazel package and cannot cross the package boundaries.
+   */
+  private fun processGlobReferences(
+    file: PsiFile,
+    scope: GlobalSearchScope,
+    processor: Processor<in PsiReference>,
+  ) {
+    val dir = file.virtualFile?.parent ?: return
+    val buildFile = generateSequence(dir) { it.parent }.firstNotNullOfOrNull(::findBuildFile) ?: return
+    if (!scope.contains(buildFile)) return
+    val starlarkFile = buildFile.findPsiFile(file.project) as? StarlarkFile ?: return
+    PsiTreeUtil.processElements(starlarkFile, StarlarkGlobExpression::class.java) { element ->
+      val ref = when {
+        element.getGlob()?.match(file.virtualFile) == true -> element.reference
+        else -> null
       }
+      ref == null || processor.process(ref)
     }
   }
 
@@ -70,24 +86,10 @@ internal class StarlarkFileUsageSearcher : QueryExecutorBase<PsiReference, Searc
     literal: StarlarkStringLiteralExpression,
     targetFile: PsiFile,
     contextFile: PsiFile,
-    psiManager: PsiManager,
   ): Boolean {
     val label = Label.parseOrNull(literal.getStringContents()) ?: return false
     val contextVFile = contextFile.originalFile.virtualFile ?: return false
     val resolvedElement = resolveLabel(targetFile.project, label, contextVFile, acceptOnlyFileTarget = true)
-    return when (resolvedElement) {
-      is PsiFile -> psiManager.areElementsEquivalent(resolvedElement, targetFile)
-      else -> false
-    }
-  }
-
-  private fun extractPatterns(arg: StarlarkArgumentElement): List<String> {
-    val literals =
-      when (val expr = arg.lastChild) {
-        is StarlarkStringLiteralExpression -> listOf(expr)
-        is StarlarkListLiteralExpression -> expr.getElements().filterIsInstance<StarlarkStringLiteralExpression>()
-        else -> emptyList()
-      }
-    return literals.map { it.getStringContents() }
+    return resolvedElement is PsiFile && PsiManager.getInstance(literal.project).areElementsEquivalent(resolvedElement, targetFile)
   }
 }
