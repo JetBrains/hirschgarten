0248067ffb169e707aa51609180b5f032da75c04
Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
2026-06-05 13:02:54 +0000

    [bazel] BAZEL-3223 Starlark function parameter validation inspection
    [bazel] apply suggestions
    
    [bazel] update branch
    
    [bazel] add function parameter validation inspection and tests
    
    
    Merge-request: IJ-MR-206410
    Merged-by: Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
    
    (cherry picked from commit 3848ec9d9dfe3aca0917e92e1c2c83689f3d700c)
    
    IJ-MR-206410
    
    GitOrigin-RevId: 948e2917800a790caf79a859d2153786d56049d7

--- intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/psi/functions/StarlarkCallable.kt
+++ intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/psi/functions/StarlarkCallable.kt
@@ -1,9 +1,10 @@
 package org.jetbrains.bazel.languages.starlark.psi.functions
 
+import com.intellij.psi.PsiElement
 import org.jetbrains.annotations.ApiStatus
 
 @ApiStatus.Internal
-interface StarlarkCallable {
+interface StarlarkCallable: PsiElement {
   fun getNamedParameters(): List<StarlarkParameter>
 
   fun getParameters(): List<StarlarkParameter>
