4eb06e17df113adbf55cf6adc79134797a9f826e
Michał Kasprzyk <michal.kasprzyk.external@jetbrains.com>
2026-06-10 08:09:31 +0000

    BAZEL-3163 [bazel]: Remove unused property
    Merge-request: IJ-MR-207586
    Merged-by: Michał Kasprzyk <michal.kasprzyk.external@jetbrains.com>
    
    GitOrigin-RevId: ca6b03c3283ec6352ea0cbf3b7e69b1bf1810be1

--- intellij.bazel.projectview/src/org/jetbrains/bazel/languages/projectview/DefaultProjectViewService.kt
+++ intellij.bazel.projectview/src/org/jetbrains/bazel/languages/projectview/DefaultProjectViewService.kt
@@ -1,7 +1,7 @@
 package org.jetbrains.bazel.languages.projectview
 
-import com.intellij.openapi.application.readAction
 import com.intellij.openapi.application.edtWriteAction
+import com.intellij.openapi.application.readAction
 import com.intellij.openapi.progress.runBlockingMaybeCancellable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
@@ -22,8 +22,6 @@ import org.jetbrains.bazel.settings.bazel.bazelProjectSettings
 class DefaultProjectViewService(private val project: Project) : ProjectViewService {
 
   private val _projectViewState = MutableStateFlow(ProjectView.EMPTY)
-
-  override val allowExternalProjectViewModification: Boolean = true
 
   override val projectViewState: StateFlow<ProjectView>
     get() = _projectViewState
--- pluginTests/test/org/jetbrains/bazel/languages/projectview/MockProjectViewService.kt
+++ pluginTests/test/org/jetbrains/bazel/languages/projectview/MockProjectViewService.kt
@@ -6,8 +6,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 internal class MockProjectViewService(private val project: Project, private val projectViewContent: String) : ProjectViewService {
-  override val allowExternalProjectViewModification: Boolean
-    get() = true
   override val projectViewState: StateFlow<ProjectView>
     get() = MutableStateFlow(
       runReadActionBlocking {
