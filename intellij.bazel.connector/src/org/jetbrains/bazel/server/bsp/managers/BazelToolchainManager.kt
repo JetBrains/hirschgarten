9e651baf4cf55e9389f2899cd57466fbd0490738
Klaus T. Aehlig <klaus.aehlig@jetbrains.com>
2026-06-16 17:35:18 +0200

    BAZEL-3271: consider all toolchains optional
    ... to ensure we don't fail the sync if a toolchain is not present
    of misconfigured.
    
    GitOrigin-RevId: f2e1d40202682161ad02168835fde4b6cc8c56c1

--- intellij.bazel.connector/src/org/jetbrains/bazel/server/bsp/managers/BazelToolchainManager.kt
+++ intellij.bazel.connector/src/org/jetbrains/bazel/server/bsp/managers/BazelToolchainManager.kt
@@ -6,10 +6,10 @@ import org.jetbrains.annotations.ApiStatus
 class BazelToolchainManager {
   fun getToolchain(rulesetLanguage: RulesetLanguage): String? =
     when (rulesetLanguage.language) {
-      Language.Scala -> rulesetLanguage.rulesetName?.asReponame()?.let { """"@$it//scala:toolchain_type"""" }
+      Language.Scala -> rulesetLanguage.rulesetName?.asReponame()?.let { """config_common.toolchain_type("@$it//scala:toolchain_type", mandatory = False)""" }
       Language.Java -> """config_common.toolchain_type("@bazel_tools//tools/jdk:runtime_toolchain_type", mandatory = False)"""
-      Language.Kotlin -> rulesetLanguage.rulesetName?.asReponame()?.let { """"@$it//kotlin/internal:kt_toolchain_type"""" }
-      Language.Go -> rulesetLanguage.rulesetName?.asReponame()?.let { """"@$it//go:toolchain"""" }
+      Language.Kotlin -> rulesetLanguage.rulesetName?.asReponame()?.let { """config_common.toolchain_type("@$it//kotlin/internal:kt_toolchain_type", mandatory = False)""" }
+      Language.Go -> rulesetLanguage.rulesetName?.asReponame()?.let { """config_common.toolchain_type("@$it//go:toolchain", mandatory = False)""" }
       Language.Python -> rulesetLanguage.rulesetName?.asReponame()?.let { """config_common.toolchain_type("@$it//python:toolchain_type", mandatory = False)""" }
       else -> null
     }
