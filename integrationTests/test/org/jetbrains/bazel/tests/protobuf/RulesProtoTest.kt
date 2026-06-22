c38fce1be71c608a014234831b99529f0f69b1e6
Klaus T. Aehlig <klaus.aehlig@jetbrains.com>
2026-06-22 13:07:55 +0200

    BAZEL-3287: add basic end-to-end test for rules_proto
    ... ensuring we don't break it accidetnally while it is still used
    by users of our plugin.
    
    GitOrigin-RevId: 02a68b3b4481509e202c9a124164cae0b7845cd6

---
+++ integrationTests/test/org/jetbrains/bazel/tests/protobuf/RulesProtoTest.kt
@@ -0,0 +1,52 @@
+package org.jetbrains.bazel.tests.protobuf
+
+import com.intellij.driver.sdk.step
+import com.intellij.driver.sdk.ui.components.common.ideFrame
+import com.intellij.ide.starter.driver.engine.runIdeWithDriver
+import com.intellij.tools.ide.performanceTesting.commands.assertCurrentFile
+import com.intellij.tools.ide.performanceTesting.commands.checkOnRedCode
+import com.intellij.tools.ide.performanceTesting.commands.goToDeclaration
+import com.intellij.tools.ide.performanceTesting.commands.goto
+import com.intellij.tools.ide.performanceTesting.commands.openFile
+import org.jetbrains.bazel.data.IdeaBazelCases
+import org.jetbrains.bazel.ideStarter.IdeStarterBaseProjectTest
+import org.jetbrains.bazel.ideStarter.buildAndSync
+import org.jetbrains.bazel.ideStarter.execute
+import org.jetbrains.bazel.ideStarter.syncBazelProject
+import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.minutes
+
+class RulesProtoTest : IdeStarterBaseProjectTest() {
+  override val timeout = 30.minutes
+
+  @Test
+  fun `rules_proto should sync correctly`() {
+    createContext("protobufResolve", IdeaBazelCases.RulesProtoTest)
+      .runIdeWithDriver(runTimeout = timeout)
+      .useDriverAndCloseIde {
+        ideFrame {
+          syncBazelProject()
+          execute { buildAndSync() }
+          waitForIndicators(10.minutes)
+
+          step("Sanity check consumer") {
+            execute { openFile("consumerJava/Main.java") }
+            takeScreenshot("afterOpenMain")
+            execute { checkOnRedCode() }
+          }
+
+          step("Check basic navigation") {
+            execute { openFile("libB/lib_b.proto") }
+            takeScreenshot("afterOpenLibB")
+            execute { checkOnRedCode() }
+            execute { goto(10, 11)}
+            takeScreenshot("beforeNavigation")
+            execute { goToDeclaration() }
+            takeScreenshot("afterNavigation")
+            execute { assertCurrentFile("lib_a.proto") }
+          }
+        }
+      }
+  }
+
+}
