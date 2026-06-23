2285821a25db8ba3ecfb8d0b4bd29a5ec0258762
Lev Leontev <lev.leontev@jetbrains.com>
2026-06-23 21:57:20 +0000

    BAZEL-3280: don't download go.mod dependencies, Bazel already does that
    Merge-request: IJ-MR-209871
    Merged-by: Lev Leontev <lev.leontev@jetbrains.com>
    
    GitOrigin-RevId: 22551cef8c39b30ef7889c0903a45495ebb81e72

--- golang/intellij.bazel.golang.common/src/sync/GoModuleSupportPostSyncHook.kt
+++ golang/intellij.bazel.golang.common/src/sync/GoModuleSupportPostSyncHook.kt
@@ -1,13 +1,19 @@
 package org.jetbrains.bazel.golang.sync
 
 import com.goide.project.GoModuleSettings
+import com.goide.vgo.configuration.VgoProjectSettings
 import org.jetbrains.bazel.golang.workspace.GoWorkspaceModuleUtil
 import org.jetbrains.bazel.sync.ProjectPostSyncHook
 
 internal class GoModuleSupportPostSyncHook : ProjectPostSyncHook {
   override suspend fun onPostSync(environment: ProjectPostSyncHook.ProjectPostSyncHookEnvironment) {
     val project = environment.project
+
+    // Bazel downloads dependencies on its own, so tracking go.mod files for changed dependencies doesn't make sense
+    VgoProjectSettings.getInstance(project).isIntegrationEnabled = false
+
     val workspaceModule = GoWorkspaceModuleUtil.findModule(project) ?: return
+    // This should come after disabling go.mod integration, otherwise this will spam the UI with "go list"/"go mod download"
     GoModuleSettings.getInstance(workspaceModule).isGoSupportEnabled = true
   }
 }
---
+++ integrationTests/test/org/jetbrains/bazel/tests/golang/GoModTest.kt
@@ -0,0 +1,51 @@
+package org.jetbrains.bazel.tests.golang
+
+import com.intellij.driver.sdk.invokeAction
+import com.intellij.driver.sdk.step
+import com.intellij.driver.sdk.ui.components.common.ideFrame
+import com.intellij.driver.sdk.wait
+import com.intellij.ide.starter.driver.engine.runIdeWithDriver
+import io.kotest.matchers.shouldBe
+import org.jetbrains.bazel.data.GoLandBazelCases
+import org.jetbrains.bazel.ideStarter.IdeStarterBaseProjectTest
+import org.jetbrains.bazel.ideStarter.buildAndSync
+import org.jetbrains.bazel.ideStarter.execute
+import org.jetbrains.bazel.ideStarter.syncBazelProject
+import org.junit.jupiter.api.Test
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.TimeSource
+
+internal class GoModTest : IdeStarterBaseProjectTest() {
+  @Test
+  fun `should not download deps via go list and go mod download`() {
+    createContext("goModTest", GoLandBazelCases.GoModTest)
+      .runIdeWithDriver(runTimeout = timeout)
+      .useDriverAndCloseIde {
+        ideFrame {
+          syncBazelProject()
+          execute { buildAndSync() }
+          waitForIndicators(10.minutes)
+
+          step("Close project") {
+            invokeAction("CloseProject")
+            takeScreenshot("afterClosingProject")
+          }
+
+          step("Reopen project from welcome screen") {
+            waitOneText("goModTest").click()
+          }
+
+          val start = TimeSource.Monotonic.markNow()
+          while (TimeSource.Monotonic.markNow() - start < 20.seconds) {
+            val unexpectedTexts = getAllTexts().filter {
+              it.text.contains("Executing 'go mod download") ||
+              it.text.contains("Executing 'go list")
+            }
+            unexpectedTexts shouldBe emptyList()
+            wait(0.1.seconds)
+          }
+        }
+      }
+  }
+}
