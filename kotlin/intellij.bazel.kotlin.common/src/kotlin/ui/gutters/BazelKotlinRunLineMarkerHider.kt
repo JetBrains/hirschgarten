0dd19df8243a44ada8b0d5339a76b61578015f1c
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2026-06-24 16:12:54 +0400

    BAZEL-2923 [bazel]: document Kotlin run marker suppression
    The Kotlin marker hider is close to the existing run configuration producer suppressor, but it operates at a different point in the UI lifecycle. Review feedback asked for that distinction to be visible.
    
    Document that the hider prevents Kotlin's platform gutter action from being offered when Bazel owns the same element, while the producer suppressor only blocks platform run configuration producers after a platform action is invoked.
    
    
    (cherry picked from commit 49b5344721d196d6bd3da836e7184fa8e00cd8e7)
    
    IJ-MR-210014
    
    GitOrigin-RevId: dc68eb8b627dc729f64ba5f41b217762fe3dc972

--- kotlin/intellij.bazel.kotlin.common/src/kotlin/ui/gutters/BazelKotlinRunLineMarkerHider.kt
+++ kotlin/intellij.bazel.kotlin.common/src/kotlin/ui/gutters/BazelKotlinRunLineMarkerHider.kt
@@ -4,6 +4,12 @@ import com.intellij.psi.PsiElement
 import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.kotlin.idea.codeInsight.KotlinRunLineMarkerHider
 
+/**
+ * Hides Kotlin's platform run marker when the Bazel contributor owns the same gutter element.
+ *
+ * [org.jetbrains.bazel.run.BazelRunConfigurationProducerSuppressor] suppresses run configuration producers after a
+ * platform action is invoked; this extension prevents the duplicate platform marker/action from being offered at all.
+ */
 @ApiStatus.Internal
 class BazelKotlinRunLineMarkerHider(
   private val bazelContributor: BazelKotlinRunLineMarkerContributor = BazelKotlinRunLineMarkerContributor(),
