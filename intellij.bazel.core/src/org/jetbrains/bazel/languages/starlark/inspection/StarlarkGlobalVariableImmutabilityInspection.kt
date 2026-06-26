697bac31dde73e8bef55959f0230f253438fd4c5
Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
2026-06-26 15:56:49 +0000

    cleanup bazel: starlark inspections - small fixes
    [bazel] use StarlarkElementVisitor in StarlarkGlobalVariableImmutabilityInspection
    
    [bazel] improve inspection descriptions
    
    
    Merge-request: IJ-MR-210502
    Merged-by: Marta Hering-Zagrocka <marta.hering-zagrocka@jetbrains.com>
    
    (cherry picked from commit cd8fd5a37a29a119f16d658206f4cc53a7c87c02)
    
    IJ-MR-210502
    
    GitOrigin-RevId: 8c0b6a643e923036a95d97d8fabba3c35bf0eadd

--- intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/inspection/StarlarkGlobalVariableImmutabilityInspection.kt
+++ intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/inspection/StarlarkGlobalVariableImmutabilityInspection.kt
@@ -9,6 +9,7 @@ import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.bazel.commons.constants.Constants
 import org.jetbrains.bazel.languages.starlark.StarlarkBundle
 import org.jetbrains.bazel.languages.starlark.StarlarkFileType
+import org.jetbrains.bazel.languages.starlark.psi.StarlarkElementVisitor
 import org.jetbrains.bazel.languages.starlark.psi.StarlarkFile
 import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkAssignmentStatement
 
@@ -18,13 +19,10 @@ class StarlarkGlobalVariableImmutabilityInspection : LocalInspectionTool() {
 
   override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = GlobalVarVisitor(holder)
 
-  class GlobalVarVisitor(private val holder: ProblemsHolder) : PsiElementVisitor() {
-    val excludedFileNames = Constants.BUILD_FILE_NAMES +
-                            arrayOf(Constants.WORKSPACE_FILE_NAME, "WORKSPACE.bazel", "WORKSPACE.bzlmod")
-
+  class GlobalVarVisitor(private val holder: ProblemsHolder) : StarlarkElementVisitor() {
     override fun visitFile(psiFile: PsiFile) {
       val file = psiFile as? StarlarkFile ?: return
-      if (file.name in excludedFileNames) return
+      if (file.name in EXCLUDED_FILE_NAMES) return
       val assignmentsByName = mutableMapOf<String, MutableList<StarlarkAssignmentStatement>>()
 
       file.childrenOfType<StarlarkAssignmentStatement>().forEach { assignment ->
@@ -43,5 +41,10 @@ class StarlarkGlobalVariableImmutabilityInspection : LocalInspectionTool() {
           }
         }
     }
+  }
+
+  companion object {
+    private val EXCLUDED_FILE_NAMES = Constants.BUILD_FILE_NAMES +
+                                      arrayOf(Constants.WORKSPACE_FILE_NAME, "WORKSPACE.bazel", "WORKSPACE.bzlmod")
   }
 }
