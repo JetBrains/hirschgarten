d4ec35e1695296109e218cfe1f3205f5eeed6372
Klaus T. Aehlig <klaus.aehlig@jetbrains.com>
2026-06-29 14:30:03 +0200

    BAZEL-3080: add completion for Starlark struct fields
    (cherry picked from commit d53b7ff7154e667876d5f3acd348cff46c922023)
    
    IJ-MR-211058
    
    GitOrigin-RevId: 9a57b9a7b21e9430bc15004b078d5aa5a99a695b

--- intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/references/StarlarkQualifiedReferenceExpressionReference.kt
+++ intellij.bazel.core/src/org/jetbrains/bazel/languages/starlark/references/StarlarkQualifiedReferenceExpressionReference.kt
@@ -1,25 +1,45 @@
 package org.jetbrains.bazel.languages.starlark.references
 
+import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReferenceBase
 import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkCallExpression
 import org.jetbrains.bazel.languages.starlark.psi.expressions.StarlarkReferenceExpression
+import org.jetbrains.bazel.languages.starlark.psi.expressions.arguments.StarlarkNamedArgumentExpression
+import org.jetbrains.bazel.languages.starlark.psi.functions.StarlarkArgumentList
 import org.jetbrains.bazel.languages.starlark.psi.statements.StarlarkAssignmentStatement
 import org.jetbrains.kotlin.psi.psiUtil.getChildOfType
 
 internal class StarlarkQualifiedReferenceExpressionReference (element: StarlarkReferenceExpression) :
-  PsiReferenceBase<StarlarkReferenceExpression>(element, TextRange(0, element.textLength), false) {
+  PsiReferenceBase<StarlarkReferenceExpression>(
+    element,
+    element.getNameIdentifier()?.textRangeInParent ?: TextRange(0, element.textLength),
+    false,
+  ) {
 
   override fun resolve(): PsiElement? {
     val identifier = element.name ?: return null
-    val qualifiedPartResolved = element.getQualifierExpression()?.reference?.resolve() ?: return null
-    val assignment = qualifiedPartResolved.parent as? StarlarkAssignmentStatement ?: return null
-    val assignedValue = assignment.getChildOfType<StarlarkCallExpression>() ?: return null
-    if (assignedValue.getCalledFunctionName() != "struct") return null
-    val definition = assignedValue.getArgumentList()?.getKeywordArgument(identifier) ?: return null
+    val definition = element.getStructArgumentList()?.getKeywordArgument(identifier) ?: return null
 
     return (definition.lastChild as? StarlarkReferenceExpression)?.reference?.resolve() ?: definition
   }
 
+  override fun getVariants(): Array<LookupElementBuilder> =
+    element.getStructArgumentList()
+      ?.getArguments()
+      ?.mapNotNull { it.name }
+      ?.map { LookupElementBuilder.create(it) }
+      ?.toTypedArray()
+    ?: emptyArray()
+}
+
+private fun StarlarkReferenceExpression.getStructArgumentList(): StarlarkArgumentList? {
+  val qualifiedPartResolved = getQualifierExpression()?.reference?.resolve() ?: return null
+  val structCall = when (qualifiedPartResolved) {
+    is StarlarkNamedArgumentExpression -> qualifiedPartResolved.getValue() as? StarlarkCallExpression
+    else -> (qualifiedPartResolved.parent as? StarlarkAssignmentStatement)?.getChildOfType()
+  } ?: return null
+  if (structCall.getCalledFunctionName() != "struct") return null
+  return structCall.getArgumentList()
 }
