0e4ab6b53fc2141d75e4850b94488315d12c523d
Damian Baczyński <damian.baczynski@jetbrains.com>
2026-07-10 10:14:15 +0200

    tests [bazel] Adjust failing KotlinCompilerPluginLeakingClassPathTest to changes in Kotlin plugin
    The error message is no longer included in highlighting tests. Now it's just an error code.
    
    GitOrigin-RevId: 98fda7309f07e88a32283c3bfa2f4e20aa04b5a2

--- pluginTests/testData/testProjects/redcodes/kotlin_compiler_plugin_leaking_classpath/Lib.kt
+++ pluginTests/testData/testProjects/redcodes/kotlin_compiler_plugin_leaking_classpath/Lib.kt
@@ -1,5 +1,5 @@
 package com.example
 
 class Lib {
-   private val obj = com.example.<error descr="[UNRESOLVED_REFERENCE] Unresolved reference 'plugin'.">plugin</error>.PluginClass()
+   private val obj = com.example.<error descr="[UNRESOLVED_REFERENCE]">plugin</error>.PluginClass()
 }
