0eb1606bd624ca1d2295f1d2cf8b7a435fcfebcf
Krystian Gajewski <krystian.gajewski.external@jetbrains.com>
2026-07-28 21:38:55 +0200

    misc bazel: add debug renderer to `SourceFileCollection`
    (cherry picked from commit 6e8fa3a09ca23a654f8fbc8f363bc1c4e272f263)
    
    IJ-MR-215844
    
    GitOrigin-RevId: 2626175a2eeae846c4f19eff12ca3c0a343668f6

--- intellij.bazel.commons/src/org/jetbrains/bsp/protocol/SourceFileCollection.kt
+++ intellij.bazel.commons/src/org/jetbrains/bsp/protocol/SourceFileCollection.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.bsp.protocol
 
 import org.jetbrains.annotations.ApiStatus
+import org.jetbrains.annotations.Debug
 import java.nio.file.Path
 
 // TODO: move to backend module together with `RawBuildTarget`
@@ -8,6 +9,7 @@ import java.nio.file.Path
 /**
  * Unordered file set container
  */
+@Debug.Renderer(hasChildren = "!isEmpty()", childrenArray = "arrayOfFiles()")
 @ApiStatus.Internal
 interface SourceFileCollection {
   companion object {
@@ -19,4 +21,8 @@ interface SourceFileCollection {
 
   fun isEmpty(): Boolean
   fun getFiles(): Sequence<Path>
+
+  @Suppress("unused") // used in debug renderer
+  fun arrayOfFiles(): Array<Path> = getFiles().toList().toTypedArray()
+
 }
