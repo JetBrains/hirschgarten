0687dd01f91c069f98fdd9370fa665eed91c566e
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2026-08-11 11:47:55 +0400

    BAZEL-3451 [bazel]: remove obsolete compatibility test
    GitOrigin-RevId: 04f57db4a49072e39f6d87c653e7fd7f886d2811

--- integrationTests/compat/ultimate/bazelPluginCompatibility.kt
+++ integrationTests/compat/ultimate/bazelPluginCompatibility.kt
@@ -2,13 +2,11 @@
 package org.jetbrains.bazel.build
 
 import jetbrains.buildServer.messages.serviceMessages.PublishArtifacts
-import org.jetbrains.intellij.build.BuildOptionsSpec
 import org.jetbrains.intellij.build.BuildPaths.Companion.COMMUNITY_ROOT
 import org.jetbrains.intellij.build.BuildPaths.Companion.ULTIMATE_HOME
 import org.jetbrains.intellij.build.PluginVerifier
 import org.jetbrains.intellij.build.VerifierIdeInfo
 import org.jetbrains.intellij.build.VerifierPluginInfo
-import org.jetbrains.intellij.build.buildPlugin
 import org.jetbrains.intellij.build.dependencies.BuildDependenciesUtil.cleanDirectory
 import org.jetbrains.intellij.build.dependencies.JdkDownloader
 import org.jetbrains.intellij.build.telemetry.block
@@ -20,7 +18,6 @@ import kotlin.io.path.name
 import kotlin.io.path.readText
 
 internal const val BAZEL_PLUGIN_ID: String = "org.jetbrains.bazel"
-internal const val BAZEL_PLUGIN_MODULE: String = "intellij.bazel.plugin"
 
 private const val BAZEL_PLUGIN_ZIP_PREFIX = "bazel-plugin-"
 private const val BLOCKMAP_ZIP_SUFFIX = "blockmap.zip"
@@ -29,68 +26,6 @@ internal data class BazelPluginInfo(
   val path: Path,
   val pluginVersion: String,
 )
-
-internal data class BazelVersions(
-  val pluginVersion: String,
-  val sinceVersion: String,
-  val untilVersion: String,
-)
-
-/**
- * Reads `plugins/bazel/versions.<platform>.bzl` when `-Dbazel.plugin.platform` names a platform that has such a file,
- * and `plugins/bazel/versions.bzl` otherwise.
- */
-internal fun parseBzlVersions(): BazelVersions {
-  val platform = System.getProperty("bazel.plugin.platform")
-  val platformVersionsFile = platform?.let { ULTIMATE_HOME.resolve("plugins/bazel/versions.$it.bzl") }
-  val defaultVersionsFile = ULTIMATE_HOME.resolve("plugins/bazel/versions.bzl")
-
-  val versionsFile = when {
-    platformVersionsFile?.exists() == true -> platformVersionsFile
-    defaultVersionsFile.exists() -> defaultVersionsFile
-    else -> error("No version file found: tried $platformVersionsFile and $defaultVersionsFile")
-  }
-
-  val content = versionsFile.readText()
-
-  fun extractValue(name: String): String {
-    val regex = """$name\s*=\s*"([^"]+)"""".toRegex()
-    return regex.find(content)?.groupValues?.get(1)
-      ?: error("Could not find $name in $versionsFile")
-  }
-
-  return BazelVersions(
-    pluginVersion = extractValue("INTELLIJ_BAZEL_VERSION"),
-    sinceVersion = extractValue("SINCE_VERSION"),
-    untilVersion = extractValue("UNTIL_VERSION"),
-  )
-}
-
-internal suspend fun buildBazelPluginZip(
-  outRootDir: Path,
-  versions: BazelVersions,
-  configureBuildOptions: BuildOptionsSpec.() -> Unit = {},
-): BazelPluginInfo {
-  cleanDirectory(outRootDir)
-
-  buildPlugin(BAZEL_PLUGIN_MODULE) {
-    sinceBuild { versions.sinceVersion }
-    untilBuild { versions.untilVersion }
-    version { versions.pluginVersion }
-    options {
-      outRootDir(outRootDir)
-      configureBuildOptions()
-    }
-  }
-
-  val pluginFile = findBazelPluginZips(outRootDir).firstOrNull()
-                   ?: error("Cannot find built Bazel plugin zip (bazel-plugin-*.zip) in $outRootDir")
-
-  return BazelPluginInfo(
-    path = pluginFile,
-    pluginVersion = versions.pluginVersion,
-  )
-}
 
 private fun isBazelPluginZip(fileName: String): Boolean =
   fileName.startsWith(BAZEL_PLUGIN_ZIP_PREFIX) && fileName.endsWith(".zip") && !fileName.endsWith(BLOCKMAP_ZIP_SUFFIX)
