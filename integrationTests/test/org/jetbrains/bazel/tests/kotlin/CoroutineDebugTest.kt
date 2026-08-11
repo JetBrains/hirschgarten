39666afcde942a0d6f9db43d86d8c27654294228
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2026-08-11 22:55:47 +0400

    BAZEL-3456 [bazel]: stabilize remaining debugger waits
    GitOrigin-RevId: f713d77eea5ffb1c84cdcb49a3f22b55b67b2a61

--- integrationTests/test/org/jetbrains/bazel/tests/kotlin/CoroutineDebugTest.kt
+++ integrationTests/test/org/jetbrains/bazel/tests/kotlin/CoroutineDebugTest.kt
@@ -19,6 +19,7 @@ import org.jetbrains.bazel.ideStarter.IdeStarterBaseProjectTest
 import org.jetbrains.bazel.ideStarter.syncBazelProject
 import org.jetbrains.bazel.ideStarter.withBazelFeatureFlag
 import org.jetbrains.bazel.data.simpleBazelProject
+import org.jetbrains.bazel.tests.ui.waitForDebuggerPausedAt
 import org.junit.jupiter.api.Test
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
@@ -65,7 +66,7 @@ class CoroutineDebugTest : IdeStarterBaseProjectTest() {
           }
 
           step("Check if async stack trace is displayed") {
-            waitOneContainsText("secondLevel:30", timeout = 1.minutes)
+            waitForDebuggerPausedAt("secondLevel:30")
             waitFor(message = "Async stack traces to appear", timeout = 30.seconds, interval = 2.seconds) {
               runCatching {
                 x("//div[@class='Splitter']").verticalScrollBar { scrollBlockDown(6) }
