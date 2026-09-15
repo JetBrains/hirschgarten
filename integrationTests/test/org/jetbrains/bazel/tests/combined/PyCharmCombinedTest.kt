0e8ded5db8eeaf15fb8503732e16541ad6c966bf
Klaus T. Aehlig <klaus.aehlig@jetbrains.com>
2026-09-15 14:57:34 +0200

    BAZEL-3370: disable PyCharmCombinedTest test case blocked on BAZEL-3382
    Due to https://youtrack.jetbrains.com/issue/BAZEL-3382 we cannot
    enable "build and sync" on that test case; however, without enabling
    "build and sync", the test case asserts that a plain sync ensures
    the presence of generated source files, which is wrong (BAZEL-3370).
    
    (cherry picked from commit 6f4eec8b41855ba99df606935add5dccd4fb8599)
    
    GitOrigin-RevId: 0469df86294a4eebabc1d34c0bb33571fdc303a6

--- integrationTests/test/org/jetbrains/bazel/tests/combined/PyCharmCombinedTest.kt
+++ integrationTests/test/org/jetbrains/bazel/tests/combined/PyCharmCombinedTest.kt
@@ -18,6 +18,7 @@ import org.jetbrains.bazel.ideStarter.navigateToFile
 import org.jetbrains.bazel.ideStarter.syncBazelProjectCloseDialog
 import org.jetbrains.bazel.data.BazelProjectConfigurer
 import org.jetbrains.bazel.data.simpleBazelProject
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import kotlin.time.Duration.Companion.seconds
@@ -44,6 +45,8 @@ class PyCharmCombinedTest : IdeStarterCombinedBaseTest() {
   @Test @Order(2)
   fun `Python test line markers should be available for test files`() = pyCharmTestLineMarkers()
 
+  // Build and sync triggers BAZEL-3382. Should be reenabled once BAZEL-3382 is fixed.
+  @Disabled
   @Test @Order(3)
   fun `Python import statements should resolve correctly`() = pyCharmImportStatements()
 
